a1f106a2a42d352bc4e80ac8a78e41bf0ad4ce1d
Frank Filius <f.filius@amsterdam.nl>
2021-06-02 13:42:47 +0200

    Create Text-Input

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {SafeAreaView} from 'react-native'
+import styled from 'styled-components/native'
+
+const UselessTextInput = () => {
+    const [text, onChangeText] = React.useState('Useless Text')
+    const [number, onChangeNumber] = React.useState('')
+
+    return (
+        <SafeAreaView>
+            <StyledTextInput onChangeText={onChangeText} value={text} />
+            <StyledTextInput
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder="useless placeholder"
+                keyboardType="numeric"
+            />
+        </SafeAreaView>
+    )
+}
+
+const StyledTextInput = styled.TextInput`
+    height: 40;
+    padding: 5;
+`
+
+export default UselessTextInput
