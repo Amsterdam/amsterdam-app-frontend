a77eb575ad0f753aceaf2ba154eafafba62bccee
Frank Filius <f.filius@amsterdam.nl>
2021-06-24 14:45:03 +0200

    Cut top-part of webview so view starts beneath header

--- src/components/WebView.tsx
+++ src/components/WebView.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {StyleSheet} from 'react-native'
 import {WebView as WebViewRN} from 'react-native-webview'
 
 const WebView = ({uri}: {uri: string}) => {
-  return <WebViewRN source={{uri}} />
+  return <WebViewRN source={{uri}} style={styles.withoutHeader} />
 }
+
+const styles = StyleSheet.create({
+  withoutHeader: {
+    marginTop: -52,
+  },
+})
 
 export default WebView
