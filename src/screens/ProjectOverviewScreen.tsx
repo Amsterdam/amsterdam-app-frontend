474752c812de73c87139085a535616ed060f969b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-07-01 17:54:58 +0200

    Implement FlatList for the scrollable row of projects

--- src/screens/ProjectOverviewScreen.tsx
+++ src/screens/ProjectOverviewScreen.tsx
@@ -1,38 +1,34 @@
 import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import {FlatList, StyleSheet, View} from 'react-native'
 import {ProjectCard} from '../components/features'
 import {ScreenWrapper, Title} from '../components/ui'
+import {projects} from '../data/projects'
 
-export const ProjectOverviewScreen = () => (
-  <ScreenWrapper>
-    <View style={styles.screen}>
-      <View style={styles.titleRow}>
-        <Title level={2} text="Centrum" />
-      </View>
-      <View style={styles.cardsRow}>
-        <ProjectCard
-          imageSource={{
-            uri: 'https://www.amsterdam.nl/publish/pages/961922/stp_marnixstraat_940.jpg',
-          }}
-          text="Herinrichting kruispunt"
-          title="Marnixstraat-Rozengracht"
-        />
-        <ProjectCard
-          imageSource={{
-            uri: 'https://www.amsterdam.nl/publish/pages/966195/940x415_a_programma_brug.jpg',
-          }}
-          text="Vernieuwing 9 bruggen en 5 straten"
-          title="Oranje Loper"
+export const ProjectOverviewScreen = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.screen}>
+        <View style={styles.titleRow}>
+          <Title level={2} text="Centrum" />
+        </View>
+        <FlatList
+          data={projects}
+          horizontal
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <ProjectCard
+              imageSource={item.imageSource}
+              text={item.text}
+              title={item.title}
+            />
+          )}
         />
       </View>
-    </View>
-  </ScreenWrapper>
-)
+    </ScreenWrapper>
+  )
+}
 
 const styles = StyleSheet.create({
-  cardsRow: {
-    flexDirection: 'row',
-  },
   titleRow: {
     marginBottom: 15,
   },
