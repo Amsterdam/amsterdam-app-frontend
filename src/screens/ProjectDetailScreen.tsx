44c04fccf290e816471cf8c2759663eebf274aa0
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-07-03 23:16:35 +0200

    Implement project detail screen and link to it

--- src/screens/ProjectDetailScreen.tsx
+++ src/screens/ProjectDetailScreen.tsx
@@ -1,7 +1,10 @@
 import {RouteProp} from '@react-navigation/native'
 import React from 'react'
+import {Image, ScrollView, StyleSheet, View} from 'react-native'
 import {RootStackParamList} from '../../App'
-import {Text} from '../components/ui'
+import {Text, Title} from '../components/ui'
+import {Project, projects} from '../data/projects'
+import {color} from '../tokens'
 
 type ProjectDetailScreenRouteProp = RouteProp<
   RootStackParamList,
@@ -13,5 +16,67 @@ type Props = {
 }
 
 export const ProjectDetailScreen = ({route}: Props) => {
-  return <Text>Project detail page for ‘{route.params.id}’.</Text>
+  const project: Project | undefined = projects.find(
+    p => p.id === route.params.id,
+  )
+
+  if (!project) {
+    return <Text>`Project ${route.params.id} niet gevonden.`</Text>
+  }
+
+  const Section = ({title, text}: {title: string; text?: string}) =>
+    text ? (
+      <>
+        <Title level={2} prose text={title} />
+        <Text>{text}</Text>
+      </>
+    ) : null
+
+  return (
+    <ScrollView>
+      <Image source={project.imageSource} style={styles.image} />
+      <View style={styles.screen}>
+        <Title prose text={project.title} />
+        {project.lead?.text && <Text>{project.lead?.text}</Text>}
+        <Section title="Wat" text={project.body?.what} />
+        <Section title="Waar" text={project.body?.where} />
+        <Section title="Wanneer" text={project.body?.when} />
+      </View>
+      {project.contact && (
+        <View style={styles.contact}>
+          <Title inverse level={2} prose text="Contact" />
+          {(project.contact.name || project.contact.jobDescription) && (
+            <Text inverse>
+              {project.contact.name}, {project.contact.jobDescription}
+            </Text>
+          )}
+          {project.contact.phone && (
+            <Text inverse>{project.contact.phone}</Text>
+          )}
+          {project.contact.email && (
+            <Text inverse>{project.contact.email}</Text>
+          )}
+          {project.contact.list?.map(item => (
+            <Text inverse>{item}</Text>
+          ))}
+        </View>
+      )}
+    </ScrollView>
+  )
 }
+
+const styles = StyleSheet.create({
+  contact: {
+    backgroundColor: color.secondary.main,
+    color: color.bright.main,
+    padding: 15,
+  },
+  image: {
+    height: 150,
+    maxWidth: '100%',
+    resizeMode: 'cover',
+  },
+  screen: {
+    padding: 15,
+  },
+})
--- src/screens/ProjectOverviewByBoroughScreen.tsx
+++ src/screens/ProjectOverviewByBoroughScreen.tsx
@@ -1,22 +1,33 @@
+import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {FlatList, StyleSheet, View} from 'react-native'
+import {RootStackParamList} from '../../App'
 import {ProjectCard} from '../components/features'
 import {Gutter, ScreenWrapper, Title} from '../components/ui'
 import {projects} from '../data/projects'
 
-export const ProjectOverviewByBoroughScreen = () => {
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'ProjectDetail'>
+}
+
+export const ProjectOverviewByBoroughScreen = ({navigation}: Props) => {
   return (
     <ScreenWrapper>
       <View style={styles.screen}>
         <View style={styles.titleRow}>
-          <Title level={2} text="Centrum" />
+          <Title level={2} prose text="Centrum" />
         </View>
         <FlatList
           data={projects}
           ItemSeparatorComponent={() => <Gutter height={10} />}
           keyExtractor={item => item.id}
           renderItem={({item}) => (
-            <ProjectCard imageSource={item.imageSource} title={item.title} />
+            <ProjectCard
+              id={item.id}
+              imageSource={item.imageSource}
+              title={item.title}
+              navigation={navigation}
+            />
           )}
         />
       </View>
