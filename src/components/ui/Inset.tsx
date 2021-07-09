9bde7307fe09347a3d5e0129a3efdbad53698b99
Frank Filius <f.filius@amsterdam.nl>
2021-07-09 17:17:56 +0200

    Enable style props passed into Inset for more flexible use

--- src/components/ui/Inset.tsx
+++ src/components/ui/Inset.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, View, ViewProps} from 'react-native'
 import {Spacing, spacing} from '../../tokens'
 
 type Props = {
   children: React.ReactNode
   size?: keyof Spacing
-}
+} & ViewProps
 
-export const Inset = ({children, size = 'md'}: Props) => {
+export const Inset = ({children, size = 'md', style}: Props) => {
   const styles = StyleSheet.create({
     inset: {
       padding: spacing[size],
     },
   })
 
-  return <View style={styles.inset}>{children}</View>
+  return <View style={[styles.inset, style]}>{children}</View>
 }
