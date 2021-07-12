92b87d4394c09257506098e45a4fcd1acd2cb160
Frank Filius <f.filius@amsterdam.nl>
2021-07-12 09:25:59 +0200

    Use newsarticle-card component

--- src/components/features/NewsItemsOverview.tsx
+++ src/components/features/NewsItemsOverview.tsx
@@ -2,8 +2,8 @@ import {useNavigation} from '@react-navigation/native'
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
 import {NewsArticle} from '../../data/projects'
-import {Button, Gutter, Inset, Text} from '../ui'
-import {Image} from '../ui/Image'
+import {Gutter, Inset} from '../ui'
+import {NewsArticleCard} from './NewsArticleCard'
 
 type Props = {
   newsArticles: NewsArticle[]
@@ -12,46 +12,28 @@ type Props = {
 export const NewsItemsOverview = ({newsArticles}: Props) => {
   const navigation = useNavigation()
   return (
-    <Inset style={styles.container}>
-      {newsArticles.map(article => {
-        return (
-          <React.Fragment key={article.title}>
-            <Button
-              style={styles.button}
-              onPress={() =>
-                navigation.navigate('ProjectNews', {newsArticle: article})
-              }>
-              <View style={styles.imageWrapper}>
-                <Image source={article.imageSource} />
-              </View>
-              <Gutter width={10} />
-              <View style={styles.textWrapper}>
-                <Text>{article.title}</Text>
-              </View>
-            </Button>
-            <Gutter height={10} />
-          </React.Fragment>
-        )
-      })}
-    </Inset>
+    <View style={styles.container}>
+      <Inset>
+        {newsArticles.map(article => {
+          return (
+            <React.Fragment key={article.title}>
+              <NewsArticleCard
+                newsArticle={article}
+                onPress={() =>
+                  navigation.navigate('ProjectNews', {newsArticle: article})
+                }
+              />
+              <Gutter height={10} />
+            </React.Fragment>
+          )
+        })}
+      </Inset>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
-    flexDirection: 'row',
-    padding: 15,
-    backgroundColor: 'white',
-    minHeight: 120,
-  },
   container: {
     backgroundColor: '#F5F5F5',
-  },
-  imageWrapper: {
-    width: '40%',
-    height: '100%',
-  },
-  textWrapper: {
-    width: '60%',
   },
 })
