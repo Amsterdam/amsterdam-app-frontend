96ebfeb3804efa084896a1e7c9718d3d810a1c5b
Frank Filius <f.filius@amsterdam.nl>
2021-07-26 16:01:05 +0200

    Use updated provider and improve readability

--- src/components/ui/WebView.tsx
+++ src/components/ui/WebView.tsx
@@ -4,25 +4,26 @@ import {StyleSheet} from 'react-native'
 import {WebView as WebViewRN} from 'react-native-webview'
 import {OrientationContext} from '../../providers/orientation.provider'
 
+const WEB_HEADER_HEIGHT = {
+  PORTRAIT: 54,
+  LANDSCAPE: 134,
+}
+
 export const WebView = ({uri}: {uri: string}) => {
   const orientationContext = useContext(OrientationContext)
   return (
     <WebViewRN
       source={{uri}}
-      style={
-        orientationContext.orientation === 'portrait'
-          ? styles.portrait
-          : styles.landscape
-      }
+      style={orientationContext.portrait ? styles.portrait : styles.landscape}
     />
   )
 }
 
 const styles = StyleSheet.create({
   portrait: {
-    marginTop: -54,
+    marginTop: -WEB_HEADER_HEIGHT.PORTRAIT,
   },
   landscape: {
-    marginTop: -134,
+    marginTop: -WEB_HEADER_HEIGHT.LANDSCAPE,
   },
 })
