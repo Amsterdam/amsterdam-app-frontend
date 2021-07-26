e654ec6ca34695be472fca43ebc7e9fc0342bbc4
Frank Filius <f.filius@amsterdam.nl>
2021-07-26 15:18:06 +0200

    Style webview depending on orientation

--- src/components/ui/WebView.tsx
+++ src/components/ui/WebView.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import {useContext} from 'react'
 import {StyleSheet} from 'react-native'
 import {WebView as WebViewRN} from 'react-native-webview'
+import {OrientationContext} from '../../providers/orientation.provider'
 
 export const WebView = ({uri}: {uri: string}) => {
-  return <WebViewRN source={{uri}} style={styles.withoutHeader} />
+  const orientationContext = useContext(OrientationContext)
+  return (
+    <WebViewRN
+      source={{uri}}
+      style={
+        orientationContext.orientation === 'portrait'
+          ? styles.portrait
+          : styles.landscape
+      }
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-  withoutHeader: {
+  portrait: {
     marginTop: -54,
+  },
+  landscape: {
+    marginTop: -134,
   },
 })
