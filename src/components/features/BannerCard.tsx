26d4542de284287dbcfcb3a6f2be02178c2a9f0c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-08-19 11:43:41 +0200

    Remove `imageComponent` prop again from banner card

--- src/components/features/BannerCard.tsx
+++ src/components/features/BannerCard.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react'
+import React from 'react'
 import {
   Image,
   ImageSourcePropType,
@@ -9,24 +9,16 @@ import {image} from '../../tokens'
 import {Card, CardBody, Text, Title} from '../ui'
 
 type Props = {
-  imageComponent?: ReactElement
   imageSource?: ImageSourcePropType
   onPress: () => void
   title: string
   subtitle: string
 }
 
-export const BannerCard = ({
-  imageComponent,
-  imageSource,
-  onPress,
-  subtitle,
-  title,
-}: Props) => (
+export const BannerCard = ({imageSource, onPress, subtitle, title}: Props) => (
   <TouchableHighlight onPress={onPress}>
     <Card>
       {imageSource && <Image source={imageSource} style={styles.image} />}
-      {imageComponent}
       <CardBody>
         <Title level={4} text={title} />
         <Text>{subtitle}</Text>
