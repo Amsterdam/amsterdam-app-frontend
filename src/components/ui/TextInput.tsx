44d770d2a1cad63f8dc831ed943cd9b6515352d1
Frank Filius <f.filius@amsterdam.nl>
2021-08-20 08:36:03 +0200

    Add clear-icon and let it remove text

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,23 +1,50 @@
+import Close from '@amsterdam/asc-assets/static/icons/Close.svg'
 import React from 'react'
 import {
   StyleSheet,
   TextInput as TextInputRN,
   TextInputProps as TextInputRNProps,
+  View,
 } from 'react-native'
-import {color, font} from '../../tokens'
+import {color, font, size} from '../../tokens'
 
-export const TextInput = (props: TextInputRNProps) => (
-  <TextInputRN {...props} style={styles.textInput} />
+export const TextInput = React.forwardRef(
+  (props: TextInputRNProps, ref: any) => {
+    const clearInput = () => {
+      console.log('clear')
+    }
+    return (
+      <View style={styles.searchSection}>
+        <TextInputRN ref={ref} {...props} style={styles.textInput} />
+        <Close
+          onPress={clearInput}
+          style={styles.searchIcon}
+          width={20}
+          height={20}
+          fill={'black'}
+        />
+      </View>
+    )
+  },
 )
 
 const styles = StyleSheet.create({
-  textInput: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: color.background.lighter,
+  searchSection: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: size.spacing.xl,
     borderColor: color.border.input,
     borderStyle: 'solid',
     borderWidth: 1,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  textInput: {
+    flex: 1,
+    padding: size.spacing.sm,
+    backgroundColor: color.background.lighter,
     color: color.font.regular,
     fontFamily: font.weight.regular,
     fontSize: font.size.p1,
