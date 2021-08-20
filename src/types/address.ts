561ac55c6653582b2f12b3d72e538206a1eda670
Frank Filius <f.filius@amsterdam.nl>
2021-08-20 15:23:46 +0200

    Add addresstype

---
+++ src/types/address.ts
@@ -0,0 +1,12 @@
+export type Address = {
+  results: [
+    {
+      adres: string
+      centroid: number[]
+      huisnummer: number
+      postcode: string
+      straatnaam: string
+      woonplaats: string
+    },
+  ]
+}
