a63a708fdb3ca696f481f305dee3a61602d8f827
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-08-26 11:23:44 +0200

    Add ‘emphasis’ prop to Links for bold black links

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -3,21 +3,30 @@ import {StyleSheet, Text} from 'react-native'
 import {color, font} from '../../tokens'
 
 type Props = {
-  text: string
+  emphasis?: Boolean // Bold black text instead of regular blue
   onPress: () => void
+  text: string
 }
 
-export const Link = ({text, onPress}: Props) => (
-  <Text style={styles.link} onPress={onPress}>
+export const Link = ({emphasis, onPress, text}: Props) => (
+  <Text
+    style={[styles.link, emphasis ? styles.emphasis : styles.notEmphasis]}
+    onPress={onPress}>
     {text}
   </Text>
 )
 
 const styles = StyleSheet.create({
+  emphasis: {
+    color: color.font.regular,
+    fontFamily: font.weight.demi,
+  },
   link: {
-    fontFamily: font.weight.regular,
     fontSize: font.size.p1,
     lineHeight: font.height.p1,
+  },
+  notEmphasis: {
+    fontFamily: font.weight.regular,
     color: color.touchable.primary,
     textDecorationLine: 'underline',
   },
