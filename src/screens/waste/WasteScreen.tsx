71adf6c9a10600c8457b3465a883d5af7c30f0fc
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-08-30 12:05:58 +0200

    Allow waste screen to use the non-safe area

--- src/screens/waste/WasteScreen.tsx
+++ src/screens/waste/WasteScreen.tsx
@@ -1,7 +1,7 @@
 import {useNavigation} from '@react-navigation/native'
 import React from 'react'
 import {ScrollView} from 'react-native'
-import {Box, Gutter, ScreenWrapper} from '../../components/ui'
+import {Box, Gutter} from '../../components/ui'
 import {size} from '../../tokens'
 import {
   RecyclingGuideBanner,
@@ -11,15 +11,13 @@ import {
 
 export const WasteScreen = () => {
   return (
-    <ScreenWrapper>
-      <ScrollView>
-        <WasteGuideByAddress />
-        <Box>
-          <RecyclingGuideBanner />
-          <Gutter height={size.spacing.md} />
-          <ReportNotCollectedBanner navigation={useNavigation()} />
-        </Box>
-      </ScrollView>
-    </ScreenWrapper>
+    <ScrollView>
+      <WasteGuideByAddress />
+      <Box>
+        <RecyclingGuideBanner />
+        <Gutter height={size.spacing.md} />
+        <ReportNotCollectedBanner navigation={useNavigation()} />
+      </Box>
+    </ScrollView>
   )
 }
