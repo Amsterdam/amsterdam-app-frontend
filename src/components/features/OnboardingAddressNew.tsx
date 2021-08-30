e841673438e91cf3be9b7bc2bf0d441a168c90fb
Frank Filius <f.filius@amsterdam.nl>
2021-08-30 20:07:57 +0200

    Create dummy-input component

---
+++ src/components/features/OnboardingAddressNew.tsx
@@ -0,0 +1,29 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
+import React from 'react'
+import {RootStackParamList} from '../../../App'
+import {Box, Card, CardBody, Text, TextInput, Title} from '../ui'
+
+export const OnboardingAddressNew = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<RootStackParamList, 'AddressForm'>>()
+
+  return (
+    <Box>
+      <Card>
+        <CardBody>
+          <Title margin text="Uw buurt" />
+          <Text margin>
+            Vul uw adres en huisnummer in zodat we informatie uit uw buurt
+            kunnen tonen.
+          </Text>
+          <TextInput
+            label="Vul uw postcode of straatnaam in"
+            onFocus={() => navigation.navigate('AddressForm')}
+            placeholder="1234AB of voorbeeldstraat"
+          />
+        </CardBody>
+      </Card>
+    </Box>
+  )
+}
