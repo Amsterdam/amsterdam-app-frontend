aa13c36df5d8dad21b009411ee2456516946d3ee
Frank Filius <f.filius@amsterdam.nl>
2021-08-30 20:11:09 +0200

    Put label in text-input component

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -6,38 +6,36 @@ import {
   View,
 } from 'react-native'
 import {color, font, size} from '../../tokens'
+import {Gutter} from './Gutter'
+import {Text} from './Text'
 
-type clearProps = {
-  onClear: () => void
-}
+type Props = {
+  label?: string
+  onClear?: () => void
+} & TextInputRNProps
 
-export const TextInput = React.forwardRef(
-  (props: TextInputRNProps & clearProps, ref: any) => {
-    return (
-      <View style={styles.searchSection}>
-        <TextInputRN ref={ref} {...props} style={styles.textInput} />
-      </View>
-    )
-  },
-)
+export const TextInput = React.forwardRef((props: Props, ref: any) => {
+  return (
+    <View style={styles.searchSection}>
+      <Text secondary>{props.label}</Text>
+      <Gutter height={size.spacing.sm} />
+      <TextInputRN ref={ref} {...props} style={styles.textInput} />
+    </View>
+  )
+})
 
 const styles = StyleSheet.create({
-  searchSection: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: size.spacing.xl,
-    borderColor: color.border.input,
-    borderStyle: 'solid',
-    borderWidth: 1,
-  },
+  searchSection: {},
   textInput: {
-    flex: 1,
     padding: size.spacing.sm,
     backgroundColor: color.background.lighter,
     color: color.font.regular,
     fontFamily: font.weight.regular,
     fontSize: font.size.p1,
     lineHeight: font.height.p1,
+    borderColor: color.border.input,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    height: size.spacing.xl,
   },
 })
