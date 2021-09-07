fded20c8ecb64760bf079dd8e4aca834a6fe02c5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-07 18:58:45 +0200

    Open waste banner links in web view rather than browser

--- src/screens/waste/waste-guide/WasteGuideCollectionPoints.tsx
+++ src/screens/waste/waste-guide/WasteGuideCollectionPoints.tsx
@@ -1,5 +1,8 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {Linking, StyleSheet, View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
+import {RootStackParamList, routes} from '../../../../App'
 import {
   Card,
   CardBody,
@@ -12,37 +15,43 @@ import {
 } from '../../../components/ui'
 import {size} from '../../../tokens'
 
-export const WasteGuideCollectionPoints = () => (
-  <Card>
-    <CardHeader>
-      <Title level={4} text="Afvalpunten" />
-    </CardHeader>
-    <CardBody>
-      <Text>
-        Op een Afvalpunt kunt u gratis uw grof afval, klein chemisch afval en{' '}
-        spullen voor de kringloop kwijt.
-      </Text>
-      <Gutter height={size.spacing.md} />
-      <Link
-        direction="forward"
-        emphasis
-        onPress={() =>
-          Linking.openURL(
-            'https://kaart.amsterdam.nl/#52.2744/4.7151/52.4355/5.0667/brt/9776/244/',
-          )
-        }
-        text="Bekijk de kaart met afvalpunten in de buurt"
-      />
-      <Gutter height={size.spacing.md} />
-      <View style={styles.figure}>
-        <Image
-          source={require('../../../assets/images/placeholder-map-collection-points.jpg')}
-          style={styles.image}
+export const WasteGuideCollectionPoints = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<RootStackParamList, 'Waste'>>()
+
+  return (
+    <Card>
+      <CardHeader>
+        <Title level={4} text="Afvalpunten" />
+      </CardHeader>
+      <CardBody>
+        <Text>
+          Op een Afvalpunt kunt u gratis uw grof afval, klein chemisch afval en{' '}
+          spullen voor de kringloop kwijt.
+        </Text>
+        <Gutter height={size.spacing.md} />
+        <Link
+          direction="forward"
+          emphasis
+          onPress={() =>
+            navigation.navigate(routes.webView.name, {
+              title: 'Afvalpunten in de buurt',
+              uri: 'https://kaart.amsterdam.nl/#52.2744/4.7151/52.4355/5.0667/brt/9776/244/',
+            })
+          }
+          text="Bekijk de kaart met afvalpunten in de buurt"
         />
-      </View>
-    </CardBody>
-  </Card>
-)
+        <Gutter height={size.spacing.md} />
+        <View style={styles.figure}>
+          <Image
+            source={require('../../../assets/images/placeholder-map-collection-points.jpg')}
+            style={styles.image}
+          />
+        </View>
+      </CardBody>
+    </Card>
+  )
+}
 
 const styles = StyleSheet.create({
   figure: {
--- src/screens/waste/waste-guide/WasteGuideContainers.tsx
+++ src/screens/waste/waste-guide/WasteGuideContainers.tsx
@@ -1,5 +1,8 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {Linking, StyleSheet, View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
+import {RootStackParamList, routes} from '../../../../App'
 import {
   Card,
   CardBody,
@@ -12,37 +15,43 @@ import {
 } from '../../../components/ui'
 import {size} from '../../../tokens'
 
-export const WasteGuideContainers = () => (
-  <Card>
-    <CardHeader>
-      <Title level={4} text="Containers in de buurt" />
-    </CardHeader>
-    <CardBody>
-      <Text>
-        Zoekt u een container voor glas, papier, textiel, plastic verpakkingen
-        of restafval?
-      </Text>
-      <Gutter height={size.spacing.md} />
-      <Link
-        direction="forward"
-        emphasis
-        onPress={() =>
-          Linking.openURL(
-            'https://kaart.amsterdam.nl/afvalcontainers#52.3524/4.8854/52.3575/4.8964/brt/12491,12492,12493,12494,12495,12496,12497//',
-          )
-        }
-        text="Bekijk de kaart met containers in de buurt"
-      />
-      <Gutter height={size.spacing.md} />
-      <View style={styles.figure}>
-        <Image
-          source={require('../../../assets/images/placeholder-map-containers.jpg')}
-          style={styles.image}
+export const WasteGuideContainers = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<RootStackParamList, 'Waste'>>()
+
+  return (
+    <Card>
+      <CardHeader>
+        <Title level={4} text="Containers in de buurt" />
+      </CardHeader>
+      <CardBody>
+        <Text>
+          Zoekt u een container voor glas, papier, textiel, plastic verpakkingen
+          of restafval?
+        </Text>
+        <Gutter height={size.spacing.md} />
+        <Link
+          direction="forward"
+          emphasis
+          onPress={() =>
+            navigation.navigate(routes.webView.name, {
+              title: 'Containers in de buurt',
+              uri: 'https://kaart.amsterdam.nl/#52.2744/4.7151/52.4355/5.0667/brt/9776/244/',
+            })
+          }
+          text="Bekijk de kaart met containers in de buurt"
         />
-      </View>
-    </CardBody>
-  </Card>
-)
+        <Gutter height={size.spacing.md} />
+        <View style={styles.figure}>
+          <Image
+            source={require('../../../assets/images/placeholder-map-containers.jpg')}
+            style={styles.image}
+          />
+        </View>
+      </CardBody>
+    </Card>
+  )
+}
 
 const styles = StyleSheet.create({
   figure: {
