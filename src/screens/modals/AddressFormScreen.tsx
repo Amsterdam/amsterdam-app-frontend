6309928bf507ef4e7facfed08cab7e3db4f8c71f
Frank Filius <f.filius@amsterdam.nl>
2021-09-07 15:15:54 +0200

    Use new component

--- src/screens/modals/AddressFormScreen.tsx
+++ src/screens/modals/AddressFormScreen.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import {NumberInput} from '../../components/features/address/NumberInput'
-import {StreetInput} from '../../components/features/address/StreetInput'
+import {AddressForm} from '../../components/features/address/AddressForm'
 import {ScreenWrapper} from '../../components/ui'
 
 export const AddressFormScreen = () => {
   return (
-    <ScreenWrapper>
-      <StreetInput />
-      <NumberInput />
+    <ScreenWrapper background="lighter">
+      <AddressForm />
     </ScreenWrapper>
   )
 }
