b635fac62274798e24bd3f96e68f9c841dc6d36b
Frank Filius <f.filius@amsterdam.nl>
2021-09-07 09:14:45 +0200

    Add clear-button, emit onChangeText

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Close from '@amsterdam/asc-assets/static/icons/Close.svg'
+import React, {useState} from 'react'
 import {
   StyleSheet,
   TextInput as TextInputRN,
   TextInputProps as TextInputRNProps,
+  TouchableOpacity,
   View,
 } from 'react-native'
 import {color, font, size} from '../../tokens'
@@ -11,31 +13,69 @@ import {Text} from './Text'
 
 type Props = {
   label?: string
-  onClear?: () => void
+  onChangeText: (event: string) => void
 } & TextInputRNProps
 
 export const TextInput = React.forwardRef((props: Props, ref: any) => {
+  const [value, setValue] = useState('')
+
+  const handleChangeText = (text: string) => {
+    setValue(text)
+    props.onChangeText(text)
+  }
+
+  const handleClearText = () => {
+    setValue('')
+    props.onChangeText('')
+  }
+
   return (
-    <View style={styles.searchSection}>
+    <View>
       <Text secondary>{props.label}</Text>
       <Gutter height={size.spacing.sm} />
-      <TextInputRN ref={ref} {...props} style={styles.textInput} />
+      <View style={styles.searchSection}>
+        <TextInputRN
+          ref={ref}
+          {...props}
+          onChangeText={text => handleChangeText(text)}
+          style={styles.textInput}
+          value={value}
+        />
+        {value ? (
+          <TouchableOpacity onPress={handleClearText}>
+            <Close
+              style={styles.searchIcon}
+              width={20}
+              height={20}
+              fill={'black'}
+            />
+          </TouchableOpacity>
+        ) : null}
+      </View>
     </View>
   )
 })
 
 const styles = StyleSheet.create({
-  searchSection: {},
+  searchSection: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: size.spacing.xl,
+    borderColor: color.border.input,
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
   textInput: {
+    flex: 1,
     padding: size.spacing.sm,
     backgroundColor: color.background.lighter,
     color: color.font.regular,
     fontFamily: font.weight.regular,
     fontSize: font.size.p1,
     lineHeight: font.height.p1,
-    borderColor: color.border.input,
-    borderStyle: 'solid',
-    borderWidth: 1,
-    height: size.spacing.xl,
   },
 })
