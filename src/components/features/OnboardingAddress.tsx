3e9ab116cda32ae880db30db75a3e0eb435499a8
Frank Filius <f.filius@amsterdam.nl>
2021-09-07 15:52:03 +0200

    Make component re-usable

--- src/components/features/OnboardingAddress.tsx
+++ src/components/features/OnboardingAddress.tsx
@@ -2,28 +2,28 @@ import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {RootStackParamList} from '../../../App'
-import {Box, Card, CardBody, Text, TextInput, Title} from '../ui'
+import {Card, CardBody, Text, TextInput, Title} from '../ui'
 
-export const OnboardingAddress = () => {
+type Props = {
+  text: string
+  title: string
+}
+
+export const OnboardingAddress = ({text, title}: Props) => {
   const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, 'AddressForm'>>()
+    useNavigation<StackNavigationProp<RootStackParamList, 'Home'>>()
 
   return (
-    <Box>
-      <Card>
-        <CardBody>
-          <Title margin text="Uw buurt" />
-          <Text margin>
-            Vul uw adres en huisnummer in zodat we informatie uit uw buurt
-            kunnen tonen.
-          </Text>
-          <TextInput
-            label="Vul uw postcode of straatnaam in"
-            onFocus={() => navigation.navigate('AddressForm')}
-            placeholder="1234AB of voorbeeldstraat"
-          />
-        </CardBody>
-      </Card>
-    </Box>
+    <Card>
+      <CardBody>
+        <Title margin text={title} />
+        <Text margin>{text}</Text>
+        <TextInput
+          label="Vul uw postcode of straatnaam in"
+          onFocus={() => navigation.navigate('AddressForm')}
+          placeholder="1234AB of voorbeeldstraat"
+        />
+      </CardBody>
+    </Card>
   )
 }
