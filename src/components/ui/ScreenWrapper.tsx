bb30cab1c1031ab573778915c35100e0ef95d4e1
Frank Filius <f.filius@amsterdam.nl>
2021-09-07 15:09:12 +0200

    Create a prop for background-color like in Box

--- src/components/ui/ScreenWrapper.tsx
+++ src/components/ui/ScreenWrapper.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {SafeAreaView, StyleSheet} from 'react-native'
+import {color} from '../../tokens'
 
 type Props = {
+  background?: 'emphasis' | 'invalid' | 'light' | 'lighter'
   children: React.ReactNode
 }
 
-export const ScreenWrapper = ({children}: Props) => {
+export const ScreenWrapper = ({background, children}: Props) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: background && color.background[background],
+    },
+  })
   return <SafeAreaView style={styles.container}>{children}</SafeAreaView>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-})
