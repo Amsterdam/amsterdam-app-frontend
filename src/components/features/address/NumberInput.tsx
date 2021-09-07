f7a37bb06a9a99acad497ec4da2079c1f6ccb2c8
Frank Filius <f.filius@amsterdam.nl>
2021-09-07 15:10:26 +0200

    Create separate input components for street and number

--- src/components/features/address/NumberInput.tsx
+++ src/components/features/address/NumberInput.tsx
@@ -1,6 +1,64 @@
+import Location from '@amsterdam/asc-assets/static/icons/Location.svg'
 import React from 'react'
-import {Text} from '../../ui'
+import {TouchableOpacity} from 'react-native'
+import {color, size} from '../../../tokens'
+import {Gutter, Text, TextInput} from '../../ui'
+import {BagResponseContent} from './AddressForm'
 
-export const NumberInput = () => {
-  return <Text>NumberInput</Text>
+type Props = {
+  bagList: BagResponseContent | null | undefined
+  changeNumber: (text: string) => void
+  inputNumberRef: any
+  isNumberSelected: boolean
+  isStreetSelected: boolean
+  number: string
+  selectNumber: (text: string) => void
+  styles: {suggestedItem: {}}
+}
+
+const getNumberFromAddress = (text: string) => {
+  return (
+    text
+      .split(' ')
+      .reverse()
+      .find(el => el.match(/^[0-9]/)) || ''
+  )
+}
+
+export const NumberInput = ({
+  bagList,
+  changeNumber,
+  inputNumberRef,
+  isNumberSelected,
+  isStreetSelected,
+  number,
+  selectNumber,
+  styles,
+}: Props) => {
+  return (
+    <>
+      <TextInput
+        autoFocus={isStreetSelected}
+        label="Huisnummer + toevoeging"
+        onChangeText={text => changeNumber(text)}
+        placeholder="Huisnummer"
+        ref={inputNumberRef}
+        value={number}
+      />
+      {isStreetSelected && !isNumberSelected && number
+        ? bagList?.map(bagItem => (
+            <TouchableOpacity
+              key={bagItem.uri}
+              onPress={() => {
+                selectNumber(getNumberFromAddress(bagItem._display))
+              }}
+              style={styles.suggestedItem}>
+              <Location width={24} height={24} fill={color.font.tertiary} />
+              <Gutter width={size.spacing.xs} />
+              <Text>{getNumberFromAddress(bagItem._display)}</Text>
+            </TouchableOpacity>
+          ))
+        : null}
+    </>
+  )
 }
