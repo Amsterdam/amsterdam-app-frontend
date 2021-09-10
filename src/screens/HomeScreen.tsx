f38a5be044522c041db6a2b13565bd31b273f7bc
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-10 10:25:41 +0200

    Experiment with landscape orientation on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,10 @@
 import {StackNavigationProp} from '@react-navigation/stack'
-import React from 'react'
+import React, {useContext} from 'react'
+import {StyleSheet, View} from 'react-native'
 import {RootStackParamList, routes} from '../../App'
 import {Address} from '../components/features/address/Address'
-import {Box, Button, Gutter, ScreenWrapper} from '../components/ui'
+import {Box, Button, Gutter} from '../components/ui'
+import {OrientationContext} from '../providers'
 import {size} from '../tokens'
 
 type Props = {
@@ -10,31 +12,56 @@ type Props = {
 }
 
 export const HomeScreen = ({navigation}: Props) => {
+  const orientationContext = useContext(OrientationContext)
+
   return (
-    <ScreenWrapper>
-      <Address />
-      <Box inset="xl">
-        <Button
-          onPress={() =>
-            navigation.navigate(routes.webView.name, {
-              title: 'Melding',
-              uri: 'https://acc.meldingen.amsterdam.nl/',
-            })
-          }
-          text="Maak een melding"
-        />
-        <Gutter height={size.spacing.md} />
-        <Button
-          onPress={() => navigation.navigate(routes.projectOverview.name)}
-          text="Bekijk werkzaamheden"
-        />
-        <Gutter height={size.spacing.md} />
-        <Button
-          onPress={() => navigation.navigate(routes.wasteGuide.name)}
-          text="Raadpleeg afvalinformatie"
-        />
-        <Gutter height={size.spacing.md} />
+    <>
+      <Box inset="lg">
+        <View style={!orientationContext.isPortrait && styles.row}>
+          <Address />
+          <Gutter
+            height={orientationContext.isPortrait ? size.spacing.xl : undefined}
+            width={orientationContext.isPortrait ? undefined : size.spacing.xl}
+          />
+          <View
+            style={[
+              styles.grow,
+              !orientationContext.isPortrait && styles.alignStart,
+            ]}>
+            <Button
+              onPress={() =>
+                navigation.navigate(routes.webView.name, {
+                  title: 'Melding',
+                  uri: 'https://acc.meldingen.amsterdam.nl/',
+                })
+              }
+              text="Maak een melding"
+            />
+            <Gutter height={size.spacing.md} />
+            <Button
+              onPress={() => navigation.navigate(routes.projectOverview.name)}
+              text="Bekijk werkzaamheden"
+            />
+            <Gutter height={size.spacing.md} />
+            <Button
+              onPress={() => navigation.navigate(routes.wasteGuide.name)}
+              text="Raadpleeg afvalinformatie"
+            />
+          </View>
+        </View>
       </Box>
-    </ScreenWrapper>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  alignStart: {
+    alignItems: 'flex-start',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+})
