a3aca5c4eef65abfc28e47f62d556ae5cee725fc
Frank Filius <f.filius@amsterdam.nl>
2021-09-22 12:54:34 +0200

    Use new components in push-screen

--- src/screens/PushNotificationScreen.tsx
+++ src/screens/PushNotificationScreen.tsx
@@ -1,9 +1,12 @@
 import {RouteProp} from '@react-navigation/core'
 import React, {useEffect, useState} from 'react'
 import {useForm, Controller} from 'react-hook-form'
-import {Platform} from 'react-native'
 import {RootStackParamList} from '../../App'
-import {Box, Button, Gutter, Text, TextInput, Title} from '../components/ui'
+import {
+  CharactersLeftDisplay,
+  ValidationWarning,
+} from '../components/features/form'
+import {Box, Button, Gutter, TextInput, Title} from '../components/ui'
 import {size} from '../tokens'
 import {NewNotification} from '../types/notification'
 
@@ -84,15 +87,10 @@ export const PushNotificationScreen = ({route}: Props) => {
         defaultValue=""
       />
       <Gutter height={size.spacing.xs} />
-      <Text secondary>
-        U kunt nog {maxCharacters.title - characterCountTitle} letters of tekens
-        typen.
-      </Text>
-      {errors.title && (
-        <Text accessibilityRole="alert" warning>
-          Vul een titel in
-        </Text>
-      )}
+      <CharactersLeftDisplay
+        charactersLeft={maxCharacters.title - characterCountTitle}
+      />
+      {errors.title && <ValidationWarning warning="Vul een titel in" />}
       <Gutter height={size.spacing.lg} />
       <Title level={2} text="Wat is de tekst van de pushnotificatie?" />
       <Gutter height={size.spacing.xs} />
@@ -105,9 +103,8 @@ export const PushNotificationScreen = ({route}: Props) => {
           <TextInput
             label="Vertel hier in een paar zinnen wat de situatie is."
             maxLength={maxCharacters.message}
-            minHeight={Platform.OS === 'ios' ? 82 : undefined}
             multiline={true}
-            numberOfLines={Platform.OS === 'ios' ? undefined : 3}
+            numberOfLines={3}
             onChangeText={onChange}
             value={value}
             warning={errors.message}
@@ -117,15 +114,10 @@ export const PushNotificationScreen = ({route}: Props) => {
         defaultValue=""
       />
       <Gutter height={size.spacing.xs} />
-      <Text secondary>
-        U kunt nog {maxCharacters.message - characterCountMessage} letters of
-        tekens typen.
-      </Text>
-      {errors.message && (
-        <Text accessibilityRole="alert" warning>
-          Type een bericht
-        </Text>
-      )}
+      <CharactersLeftDisplay
+        charactersLeft={maxCharacters.message - characterCountMessage}
+      />
+      {errors.message && <ValidationWarning warning="Type een bericht" />}
       <Gutter height={size.spacing.md} />
       <Button
         onPress={handleSubmit(onSubmit)}
