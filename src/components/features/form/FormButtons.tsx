9b30f5b332f6a5c4486f524b0e7b65dbdb8f5315
Frank Filius <f.filius@amsterdam.nl>
2021-09-22 17:08:09 +0200

    Vertically center buttons

--- src/components/features/form/FormButtons.tsx
+++ src/components/features/form/FormButtons.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
 
-export const FormButtons = ({children}: {children: React.ReactChildren}) => {
+export const FormButtons = ({
+  children,
+}: {
+  children: React.ReactNode | React.ReactChildren
+}) => {
   const styles = StyleSheet.create({
     buttons: {
       flexDirection: 'row',
       justifyContent:
         React.Children.count(children) > 1 ? 'space-between' : 'flex-end',
+      alignItems: 'center',
     },
   })
   return <View style={styles.buttons}>{children}</View>
