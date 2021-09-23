a04314d73709ca453fb45f1c522d25858402f02d
Frank Filius <f.filius@amsterdam.nl>
2021-09-23 14:26:53 +0200

    Create form-submit-feedback component

---
+++ src/components/features/form/FormSubmitFeedback.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+import Checkmark from '../../../assets/icons/checkmark.svg'
+import Close from '../../../assets/icons/close.svg'
+import {size} from '../../../tokens'
+import {Button, Card, CardBody, Gutter, Text, Title} from '../../ui'
+
+type Status = 'success' | 'failure'
+
+type StatusCollection = {
+  [K in Status]: {
+    body: string
+    button: React.ReactElement
+    icon: React.ReactElement
+    title: string
+  }
+}
+
+type Props = {
+  status: Status
+}
+
+const statusCollection: StatusCollection = {
+  failure: {
+    body: 'Het is niet gelukt om de pushnotificatie te versturen',
+    button: <Button onPress={() => {}} text="Ga naar de hel" />,
+    icon: <Close />,
+    title: 'Helaas...',
+  },
+  success: {
+    body: 'De pushnotificatie is verstuurd.',
+    button: <Button onPress={() => {}} text="Naar projectpagina" />,
+    icon: <Checkmark />,
+    title: 'Gelukt!',
+  },
+}
+
+export const FormSubmitFeedback = ({status}: Props) => {
+  return (
+    <Card>
+      <CardBody>
+        {statusCollection[status].icon}
+        <Gutter height={size.spacing.lg} />
+        <Title text={statusCollection[status].title} />
+        <Gutter height={size.spacing.sm} />
+        <Text>{statusCollection[status].body}</Text>
+        <Gutter height={size.spacing.md} />
+        <View style={styles.button}>{statusCollection[status].button}</View>
+      </CardBody>
+    </Card>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'flex-start',
+  },
+})
