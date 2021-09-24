3afdc3236c86befe4a6fa9f5635024b598c7e5e0
Frank Filius <f.filius@amsterdam.nl>
2021-09-24 13:29:03 +0200

    Create news-article screen

---
+++ src/screens/create-notification/SelectNewsArticleScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box, Title} from '../../components/ui'
+
+export const SelectNewsArticleScreen = () => {
+  return (
+    <Box>
+      <Title text="Kies een bericht" />
+    </Box>
+  )
+}
