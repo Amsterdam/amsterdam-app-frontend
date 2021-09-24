3850d9077101821209dd2283da1bde2270d3572d
Frank Filius <f.filius@amsterdam.nl>
2021-09-24 13:33:28 +0200

    Create radio and radiogroup components

---
+++ src/components/ui/Radio/Radio.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Text} from '..'
+
+export const Radio = () => {
+  return <Text>Radio</Text>
+}
---
+++ src/components/ui/Radio/RadioGroup.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Text} from '..'
+
+export const RadioGroup = () => {
+  return <Text>RadioGroup</Text>
+}
