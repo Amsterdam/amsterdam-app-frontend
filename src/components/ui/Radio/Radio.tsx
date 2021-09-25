741d7aff17ab65e342279d7c08882af053a68016
Frank Filius <f.filius@amsterdam.nl>
2021-09-25 08:58:18 +0200

    First attempt radio-button

--- src/components/ui/Radio/Radio.tsx
+++ src/components/ui/Radio/Radio.tsx
@@ -1,6 +1,46 @@
-import React from 'react'
-import {Text} from '..'
+import React, {useState} from 'react'
+import {Pressable, StyleSheet} from 'react-native'
+import {Center} from '..'
+import CircleIcon from '../../../assets/icons/circle.svg'
+import {color} from '../../../tokens'
 
-export const Radio = () => {
-  return <Text>Radio</Text>
+type Props = {
+  children: React.ReactNode
+  value: string
 }
+
+export const Radio = ({children, value}: Props) => {
+  console.log(value)
+  const [isChecked, setIsChecked] = useState<boolean>()
+  const handlePress = () => {
+    !isChecked && setIsChecked(true)
+  }
+  return (
+    <Pressable onPress={handlePress}>
+      <Center style={styles.outerCircle}>
+        <CircleIcon
+          fill={color.background.darker}
+          opacity={isChecked ? 1 : 0}
+          style={styles.innerCircle}
+        />
+      </Center>
+      {children}
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  outerCircle: {
+    flexDirection: 'row',
+    width: 24,
+    height: 24,
+    borderColor: color.border.input,
+    borderRadius: 50,
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  innerCircle: {
+    height: 16,
+    width: 16,
+  },
+})
