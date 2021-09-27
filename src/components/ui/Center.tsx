5e0b0856a2ad3d3dd2d1171e648c836f06113aff
Frank Filius <f.filius@amsterdam.nl>
2021-09-27 17:40:15 +0200

    Enable using style-prop on center-component

--- src/components/ui/Center.tsx
+++ src/components/ui/Center.tsx
@@ -5,8 +5,8 @@ type Props = {
   children: React.ReactNode
 } & ViewProps
 
-export const Center = ({children}: Props) => (
-  <View style={styles.center}>{children}</View>
+export const Center = ({children, ...otherProps}: Props) => (
+  <View style={[styles.center, otherProps.style]}>{children}</View>
 )
 
 const styles = StyleSheet.create({
