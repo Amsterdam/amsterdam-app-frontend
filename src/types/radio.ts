ffe7db90ea3125073e5702056dd1d667c8d07e1d
Frank Filius <f.filius@amsterdam.nl>
2021-09-27 17:39:42 +0200

    Create types for radio-btns

---
+++ src/types/radio.ts
@@ -0,0 +1,69 @@
+import React from 'react'
+import {ViewProps} from 'react-native'
+
+type IRadioValue = string
+
+type IRadioGroupOnChangeHandler = (value: IRadioValue) => any
+
+export interface IRadioProps extends ViewProps {
+  /**
+   * The value to be used in the radio input. This is the value that will be returned on form submission
+   */
+  value: IRadioValue
+  /**
+   * 	If true, the radio will be disabled
+   */
+  isDisabled?: boolean
+  /**
+   * If true, the radio is marked as invalid. Changes style of unchecked state.
+   */
+  isInvalid?: boolean
+  /**
+   * 	The size (width and height) of the radio.
+   */
+}
+
+export interface IRadioGroupProps extends ViewProps {
+  /**
+   * The value of the radio group.
+   */
+  value?: IRadioValue
+  /**
+   * The name of the input field in a radio (Useful for form submission).
+   */
+  name: string
+  /**
+   * The initial value of the radio group.
+   */
+  defaultValue?: IRadioValue
+  /**
+   *
+   */
+  // TODO: removing
+  children: React.ReactElement<IRadioProps> | React.ReactElement<IRadioProps>[]
+  /**
+   * The callback fired when any children radio is checked or unchecked.
+   */
+  onChange?: IRadioGroupOnChangeHandler
+}
+
+export interface RadioGroupState {
+  /**
+   * The name for the group, used for native form submission.
+   * @deprecated
+   * @private
+   */
+  readonly name: string
+  /** Whether the radio group is disabled. */
+  readonly isDisabled: boolean
+  /** Whether the radio group is read only. */
+  readonly isReadOnly: boolean
+  /** The currently selected value. */
+  readonly selectedValue: string | null
+  /** Sets the selected value. */
+  setSelectedValue(value: string): void
+  /** The value of the last focused radio. */
+  readonly lastFocusedValue: string | null
+  /** Sets the last focused value. */
+  setLastFocusedValue(value: string): void
+}
