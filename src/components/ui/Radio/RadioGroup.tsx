d62ddaaacb1bcf1e2a3883ee69f7f5fce813c344
Frank Filius <f.filius@amsterdam.nl>
2021-09-27 17:40:59 +0200

    Create radio-group

--- src/components/ui/Radio/RadioGroup.tsx
+++ src/components/ui/Radio/RadioGroup.tsx
@@ -1,6 +1,23 @@
+import {useRadioGroup} from '@react-native-aria/radio'
+import {useRadioGroupState} from '@react-stately/radio'
 import React from 'react'
-import {Text} from '..'
+import {Box} from '../'
+import {IRadioGroupProps, RadioGroupState} from '../../../types/radio'
 
-export const RadioGroup = () => {
-  return <Text>RadioGroup</Text>
+export const RadioContext = React.createContext({} as RadioGroupState)
+
+export const RadioGroup = (props: IRadioGroupProps) => {
+  const state = useRadioGroupState(props)
+  const {radioGroupProps} = useRadioGroup(
+    {...props, 'aria-label': props.accessibilityLabel},
+    state,
+  )
+  console.log(radioGroupProps)
+  return (
+    <Box {...radioGroupProps}>
+      <RadioContext.Provider value={{...state}}>
+        {props.children}
+      </RadioContext.Provider>
+    </Box>
+  )
 }
