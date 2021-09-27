879e42a37b1d752392a537c163070d514e7fbb2d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-27 10:35:04 +0200

    Make component a bit easier to read

--- src/components/features/form/FormButtons.tsx
+++ src/components/features/form/FormButtons.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {Children} from 'react'
 import {StyleSheet, View} from 'react-native'
 
-export const FormButtons = ({
-  children,
-}: {
+type Props = {
   children: React.ReactNode | React.ReactChildren
-}) => {
+}
+
+export const FormButtons = ({children}: Props) => {
+  const justifyContent =
+    Children.count(children) > 1 ? 'space-between' : 'flex-end'
+
   const styles = StyleSheet.create({
     buttons: {
       flexDirection: 'row',
-      justifyContent:
-        React.Children.count(children) > 1 ? 'space-between' : 'flex-end',
+      justifyContent,
       alignItems: 'center',
     },
   })
+
   return <View style={styles.buttons}>{children}</View>
 }
