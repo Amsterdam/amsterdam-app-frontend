b0fdc590c957426d2797189f0af41619c17f2358
Frank Filius <f.filius@amsterdam.nl>
2021-09-28 20:04:58 +0200

    Create preview-component

---
+++ src/components/ui/Preview.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {size} from '../../tokens'
+import {Box, Gutter, Text} from '.'
+
+type Props = {
+  children: React.ReactNode
+  label: string
+}
+
+export const Preview = ({children, label}: Props) => {
+  return (
+    <>
+      <Text secondary>{label}</Text>
+      <Gutter height={size.spacing.sm} />
+      <Box bordered>{children}</Box>
+    </>
+  )
+}
