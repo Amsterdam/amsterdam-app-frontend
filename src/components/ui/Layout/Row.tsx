845349948a598afca8db46f4c41742507e5f5448
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-30 13:37:11 +0200

    Add ‘end or between’ alignment option to `Row`

--- src/components/ui/Layout/Row.tsx
+++ src/components/ui/Layout/Row.tsx
@@ -1,23 +1,42 @@
-import React, {ReactNode} from 'react'
-import {StyleSheet, View} from 'react-native'
+import React, {Children, ReactNode} from 'react'
+import {FlexStyle, StyleSheet, View} from 'react-native'
+
+type Alignment =
+  | 'around'
+  | 'between'
+  | 'center'
+  | 'end'
+  | 'end-or-between'
+  | 'evenly'
+  | 'start'
 
 type Props = {
+  align?: Alignment
   children: ReactNode
-  align?: 'start' | 'end'
 }
 
-export const Row = ({align, children}: Props) => (
-  <View style={[styles.row, align && styles[align]]}>{children}</View>
-)
+export const Row = ({align, children}: Props) => {
+  const justifyContent: Record<string, FlexStyle['justifyContent']> = {
+    around: 'space-around',
+    between: 'space-between',
+    center: 'center',
+    end: 'flex-end',
+    'end-or-between':
+      Children.count(children) > 1 ? 'space-between' : 'flex-end',
+    evenly: 'space-evenly',
+    start: 'flex-start',
+  }
+
+  return (
+    <View
+      style={[styles.row, align && {justifyContent: justifyContent[align]}]}>
+      {children}
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
-  },
-  end: {
-    justifyContent: 'flex-end',
-  },
-  start: {
-    justifyContent: 'flex-start',
   },
 })
