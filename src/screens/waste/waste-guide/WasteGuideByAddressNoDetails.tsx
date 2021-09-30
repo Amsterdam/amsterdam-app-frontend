7093579d448f22c349891b1841001eaaba83f16f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-30 11:18:05 +0200

    Configure content instead of duplicating template

--- src/screens/waste/waste-guide/WasteGuideByAddressNoDetails.tsx
+++ src/screens/waste/waste-guide/WasteGuideByAddressNoDetails.tsx
@@ -23,63 +23,57 @@ export const WasteGuideByAddressNoDetails = ({address}: Props) => {
   const navigation =
     useNavigation<StackNavigationProp<RootStackParamList, 'Waste'>>()
 
+  const content =
+    address.woonplaats === 'Weesp'
+      ? {
+          title: 'GAD',
+          text:
+            'Het afval in Weesp wordt niet opgehaald door Gemeente Amsterdam ' +
+            'maar door het GAD. Bekijk hun website voor meer informatie.',
+          button: {
+            onPress: () =>
+              Linking.openURL(
+                'https://inzamelkalender.gad.nl/adres/' +
+                  [
+                    address.postcode,
+                    address.huisnummer,
+                    address.bag_toevoeging,
+                  ].join(':'),
+              ),
+            label: 'Ga naar GAD.nl',
+          },
+        }
+      : {
+          title: 'Niet gevonden',
+          text: `We konden geen afvalinformatie vinden voor het adres ${address.adres}, ${address.postcode} ${address.woonplaats}.`,
+          button: {
+            onPress: () =>
+              navigation.navigate(routes.webView.name, {
+                sliceFromTop: {portrait: 161, landscape: 207},
+                title: 'Melding afvalinformatie',
+                uri: 'https://formulier.amsterdam.nl/thema/afval-grondstoffen/klopt-afvalwijzer/Reactie/',
+              }),
+            label: 'Hier klopt iets niet',
+            secondary: true,
+          },
+        }
+
   return (
     <Card>
-      {address.woonplaats === 'Weesp' ? (
-        <>
-          <CardHeader>
-            <Title level={4} text="GAD" />
-          </CardHeader>
-          <CardBody>
-            <Text>
-              Het afval in Weesp wordt niet opgehaald door Gemeente Amsterdam
-              maar door het GAD. Bekijk hun website voor meer informatie.
-            </Text>
-            <Gutter height={size.spacing.md} />
-            <View style={styles.alignLeft}>
-              <Button
-                onPress={() =>
-                  Linking.openURL(
-                    'https://inzamelkalender.gad.nl/adres/' +
-                      [
-                        address.postcode,
-                        address.huisnummer,
-                        address.bag_toevoeging,
-                      ].join(':'),
-                  )
-                }
-                text="Ga naar GAD.nl"
-              />
-            </View>
-          </CardBody>
-        </>
-      ) : (
-        <>
-          <CardHeader>
-            <Title level={4} text="Niet gevonden" />
-          </CardHeader>
-          <CardBody>
-            <Text>
-              Geen afvalinformatie gevonden voor {address.adres},{' '}
-              {address.postcode} {address.woonplaats}.
-            </Text>
-            <Gutter height={size.spacing.md} />
-            <View style={styles.alignLeft}>
-              <Button
-                onPress={() =>
-                  navigation.navigate(routes.webView.name, {
-                    sliceFromTop: {portrait: 161, landscape: 207},
-                    title: 'Melding afvalinformatie',
-                    uri: 'https://formulier.amsterdam.nl/thema/afval-grondstoffen/klopt-afvalwijzer/Reactie/',
-                  })
-                }
-                text="Hier klopt iets niet"
-                variant="secondary"
-              />
-            </View>
-          </CardBody>
-        </>
-      )}
+      <CardHeader>
+        <Title level={4} text={content.title} />
+      </CardHeader>
+      <CardBody>
+        <Text>{content.text}</Text>
+        <Gutter height={size.spacing.md} />
+        <View style={styles.alignLeft}>
+          <Button
+            onPress={content.button.onPress}
+            text={content.button.label}
+            variant={content.button.secondary ? 'secondary' : undefined}
+          />
+        </View>
+      </CardBody>
     </Card>
   )
 }
