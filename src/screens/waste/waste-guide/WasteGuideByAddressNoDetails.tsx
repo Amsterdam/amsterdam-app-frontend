9902a677d0586231197e23e991185ffc45a1e2d5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-09-30 10:52:44 +0200

    Change boxes to cards and improve copy

--- src/screens/waste/waste-guide/WasteGuideByAddressNoDetails.tsx
+++ src/screens/waste/waste-guide/WasteGuideByAddressNoDetails.tsx
@@ -3,7 +3,15 @@ import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {Linking, StyleSheet, View} from 'react-native'
 import {RootStackParamList, routes} from '../../../../App'
-import {Box, Button, Gutter, Text} from '../../../components/ui'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Gutter,
+  Text,
+  Title,
+} from '../../../components/ui'
 import {size} from '../../../tokens'
 import {Address} from '../../../types'
 
@@ -16,50 +24,63 @@ export const WasteGuideByAddressNoDetails = ({address}: Props) => {
     useNavigation<StackNavigationProp<RootStackParamList, 'Waste'>>()
 
   return (
-    <Box background="lighter">
+    <Card>
       {address.woonplaats === 'Weesp' ? (
         <>
-          <Text>In Weesp wordt het afval opgehaald door de GAD.</Text>
-          <Gutter height={size.spacing.md} />
-          <View style={styles.alignLeft}>
-            <Button
-              onPress={() =>
-                Linking.openURL(
-                  'https://inzamelkalender.gad.nl/adres/' +
-                    [
-                      address.postcode,
-                      address.huisnummer,
-                      address.bag_toevoeging,
-                    ].join(':'),
-                )
-              }
-              text="Raadpleeg gad.nl"
-            />
-          </View>
+          <CardHeader>
+            <Title level={4} text="GAD" />
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Het afval in Weesp wordt niet opgehaald door Gemeente Amsterdam
+              maar door het GAD. Bekijk hun website voor meer informatie.
+            </Text>
+            <Gutter height={size.spacing.md} />
+            <View style={styles.alignLeft}>
+              <Button
+                onPress={() =>
+                  Linking.openURL(
+                    'https://inzamelkalender.gad.nl/adres/' +
+                      [
+                        address.postcode,
+                        address.huisnummer,
+                        address.bag_toevoeging,
+                      ].join(':'),
+                  )
+                }
+                text="Ga naar GAD.nl"
+              />
+            </View>
+          </CardBody>
         </>
       ) : (
         <>
-          <Text>
-            Geen afvalinformatie gevonden voor {address.adres},{' '}
-            {address.postcode} {address.woonplaats}.
-          </Text>
-          <Gutter height={size.spacing.md} />
-          <View style={styles.alignLeft}>
-            <Button
-              onPress={() =>
-                navigation.navigate(routes.webView.name, {
-                  sliceFromTop: {portrait: 161, landscape: 207},
-                  title: 'Melding afvalinformatie',
-                  uri: 'https://formulier.amsterdam.nl/thema/afval-grondstoffen/klopt-afvalwijzer/Reactie/',
-                })
-              }
-              text="Hier klopt iets niet"
-              variant="secondary"
-            />
-          </View>
+          <CardHeader>
+            <Title level={4} text="Niet gevonden" />
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Geen afvalinformatie gevonden voor {address.adres},{' '}
+              {address.postcode} {address.woonplaats}.
+            </Text>
+            <Gutter height={size.spacing.md} />
+            <View style={styles.alignLeft}>
+              <Button
+                onPress={() =>
+                  navigation.navigate(routes.webView.name, {
+                    sliceFromTop: {portrait: 161, landscape: 207},
+                    title: 'Melding afvalinformatie',
+                    uri: 'https://formulier.amsterdam.nl/thema/afval-grondstoffen/klopt-afvalwijzer/Reactie/',
+                  })
+                }
+                text="Hier klopt iets niet"
+                variant="secondary"
+              />
+            </View>
+          </CardBody>
         </>
       )}
-    </Box>
+    </Card>
   )
 }
 
