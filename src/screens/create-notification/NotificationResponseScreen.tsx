7d79ae91941a5d7b970c9c85757ebfa92f8f9c4c
Frank Filius <f.filius@amsterdam.nl>
2021-10-01 13:15:54 +0200

    Create the notification-response screen

---
+++ src/screens/create-notification/NotificationResponseScreen.tsx
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react'
+import {Checkmark, Close} from '../../assets/icons'
+import {Confirmation} from '../../components/features'
+import {NotificationContext} from '.'
+
+export const NotificationResponseScreen = () => {
+  const {responseStatus} = useContext(NotificationContext)
+
+  return (
+    <>
+      {responseStatus === 'success' ? (
+        <Confirmation
+          body="De pushnotificatie is verstuurd."
+          button={{onPress: 'popModal', text: 'Naar projectpagina'}}
+          icon={<Checkmark />}
+          title="Gelukt!"
+        />
+      ) : (
+        <Confirmation
+          body="Het is niet gelukt om de pushnotificatie te versturen."
+          button={{onPress: 'goBack', text: 'Probeer het nog eens'}}
+          icon={<Close />}
+          title="Helaas..."
+        />
+      )}
+    </>
+  )
+}
