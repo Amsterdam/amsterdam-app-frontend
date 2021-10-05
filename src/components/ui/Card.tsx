6f58506b5efacab48b1c7e59f32bd1f7e0efe14f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-10-05 13:35:48 +0200

    Fix token names in card and timeline

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
   cardHeader: {
     paddingHorizontal: size.spacing.md,
     paddingVertical: size.spacing.sm,
-    borderBottomColor: color.background.light,
+    borderBottomColor: color.border.default,
     borderBottomWidth: 1,
   },
 })
