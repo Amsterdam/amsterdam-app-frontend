b159cdc82813e0fa8be692f121881cbfa49ff33e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-10-05 13:32:29 +0200

    Refactor primary black and white colour tokens

--- src/tokens/color.ts
+++ src/tokens/color.ts
@@ -1,16 +1,16 @@
 const baseColor = {
   primary: {
+    black: '#000000',
     darkblue: '#004699',
     red: '#ec0000',
+    white: '#ffffff',
   },
   tint: {
-    level1: '#ffffff',
     level2: '#f5f5f5',
     level3: '#e6e6e6',
     level4: '#b4b4b4',
     level5: '#767676',
     level6: '#323232',
-    level7: '#000000',
   },
   support: {
     valid: '#00a03c',
@@ -30,27 +30,27 @@ const baseColor = {
 
 export const color = {
   background: {
-    darker: baseColor.tint.level7,
+    darker: baseColor.primary.black,
     emphasis: baseColor.primary.darkblue,
     inactive: baseColor.tint.level5,
     invalid: baseColor.support.invalid,
     light: baseColor.tint.level3,
-    lighter: baseColor.tint.level1,
+    lighter: baseColor.primary.white,
     lightish: baseColor.tint.level2,
   },
   border: {
     default: baseColor.tint.level3,
     input: baseColor.tint.level5,
-    inputFocus: baseColor.tint.level7,
+    inputFocus: baseColor.primary.black,
     invalid: baseColor.support.invalid,
     primary: baseColor.primary.darkblue,
   },
   font: {
     invalid: baseColor.support.invalid,
-    inverse: baseColor.tint.level1,
+    inverse: baseColor.primary.white,
     light: baseColor.tint.level6,
     primary: baseColor.primary.darkblue,
-    regular: baseColor.tint.level7,
+    regular: baseColor.primary.black,
     secondary: baseColor.tint.level5,
     tertiary: baseColor.tint.level4,
   },
