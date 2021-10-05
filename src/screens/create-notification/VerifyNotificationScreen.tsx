d83465b8953a8c121346bcbe9258a5d1c30f33fa
Frank Filius <f.filius@amsterdam.nl>
2021-10-05 13:34:28 +0200

    Remove unused import

--- src/screens/create-notification/VerifyNotificationScreen.tsx
+++ src/screens/create-notification/VerifyNotificationScreen.tsx
@@ -1,5 +1,5 @@
 import {StackNavigationProp} from '@react-navigation/stack'
-import React, {useContext, useEffect, useRef, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {StyleSheet, View} from 'react-native'
 import {
   Box,
