f48224fc0899624e6d25c74399877f099733d218
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-10-28 12:06:34 +0200

    Store push notification settings on device

--- src/screens/user/SettingsScreen.tsx
+++ src/screens/user/SettingsScreen.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
-import {Box, Text} from '../../components/ui'
-import {ScrollView} from '../../components/ui/layout'
+import React, {useCallback, useState} from 'react'
+import {Box, Button, Text, Title} from '../../components/ui'
+import {Column, Row, ScrollView} from '../../components/ui/layout'
+import {useAsyncStorage} from '../../hooks'
 
-export const SettingsScreen = () => (
-  <ScrollView>
-    <Box>
-      <Text>Hello settings!</Text>
-    </Box>
-  </ScrollView>
-)
+type PushSettings = {
+  active: Boolean
+}
+
+export const SettingsScreen = () => {
+  const [pushSettings, setPushSettings] = useState<PushSettings | undefined>(
+    undefined,
+  )
+
+  const asyncStorage = useAsyncStorage()
+
+  useCallback(async () => {
+    const pushNotificationsFromStore = await asyncStorage.getData(
+      'pushNotifications',
+    )
+    setPushSettings(pushNotificationsFromStore)
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <ScrollView>
+      <Box>
+        <Column gutter="md">
+          <Title text="Pushnotificaties" />
+          <Text>
+            U wilt momenteel {pushSettings?.active ? 'wel ' : 'geen '}
+            pushnotificaties ontvangen.
+          </Text>
+          <Row gutter="md">
+            <Button
+              onPress={() => setPushSettings({active: true})}
+              text="Ontvangen"
+            />
+            <Button
+              onPress={() => setPushSettings({active: false})}
+              text="Niet ontvangen"
+            />
+          </Row>
+        </Column>
+      </Box>
+    </ScrollView>
+  )
+}
