cb14efac9b33da87ec8e14eea3139da73e4eb824
Frank Filius <f.filius@amsterdam.nl>
2021-10-29 16:42:23 +0200

    Return authorizationstatus

--- src/utils/requestFirebasePermission.ts
+++ src/utils/requestFirebasePermission.ts
@@ -1,12 +1,6 @@
 import messaging from '@react-native-firebase/messaging'
 
-export const requestUserPermission = async () => {
-  const authStatus = await messaging().requestPermission()
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL
-
-  if (enabled) {
-    console.log('Authorization status:', authStatus)
-  }
+export const requestFirebasePermission = async () => {
+  await messaging().requestPermission()
+  return messaging.AuthorizationStatus
 }
