258e5f39a16a7ab08899c8c6cbe9f32a934c3025
Frank Filius <f.filius@amsterdam.nl>
2021-11-01 15:05:13 +0100

    Create function to request for firebase permission

--- src/utils/requestFirebasePermission.ts
+++ src/utils/requestFirebasePermission.ts
@@ -1,6 +1,39 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import messaging from '@react-native-firebase/messaging'
 
-export const requestFirebasePermission = async () => {
-  await messaging().requestPermission()
-  return messaging.AuthorizationStatus
+export const mapNumberToAuthorizationStatus = (number: number): string => {
+  switch (number) {
+    case -1:
+      return 'NOT_DETERMINED'
+    case 0:
+      return 'DENIED'
+    case 1:
+      return 'AUTHORIZED'
+    case 2:
+      return 'PROVISIONAL'
+    default:
+      return 'Not found'
+  }
+}
+
+export const requestFirebasePermission = async (): Promise<
+  number | unknown
+> => {
+  try {
+    const authStatus = await messaging().requestPermission()
+    return authStatus
+  } catch (error) {
+    return error
+  }
+}
+
+export const requestAndStoreFirebasePermission = async (): Promise<
+  void | unknown
+> => {
+  try {
+    const status = await requestFirebasePermission()
+    AsyncStorage.setItem('firebase', JSON.stringify({permissionStatus: status}))
+  } catch (error) {
+    return error
+  }
 }
