2fa821bcecad6c3379ec41f403f1557f4b72a8d1
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-11-19 21:53:04 +0100

    Fix name of query parameter

--- src/screens/ProjectOverviewByDistrictScreen.tsx
+++ src/screens/ProjectOverviewByDistrictScreen.tsx
@@ -29,7 +29,7 @@ export const ProjectOverviewByDistrictScreen = ({navigation, route}: Props) => {
     url: getEnvironment().apiUrl + '/projects',
     options: {
       params: {
-        district_id: districtId,
+        'district-id': districtId,
       },
     },
   })
