b1e4fac7364332425a19390b8e778a2ec83e4388
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-11-20 22:38:17 +0100

    Display project cards in multiple columns if screen width allows

--- src/screens/ProjectOverviewByDistrictScreen.tsx
+++ src/screens/ProjectOverviewByDistrictScreen.tsx
@@ -1,6 +1,6 @@
 import {RouteProp} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
-import React, {useLayoutEffect} from 'react'
+import React, {useLayoutEffect, useState} from 'react'
 import {ActivityIndicator, FlatList} from 'react-native'
 import {RootStackParamList} from '../../App'
 import {ProjectCard} from '../components/features/project'
@@ -23,6 +23,7 @@ type Props = {
 }
 
 export const ProjectOverviewByDistrictScreen = ({navigation, route}: Props) => {
+  const [gridWidth, setGridWidth] = useState(0)
   const districtId = route.params.id
 
   const {data: projects, isLoading} = useFetch<ProjectOverviewItem[]>({
@@ -44,26 +45,43 @@ export const ProjectOverviewByDistrictScreen = ({navigation, route}: Props) => {
     })
   })
 
+  // We need to calculate widths because FlatList items don’t flex as expected
+  const projectCardMinWidth = 280
+  const numColumns = Math.floor(gridWidth / projectCardMinWidth)
+  const gutterWidth = size.spacing.md
+  const itemWidth = (gridWidth - (numColumns + 1) * gutterWidth) / numColumns
+
   return (
-    <Box>
-      {isLoading ? (
+    <Box
+      onLayout={event => {
+        setGridWidth(event.nativeEvent.layout.width)
+      }}>
+      {isLoading || !gridWidth ? (
         <ActivityIndicator />
       ) : (
         <FlatList
+          key={`re-render-${numColumns}`}
           data={projects}
-          ItemSeparatorComponent={() => <Gutter height={size.spacing.md} />}
+          ItemSeparatorComponent={() => <Gutter height={gutterWidth} />}
           keyExtractor={item => item.identifier.toString()}
-          renderItem={({item}) => (
-            <ProjectCard
-              imageSource={{
-                uri: item.images[0].sources['460px'].url,
-              }}
-              onPress={() =>
-                navigation.navigate('ProjectDetail', {id: item.identifier})
-              }
-              subtitle={item.subtitle}
-              title={item.title}
-            />
+          numColumns={numColumns}
+          renderItem={({item, index}) => (
+            <>
+              <ProjectCard
+                imageSource={{
+                  uri: item.images[0].sources['460px'].url,
+                }}
+                onPress={() =>
+                  navigation.navigate('ProjectDetail', {id: item.identifier})
+                }
+                subtitle={item.subtitle}
+                title={item.title}
+                width={itemWidth}
+              />
+              {index % numColumns < numColumns - 1 && (
+                <Gutter width={gutterWidth} />
+              )}
+            </>
           )}
         />
       )}
