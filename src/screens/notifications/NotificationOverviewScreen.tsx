d97106d2d034840e35166b2df131d5804df964d6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-12-02 21:14:14 +0100

    Display notifications for subscribed projects only, match design more closely

--- src/screens/notifications/NotificationOverviewScreen.tsx
+++ src/screens/notifications/NotificationOverviewScreen.tsx
@@ -1,38 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {ScrollView, StyleSheet, View} from 'react-native'
 import {BellActive, BellInactive} from '../../assets/icons'
 import {Box, PleaseWait, Text} from '../../components/ui'
-import {Gutter} from '../../components/ui/layout'
+import {Gutter, Row} from '../../components/ui/layout'
 import {getEnvironment} from '../../environment'
-import {useFetch} from '../../hooks'
+import {useAsyncStorage, useFetch} from '../../hooks'
 import {color, size} from '../../tokens'
-import {Notification, ProjectOverviewItem} from '../../types'
+import {
+  Notification,
+  NotificationSettings,
+  ProjectOverviewItem,
+} from '../../types'
 import {formatDate} from '../../utils'
 
 export const NotificationOverviewScreen = () => {
-  // TEMP Get all projects to display all notifications
+  const asyncStorage = useAsyncStorage()
+  const [notificationSettings, setNotificationSettings] = useState<
+    NotificationSettings | undefined
+  >(undefined)
+
+  // Get all projects as we need to display their titles
   const {data: projects, isLoading: isProjectsLoading} = useFetch<
     ProjectOverviewItem[]
   >({
     url: getEnvironment().apiUrl + '/projects',
   })
 
-  const projectNames: Record<string, string> = projects
-    ? projects.reduce((obj, item) => {
-        return {
-          ...obj,
-          [item.identifier]: item.title,
-        }
-      }, {})
-    : {}
+  // Retrieve notification settings from device
+  useEffect(() => {
+    const retrieveNotificationSettings = async () => {
+      const currentNotificationSetting: NotificationSettings | undefined =
+        await asyncStorage.getData('notifications')
+      setNotificationSettings(currentNotificationSetting)
+    }
+    retrieveNotificationSettings()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
+  // Retrieve notifications for subscribed projects
   const {data: notifications, isLoading: isNotificationsLoading} = useFetch<
     Notification[]
   >({
     url: getEnvironment().apiUrl + '/notifications',
     options: {
       params: {
-        'project-ids': Object.keys(projectNames).join(','),
+        'project-ids': Object.keys(notificationSettings?.projects ?? {}).join(
+          ',',
+        ),
       },
     },
   })
@@ -41,12 +54,22 @@ export const NotificationOverviewScreen = () => {
     return <PleaseWait />
   }
 
-  const notificationsWithReadStatus: Notification[] = (notifications ?? []).map(
-    (notification, index) => ({
+  // Create mapping from project id to name
+  const projectTitles: Record<string, string> = projects
+    ? projects.reduce((acc, project) => {
+        return {
+          ...acc,
+          [project.identifier]: [project.title, project.subtitle].join(', '),
+        }
+      }, {})
+    : {}
+
+  const notificationsWithReadStatus: Notification[] = (notifications ?? [])
+    .sort((a, b) => (a.publication_date < b.publication_date ? 1 : -1))
+    .map(notification => ({
       ...notification,
-      isRead: index > 2, // TEMP
-    }),
-  )
+      isRead: true, // TEMP
+    }))
 
   return (
     <ScrollView>
@@ -54,22 +77,25 @@ export const NotificationOverviewScreen = () => {
         notificationsWithReadStatus.map(notification => (
           <View
             key={notification.publication_date}
-            style={[styles.item, !notification.isRead && styles.itemIsUnread]}>
-            <View style={styles.icon}>
-              {notification.isRead ? <BellInactive /> : <BellActive />}
-            </View>
-            <View style={styles.content}>
-              <Text intro small>
-                {notification.title}
+            style={[
+              styles.notification,
+              !notification.isRead && styles.notRead,
+            ]}>
+            <Row gutter="sm">
+              <Box insetVertical="xs">
+                {notification.isRead ? <BellInactive /> : <BellActive />}
+              </Box>
+              <Text small>
+                {projectTitles[notification.project_identifier]}
               </Text>
-              <Gutter height={size.spacing.xs} />
-              <Text>{notification.body}</Text>
-              <Gutter height={size.spacing.xs} />
-              <Text small>{projectNames[notification.project_identifier]}</Text>
-              <Text secondary small>
-                {formatDate(notification.publication_date)}
-              </Text>
-            </View>
+            </Row>
+            <Gutter height={size.spacing.sm} />
+            <Text intro accessibilityRole="header">
+              {notification.title}
+            </Text>
+            <Text>{notification.body}</Text>
+            <Gutter height={size.spacing.xs} />
+            <Text small>{formatDate(notification.publication_date)}</Text>
           </View>
         ))
       ) : (
@@ -82,20 +108,13 @@ export const NotificationOverviewScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-  },
-  icon: {
-    marginRight: size.spacing.sm,
-  },
-  item: {
-    flexDirection: 'row',
+  notification: {
     backgroundColor: color.background.grey,
     borderBottomWidth: 2,
     borderBottomColor: color.border.divider,
     padding: size.spacing.md,
   },
-  itemIsUnread: {
+  notRead: {
     backgroundColor: color.background.white,
     borderBottomColor: color.border.invalid,
   },
