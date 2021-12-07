ebccccc1f2a47a485f5109dd6638d99111b14f08
Frank Filius <f.filius@amsterdam.nl>
2021-12-07 18:11:41 +0100

    Create a type for Settings

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,11 +1,16 @@
 export type SubscribedProjects = Record<string, boolean>
 
 export type NotificationSettings = {
-  projectsEnabled?: boolean
-  projects?: SubscribedProjects
+  projectsEnabled: boolean
+  projects: SubscribedProjects
 }
 
 export type ProjectManagerSettings = {
   id: string
   projects: string[]
+}
+
+export type Settings = {
+  notifications?: NotificationSettings
+  'project-manager'?: ProjectManagerSettings
 }
