3d7142b23681a008189eb2929312b03ada46caee
Frank Filius <f.filius@amsterdam.nl>
2021-12-08 15:18:13 +0100

    Make implicit assumption explicit by commenting

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,3 +1,6 @@
+// if boolean is true, it means the user is subscribed to
+// projects with a 'false' value are held in store
+// to easily re-subscribe later
 export type SubscribedProjects = Record<string, boolean>
 
 export type NotificationSettings = {
