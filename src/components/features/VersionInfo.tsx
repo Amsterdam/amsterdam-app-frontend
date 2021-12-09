fb4eb7814f0608b2314817e45826dd98327549f2
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-12-09 22:59:22 +0100

    Implement final visual design

--- src/components/features/VersionInfo.tsx
+++ src/components/features/VersionInfo.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import VersionNumber from 'react-native-version-info'
 import {Text} from '../ui'
-import {Column} from '../ui/layout'
 
 export const VersionInfo = () => (
-  <Column halign="center">
-    <Text small secondary>
-      versie {VersionNumber.appVersion}
-    </Text>
-    <Text small secondary>
-      build {VersionNumber.buildVersion}
-    </Text>
-  </Column>
+  <Text small>
+    Versie {VersionNumber.appVersion}, build {VersionNumber.buildVersion}
+  </Text>
 )
