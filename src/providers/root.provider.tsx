354676469c5ee7f868d6c5f8a1acfb6a3172fa31
Frank Filius <f.filius@amsterdam.nl>
2021-12-17 16:11:56 +0100

    Render hook within the root provider

--- src/providers/root.provider.tsx
+++ src/providers/root.provider.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+
+import {useNotifee} from '../hooks'
 import {SettingsProvider} from './settings.provider'
 import {AddressProvider, DeviceProvider} from '.'
 
 export const RootProvider = ({children}: {children: React.ReactNode}) => {
+  useNotifee()
   return (
     <DeviceProvider>
       <AddressProvider>
