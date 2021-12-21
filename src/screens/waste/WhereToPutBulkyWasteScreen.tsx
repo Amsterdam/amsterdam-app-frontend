020ba2557f73cd665252d6ae36cd87d9ac2d2cf6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-12-21 08:43:40 +0100

    Delete unnecessary link

--- src/screens/waste/WhereToPutBulkyWasteScreen.tsx
+++ src/screens/waste/WhereToPutBulkyWasteScreen.tsx
@@ -1,84 +1,61 @@
-import {useNavigation} from '@react-navigation/native'
-import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {ScrollView, StyleSheet, View} from 'react-native'
-import {RootStackParamList, routes} from '../../../App'
-import {
-  Box,
-  Card,
-  CardBody,
-  Image,
-  Text,
-  TextButton,
-  Title,
-} from '../../components/ui'
+import {Box, Card, CardBody, Image, Text, Title} from '../../components/ui'
 import {Gutter} from '../../components/ui/layout'
 import {size} from '../../tokens'
 
-export const WhereToPutBulkyWasteScreen = () => {
-  const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, 'Waste'>>()
-
-  return (
-    <ScrollView>
-      <Box background="white">
-        <Title text="Buiten zetten of naar een afvalpunt?" />
-        <Gutter height={size.spacing.sm} />
-        <Text>
-          Grof afval zijn grote spullen die niet in een vuilniszak of
-          ondergrondse afvalcontainer passen.
-        </Text>
-      </Box>
-      <Box>
-        <Card>
-          <CardBody>
-            <View style={styles.figure}>
-              <Image
-                source={require('../../assets/images/grofafval-buiten.png')}
-                style={styles.image}
-              />
-            </View>
-            <Gutter height={size.spacing.md} />
-            <Title level={2} text="Grof afval dat we ophalen" />
-            <Text>
-              Spullen uit uw woning die niet passen in een ondergrondse
-              container. Bijvoorbeeld een bank, stoel, kast, bed, tuinmeubel,
-              tapijt, plank, matras en grote elektrische apparaten (ijskast en
-              wasmachine).
-            </Text>
-          </CardBody>
-        </Card>
-        <Gutter height={size.spacing.md} />
-        <Card>
-          <CardBody>
-            <View style={styles.figure}>
-              <Image
-                source={require('../../assets/images/grofafval-afvalpunt.png')}
-                style={styles.image}
-              />
-            </View>
-            <Gutter height={size.spacing.md} />
-            <Title
-              level={2}
-              text="Dit mag weggebracht worden naar een afvalpunt"
+export const WhereToPutBulkyWasteScreen = () => (
+  <ScrollView>
+    <Box background="white">
+      <Title text="Buiten zetten of naar een afvalpunt?" />
+      <Gutter height={size.spacing.sm} />
+      <Text>
+        Grof afval zijn grote spullen die niet in een vuilniszak of ondergrondse
+        afvalcontainer passen.
+      </Text>
+    </Box>
+    <Box>
+      <Card>
+        <CardBody>
+          <View style={styles.figure}>
+            <Image
+              source={require('../../assets/images/grofafval-buiten.png')}
+              style={styles.image}
             />
-            <Text>
-              Bouw- en sloopafval, tuintegels, klein chemisch afval, kleine
-              elektrische apparaten, auto-onderdelen, overige motoren, stenen,
-              zand, aarde, autobanden, karton en glas(platen).
-            </Text>
-          </CardBody>
-        </Card>
-        <Gutter height={size.spacing.md} />
-        <TextButton
-          direction="backward"
-          onPress={() => navigation.navigate(routes.wasteGuide.name)}
-          text="Afvalinformatie op adres"
-        />
-      </Box>
-    </ScrollView>
-  )
-}
+          </View>
+          <Gutter height={size.spacing.md} />
+          <Title level={2} text="Grof afval dat we ophalen" />
+          <Text>
+            Spullen uit uw woning die niet passen in een ondergrondse container.
+            Bijvoorbeeld een bank, stoel, kast, bed, tuinmeubel, tapijt, plank,
+            matras en grote elektrische apparaten (ijskast en wasmachine).
+          </Text>
+        </CardBody>
+      </Card>
+      <Gutter height={size.spacing.md} />
+      <Card>
+        <CardBody>
+          <View style={styles.figure}>
+            <Image
+              source={require('../../assets/images/grofafval-afvalpunt.png')}
+              style={styles.image}
+            />
+          </View>
+          <Gutter height={size.spacing.md} />
+          <Title
+            level={2}
+            text="Dit mag weggebracht worden naar een afvalpunt"
+          />
+          <Text>
+            Bouw- en sloopafval, tuintegels, klein chemisch afval, kleine
+            elektrische apparaten, auto-onderdelen, overige motoren, stenen,
+            zand, aarde, autobanden, karton en glas(platen).
+          </Text>
+        </CardBody>
+      </Card>
+    </Box>
+  </ScrollView>
+)
 
 const styles = StyleSheet.create({
   figure: {
