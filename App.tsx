d379bdb5e807738d4dcadd1428344d1d28c57681
Frank Filius <f.filius@amsterdam.nl>
2021-12-22 18:03:13 +0100

    Add comment

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ export const App = () => {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content" />
+      {/* Find out whats going on here */}
       <NavigationContainer linking={linking}>
         <RootProvider>
           <TabNavigator />
