99722aa223d1d120549b3a6bfb92b9d7211d289f
Frank Filius <f.filius@amsterdam.nl>
2021-12-22 18:49:50 +0100

    Fix navigation

--- src/components/features/best-wishes-21/BestWishes21Banner.tsx
+++ src/components/features/best-wishes-21/BestWishes21Banner.tsx
@@ -1,18 +1,34 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {RootStackParamList, routes} from '../../../../App'
+import {
+  homeScreenOptions,
+  tabNavOptions,
+} from '../../../App/navigation/screenOptions'
+import {
+  HomeStackParamList,
+  RootStackParamList,
+} from '../../../App/navigation/types'
 import {BannerCard} from '../BannerCard'
 
 export const BestWishes21Banner = () => {
   const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, 'BestWishes21'>>()
+    useNavigation<
+      StackNavigationProp<
+        HomeStackParamList & RootStackParamList,
+        'BestWishes21'
+      >
+    >()
 
   return (
     <BannerCard
       border
       imageSource={require('../../../assets/images/best-wishes-21/kerstbanner.gif')}
-      onPress={() => navigation.navigate(routes.bestWishes21.name)}
+      onPress={() =>
+        navigation.navigate(tabNavOptions.home.name, {
+          screen: homeScreenOptions.bestWishes21.name,
+        })
+      }
       subtitle="Eén app voor de Amsterdammer"
       title="Terugblikken & vooruitkijken"
     />
