f5e11373ccaf26b3394ef5cd389c18cc43df251d
Frank Filius <f.filius@amsterdam.nl>
2021-12-22 17:58:38 +0100

    Return accidentally removed component

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import {StatusBar} from 'react-native'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {TabNavigator} from './src/App/navigation/TabNavigator'
 import {linking} from './src/App/navigation/linking'
+import {RootProvider} from './src/providers/root.provider'
 
 export const App = () => {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer linking={linking}>
-        <TabNavigator />
+        <RootProvider>
+          <TabNavigator />
+        </RootProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   )
