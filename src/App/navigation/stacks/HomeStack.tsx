1e4ef69a295a5bb978978d51174a96cacdafd52a
Frank Filius <f.filius@amsterdam.nl>
2021-12-22 18:51:10 +0100

    Add 21wishes-component to home-stack

--- src/App/navigation/stacks/HomeStack.tsx
+++ src/App/navigation/stacks/HomeStack.tsx
@@ -1,15 +1,20 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
-import {HomeScreen} from '../../../screens'
+import {BestWishes21Screen, HomeScreen} from '../../../screens'
 import {homeScreenOptions} from '../screenOptions'
 
 const Stack = createStackNavigator()
 
 export const HomeStack = () => {
-  const {home} = homeScreenOptions
+  const {bestWishes21, home} = homeScreenOptions
   return (
     <Stack.Navigator>
       <Stack.Screen component={HomeScreen} name="Home" options={home.options} />
+      <Stack.Screen
+        component={BestWishes21Screen}
+        name="BestWishes21"
+        options={bestWishes21.options}
+      />
     </Stack.Navigator>
   )
 }
