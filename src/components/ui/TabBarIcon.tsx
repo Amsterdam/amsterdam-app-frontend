7277ee5140cd501fa3985d90555743b66622576b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2021-12-23 14:46:14 +0100

    Add correct tab bar icon for ‘Melden’

--- src/components/ui/TabBarIcon.tsx
+++ src/components/ui/TabBarIcon.tsx
@@ -1,8 +1,8 @@
 import Housing from '@amsterdam/asc-assets/static/icons/Housing.svg'
 import Menu from '@amsterdam/asc-assets/static/icons/Menu.svg'
-import Pointer from '@amsterdam/asc-assets/static/icons/Pointer.svg'
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
+import Melden from '../../assets/icons/melden.svg'
 import {color} from '../../tokens'
 
 type Props = {
@@ -12,35 +12,55 @@ type Props = {
 
 export const TabBarIcon = ({focused, name}: Props) => {
   const iconConfig = {
-    home: {component: Housing, inactiveColor: color.font.regular},
-    report: {component: Pointer, inactiveColor: color.touchable.primary},
-    menu: {component: Menu, inactiveColor: color.font.regular},
+    home: {
+      component: Housing,
+      inactiveColor: color.font.regular,
+      marginTop: undefined,
+      size: 24,
+    },
+    report: {
+      component: Melden,
+      inactiveColor: color.touchable.primary,
+      marginTop: -16,
+      size: 56,
+    },
+    menu: {
+      component: Menu,
+      inactiveColor: color.font.regular,
+      marginTop: undefined,
+      size: 24,
+    },
   }
-  const Icon = iconConfig[name].component
+
+  const icon = iconConfig[name]
+  const Icon = icon.component
   const foregroundColour = focused
     ? color.touchable.secondary
-    : iconConfig[name].inactiveColor
+    : icon.inactiveColor
+
+  const styles = StyleSheet.create({
+    focused: {
+      borderTopColor:
+        name === 'report' ? 'transparent' : color.touchable.secondary,
+    },
+    icon: {
+      fill: foregroundColour,
+      width: icon.size,
+      height: icon.size,
+      marginTop: icon.marginTop,
+    },
+    tabBarIcon: {
+      paddingHorizontal: 20,
+      paddingVertical: 4,
+      alignItems: 'center',
+      borderTopWidth: 3,
+      borderTopColor: 'transparent',
+    },
+  })
 
   return (
     <View style={[styles.tabBarIcon, focused && styles.focused]}>
-      <Icon fill={foregroundColour} style={styles.iconSize} />
+      <Icon style={styles.icon} />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  focused: {
-    borderTopColor: color.touchable.secondary,
-  },
-  iconSize: {
-    width: 24,
-    height: 24,
-  },
-  tabBarIcon: {
-    paddingHorizontal: 20,
-    paddingVertical: 4,
-    alignItems: 'center',
-    borderTopWidth: 3,
-    borderTopColor: 'transparent',
-  },
-})
