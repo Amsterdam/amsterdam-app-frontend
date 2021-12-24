8419a32d8d863a490409c20d4f48ec30cb18f8e5
Frank Filius <f.filius@amsterdam.nl>
2021-12-24 10:06:04 +0100

    Add screen to home stack

--- src/App/navigation/stacks/HomeStack.tsx
+++ src/App/navigation/stacks/HomeStack.tsx
@@ -1,15 +1,24 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
+import {stackScreenOptions} from '..'
 import {BestWishes21Screen, HomeScreen} from '../../../screens'
+import {AddressFormScreen} from '../../../screens/modals/AddressFormScreen'
 import {homeScreenOptions} from '../screenOptions'
 
 const Stack = createStackNavigator()
 
 export const HomeStack = () => {
-  const {bestWishes21, home} = homeScreenOptions
+  const {addressForm, bestWishes21, home} = homeScreenOptions
   return (
-    <Stack.Navigator initialRouteName={home.name}>
+    <Stack.Navigator
+      initialRouteName={home.name}
+      screenOptions={stackScreenOptions}>
       <Stack.Screen component={HomeScreen} name="Home" options={home.options} />
+      <Stack.Screen
+        component={AddressFormScreen}
+        name={addressForm.name}
+        options={addressForm.options}
+      />
       <Stack.Screen
         component={BestWishes21Screen}
         name="BestWishes21"
