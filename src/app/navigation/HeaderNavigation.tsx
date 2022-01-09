a89365dbba7a20d23c26f3d0f59a4ba0f2c3d7d1
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-01-09 15:45:12 +0100

    Link header navigation icons

--- src/app/navigation/HeaderNavigation.tsx
+++ src/app/navigation/HeaderNavigation.tsx
@@ -1,37 +1,57 @@
 import PersonalLogin from '@amsterdam/asc-assets/static/icons/PersonalLogin.svg'
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, TouchableOpacity} from 'react-native'
 import {BellInactive} from '../../assets/icons'
 import {Row} from '../../components/ui/layout'
 import {color} from '../../tokens'
+import {menuRoutes} from './routes/menuRoutes'
+import {MenuStackParams, TabParams} from './types'
+import {tabRoutes} from '.'
 
 type MenuItem = {
+  icon: React.ReactNode
   name: string
-  component: React.ReactNode
+  route: keyof MenuStackParams
 }
 
 const iconProps = {fill: color.font.regular}
 
 const menu: MenuItem[] = [
   {
+    icon: <BellInactive {...iconProps} />,
     name: 'notifications',
-    component: <BellInactive {...iconProps} />,
+    route: menuRoutes.notificationOverview.name,
   },
   {
+    icon: <PersonalLogin {...iconProps} />,
     name: 'settings',
-    component: <PersonalLogin {...iconProps} />,
+    route: menuRoutes.settings.name,
   },
 ]
 
-export const HeaderNavigation = () => (
-  <Row gutter="md">
-    {menu.map(({name, component}) => (
-      <View style={styles.icon} key={name}>
-        {component}
-      </View>
-    ))}
-  </Row>
-)
+export const HeaderNavigation = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<MenuStackParams & TabParams, 'Menu'>>()
+
+  return (
+    <Row gutter="md">
+      {menu.map(({icon, name, route}) => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate(tabRoutes.menu.name, {
+              screen: route,
+            })
+          }
+          style={styles.icon}
+          key={name}>
+          {icon}
+        </TouchableOpacity>
+      ))}
+    </Row>
+  )
+}
 
 const styles = StyleSheet.create({
   icon: {
