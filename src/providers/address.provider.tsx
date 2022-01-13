851554bb9723bdc70a4e2204bba18ed819df4c8a
Frank Filius <f.filius@amsterdam.nl>
2022-01-13 16:30:05 +0100

    Use remove functionality to remove address

--- src/providers/address.provider.tsx
+++ src/providers/address.provider.tsx
@@ -8,12 +8,14 @@ type Context = {
   address: Address | undefined
   saveAddress: ChangeAddress
   changeTempAddress: ChangeAddress
+  removeAddress: () => void
   tempAddress: Address | undefined
 }
 
 export const AddressContext = createContext<Context>({
   address: undefined,
   changeTempAddress: () => {},
+  removeAddress: () => {},
   saveAddress: () => {},
   tempAddress: undefined,
 })
@@ -22,7 +24,7 @@ export const AddressProvider = ({children}: {children: React.ReactNode}) => {
   const asyncStorage = useAsyncStorage()
   const [address, setAddress] = useState<Address | undefined>()
   const [tempAddress, setTempAddress] = useState<Address | undefined>()
-  const [isAddressSaved, setAddressSaved] = useState<boolean | undefined>()
+  const [isAddressSaved, setAddressSaved] = useState<boolean | undefined>(false)
 
   const retrieveAddress = useCallback(async () => {
     const newAddress = await asyncStorage.getValue('address')
@@ -37,6 +39,7 @@ export const AddressProvider = ({children}: {children: React.ReactNode}) => {
   // Also retrieve new address from local storage when new one is saved
   useEffect(() => {
     isAddressSaved && retrieveAddress()
+    setAddressSaved(false)
   }, [isAddressSaved, retrieveAddress])
 
   const saveAddress: ChangeAddress = async newAddress => {
@@ -48,9 +51,20 @@ export const AddressProvider = ({children}: {children: React.ReactNode}) => {
     setTempAddress(newAddress)
   }
 
+  const removeAddress = () => {
+    asyncStorage.removeValue('address')
+    setAddressSaved(true)
+  }
+
   return (
     <AddressContext.Provider
-      value={{address, changeTempAddress, saveAddress, tempAddress}}>
+      value={{
+        address,
+        changeTempAddress,
+        saveAddress,
+        removeAddress,
+        tempAddress,
+      }}>
       {children}
     </AddressContext.Provider>
   )
