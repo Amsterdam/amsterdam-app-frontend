2d62263c7eb58500a598e5e03489c9bb310f8825
Frank Filius <f.filius@amsterdam.nl>
2022-01-13 15:09:12 +0100

    Upgrade with temp and saved address split

--- src/providers/address.provider.tsx
+++ src/providers/address.provider.tsx
@@ -1,35 +1,54 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useCallback, useEffect, useState} from 'react'
+import {useAsyncStorage} from '../hooks'
 import {Address} from '../types'
+
+type ChangeAddress = (newAddress: Address) => void
 
 type Context = {
   address: Address | undefined
-  changeAddress: (text: Address | undefined) => void
-  changeSaveInStore: (state: boolean) => void
-  saveInStore: boolean | undefined
+  saveAddress: ChangeAddress
+  changeTempAddress: ChangeAddress
+  tempAddress: Address | undefined
 }
 
 export const AddressContext = createContext<Context>({
   address: undefined,
-  changeAddress: () => {},
-  changeSaveInStore: () => {},
-  saveInStore: true,
+  changeTempAddress: () => {},
+  saveAddress: () => {},
+  tempAddress: undefined,
 })
 
 export const AddressProvider = ({children}: {children: React.ReactNode}) => {
-  const [saveInStore, setSaveInStore] = useState<boolean>(true)
+  const asyncStorage = useAsyncStorage()
   const [address, setAddress] = useState<Address | undefined>()
+  const [tempAddress, setTempAddress] = useState<Address | undefined>()
+  const [isAddressSaved, setAddressSaved] = useState<boolean | undefined>()
 
-  const changeSaveInStore = (state: boolean) => {
-    setSaveInStore(state)
+  const retrieveAddress = useCallback(async () => {
+    const newAddress = await asyncStorage.getValue('address')
+    setAddress(newAddress)
+  }, [asyncStorage])
+
+  useEffect(() => {
+    retrieveAddress()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    isAddressSaved && retrieveAddress()
+  }, [isAddressSaved, retrieveAddress])
+
+  const saveAddress: ChangeAddress = async newAddress => {
+    await asyncStorage.storeData('address', newAddress)
+    setAddressSaved(true)
   }
 
-  const changeAddress = (newAddress: Address | undefined) => {
-    setAddress(newAddress)
+  const changeTempAddress: ChangeAddress = newAddress => {
+    setTempAddress(newAddress)
   }
 
   return (
     <AddressContext.Provider
-      value={{address, changeAddress, changeSaveInStore, saveInStore}}>
+      value={{address, changeTempAddress, saveAddress, tempAddress}}>
       {children}
     </AddressContext.Provider>
   )
