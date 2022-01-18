f7ecfac00a81d3d50aac019801549253079f084a
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-01-18 12:37:04 +0100

    Make header navigation buttons easier to hit

--- src/app/navigation/HeaderNavigation.tsx
+++ src/app/navigation/HeaderNavigation.tsx
@@ -2,10 +2,10 @@ import PersonalLogin from '@amsterdam/asc-assets/static/icons/PersonalLogin.svg'
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {StyleSheet, TouchableOpacity} from 'react-native'
+import {Insets, StyleSheet, TouchableOpacity} from 'react-native'
 import {BellInactive} from '../../assets/icons'
 import {Row} from '../../components/ui/layout'
-import {color} from '../../tokens'
+import {color, size} from '../../tokens'
 import {routes} from './routes'
 import {StackParams, TabParams} from './types'
 
@@ -14,6 +14,13 @@ type MenuItem = {
   label: string
   name: string
   route: keyof StackParams
+}
+
+const hitSlop: Insets = {
+  top: size.spacing.sm,
+  bottom: size.spacing.sm,
+  left: size.spacing.sm,
+  right: size.spacing.sm,
 }
 
 const iconProps = {
@@ -45,6 +52,7 @@ export const HeaderNavigation = () => {
         <TouchableOpacity
           accessibilityLabel={label}
           accessibilityRole="button"
+          hitSlop={hitSlop}
           key={name}
           onPress={() => navigation.navigate(route)}
           style={styles.icon}>
