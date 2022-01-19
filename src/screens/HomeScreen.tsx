3a90af8e41d638f621ef846a7e43e7f4b359ae68
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-01-19 23:37:03 +0100

    Hide banner if address has been provided

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {ScrollView} from 'react-native'
 import {ProvideAddressBanner, QuickLinks} from '../components/features/home'
 import {Box} from '../components/ui'
 import {Column} from '../components/ui/layout'
+import {AddressContext} from '../providers'
 
-export const HomeScreen = () => (
-  <ScrollView>
-    <Box background="white">
-      <Column gutter="lg">
-        <ProvideAddressBanner />
-        <QuickLinks />
-      </Column>
-    </Box>
-  </ScrollView>
-)
+export const HomeScreen = () => {
+  const addressContext = useContext(AddressContext)
+
+  return (
+    <ScrollView>
+      <Box background="white">
+        <Column gutter="lg">
+          {!addressContext.address && <ProvideAddressBanner />}
+          <QuickLinks />
+        </Column>
+      </Box>
+    </ScrollView>
+  )
+}
