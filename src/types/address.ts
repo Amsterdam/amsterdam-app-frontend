9b811f9f14dc8e02687c073bf5ac8fe0758eecfa
Frank Filius <f.filius@amsterdam.nl>
2022-01-20 17:09:01 +0100

    Intersect types instead of repetition

--- src/types/address.ts
+++ src/types/address.ts
@@ -9,27 +9,19 @@ export type Address = {
   woonplaats: string
 }
 
-export type ApiAddress = {
-  type: string
-  dataset: string
-  adres: string
-  postcode: string
-  straatnaam: string
-  straatnaam_no_ws: string
-  huisnummer: number
-  toevoeging: string
-  bag_huisletter: string
-  bag_toevoeging: string
-  woonplaats: string
-  type_adres: string
-  status: string
-  landelijk_id: string
-  vbo_status: string
-  adresseerbaar_object_id: string
-  subtype: string
-  centroid: [number, number]
-  subtype_id: string
+export type ApiAddress = Address & {
   _display: string
+  adresseerbaar_object_id: string
+  dataset: string
+  landelijk_id: string
+  status: string
+  straatnaam_no_ws: string
+  subtype: string
+  subtype_id: string
+  toevoeging: string
+  type: string
+  type_adres: string
+  vbo_status: string
 }
 
 export type ResponseAddress = {
