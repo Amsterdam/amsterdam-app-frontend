84ded18f55564bd98d8145ddd2e24fed22c47a9c
Frank Filius <f.filius@amsterdam.nl>
2022-01-20 16:14:50 +0100

    Specify address from api

--- src/types/address.ts
+++ src/types/address.ts
@@ -9,6 +9,29 @@ export type Address = {
   woonplaats: string
 }
 
+export type ApiAddress = {
+  type: string
+  dataset: string
+  adres: string
+  postcode: string
+  straatnaam: string
+  straatnaam_no_ws: string
+  huisnummer: number
+  toevoeging: string
+  bag_huisletter: string
+  bag_toevoeging: string
+  woonplaats: string
+  type_adres: string
+  status: string
+  landelijk_id: string
+  vbo_status: string
+  adresseerbaar_object_id: string
+  subtype: string
+  centroid: [number, number]
+  subtype_id: string
+  _display: string
+}
+
 export type ResponseAddress = {
-  results: [Address]
+  results: [ApiAddress]
 }
