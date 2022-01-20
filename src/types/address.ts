8ac45629de8890d135a04164bfaf8ba809bf90e6
Frank Filius <f.filius@amsterdam.nl>
2022-01-20 16:35:13 +0100

    Use a tuple when array element types are known

--- src/types/address.ts
+++ src/types/address.ts
@@ -2,7 +2,7 @@ export type Address = {
   adres: string
   bag_huisletter: string
   bag_toevoeging: string
-  centroid: number[]
+  centroid: [number, number]
   huisnummer: number
   postcode: string
   straatnaam: string
