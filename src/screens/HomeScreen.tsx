fe8f070dae444d5844377b1d52f1a07acc02b329
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-01-21 16:05:57 +0100

    Use new settings context for address banner on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,16 +3,16 @@ import {ScrollView} from 'react-native'
 import {ProvideAddressBanner, QuickLinks} from '../components/features/home'
 import {Box} from '../components/ui'
 import {Column} from '../components/ui/layout'
-import {AddressContext} from '../providers'
+import {SettingsContext} from '../providers'
 
 export const HomeScreen = () => {
-  const addressContext = useContext(AddressContext)
+  const settingsContext = useContext(SettingsContext)
 
   return (
     <ScrollView>
       <Box background="white">
         <Column gutter="lg">
-          {!addressContext.address && <ProvideAddressBanner />}
+          {!settingsContext.settings?.address && <ProvideAddressBanner />}
           <QuickLinks />
         </Column>
       </Box>
