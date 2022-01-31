6b61b9295fa9bd9cfe5c330fde6cd90562f662c0
Frank Filius <f.filius@amsterdam.nl>
2022-01-31 13:50:02 +0100

    Transform data

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -2,6 +2,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {getEnvironment} from '../environment'
 import {Articles} from '../types'
+import {generateRequestUrl} from '../utils'
 
 type QueryArg = {
   limit?: number
@@ -15,12 +16,13 @@ export const articlesApi = createApi({
   reducerPath: 'articlesApi',
   baseQuery: fetchBaseQuery({baseUrl: getEnvironment().apiUrl}),
   endpoints: builder => ({
-    getArticlesByProjectIds: builder.query<Articles, QueryArg>({
-      query: () => 'articles',
+    getArticles: builder.query<Articles, QueryArg>({
+      query: params => generateRequestUrl('/articles', {...params}),
+      transformResponse: (response: {result: Articles}) => response.result,
     }),
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const {useGetArticlesByProjectIdsQuery} = articlesApi
+export const {useGetArticlesQuery} = articlesApi
