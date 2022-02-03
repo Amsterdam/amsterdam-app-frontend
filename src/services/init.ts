71700bfdb6dc6212cf7ff6f3cb9997813fe75493
Frank Filius <f.filius@amsterdam.nl>
2022-02-03 14:49:09 +0100

    Set userauthorizationheader to project-manager requests

--- src/services/init.ts
+++ src/services/init.ts
@@ -1,10 +1,21 @@
 // Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {getEnvironment} from '../environment'
+import {RootState} from '../store'
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
-  baseQuery: fetchBaseQuery({baseUrl: getEnvironment().apiUrl}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: getEnvironment().apiUrl,
+    prepareHeaders: (headers, {getState}) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      const token = (getState() as RootState).auth.managerToken
+      if (token) {
+        headers.set('userauthorization', token)
+      }
+      return headers
+    },
+  }),
   endpoints: () => ({}),
   reducerPath: 'api',
 })
