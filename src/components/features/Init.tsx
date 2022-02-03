de322e93529b20fad63bb91f0a4b93e8cb0835c8
Frank Filius <f.filius@amsterdam.nl>
2022-02-03 14:49:52 +0100

    Set projectmanager-token on init

--- src/components/features/Init.tsx
+++ src/components/features/Init.tsx
@@ -1,12 +1,30 @@
 import {useContext, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import {SettingsContext} from '../../providers'
+import {setCredentials} from '../../store/authSlice'
+import {encryptWithAES} from '../../utils'
 
 export const Init = () => {
-  const {removeSetting} = useContext(SettingsContext)
+  const dispatch = useDispatch()
+  const {removeSetting, settings} = useContext(SettingsContext)
 
   useEffect(() => {
     removeSetting('temp')
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (settings?.['project-manager']) {
+      const {id} = settings?.['project-manager']
+      dispatch(
+        setCredentials({
+          managerToken: encryptWithAES({
+            password: '6886b31dfe27e9306c3d2b553345d9e5',
+            plaintext: id,
+          }),
+        }),
+      )
+    }
+  }, [settings, dispatch])
 
   return null
 }
