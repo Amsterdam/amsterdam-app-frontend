895341bcce0ffb36d85bd9354517974a85af745e
Frank Filius <f.filius@amsterdam.nl>
2022-02-03 12:55:12 +0100

    Create util that gets the auth-token

---
+++ src/utils/getAuthToken.ts
@@ -0,0 +1,8 @@
+import {encryptWithAES} from '.'
+
+export const getAuthToken = (plaintext: string) => {
+  return encryptWithAES({
+    password: process.env.AUTH_PASSWORD!,
+    plaintext,
+  })
+}
