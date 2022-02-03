850251d6d020e631da5fec8c185eb059f0bbf5ac
Frank Filius <f.filius@amsterdam.nl>
2022-02-03 13:27:33 +0100

    Use transform-function

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -1,37 +1,20 @@
-import {Articles} from '../types'
+import {Articles, QueryArgs} from '../types'
 import {generateRequestUrl} from '../utils'
+import {formatQueryParams} from '../utils/formatQueryParams'
 import {emptySplitApi} from './init'
-
-type QueryArg = {
-  limit?: number
-  projectIds?: string[]
-  sortBy?: string
-  sortOrder?: 'asc' | 'desc'
-}
-
-const mapQueryParamsToBackendFormat = ({
-  limit,
-  projectIds,
-  sortBy,
-  sortOrder,
-}: QueryArg) => ({
-  ...(limit && {limit}),
-  ...(projectIds && {'project-ids': projectIds.join(',')}),
-  ...(sortBy && {'sort-by': sortBy}),
-  ...(sortOrder && {'sort-order': sortOrder}),
-})
 
 // Define a service using a base URL and expected endpoints
 export const articlesApi = emptySplitApi.injectEndpoints({
   endpoints: builder => ({
-    getArticles: builder.query<Articles, QueryArg>({
+    getArticles: builder.query<Articles, Partial<QueryArgs>>({
       query: params => {
-        const q = mapQueryParamsToBackendFormat(params)
+        const q = formatQueryParams(params)
         return generateRequestUrl('/articles', q)
       },
       transformResponse: (response: {result: Articles}) => response.result,
     }),
   }),
+  overrideExisting: true,
 })
 
 // Export hooks for usage in functional components, which are
