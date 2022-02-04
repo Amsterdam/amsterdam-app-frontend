3e22e0dcb0fc719869a3bb506c39a9476c817a52
Frank Filius <f.filius@amsterdam.nl>
2022-02-04 14:35:05 +0100

    Format query-params and export getNotifications

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -5,6 +5,7 @@ import {
   QueryArgs,
 } from '../types'
 import {generateRequestUrl} from '../utils'
+import {formatQueryParams} from '../utils/formatQueryParams'
 import {emptySplitApi} from './init'
 
 // Define a service using a base URL and expected endpoints
@@ -13,7 +14,8 @@ export const notificationsApi = emptySplitApi.injectEndpoints({
     getNotifications: builder.query<Notifications, Partial<QueryArgs>>({
       providesTags: ['Notifications'],
       query: params => {
-        return generateRequestUrl('/notifications', params)
+        const q = formatQueryParams(params)
+        return generateRequestUrl('/notifications', q)
       },
       transformResponse: (response: {result: Notifications}) => response.result,
     }),
@@ -32,4 +34,5 @@ export const notificationsApi = emptySplitApi.injectEndpoints({
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const {useAddNotificationMutation} = notificationsApi
+export const {useAddNotificationMutation, useGetNotificationsQuery} =
+  notificationsApi
