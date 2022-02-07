41977aa8d5f8cc65fde9b9a2aabc09c0acde1b22
Frank Filius <f.filius@amsterdam.nl>
2022-02-07 10:53:57 +0100

    Delete comments

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,7 +1,6 @@
 import {NewWarning, WarningResponse} from '../types'
 import {emptySplitApi} from './init'
 
-// Define a service using a base URL and expected endpoints
 export const projectsApi = emptySplitApi.injectEndpoints({
   endpoints: builder => ({
     addWarning: builder.mutation<WarningResponse, NewWarning>({
@@ -19,6 +18,4 @@ export const projectsApi = emptySplitApi.injectEndpoints({
   }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const {useAddWarningMutation} = projectsApi
