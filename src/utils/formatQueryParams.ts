f1f69683eec05ec37581280fb0b45ae75223b7eb
Frank Filius <f.filius@amsterdam.nl>
2022-02-10 13:12:38 +0100

    Keep 1 list for query-args and change name
    To keep it consisten

--- src/utils/formatQueryParams.ts
+++ src/utils/formatQueryParams.ts
@@ -1,20 +1,20 @@
-import {ListQueryArgs, ProjectIdsQueryArgs} from '../types'
+import {ListQueryArgs, ProjectIdsQueryArgs, ProjectsQueryArgs} from '../types'
 
 /**
  * Maps query parameter names from front-end to backend syntax
  */
-export const formatListQueryParams = ({
-  limit,
+export const formatQueryArgs = ({
+  districtId,
+  projectIds,
+  projectType,
   sortBy,
   sortOrder,
-}: Partial<ListQueryArgs>) => ({
-  ...(limit && {limit}),
+  ...rest
+}: Partial<ListQueryArgs & ProjectIdsQueryArgs & ProjectsQueryArgs>) => ({
+  ...(districtId && {'district-id': districtId}),
+  ...(projectType && {'project-type': projectType}),
+  ...(projectIds && {'project-ids': projectIds.join(',')}),
   ...(sortBy && {'sort-by': sortBy}),
   ...(sortOrder && {'sort-order': sortOrder}),
-})
-
-export const formatProjectQueryParams = ({
-  projectIds,
-}: Partial<ProjectIdsQueryArgs>) => ({
-  ...(projectIds && {'project-ids': projectIds.join(',')}),
+  ...rest,
 })
