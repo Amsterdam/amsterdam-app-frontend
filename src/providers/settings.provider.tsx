6a7ac57e2b7df9bfb8fe4ede617641de34014286
Frank Filius <f.filius@amsterdam.nl>
2022-02-10 14:00:28 +0100

    Remove worthless prop

--- src/providers/settings.provider.tsx
+++ src/providers/settings.provider.tsx
@@ -10,14 +10,12 @@ import {Settings} from '../types'
 
 const initialState = {
   changeSettings: () => {},
-  isLoading: true,
   removeSetting: () => {},
   settings: undefined,
 }
 
 type Context = {
   changeSettings: (key: keyof Settings, value: Record<string, any>) => void
-  isLoading: boolean
   removeSetting: (key: keyof Settings) => void
   settings: Settings | undefined
 }
@@ -30,13 +28,11 @@ type Props = {
 
 export const SettingsProvider = ({children}: Props) => {
   const [settings, setSettings] = useState<Settings | undefined>()
-  const [isLoading, setLoading] = useState(true)
   const asyncStorage = useAsyncStorage()
   const deviceRegistration = useDeviceRegistration(settings)
 
   const retrieveSettings = useCallback(async () => {
     if (!settings) {
-      setLoading(true)
       const data = await asyncStorage.getAllValues()
       const settingsFromStore = Object.fromEntries(data ?? [])
 
@@ -46,7 +42,6 @@ export const SettingsProvider = ({children}: Props) => {
       }
 
       setSettings(parsedSettings)
-      setLoading(false)
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -54,24 +49,20 @@ export const SettingsProvider = ({children}: Props) => {
     key: keyof Settings,
     value: Record<string, any>,
   ) => {
-    setLoading(true)
     await asyncStorage.storeData(key, value)
     setSettings({
       ...settings,
       [key]: value,
     })
-    setLoading(false)
   }
 
   const removeSetting = async (key: keyof Settings) => {
-    setLoading(true)
     await asyncStorage.removeValue(key)
 
     let copyOfSettings = {...settings}
     delete copyOfSettings[key]
 
     setSettings(copyOfSettings)
-    setLoading(false)
   }
 
   useEffect(() => {
@@ -88,7 +79,6 @@ export const SettingsProvider = ({children}: Props) => {
     <SettingsContext.Provider
       value={{
         changeSettings,
-        isLoading,
         removeSetting,
         settings,
       }}>
