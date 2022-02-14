2419aeccb3a13ea05731dff8196f5f7a857c401d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-02-14 20:44:32 +0100

    Allow `ScrollView` props on our wrapper component

--- src/components/ui/layout/ScrollView.tsx
+++ src/components/ui/layout/ScrollView.tsx
@@ -1,17 +1,18 @@
 import React, {ReactNode} from 'react'
-import {ScrollView as RNScrollView} from 'react-native'
+import {ScrollView as RNScrollView, ScrollViewProps} from 'react-native'
 import {layoutStyles} from '../../../styles'
 
 type Props = {
   children: ReactNode
   grow?: boolean
-}
+} & ScrollViewProps
 
-export const ScrollView = ({children, grow}: Props) => {
+export const ScrollView = ({children, grow, ...otherProps}: Props) => {
   return (
     <RNScrollView
       contentContainerStyle={grow && layoutStyles.grow}
-      keyboardShouldPersistTaps={grow && 'handled'}>
+      keyboardShouldPersistTaps={grow && 'handled'}
+      {...otherProps}>
       {children}
     </RNScrollView>
   )
