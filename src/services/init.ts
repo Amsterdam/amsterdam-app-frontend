f727b001581af68f6d576617efe23eb77f591dd6
Frank Filius <f.filius@amsterdam.nl>
2022-02-15 14:25:32 +0100

    Add endpoint to auth endpoints

--- src/services/init.ts
+++ src/services/init.ts
@@ -2,7 +2,11 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {getEnvironment} from '../environment'
 import {RootState, selectAuthManagerToken} from '../store'
 
-const managerAuthorizedEndpoints = ['addNotification', 'addWarning']
+const managerAuthorizedEndpoints = [
+  'addNotification',
+  'addWarning',
+  'getProjectManager',
+]
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
