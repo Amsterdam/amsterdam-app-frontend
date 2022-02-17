fb114460e951cc303c6af83c62a167007cb0594f
Frank Filius <f.filius@amsterdam.nl>
2022-02-17 17:34:55 +0100

    Fill screen with UI

--- src/screens/create-notification/SelectHeaderImageScreen.tsx
+++ src/screens/create-notification/SelectHeaderImageScreen.tsx
@@ -1,8 +1,17 @@
 import {StackNavigationProp} from '@react-navigation/stack'
 import React, {useContext, useEffect} from 'react'
 import {useForm} from 'react-hook-form'
-import {View} from 'react-native'
-import {Box, SubmitButton, TextButton} from '../../components/ui'
+import {StyleSheet, View} from 'react-native'
+import {TouchableOpacity} from 'react-native-gesture-handler'
+import HeroImage from '../../assets/images/warning-hero.svg'
+import {
+  AddButton,
+  Box,
+  SubmitButton,
+  Text,
+  TextButton,
+  Title,
+} from '../../components/ui'
 import {Column, Gutter, Row, ScrollView} from '../../components/ui/layout'
 import {NotificationContext, NotificationStackParams} from '.'
 
@@ -26,9 +35,28 @@ export const SelectHeaderImageScreen = ({navigation}: Props) => {
 
   return (
     <ScrollView grow>
-      <Column align="between">
-        <View />
-        <Box>
+      <Box grow>
+        <Column align="between">
+          <Column gutter="lg">
+            <View>
+              <Title margin text="Kies een afbeelding" />
+              <Column gutter="sm">
+                <Title level={4} text="Upload een foto" />
+                <Text secondary>
+                  Mensen onherkenbaar in beeld i.v.m. portretrecht.
+                </Text>
+                <AddButton />
+              </Column>
+            </View>
+            <Column gutter="sm">
+              <Title level={4} text="Of kies de standaardaardafbeelding" />
+              <TouchableOpacity style={styles.button}>
+                <View>
+                  <HeroImage />
+                </View>
+              </TouchableOpacity>
+            </Column>
+          </Column>
           <Row align="between" valign="center">
             <TextButton
               direction="backward"
@@ -36,11 +64,21 @@ export const SelectHeaderImageScreen = ({navigation}: Props) => {
               onPress={navigation.goBack}
               text="Vorige"
             />
-            <SubmitButton onPress={handleSubmit(onSubmit)} text="Controleer" />
+            <SubmitButton
+              onPress={handleSubmit(onSubmit)}
+              text="Schrijf bericht"
+            />
           </Row>
-          <Gutter height="xl" />
-        </Box>
-      </Column>
+        </Column>
+      </Box>
+      <Gutter height="xl" />
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 165,
+    height: 74,
+  },
+})
