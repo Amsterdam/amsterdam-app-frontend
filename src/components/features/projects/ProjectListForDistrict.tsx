c1462162c2dcfe9060bf3cd347cf595f9f64fdb1
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-02-23 12:33:02 +0100

    Extract function and handle zero projects

--- src/components/features/projects/ProjectListForDistrict.tsx
+++ src/components/features/projects/ProjectListForDistrict.tsx
@@ -1,7 +1,7 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React, {useContext, useState} from 'react'
-import {StyleSheet, View} from 'react-native'
+import {LayoutChangeEvent, StyleSheet, View} from 'react-native'
 import {FlatGrid} from 'react-native-super-grid'
 import {StackParams} from '../../../app/navigation'
 import {routes} from '../../../app/navigation/routes'
@@ -10,7 +10,7 @@ import {useGetProjectsQuery} from '../../../services'
 import {layoutStyles} from '../../../styles'
 import {size} from '../../../tokens'
 import {mapImageSources} from '../../../utils'
-import {PleaseWait} from '../../ui'
+import {Box, PleaseWait, Text} from '../../ui'
 import {ProjectCard} from '../project'
 
 type Props = {
@@ -21,27 +21,36 @@ export const ProjectListForDistrict = ({districtId}: Props) => {
   const device = useContext(DeviceContext)
   const navigation =
     useNavigation<StackNavigationProp<StackParams, 'ProjectOverview'>>()
+
   const [gridWidth, setGridWidth] = useState(0)
+  const itemDimension = 16 * size.spacing.md * Math.max(device.fontScale, 1)
+
+  const handleLayoutChange = (event: LayoutChangeEvent) => {
+    setGridWidth(event.nativeEvent.layout.width)
+  }
 
   const {data: projects, isLoading: isProjectsLoading} = useGetProjectsQuery({
     districtId,
     fields: ['identifier', 'images', 'subtitle', 'title'],
   })
 
-  const itemDimension = 16 * size.spacing.md * Math.max(device.fontScale, 1)
-
   if (isProjectsLoading) {
     return <PleaseWait />
   }
 
+  if (!projects?.length) {
+    return (
+      <Box>
+        <Text>Geen projecten voor dit stadsdeel.</Text>
+      </Box>
+    )
+  }
+
   return (
-    <View
-      onLayout={event => {
-        setGridWidth(event.nativeEvent.layout.width)
-      }}>
+    <View onLayout={handleLayoutChange}>
       {!!gridWidth && (
         <FlatGrid
-          data={projects ?? []}
+          data={projects}
           itemContainerStyle={styles.alignment}
           itemDimension={itemDimension}
           keyExtractor={item => item.identifier}
