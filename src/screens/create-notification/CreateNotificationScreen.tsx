a96733349786afa7615f6e44ca0a1136608462bc
Frank Filius <f.filius@amsterdam.nl>
2022-02-23 16:59:13 +0100

    Remove context

--- src/screens/create-notification/CreateNotificationScreen.tsx
+++ src/screens/create-notification/CreateNotificationScreen.tsx
@@ -3,25 +3,17 @@ import {
   createStackNavigator,
   StackNavigationOptions,
 } from '@react-navigation/stack'
-import React, {createContext, useContext, useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {StackParams} from '../../app/navigation'
-import {
-  Box,
-  KeyboardAvoidingView,
-  PleaseWait,
-  Stepper,
-} from '../../components/ui'
+import {Box, KeyboardAvoidingView, Stepper} from '../../components/ui'
 import {Gutter} from '../../components/ui/layout'
-import {SettingsContext} from '../../providers'
-import {useGetArticlesQuery} from '../../services/articles'
 import {color} from '../../tokens'
 import {
-  ArticleSummary,
-  NotificationQueryArg,
-  NewProjectWarning,
-  ProjectManagerSettings,
-  ResponseStatus,
-} from '../../types'
+  selectStep,
+  selectTotalSteps,
+  setProject,
+} from './notificationDraftSlice'
 import {
   NotificationFormScreen,
   NotificationResponseScreen,
@@ -30,28 +22,6 @@ import {
   SelectNewsArticleScreen,
   VerifyNotificationScreen,
 } from './'
-
-type Context = {
-  articles?: ArticleSummary[]
-  changeCurrentStep: (value: number) => void
-  changeNewsDetails: (value: NewsDetails) => void
-  changeNotification: (newNotification: NotificationQueryArg) => void
-  changeResponseStatus: (value: ResponseStatus) => void
-  changeProjectWarning: (newProjectWarning: NewProjectWarning) => void
-  newsDetails?: NewsDetails
-  notification?: NotificationQueryArg
-  projectDetails: ProjectDetails
-  projectManagerSettings?: ProjectManagerSettings
-  responseStatus?: ResponseStatus
-  projectWarning?: NewProjectWarning
-}
-
-type NewsDetails = {
-  id: string
-  title: string
-}
-
-export const NotificationContext = createContext<Context>({} as Context)
 
 type NotificationScreenRouteProp = RouteProp<StackParams, 'Notification'>
 
@@ -64,49 +34,24 @@ export type NotificationStackParams = {
   ProjectWarningForm: undefined
 }
 
-export type ProjectDetails = {
-  id: string
-  title: string
-}
-
 type Props = {
   route: NotificationScreenRouteProp
 }
 
 export const CreateNotificationScreen = ({route}: Props) => {
-  const [currentStep, setCurrentStep] = useState(1)
-  const [newsDetails, setNewsDetails] = useState<NewsDetails>()
-  const [notification, setNotification] = useState<NotificationQueryArg>()
-  const [projectDetails, setProjectDetails] = useState({} as ProjectDetails)
-  const [responseStatus, setResponseStatus] = useState<ResponseStatus>()
-  const [projectWarning, setProjectWarning] = useState<NewProjectWarning>()
-  const {settings} = useContext(SettingsContext)
-  const projectManagerSettings = settings?.['project-manager']
-
-  const changeCurrentStep = (value: number) => setCurrentStep(value)
-  const changeNewsDetails = (value: NewsDetails) => setNewsDetails(value)
-  const changeNotification = (value: NotificationQueryArg) =>
-    setNotification(value)
-  const changeResponseStatus = (value: ResponseStatus) =>
-    setResponseStatus(value)
-  const changeProjectWarning = (value: NewProjectWarning) =>
-    setProjectWarning(value)
-
-  const {data: articles, isLoading} = useGetArticlesQuery({
-    projectIds: [route.params.projectDetails.id],
-  })
+  const dispatch = useDispatch()
+  const step = useSelector(selectStep)
+  const totalSteps = useSelector(selectTotalSteps)
 
   useEffect(() => {
     const {id, title} = route.params.projectDetails
-    setProjectDetails({
-      id,
-      title,
-    })
-  }, [route])
-
-  if (isLoading) {
-    return <PleaseWait />
-  }
+    dispatch(
+      setProject({
+        id,
+        title,
+      }),
+    )
+  }, [dispatch, route])
 
   const Stack = createStackNavigator()
 
@@ -118,60 +63,44 @@ export const CreateNotificationScreen = ({route}: Props) => {
   }
 
   return (
-    <NotificationContext.Provider
-      value={{
-        articles,
-        changeCurrentStep,
-        changeNewsDetails,
-        changeNotification,
-        changeProjectWarning,
-        changeResponseStatus,
-        newsDetails,
-        notification,
-        projectDetails,
-        projectManagerSettings,
-        projectWarning,
-        responseStatus,
-      }}>
-      <KeyboardAvoidingView>
-        {currentStep !== 0 && (
-          <Box background="grey">
-            <Stepper current={currentStep} length={4} />
-          </Box>
-        )}
-        <Stack.Navigator screenOptions={screenOptions}>
-          <Stack.Screen
-            component={NotificationFormScreen}
-            name="NotificationForm"
-          />
-          <Stack.Screen
-            component={SelectMainImageScreen}
-            name="SelectMainImage"
-          />
-          <Stack.Screen
-            component={SelectNewsArticleScreen}
-            name="SelectNewsArticle"
-          />
-          <Stack.Screen
-            name="ProjectWarningForm"
-            component={ProjectWarningFormScreen}
-          />
-          <Stack.Screen
-            component={VerifyNotificationScreen}
-            name="VerifyNotification"
-          />
-          <Stack.Screen
-            component={NotificationResponseScreen}
-            name="NotificationResponse"
-            options={{
-              cardStyle: {
-                backgroundColor: color.background.app,
-              },
-            }}
-          />
-        </Stack.Navigator>
-      </KeyboardAvoidingView>
+    <KeyboardAvoidingView>
+      {step && (
+        <Box background="grey">
+          <Stepper current={step} length={totalSteps} />
+        </Box>
+      )}
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen
+          component={NotificationFormScreen}
+          name="NotificationForm"
+        />
+        <Stack.Screen
+          component={SelectMainImageScreen}
+          name="SelectMainImage"
+        />
+        <Stack.Screen
+          component={SelectNewsArticleScreen}
+          name="SelectNewsArticle"
+        />
+        <Stack.Screen
+          name="ProjectWarningForm"
+          component={ProjectWarningFormScreen}
+        />
+        <Stack.Screen
+          component={VerifyNotificationScreen}
+          name="VerifyNotification"
+        />
+        <Stack.Screen
+          component={NotificationResponseScreen}
+          name="NotificationResponse"
+          options={{
+            cardStyle: {
+              backgroundColor: color.background.app,
+            },
+          }}
+        />
+      </Stack.Navigator>
       <Gutter height="xl" />
-    </NotificationContext.Provider>
+    </KeyboardAvoidingView>
   )
 }
