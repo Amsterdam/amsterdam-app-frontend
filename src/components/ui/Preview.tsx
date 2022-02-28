d858f50236ace2be5a12e5268f31e25fa8806a4d
Frank Filius <f.filius@amsterdam.nl>
2022-02-28 15:05:28 +0100

    Update to optionally show an image

--- src/components/ui/Preview.tsx
+++ src/components/ui/Preview.tsx
@@ -1,19 +1,24 @@
 import React, {ReactNode} from 'react'
+import {View} from 'react-native'
 import {Column} from './layout'
 import {Box, Text} from './'
 
 type Props = {
   children: ReactNode
+  image?: ReactNode
   label: string
 }
 
-export const Preview = ({children, label}: Props) => {
+export const Preview = ({children, image, label}: Props) => {
   return (
     <Column gutter="sm">
       <Text>{label}</Text>
-      <Box background="grey">
-        <Column gutter="sm">{children}</Column>
-      </Box>
+      <View>
+        {image}
+        <Box background="grey">
+          <Column gutter="sm">{children}</Column>
+        </Box>
+      </View>
     </Column>
   )
 }
