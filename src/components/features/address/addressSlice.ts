df19c7c48e6a541a08d952f14d12724b5243ab4f
Frank Filius <f.filius@amsterdam.nl>
2022-03-31 10:52:55 +0200

    Make distinction between primary in temp

--- src/components/features/address/addressSlice.ts
+++ src/components/features/address/addressSlice.ts
@@ -2,14 +2,24 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import {RootState} from '../../../store'
 import {Address} from '../../../types'
 
+type AddressState = {
+  primary: Address
+  temp: Address
+}
+
 export const addressSlice = createSlice({
   name: 'address',
-  initialState: {} as Address,
+  initialState: {primary: {}, temp: {}} as AddressState,
   reducers: {
-    addAddress: (state, {payload}: PayloadAction<Address>) => (state = payload),
+    addAddress: (state, {payload}: PayloadAction<Address>) => {
+      state.primary = payload
+    },
+    addTempAddress: (state, {payload}: PayloadAction<Address>) => {
+      state.temp = payload
+    },
   },
 })
 
-export const {addAddress} = addressSlice.actions
+export const {addAddress, addTempAddress} = addressSlice.actions
 
 export const selectAddress = (state: RootState) => state.address
