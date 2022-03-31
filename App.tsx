300055e63cda4ee4a1f8806c6f8d6bfa18ce5baf
Frank Filius <f.filius@amsterdam.nl>
2022-03-31 15:07:42 +0200

    Enable access to persisted store data

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import {NavigationContainer} from '@react-navigation/native'
 import React from 'react'
 import {StatusBar} from 'react-native'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
+import {persistStore} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
 import {linking, TabNavigator} from './src/app/navigation'
 import {Init} from './src/components/features/Init'
 import {RootProvider} from './src/providers'
+import {store} from './src/store'
+
+let persistor = persistStore(store)
 
 export const App = () => {
   return (
@@ -13,8 +18,10 @@ export const App = () => {
       {/* Find out whats going on here */}
       <NavigationContainer linking={linking}>
         <RootProvider>
-          <Init />
-          <TabNavigator />
+          <PersistGate loading={null} persistor={persistor}>
+            <Init />
+            <TabNavigator />
+          </PersistGate>
         </RootProvider>
       </NavigationContainer>
     </SafeAreaProvider>
