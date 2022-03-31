fe6686dadc2ae8caa68f6e34df4d5983744aaf75
Frank Filius <f.filius@amsterdam.nl>
2022-03-31 11:16:39 +0200

    Composed:
    - use isLoading from async storage
    - listen to primary address from state to rerender

--- src/components/features/home/ProvideAddressBanner.tsx
+++ src/components/features/home/ProvideAddressBanner.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
 import {StackParams, TabParams} from '../../../app/navigation'
 import {routes} from '../../../app/navigation/routes'
 import {useAsyncStorage} from '../../../hooks'
@@ -8,22 +9,26 @@ import {Address} from '../../../types'
 import {PleaseWait} from '../../ui'
 import {Gutter} from '../../ui/layout'
 import {BannerCard} from '../BannerCard'
+import {selectAddress} from '../address/addressSlice'
 
 export const ProvideAddressBanner = () => {
   const asyncStorage = useAsyncStorage()
   const navigation =
     useNavigation<StackNavigationProp<StackParams & TabParams, 'Home'>>()
   const [address, setAddress] = useState<Address | undefined>()
-  const [isLoading, setIsLoading] = useState(true)
+  const {primary: primaryAddress} = useSelector(selectAddress)
 
   useEffect(() => {
     asyncStorage.getValue<Address>('address').then(storedAddress => {
       setAddress(storedAddress)
-      setIsLoading(false)
     })
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  if (isLoading) {
+  useEffect(() => {
+    setAddress(primaryAddress)
+  }, [primaryAddress])
+
+  if (asyncStorage.isLoading) {
     return <PleaseWait />
   }
 
