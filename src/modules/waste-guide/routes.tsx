7b51da1f4dc85de70ad232d7b636d5019e52f9dc
Frank Filius <f.filius@amsterdam.nl>
2022-04-22 16:56:08 +0200

    Add title and therefor transform to tsx

--- src/modules/waste-guide/routes.tsx
+++ src/modules/waste-guide/routes.tsx
@@ -1,11 +1,16 @@
+import React from 'react'
 import {StackNavigationRoutes} from '../../app/navigation'
 import {WasteGuideModuleStackParams} from '../../components/features/modules'
+import {NonScalingHeaderTitle} from '../../components/ui'
 
-export const routes: StackNavigationRoutes<
+export const wasteGuideroutes: StackNavigationRoutes<
   WasteGuideModuleStackParams,
   'wasteGuideModule'
 > = {
   wasteGuideModule: {
     name: 'WasteGuideModule',
+    options: {
+      headerTitle: () => <NonScalingHeaderTitle text="Module afvalwijzer" />,
+    },
   },
 }
