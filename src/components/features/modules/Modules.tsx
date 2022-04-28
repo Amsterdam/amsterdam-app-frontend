70a5c8f33b1f74f668cb2fe3371aad3e42d881cc
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-04-28 11:13:51 +0200

    Use new module button component for overview

--- src/components/features/modules/Modules.tsx
+++ src/components/features/modules/Modules.tsx
@@ -1,34 +1,35 @@
-import {useNavigation} from '@react-navigation/native'
-import {StackNavigationProp} from '@react-navigation/stack'
-import React from 'react'
-import {Pressable} from 'react-native'
-import {RootStackParamList} from '../../../app/navigation/RootStackNavigator'
-import {modules} from '../../../modules'
-import {Box, Text} from '../../ui'
+import Alert from '@amsterdam/asc-assets/static/icons/Alert.svg'
+import ChatBubble from '@amsterdam/asc-assets/static/icons/ChatBubble.svg'
+import Chatting from '@amsterdam/asc-assets/static/icons/Chatting.svg'
+import Location from '@amsterdam/asc-assets/static/icons/Location.svg'
+import React, {ReactNode} from 'react'
+import {Container, Project} from '../../../assets/icons'
+import {color} from '../../../tokens'
 import {Column} from '../../ui/layout'
+import mock from './mock.json'
+import {Module, ModuleButton} from './'
 
-const modulesExceptHome = modules.filter(
-  module =>
-    module.name !== 'HomeModule' &&
-    module.name !== 'UserModule' &&
-    module.name !== 'SettingsModule',
-)
+const modules: Module[] = mock.modules.filter(m => m.status === 1)
 
-export const Modules = () => {
-  const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, 'HomeModule'>>()
-
-  return (
-    <Column gutter="md">
-      {modulesExceptHome.map(module => (
-        <Pressable
-          key={module.name}
-          onPress={() => navigation.navigate(module.name)}>
-          <Box insetVertical="sm" key={module.name}>
-            <Text>{module.title}</Text>
-          </Box>
-        </Pressable>
-      ))}
-    </Column>
-  )
+const iconProps = {
+  width: 24,
+  height: 24,
+  fill: color.font.regular,
 }
+
+const icons: Record<string, ReactNode> = {
+  'waste-guide': <Location {...iconProps} />,
+  'open-waste-container': <Container {...iconProps} />,
+  'construction-work': <Project {...iconProps} />,
+  'report-issue': <Alert {...iconProps} />,
+  'city-offices': <Chatting {...iconProps} />,
+  questions: <ChatBubble {...iconProps} />,
+}
+
+export const Modules = () => (
+  <Column gutter="md">
+    {modules.map(({icon, slug, title}) => (
+      <ModuleButton icon={icons[icon]} key={slug} label={title} slug={slug} />
+    ))}
+  </Column>
+)
