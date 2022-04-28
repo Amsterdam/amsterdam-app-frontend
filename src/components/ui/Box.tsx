8364ca0560501376dbc4a1ea11b2c9683f44f9ed
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-04-28 17:06:02 +0200

    Use size tokens from theme in box component

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,15 +1,17 @@
 import React, {ReactNode} from 'react'
 import {StyleSheet, View, ViewProps} from 'react-native'
 import {layoutStyles} from '../../styles'
-import {color, size, Spacing} from '../../tokens'
+import {Theme, useThemable} from '../../themes'
+import {SizeTokens} from '../../themes/tokens'
+import {color} from '../../tokens'
 
 type Props = {
   background?: 'emphasis' | 'grey' | 'invalid' | 'white'
   children: ReactNode
   grow?: boolean
-  inset?: keyof Spacing
-  insetHorizontal?: keyof Spacing
-  insetVertical?: keyof Spacing
+  inset?: keyof SizeTokens
+  insetHorizontal?: keyof SizeTokens
+  insetVertical?: keyof SizeTokens
 } & Omit<ViewProps, 'style'>
 
 export const Box = ({
@@ -21,19 +23,33 @@ export const Box = ({
   insetVertical,
   ...otherProps
 }: Props) => {
-  const styles = StyleSheet.create({
+  const styles = useThemable(
+    createStyles({inset, insetHorizontal, insetVertical}),
+  )
+
+  const oldStyles = StyleSheet.create({
     box: {
       backgroundColor: background && color.background[background],
-      padding:
-        inset && !insetHorizontal && !insetVertical ? size.spacing[inset] : 0,
-      paddingHorizontal: insetHorizontal && size.spacing[insetHorizontal],
-      paddingVertical: insetVertical && size.spacing[insetVertical],
     },
   })
 
   return (
-    <View style={[styles.box, grow && layoutStyles.grow]} {...otherProps}>
+    <View
+      style={[oldStyles.box, styles.box, grow && layoutStyles.grow]}
+      {...otherProps}>
       {children}
     </View>
   )
 }
+
+const createStyles =
+  ({inset, insetHorizontal, insetVertical}: Partial<Props>) =>
+  (theme: Theme) =>
+    StyleSheet.create({
+      box: {
+        padding:
+          inset && !insetHorizontal && !insetVertical ? theme.size[inset] : 0,
+        paddingHorizontal: insetHorizontal && theme.size[insetHorizontal],
+        paddingVertical: insetVertical && theme.size[insetVertical],
+      },
+    })
