a17ac5929f50c05d2cb861ab03a42f224f8957ba
Frank Filius <f.filius@amsterdam.nl>
2022-05-11 18:16:12 +0200

    Combine client and server modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,11 +1,25 @@
-import {module as homeModule} from './home'
-import {module as settingsModule} from './settings'
-import {module as userModule} from './user'
+import mock from '../components/features/modules/mock.json'
+import {nonNullable} from '../utils'
+import {ClientModule, ServerModule} from './types'
 import {module as wasteGuideModule} from './waste-guide'
 
-export const modules = [
-  homeModule,
-  settingsModule,
-  userModule,
-  wasteGuideModule,
-]
+const frontEndModules = [wasteGuideModule]
+
+const combineClientAndServerModules = (
+  feModules: ClientModule[],
+  beModules: ServerModule[],
+): (ClientModule & ServerModule)[] => {
+  return feModules
+    .map(feModule => {
+      const beModule = beModules.find(m => feModule.slug === m.slug)
+      if (beModule) {
+        return {...feModule, ...beModule}
+      }
+    })
+    .filter(nonNullable)
+}
+
+export const modules = combineClientAndServerModules(
+  frontEndModules,
+  mock.modules,
+)
