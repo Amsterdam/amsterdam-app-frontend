d225ae17e7215df80b4b08f1031841afeaff7df8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-05-12 12:28:26 +0200

    Add ‘add module’ button to modules overview screen

--- src/modules/home/Screen.tsx
+++ src/modules/home/Screen.tsx
@@ -1,4 +1,25 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
+import {RootStackParamList} from '../../app/navigation'
 import {Modules} from '../../components/features/modules'
+import {AddButton, Box} from '../../components/ui'
+import {Column, Gutter, ScrollView} from '../../components/ui/layout'
+import {module as settingsModule} from '../../modules/settings'
 
-export const HomeScreen = () => <Modules />
+export const HomeScreen = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<RootStackParamList, 'HomeModule'>>()
+
+  return (
+    <Column align="between">
+      <ScrollView>
+        <Modules />
+      </ScrollView>
+      <Box>
+        <AddButton onPress={() => navigation.navigate(settingsModule.name)} />
+        <Gutter height="lg" />
+      </Box>
+    </Column>
+  )
+}
