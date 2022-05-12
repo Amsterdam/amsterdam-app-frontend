acb945d2d428f2274862a75bb2cf9039011f7ba9
Frank Filius <f.filius@amsterdam.nl>
2022-05-12 11:42:27 +0200

    Export all modules and move util

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,25 +1,11 @@
-import mock from '../components/features/modules/mock.json'
-import {nonNullable} from '../utils'
-import {ClientModule, ServerModule} from './types'
+import {module as homeModule} from './home'
+import {module as settingsModule} from './settings'
+import {module as userModule} from './user'
 import {module as wasteGuideModule} from './waste-guide'
 
-const frontEndModules = [wasteGuideModule]
-
-const combineClientAndServerModules = (
-  feModules: ClientModule[],
-  beModules: ServerModule[],
-): (ClientModule & ServerModule)[] => {
-  return feModules
-    .map(feModule => {
-      const beModule = beModules.find(m => feModule.slug === m.slug)
-      if (beModule) {
-        return {...feModule, ...beModule}
-      }
-    })
-    .filter(nonNullable)
-}
-
-export const modules = combineClientAndServerModules(
-  frontEndModules,
-  mock.modules,
-)
+export const modules = [
+  homeModule,
+  userModule,
+  settingsModule,
+  wasteGuideModule,
+]
---
+++ src/utils/combineClientAndServerModules.ts
@@ -0,0 +1,16 @@
+import {ClientModule, ServerModule} from '../modules/types'
+import {nonNullable} from './nonNullable'
+
+export const combineClientAndServerModules = (
+  clientModules: ClientModule[],
+  serverModules: ServerModule[],
+): (ClientModule & ServerModule)[] => {
+  return clientModules
+    .map(clientModule => {
+      const serverModule = serverModules.find(m => clientModule.slug === m.slug)
+      if (serverModule) {
+        return {...clientModule, ...serverModule}
+      }
+    })
+    .filter(nonNullable)
+}
