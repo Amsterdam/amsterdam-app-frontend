9a4145322cba5344ca22abe5d6c5e9855172088f
Frank Filius <f.filius@amsterdam.nl>
2022-05-12 15:13:01 +0200

    Use slice and prepare variant for design

--- src/components/features/modules/Modules.tsx
+++ src/components/features/modules/Modules.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 import {clientModules} from '../../../modules'
 import {ServerModule} from '../../../modules/types'
 import {color} from '../../../tokens'
 import {combineClientAndServerModules} from '../../../utils'
 import {Box} from '../../ui'
 import mock from './mock.json'
+import {selectModules} from './modulesSlice'
 import {icons, ModuleButton} from './'
 
-// TODO Retrieve from store
-const serverModules: ServerModule[] = mock.modules.filter(m => m.status === 1)
-const modules = combineClientAndServerModules(clientModules, serverModules)
+const modules = combineClientAndServerModules(clientModules, mock.modules)
 
 const iconProps = {
   width: 24,
@@ -17,19 +17,26 @@ const iconProps = {
   fill: color.font.regular,
 }
 
-export const Modules = () => (
-  <Box insetVertical="md">
-    {modules.map(({icon, name, slug, title}) => {
-      const Icon = icons[icon]
+export const Modules = () => {
+  const {modules: storedModules} = useSelector(selectModules)
+  const serverModules: ServerModule[] = mock.modules.filter(
+    m => m.status === 1 && storedModules.includes(m.slug),
+  )
+  console.log(serverModules)
+  return (
+    <Box insetVertical="md">
+      {modules.map(({icon, name, slug, title}) => {
+        const Icon = icons[icon]
 
-      return (
-        <ModuleButton
-          icon={<Icon {...iconProps} />}
-          key={slug}
-          label={title}
-          name={name}
-        />
-      )
-    })}
-  </Box>
-)
+        return (
+          <ModuleButton
+            icon={<Icon {...iconProps} />}
+            key={slug}
+            label={title}
+            name={name}
+          />
+        )
+      })}
+    </Box>
+  )
+}
