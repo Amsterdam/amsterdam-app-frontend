4377df24eba20e0dedd8bb708467b0a88901b7f0
Frank Filius <f.filius@amsterdam.nl>
2022-05-14 09:28:47 +0200

    Create override possibility for
    backgroundcolor of header

--- src/app/navigation/screenOptions.tsx
+++ src/app/navigation/screenOptions.tsx
@@ -4,33 +4,45 @@ import {StackNavigationOptions} from '@react-navigation/stack'
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
 import {Theme} from '../../themes'
-import {color, size} from '../../tokens'
+import {color, font, size} from '../../tokens'
 
-export const stackScreenOptions: (theme: Theme) => StackNavigationOptions =
-  theme => ({
-    cardStyle: {
-      backgroundColor: theme.color.screen.background.default,
-    },
-    headerBackAccessibilityLabel: 'Terug',
-    headerBackImage: () => (
-      <View style={styles.headerBackImage}>
-        <ChevronLeft width={20} height={20} fill={color.touchable.primary} />
-      </View>
-    ),
-    headerBackTitleVisible: false,
-    headerStyle: {
-      backgroundColor: theme.color.screen.background.default,
-      shadowColor: 'transparent',
-      borderBottomWidth: 0,
-    },
-    headerLeftContainerStyle: {
-      paddingStart: size.spacing.md,
-    },
-    headerRightContainerStyle: {
-      paddingEnd: size.spacing.md,
-    },
-    headerTitleAlign: 'center',
-  })
+type Overrides = {
+  headerBackgroundColor: string
+}
+
+export const stackScreenOptions: (
+  theme: Theme,
+  overrides: Overrides,
+) => StackNavigationOptions = (theme, overrides) => ({
+  cardStyle: {
+    backgroundColor: theme.color.screen.background.settings,
+  },
+  headerBackAccessibilityLabel: 'Terug',
+  headerBackImage: () => (
+    <View style={styles.headerBackImage}>
+      <ChevronLeft width={20} height={20} fill={color.font.primary} />
+    </View>
+  ),
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: overrides?.headerBackgroundColor || color.background.white,
+    borderBottomWidth: 0,
+    elevation: 0,
+    shadowOpacity: 0,
+  },
+  headerLeftContainerStyle: {
+    paddingStart: size.spacing.md,
+  },
+  headerRightContainerStyle: {
+    paddingEnd: size.spacing.md,
+  },
+  headerTitleAlign: 'center',
+  headerTitleStyle: {
+    color: theme.color.text.default,
+    fontFamily: font.weight.demi,
+    fontSize: theme.text.fontSize.h1,
+  },
+})
 
 export const tabScreenOptions: BottomTabNavigationOptions = {
   headerShown: false,
