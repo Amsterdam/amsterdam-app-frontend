d9778067bf8f6c5762cf34a09d695f9f437c5ab5
Frank Filius <f.filius@amsterdam.nl>
2022-05-25 15:29:54 +0200

    Add slices to states

--- src/modules/projects/index.ts
+++ src/modules/projects/index.ts
@@ -1,10 +1,12 @@
 import {ClientModule} from '../types'
 import {ProjectsStack} from './Stack'
+import {projectManagerSlice} from './components/project-manager'
+import {projectsByTextSlice} from './components/projects'
 
 export const module: ClientModule = {
   linking: {},
   name: 'ProjectsModule',
   slug: 'construction-work',
   stack: ProjectsStack,
-  state: [],
+  state: [projectsByTextSlice, projectManagerSlice],
 }
