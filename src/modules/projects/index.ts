57d373fd8a5067d10798b69382343ec87a4e8b4d
Frank Filius <f.filius@amsterdam.nl>
2022-05-25 15:39:43 +0200

    Reassing linking object

--- src/modules/projects/index.ts
+++ src/modules/projects/index.ts
@@ -2,9 +2,14 @@ import {ClientModule} from '../types'
 import {ProjectsStack} from './Stack'
 import {projectManagerSlice} from './components/project-manager'
 import {projectsByTextSlice} from './components/projects'
+import {ProjectsRouteName} from './routes'
 
 export const module: ClientModule = {
-  linking: {},
+  linking: {
+    [ProjectsRouteName.projectNews]: 'news/:id',
+    [ProjectsRouteName.projectManager]: 'project-manager/:id',
+    [ProjectsRouteName.projectWarning]: 'warning/:id',
+  },
   name: 'ProjectsModule',
   slug: 'construction-work',
   stack: ProjectsStack,
