35b3c8c4c69a9c43c84fb711a297087e21234a95
Frank Filius <f.filius@amsterdam.nl>
2022-05-25 15:23:56 +0200

    Create project module entry file

---
+++ src/modules/projects/index.ts
@@ -0,0 +1,10 @@
+import {ClientModule} from '../types'
+import {ProjectsStack} from './Stack'
+
+export const module: ClientModule = {
+  linking: {},
+  name: 'ProjectsModule',
+  slug: 'construction-work',
+  stack: ProjectsStack,
+  state: [],
+}
