964321dbb8b909d5e0e3dea7d08476ad27a0f388
Frank Filius <f.filius@amsterdam.nl>
2022-05-30 16:12:34 +0200

    Expand service with more endpoints

--- src/modules/home/services.ts
+++ src/modules/home/services.ts
@@ -2,9 +2,30 @@ import {version as appVersion} from '../../../package.json'
 import {baseApi} from '../../services/init'
 import {ServerModule} from '../types'
 
+type RegisterModuleQueryArg = {
+  app_version: string
+} & ServerModule
+
+type ModuleOrderQueryArg = {
+  app_version: string
+  order: string[]
+}
+
 export const modulesApi = baseApi.injectEndpoints({
   endpoints: builder => ({
+    addModule: builder.mutation<unknown, RegisterModuleQueryArg>({
+      invalidatesTags: ['Modules'],
+      query(body) {
+        return {
+          api: 'modulesApiUrl',
+          url: '/module',
+          method: 'POST',
+          body,
+        }
+      },
+    }),
     getModules: builder.query<ServerModule[], void>({
+      providesTags: ['Modules'],
       query: () => {
         return {
           api: 'modulesApiUrl',
@@ -17,8 +38,23 @@ export const modulesApi = baseApi.injectEndpoints({
       transformResponse: (response: {result: ServerModule[]}) =>
         response.result,
     }),
+    setModulesOrder: builder.mutation<unknown, ModuleOrderQueryArg>({
+      invalidatesTags: ['Modules'],
+      query(body) {
+        return {
+          api: 'modulesApiUrl',
+          url: '/module_order',
+          method: 'POST',
+          body,
+        }
+      },
+    }),
   }),
   overrideExisting: true,
 })
 
-export const {useGetModulesQuery} = modulesApi
+export const {
+  useAddModuleMutation,
+  useGetModulesQuery,
+  useSetModulesOrderMutation,
+} = modulesApi
