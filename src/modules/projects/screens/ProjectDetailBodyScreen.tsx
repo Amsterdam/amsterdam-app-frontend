fc1a7cf2d04059ddb46279f45fb9e32ce27ec83a
Frank Filius <f.filius@amsterdam.nl>
2022-06-01 13:44:06 +0200

    Refactor comonent to match design +
    some type improvements

--- src/modules/projects/screens/ProjectDetailBodyScreen.tsx
+++ src/modules/projects/screens/ProjectDetailBodyScreen.tsx
@@ -1,26 +1,18 @@
-import Calendar from '@amsterdam/asc-assets/static/icons/Calendar.svg'
-import ChatBubble from '@amsterdam/asc-assets/static/icons/ChatBubble.svg'
-import Info from '@amsterdam/asc-assets/static/icons/Info.svg'
-import LocationFields from '@amsterdam/asc-assets/static/icons/LocationFields.svg'
 import {RouteProp, useNavigation} from '@react-navigation/native'
-import React, {Fragment, ReactNode, useLayoutEffect} from 'react'
-import {StyleSheet, useWindowDimensions, View} from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import {useWindowDimensions} from 'react-native'
 import {RenderHTML} from 'react-native-render-html'
-import {StackParams} from '../../../app/navigation'
-import {
-  Box,
-  NonScalingHeaderTitle,
-  Timeline,
-  Title,
-} from '../../../components/ui'
-import {ScrollView} from '../../../components/ui/layout'
+import {Box, NonScalingHeaderTitle, Timeline} from '../../../components/ui'
+import {Column, ScrollView} from '../../../components/ui/layout'
+import {Title} from '../../../components/ui/typography'
 import {tagsStyles} from '../../../styles/html'
-import {color, font, size} from '../../../tokens'
+import {font} from '../../../tokens'
 import {regexLibrary} from '../../../utils'
+import {ProjectsRouteName, ProjectsStackParams} from '../routes'
 
 type ProjectDetailBodyScreenRouteProp = RouteProp<
-  StackParams,
-  'ProjectDetailBody'
+  ProjectsStackParams,
+  ProjectsRouteName.projectDetailBody
 >
 
 type Props = {
@@ -34,68 +26,41 @@ export const ProjectDetailBodyScreen = ({route}: Props) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: () => <NonScalingHeaderTitle text={body.headerTitle} />,
+      headerTitle: () => <NonScalingHeaderTitle text={body.title} />,
     })
   })
 
-  const icon: Record<string, ReactNode> = {
-    Bouwprojecten: (
-      <LocationFields fill={color.font.primary} style={styles.icon} />
-    ),
-    Contact: <ChatBubble fill={color.font.primary} style={styles.icon} />,
-    Informatie: <Info fill={color.font.primary} style={styles.icon} />,
-    Tijdlijn: <Calendar fill={color.font.primary} style={styles.icon} />,
-  }
-
   return (
     <ScrollView>
-      <Box background="grey">
-        <View style={styles.title}>
-          {icon[body.title]}
-          <Title primary text={body.title} />
-        </View>
-      </Box>
-      <View style={styles.divider} />
       <Box>
-        {body.sections.map(section => (
-          <Fragment key={section.title}>
-            <Title level={3} text={section.title} />
-            <RenderHTML
-              contentWidth={width}
-              source={{
-                html: section.html
-                  .replace(
-                    regexLibrary.plainPublish.regex,
-                    regexLibrary.plainPublish.replace,
-                  )
-                  .replace(
-                    regexLibrary.quotePublish.regex,
-                    regexLibrary.quotePublish.replace,
-                  ),
-              }}
-              systemFonts={[font.weight.regular, font.weight.demi]}
-              tagsStyles={tagsStyles}
-            />
-          </Fragment>
-        ))}
-        {body.timeline?.items?.length && (
-          <Timeline items={body.timeline.items} />
-        )}
+        <Column gutter="md">
+          <Title text={body.title} />
+          {body.sections.map(section => (
+            <Column gutter="sm" key={section.title}>
+              <Title level="h2" text={section.title} />
+              <RenderHTML
+                contentWidth={width}
+                source={{
+                  html: section.html
+                    .replace(
+                      regexLibrary.plainPublish.regex,
+                      regexLibrary.plainPublish.replace,
+                    )
+                    .replace(
+                      regexLibrary.quotePublish.regex,
+                      regexLibrary.quotePublish.replace,
+                    ),
+                }}
+                systemFonts={[font.weight.regular, font.weight.demi]}
+                tagsStyles={tagsStyles}
+              />
+            </Column>
+          ))}
+          {body.timeline?.items?.length && (
+            <Timeline items={body.timeline.items} />
+          )}
+        </Column>
       </Box>
     </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  icon: {
-    width: font.height.h1,
-    marginRight: size.spacing.md,
-  },
-  title: {
-    flexDirection: 'row',
-  },
-  divider: {
-    borderBottomWidth: 2,
-    borderBottomColor: color.border.divider,
-  },
-})
