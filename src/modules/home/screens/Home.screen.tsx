4e800164b111566adb3d77b46ae87c2361581e98
Rogier <rogier.voogt@amsterdam.nl>
2022-06-02 12:15:05 +0200

    Env switch improvements and fixes

--- src/modules/home/screens/Home.screen.tsx
+++ src/modules/home/screens/Home.screen.tsx
@@ -1,13 +1,9 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {useSelector} from 'react-redux'
-import {RootStackParamList} from '../../../app/navigation'
 import {AddButton, Box} from '../../../components/ui'
 import {Column, Gutter} from '../../../components/ui/layout'
 import {Screen} from '../../../components/ui/layout/Screen'
-import {selectTheme} from '../../../themes'
-import {module as homeModule} from '../../home'
 import {Modules} from '../components'
 import {HomeRouteName, HomeStackParams} from '../routes'
 
