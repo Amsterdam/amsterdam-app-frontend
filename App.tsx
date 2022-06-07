51f52c1df8c0c49532ce85423f8a0f247b3dac0a
Rogier <rogier.voogt@amsterdam.nl>
2022-06-07 18:04:26 +0200

    Sentry initial setup WIP

--- App.tsx
+++ App.tsx
@@ -1,26 +1,47 @@
-import {NavigationContainer} from '@react-navigation/native'
-import React from 'react'
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native'
+import {wrap as SentryWrap} from '@sentry/react-native'
+import React, {useRef} from 'react'
 import {StatusBar} from 'react-native'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {persistStore} from 'redux-persist'
 import {PersistGate} from 'redux-persist/integration/react'
-import {linking, RootStackNavigator} from './src/app/navigation'
+import {
+  linking,
+  RootStackParamList,
+  RootStackNavigator,
+} from './src/app/navigation'
 import {Init} from './src/components/features/Init'
 import {RootProvider} from './src/providers'
+import {initSentry, registerNavigationContainer} from './src/services'
 import {store} from './src/store'
 
-let persistor = persistStore(store)
+const persistor = persistStore(store)
 
-export const App = () => (
-  <SafeAreaProvider>
-    <StatusBar barStyle="dark-content" />
-    <NavigationContainer linking={linking}>
-      <RootProvider>
-        <PersistGate loading={null} persistor={persistor}>
-          <Init />
-          <RootStackNavigator />
-        </PersistGate>
-      </RootProvider>
-    </NavigationContainer>
-  </SafeAreaProvider>
-)
+initSentry()
+
+const AppComponent = () => {
+  const navigation = useRef<NavigationContainerRef<RootStackParamList>>(null)
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer
+        linking={linking}
+        ref={navigation}
+        onReady={() => {
+          registerNavigationContainer(navigation)
+        }}>
+        <RootProvider>
+          <PersistGate loading={null} persistor={persistor}>
+            <Init />
+            <RootStackNavigator />
+          </PersistGate>
+        </RootProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
+
+export const App = SentryWrap(AppComponent)
