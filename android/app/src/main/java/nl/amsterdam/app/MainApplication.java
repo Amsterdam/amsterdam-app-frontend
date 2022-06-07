01986756edeff28cc57d07d9f7809d68b10a3672
Rogier <rogier.voogt@amsterdam.nl>
2022-06-07 18:04:26 +0200

    Sentry initial setup WIP

--- android/app/src/main/java/nl/amsterdam/app/MainApplication.java
+++ android/app/src/main/java/nl/amsterdam/app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.sentry.react.RNSentryPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
