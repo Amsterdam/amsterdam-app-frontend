6811b3b0098b15aad8204143315abcc44b11ef11
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-08 23:59:39 +0200

    Use new icon component

--- src/modules/home/components/Modules.tsx
+++ src/modules/home/components/Modules.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {FlatList} from 'react-native'
 import {Box, PleaseWait} from '../../../components/ui'
+import {Icon} from '../../../components/ui/media'
 import {Paragraph} from '../../../components/ui/typography'
 import {color} from '../../../tokens'
 import {Module} from '../../types'
@@ -8,19 +9,17 @@ import {icons} from '../config'
 import {useModules} from '../hooks'
 import {ModuleButton} from './ModuleButton'
 
-const iconProps = {
-  width: 24,
-  aspectRatio: 1,
-  fill: color.font.regular,
-}
-
 const renderModuleButton = (module: Module) => {
   const {icon, name, slug, title} = module
-  const Icon = icons[icon]
+  const ModuleIcon = icons[icon]
 
   return (
     <ModuleButton
-      icon={<Icon {...iconProps} />}
+      icon={
+        <Icon size={24}>
+          <ModuleIcon fill={color.font.regular} />
+        </Icon>
+      }
       key={slug}
       label={title}
       name={name}
