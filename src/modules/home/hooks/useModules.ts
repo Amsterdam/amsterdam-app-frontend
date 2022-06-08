2c3713dc046a6f24bc0a03adb90e301777ffdc47
Frank Filius <f.filius@amsterdam.nl>
2022-06-08 09:16:32 +0200

    Refactor modules hook to cover all our needs

--- src/modules/home/hooks/useModules.ts
+++ src/modules/home/hooks/useModules.ts
@@ -1,41 +1,56 @@
-import {useSelector} from 'react-redux'
+import {useCallback, useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {combineClientAndServerModules} from '../../../utils'
 import {clientModules} from '../../index'
-import {ModuleClientAndUserConfig} from '../../types'
+import {Module} from '../../types'
 import {useGetModulesQuery} from '../services'
-import {selectModules} from '../store'
+import {initializeModules, selectModules} from '../store'
 
-type Props = {
-  includeDeselected?: boolean
-  includeInactive?: boolean
-}
-
-export const useModules = ({
-  includeDeselected = true,
-  includeInactive = true,
-}: Props) => {
-  const {data: serverModules, isLoading} = useGetModulesQuery()
-  const {modules: selectedModuleSlugs} = useSelector(selectModules)
-
-  const selectedClientModules: ModuleClientAndUserConfig[] = clientModules.map(
-    m => ({
-      ...m,
-      isSelected: selectedModuleSlugs.includes(m.slug),
-    }),
-  )
-
-  let modules = combineClientAndServerModules(
-    selectedClientModules,
+export const useModules = () => {
+  const dispatch = useDispatch()
+  const {
     serverModules,
+    serverModulesBySlug,
+    isLoading: isLoadingServerModules,
+  } = useGetModulesQuery(undefined, {
+    selectFromResult: ({data, isLoading}) => ({
+      isLoading,
+      serverModules: data,
+      serverModulesBySlug: data?.map(s => s.slug),
+    }),
+  })
+  const {modules: userSelectedModulesBySlug} = useSelector(selectModules)
+  const [modules, setModules] = useState<Module[]>()
+
+  useEffect(() => {
+    if (serverModulesBySlug && userSelectedModulesBySlug === undefined) {
+      dispatch(initializeModules(serverModulesBySlug))
+    }
+  }, [dispatch, serverModulesBySlug, userSelectedModulesBySlug])
+
+  useEffect(() => {
+    serverModules &&
+      setModules(combineClientAndServerModules(clientModules, serverModules))
+  }, [serverModules])
+
+  const getModulesWithoutInactive = useCallback(
+    () => modules && modules.filter(m => m.status === 1),
+    [modules],
   )
 
-  if (!includeInactive) {
-    modules = modules.filter(m => m.status === 1)
-  }
+  const getModulesWithoutDeselected = useCallback(
+    () =>
+      modules &&
+      modules.filter(m => userSelectedModulesBySlug?.includes(m.slug)),
+    [modules, userSelectedModulesBySlug],
+  )
 
-  if (!includeDeselected) {
-    modules = modules.filter(m => selectedModuleSlugs.includes(m.slug))
+  return {
+    isLoadingServerModules,
+    modules,
+    getModulesWithoutInactive,
+    getModulesWithoutDeselected,
+    serverModules,
+    userSelectedModulesBySlug,
   }
-
-  return {modules, isLoading}
 }
