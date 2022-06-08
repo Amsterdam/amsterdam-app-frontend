db29291b99e404ef495caeed42f97a924a24639f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-09 00:21:05 +0200

    Improve scrolling and landscape layout

--- src/modules/home/components/Modules.tsx
+++ src/modules/home/components/Modules.tsx
@@ -1,31 +1,11 @@
 import React from 'react'
-import {FlatList} from 'react-native'
 import {Box, PleaseWait} from '../../../components/ui'
 import {Icon} from '../../../components/ui/media'
 import {Paragraph} from '../../../components/ui/typography'
 import {color} from '../../../tokens'
-import {Module} from '../../types'
 import {icons} from '../config'
 import {useModules} from '../hooks'
 import {ModuleButton} from './ModuleButton'
-
-const renderModuleButton = (module: Module) => {
-  const {icon, name, slug, title} = module
-  const ModuleIcon = icons[icon]
-
-  return (
-    <ModuleButton
-      icon={
-        <Icon size={24}>
-          <ModuleIcon fill={color.font.regular} />
-        </Icon>
-      }
-      key={slug}
-      label={title}
-      name={name}
-    />
-  )
-}
 
 export const Modules = () => {
   const {getSelectedModules, isLoadingModules} = useModules()
@@ -48,10 +28,23 @@ export const Modules = () => {
 
   return (
     <Box grow>
-      <FlatList
-        data={modules}
-        renderItem={({item}) => renderModuleButton(item)}
-      />
+      {modules.map(module => {
+        const {icon, name, slug, title} = module
+        const ModuleIcon = icons[icon]
+
+        return (
+          <ModuleButton
+            icon={
+              <Icon size={24}>
+                <ModuleIcon fill={color.font.regular} />
+              </Icon>
+            }
+            key={slug}
+            label={title}
+            name={name}
+          />
+        )
+      })}
     </Box>
   )
 }
--- src/modules/home/screens/Home.screen.tsx
+++ src/modules/home/screens/Home.screen.tsx
@@ -2,7 +2,7 @@ import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {AddButton, Box} from '../../../components/ui'
-import {Column} from '../../../components/ui/layout'
+import {Column, ScrollView} from '../../../components/ui/layout'
 import {Screen} from '../../../components/ui/layout/Screen'
 import {Modules} from '../components'
 import {HomeRouteName, HomeStackParams} from '../routes'
@@ -13,15 +13,17 @@ export const HomeScreen = () => {
 
   return (
     <Screen>
-      <Column align="between">
-        <Modules />
-        <Box>
-          <AddButton
-            accessibilityLabel="Instellingen"
-            onPress={() => navigation.navigate(HomeRouteName.settings)}
-          />
-        </Box>
-      </Column>
+      <ScrollView grow>
+        <Column align="between">
+          <Modules />
+          <Box>
+            <AddButton
+              accessibilityLabel="Instellingen"
+              onPress={() => navigation.navigate(HomeRouteName.settings)}
+            />
+          </Box>
+        </Column>
+      </ScrollView>
     </Screen>
   )
 }
