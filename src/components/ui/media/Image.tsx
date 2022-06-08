48322f68795fed650458a5b52b88140ea984ddc6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-08 19:43:30 +0200

    Allow custom aspect ratio on images

--- src/components/ui/media/Image.tsx
+++ src/components/ui/media/Image.tsx
@@ -9,22 +9,31 @@ import {ImageAspectRatioTokens} from '../../../themes/tokens'
 
 type Props = {
   aspectRatio?: keyof ImageAspectRatioTokens
+  customAspectRatio?: number
 } & Omit<ImageRNProps, 'style'>
 
-export const Image = ({aspectRatio, ...otherProps}: Props) => {
-  const createdStyles = useMemo(() => createStyles(aspectRatio), [aspectRatio])
+export const Image = ({
+  aspectRatio,
+  customAspectRatio,
+  ...otherProps
+}: Props) => {
+  const createdStyles = useMemo(
+    () => createStyles({aspectRatio, customAspectRatio}),
+    [aspectRatio, customAspectRatio],
+  )
   const styles = useThemable(createdStyles)
 
   return <ImageRN style={styles.image} {...otherProps} />
 }
 
 const createStyles =
-  (ratio?: keyof ImageAspectRatioTokens) =>
+  ({aspectRatio, customAspectRatio}: Partial<Props>) =>
   ({image}: Theme) =>
     StyleSheet.create({
       image: {
         maxWidth: '100%',
-        aspectRatio: image.aspectRatio[ratio ?? 'default'],
+        aspectRatio:
+          customAspectRatio ?? image.aspectRatio[aspectRatio ?? 'default'],
         resizeMode: 'cover',
       },
     })
