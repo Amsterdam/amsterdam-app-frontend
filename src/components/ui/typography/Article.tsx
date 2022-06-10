8de58decb5923a44b0e311b8d8aef4cc5f685b5c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-10 16:00:25 +0200

    Incorporate HTML stylesheet

--- src/components/ui/typography/Article.tsx
+++ src/components/ui/typography/Article.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {useWindowDimensions} from 'react-native'
-import RenderHTML, {RenderHTMLProps} from 'react-native-render-html'
-import {tagsStyles} from '../../../styles/html'
-import {font} from '../../../tokens'
+import RenderHTML, {
+  MixedStyleDeclaration,
+  RenderHTMLProps,
+} from 'react-native-render-html'
+import {color, font} from '../../../tokens'
 
 type Props = Pick<RenderHTMLProps, 'source'>
 
@@ -14,7 +16,43 @@ export const Article = ({source}: Props) => {
       contentWidth={width}
       source={source}
       systemFonts={[font.weight.demi, font.weight.regular]}
-      tagsStyles={tagsStyles}
+      tagsStyles={styles}
     />
   )
+}
+
+const baseStyles: Record<string, MixedStyleDeclaration> = {
+  h3: {
+    color: color.font.regular,
+    fontWeight: '600', // Seems to work best visually – better than 500.
+    fontFamily: font.weight.demi,
+    fontSize: font.size.h3,
+    lineHeight: font.height.h3,
+    margin: 0,
+  },
+  list: {
+    margin: 0,
+    marginLeft: -10,
+  },
+  listItem: {
+    paddingLeft: 10,
+  },
+  text: {
+    color: color.font.regular,
+    fontFamily: font.weight.regular,
+    fontSize: font.size.p1,
+    lineHeight: font.height.p1,
+    marginBottom: font.leadingBottom.p1,
+    marginTop: font.leadingTop.p1,
+  },
+  textIntro: {
+    fontFamily: font.weight.demi,
+  },
+}
+
+const styles: Record<string, MixedStyleDeclaration> = {
+  h3: baseStyles.h3,
+  li: {...baseStyles.text, ...baseStyles.listItem},
+  p: baseStyles.text,
+  ul: baseStyles.list,
 }
