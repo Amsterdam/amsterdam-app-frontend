e960baf660456e2a594b8f5ff19aee3be2f72307
Frank Filius <f.filius@amsterdam.nl>
2022-06-13 14:36:03 +0200

    Refactor navigation button

--- src/components/ui/button/NavigationButton.tsx
+++ src/components/ui/button/NavigationButton.tsx
@@ -1,7 +1,8 @@
 import ChevronRight from '@amsterdam/asc-assets/static/icons/ChevronRight.svg'
 import React from 'react'
+import {StyleSheet} from 'react-native'
 import {useSelector} from 'react-redux'
-import {selectTheme} from '../../../themes'
+import {selectTheme, Theme, useThemable} from '../../../themes'
 import {Row} from '../layout'
 import {Icon} from '../media'
 import {Link} from '../typography'
@@ -14,9 +15,10 @@ type Props = {
 
 export const NavigationButton = ({label, onPress}: Props) => {
   const {theme} = useSelector(selectTheme)
+  const styles = useThemable(createStyles)
 
   return (
-    <BlockLink onPress={onPress}>
+    <BlockLink onPress={onPress} style={styles.button}>
       <Row align="between">
         <Link label={label} />
         <Icon size={24}>
@@ -26,3 +28,11 @@ export const NavigationButton = ({label, onPress}: Props) => {
     </BlockLink>
   )
 }
+
+export const createStyles = (theme: Theme) =>
+  StyleSheet.create({
+    button: {
+      paddingHorizontal: theme.size.spacing.md,
+      paddingVertical: theme.size.spacing.sm,
+    },
+  })
