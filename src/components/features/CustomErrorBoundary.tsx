1f62eff30ee1070afc0cc2222a3424df800fd9b4
Rogier <rogier.voogt@amsterdam.nl>
2022-06-13 18:05:56 +0200

    Sourcemap upload fix, boundary fix

--- src/components/features/CustomErrorBoundary.tsx
+++ src/components/features/CustomErrorBoundary.tsx
@@ -1,5 +1,7 @@
 import React, {Component, ReactNode} from 'react'
-import {ErrorWithRestart} from '../ui/ErrorWithRestart'
+import {Pressable, Text, StyleSheet} from 'react-native'
+import RNRestart from 'react-native-restart'
+import {SafeAreaView} from 'react-native-safe-area-context'
 import {devLog} from '@/services'
 
 type Props = {children: ReactNode}
@@ -18,6 +20,39 @@ export class CustomErrorBoundary extends Component<Props, State> {
   }
 
   render() {
-    return this.state.hasError ? <ErrorWithRestart /> : this.props.children
+    if (!this.state.hasError) {
+      return this.props.children
+    }
+    const {wrapper, text, paragraph, button, buttonText} = styles
+    return (
+      <SafeAreaView style={wrapper}>
+        <Text style={[paragraph, text]}>
+          Er is iets misgegaan met de app. Sorry voor het ongemak!
+        </Text>
+        <Pressable style={button} onPress={() => RNRestart.Restart()}>
+          <Text style={[text, buttonText]}>De app opnieuw opstarten</Text>
+        </Pressable>
+      </SafeAreaView>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    lineHeight: 27,
+  },
+  paragraph: {
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#004699',
+    padding: 12,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+})
