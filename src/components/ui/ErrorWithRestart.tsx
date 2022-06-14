97ee853268c70b16bfb47b44253583ebeb40cfe2
Rogier <rogier.voogt@amsterdam.nl>
2022-06-14 12:20:04 +0200

    Revert Sentry test code, styling fixes

--- src/components/ui/ErrorWithRestart.tsx
+++ src/components/ui/ErrorWithRestart.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {StyleSheet} from 'react-native'
 import RNRestart from 'react-native-restart'
 import {Attention} from './Attention'
 import {Box} from './Box'
@@ -6,25 +7,36 @@ import {Button} from './Button'
 import {Grid, GridCell} from './layout'
 import {Screen} from './layout/Screen'
 import {Paragraph} from './typography'
+import {Theme, useThemable} from '@/themes'
 
-export const ErrorWithRestart = () => (
-  <Screen withoutNavigationHeader>
-    <Box>
-      <Grid>
-        <GridCell>
-          <Attention warning>
-            <Paragraph>
-              Er is iets misgegaan met de app. Sorry voor het ongemak!
-            </Paragraph>
-          </Attention>
-        </GridCell>
-        <GridCell>
-          <Button
-            onPress={() => RNRestart.Restart()}
-            text="De app opnieuw opstarten"
-          />
-        </GridCell>
-      </Grid>
-    </Box>
-  </Screen>
-)
+export const ErrorWithRestart = () => {
+  const styles = useThemable(createStyles)
+  return (
+    <Screen withoutNavigationHeader style={styles.screen}>
+      <Box>
+        <Grid>
+          <GridCell>
+            <Attention warning>
+              <Paragraph>
+                Er is iets misgegaan met de app. Sorry voor het ongemak!
+              </Paragraph>
+            </Attention>
+          </GridCell>
+          <GridCell>
+            <Button
+              onPress={() => RNRestart.Restart()}
+              text="De app opnieuw opstarten"
+            />
+          </GridCell>
+        </Grid>
+      </Box>
+    </Screen>
+  )
+}
+
+const createStyles = ({color}: Theme) =>
+  StyleSheet.create({
+    screen: {
+      backgroundColor: color.screen.background.default,
+    },
+  })
