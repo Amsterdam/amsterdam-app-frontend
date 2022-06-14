dfa7d9512d99ca9f39d5737035ed40441392a855
Rogier <rogier.voogt@amsterdam.nl>
2022-06-14 10:11:21 +0200

    Sourcemap fix, error boundary test

--- src/modules/open-waste-container/Screen.tsx
+++ src/modules/open-waste-container/Screen.tsx
@@ -1,24 +1,3 @@
-import {nativeCrash} from '@sentry/react-native'
-import React from 'react'
-import {Box, Button, Text} from '../../components/ui'
-import {ScrollView} from '../../components/ui/layout'
-
-export const OpenWasteContainerScreen = () => (
-  <ScrollView>
-    <Box>
-      <Text>Hallo, ik ben een module! 🎉</Text>
-      <Button
-        onPress={() => {
-          throw new Error('Sentry test 1')
-        }}
-        text="Sentry test: JS error"
-      />
-      <Button
-        onPress={() => {
-          nativeCrash()
-        }}
-        text="Sentry test: native error"
-      />
-    </Box>
-  </ScrollView>
-)
+export const OpenWasteContainerScreen = () => {
+  throw new Error('Sentry test 1')
+}
