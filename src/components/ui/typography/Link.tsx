32f61c60de06b2484f83b80f2c596d682c7d8a88
Frank Filius <f.filius@amsterdam.nl>
2022-06-14 08:34:39 +0200

    Make level of link optional and apply

--- src/components/ui/typography/Link.tsx
+++ src/components/ui/typography/Link.tsx
@@ -2,22 +2,26 @@ import React from 'react'
 import {StyleSheet, Text} from 'react-native'
 import {Theme, useThemable} from '../../../themes'
 import {font} from '../../../tokens'
+import {TitleTokensPerLevel} from '@/themes/tokens'
 
 type Props = {
   label: string
+  level: keyof TitleTokensPerLevel
 }
 
-export const Link = ({label}: Props) => {
-  const styles = useThemable(createStyles)
+export const Link = ({label, level}: Props) => {
+  const styles = useThemable(createStyles({level}))
   return <Text style={styles.text}>{label}</Text>
 }
 
-const createStyles = ({color, text}: Theme) =>
-  StyleSheet.create({
-    text: {
-      color: color.pressable.navigation,
-      fontSize: text.fontSize.h5,
-      fontFamily: font.weight.bold,
-      lineHeight: text.lineHeight.h5 * text.fontSize.h5,
-    },
-  })
+const createStyles =
+  ({level}: Pick<Props, 'level'>) =>
+  ({color, text}: Theme) =>
+    StyleSheet.create({
+      text: {
+        color: color.pressable.navigation,
+        fontSize: text.fontSize[level],
+        fontFamily: font.weight.bold,
+        lineHeight: text.lineHeight.h5 * text.fontSize.h5,
+      },
+    })
