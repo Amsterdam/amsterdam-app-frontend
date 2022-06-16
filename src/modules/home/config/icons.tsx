799ff58df7eb36f3bc9cce1b8dadb2e910f498f7
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-16 14:00:12 +0200

    Add and configure correct icons for two modules

--- src/modules/home/config/icons.tsx
+++ src/modules/home/config/icons.tsx
@@ -1,15 +1,14 @@
 import Alert from '@amsterdam/asc-assets/static/icons/Alert.svg'
 import ChatBubble from '@amsterdam/asc-assets/static/icons/ChatBubble.svg'
-import Chatting from '@amsterdam/asc-assets/static/icons/Chatting.svg'
 import TrashBin from '@amsterdam/asc-assets/static/icons/TrashBin.svg'
 import {ElementType} from 'react'
-import {Container, Project} from '@/assets/icons'
+import {CityOffice, OrganicWasteContainer, Project} from '@/assets/icons'
 
 export const icons: Record<string, ElementType> = {
   alert: Alert,
   'chat-bubble': ChatBubble,
-  chatting: Chatting,
-  container: Container,
+  chatting: CityOffice,
+  container: OrganicWasteContainer,
   location: TrashBin, // TEMP
   project: Project,
   'trash-bin': TrashBin,
