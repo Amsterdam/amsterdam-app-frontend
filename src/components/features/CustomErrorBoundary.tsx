69ce97742c6864988377e826f4dfec7852d6f8d7
Rogier <rogier.voogt@amsterdam.nl>
2022-06-23 12:54:30 +0200

    Fixes

--- src/components/features/CustomErrorBoundary.tsx
+++ src/components/features/CustomErrorBoundary.tsx
@@ -20,7 +20,7 @@ export class CustomErrorBoundary extends Component<Props, State> {
   }
 
   render() {
-    if (this.state.hasError === 0) {
+    if (this.state.hasError === 1) {
       return this.props.children
     }
     const {wrapper, text, paragraph, button, buttonText} = styles
