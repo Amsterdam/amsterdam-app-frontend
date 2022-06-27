857c48bc3d62f3df5f3cdb9db9fcab92fa1bf31a
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-27 12:24:34 +0200

    Move ‘private’ components back into ‘Projects by distance’ component file

--- src/modules/construction-work/components/projects/ProjectsByDistance.tsx
+++ src/modules/construction-work/components/projects/ProjectsByDistance.tsx
@@ -1,23 +1,121 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import {skipToken} from '@reduxjs/toolkit/dist/query'
 import React, {useContext} from 'react'
 import {StyleSheet} from 'react-native'
 import {FlatGrid} from 'react-native-super-grid'
 import {useSelector} from 'react-redux'
-import {PleaseWait, SomethingWentWrong} from '@/components/ui'
-import {Column} from '@/components/ui/layout'
+import {RootStackParamList} from '@/app/navigation'
+import {Edit} from '@/assets/icons'
+import {Box, IconButton, PleaseWait, SomethingWentWrong} from '@/components/ui'
+import {Column, Row} from '@/components/ui/layout'
+import {Icon} from '@/components/ui/media'
+import {Paragraph} from '@/components/ui/text'
 import {selectAddress} from '@/modules/address/addressSlice'
+import {AddressRouteName} from '@/modules/address/routes'
 import {
-  ProjectsByDistanceListHeader,
-  ProjectsByDistanceListItem,
-  ProjectsByDistanceNoResults,
   sanitizeProjects,
   selectIsProjectsSearching,
 } from '@/modules/construction-work/components/projects'
+import {
+  ProjectCard,
+  ProjectTraits,
+} from '@/modules/construction-work/components/shared'
 import {useGetProjectsByDistanceQuery} from '@/modules/construction-work/construction-work.service'
+import {
+  ConstructionWorkRouteName,
+  ProjectsStackParams,
+} from '@/modules/construction-work/routes'
 import {DeviceContext} from '@/providers'
+import {useEnvironment} from '@/store'
 import {useTheme} from '@/themes'
+import {ProjectsItem} from '@/types'
+import {mapImageSources} from '@/utils'
+
+type ProjectsByDistanceListHeaderProps = {
+  address: string
+  navigation: StackNavigationProp<
+    RootStackParamList & ProjectsStackParams,
+    ConstructionWorkRouteName.projects
+  >
+}
+
+const ProjectsByDistanceListHeader = ({
+  address,
+  navigation,
+}: ProjectsByDistanceListHeaderProps) => {
+  const {color} = useTheme()
+
+  return (
+    <Box>
+      <Row gutter="sm" valign="center">
+        <Paragraph accessibilityLabel={`Projecten dichtbij ${address}`}>
+          Dichtbij {address}
+        </Paragraph>
+        <IconButton
+          accessibilityLabel="Wijzig het adres"
+          icon={
+            <Icon size={32}>
+              <Edit fill={color.pressable.default.background} />
+            </Icon>
+          }
+          onPress={
+            // TODO Open as modal
+            () =>
+              navigation.navigate('AddressModule', {
+                screen: AddressRouteName.addressForm,
+              })
+          }
+        />
+      </Row>
+    </Box>
+  )
+}
+
+type ProjectsByDistanceListItemProps = {
+  navigation: StackNavigationProp<
+    RootStackParamList & ProjectsStackParams,
+    ConstructionWorkRouteName.projects
+  >
+  project: ProjectsItem
+}
+
+const ProjectsByDistanceListItem = ({
+  navigation,
+  project,
+}: ProjectsByDistanceListItemProps) => {
+  const environment = useEnvironment()
+
+  return (
+    <ProjectCard
+      imageSource={mapImageSources(project.images?.[0].sources, environment)}
+      kicker={<ProjectTraits projectId={project.identifier} />}
+      onPress={() =>
+        navigation.navigate(ConstructionWorkRouteName.project, {
+          id: project.identifier,
+        })
+      }
+      subtitle={project.subtitle ?? undefined}
+      title={project.title}
+    />
+  )
+}
+
+const ProjectsByDistanceNoResults = () => (
+  <Box>
+    <Paragraph>Geen projecten in de buurt.</Paragraph>
+  </Box>
+)
 
 export const ProjectsByDistance = () => {
+  const navigation =
+    useNavigation<
+      StackNavigationProp<
+        RootStackParamList & ProjectsStackParams,
+        ConstructionWorkRouteName.projects
+      >
+    >()
+
   const {primary: address} = useSelector(selectAddress)
   const {size} = useTheme()
   const {fontScale} = useContext(DeviceContext)
@@ -60,9 +158,14 @@ export const ProjectsByDistance = () => {
         keyExtractor={project => project.identifier}
         ListEmptyComponent={ProjectsByDistanceNoResults}
         ListHeaderComponent={
-          <ProjectsByDistanceListHeader address={address.adres} />
+          <ProjectsByDistanceListHeader
+            address={address.adres}
+            navigation={navigation}
+          />
         }
-        renderItem={({item}) => <ProjectsByDistanceListItem project={item} />}
+        renderItem={({item}) => (
+          <ProjectsByDistanceListItem navigation={navigation} project={item} />
+        )}
         spacing={size.spacing.md}
       />
     </Column>
--- src/modules/construction-work/components/projects/ProjectsByDistanceNoResults.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Box} from '@/components/ui'
-import {Paragraph} from '@/components/ui/text'
-
-export const ProjectsByDistanceNoResults = () => (
-  <Box>
-    <Paragraph>Geen projecten in de buurt.</Paragraph>
-  </Box>
-)
