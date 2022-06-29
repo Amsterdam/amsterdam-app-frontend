e9aa02294b9d9b4ab7298cc97421000ed218b198
Frank Filius <f.filius@amsterdam.nl>
2022-06-29 14:28:40 +0200

    Move hooks into own folder

---
+++ src/modules/construction-work/hooks/useMarkArticleAsRead.ts
@@ -0,0 +1,37 @@
+import {useCallback} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {
+  deleteReadArticle,
+  ReadArticle,
+  addReadArticle,
+  selectConstructionWorkReadArticles,
+} from '../construction-work.slice'
+import {getDateDiffInDays} from '@/utils'
+
+export const useMarkArticleAsRead = () => {
+  const readArticles = useSelector(selectConstructionWorkReadArticles)
+  const dispatch = useDispatch()
+
+  const deleteOldArticles = useCallback(() => {
+    readArticles.forEach(readArticle => {
+      getDateDiffInDays(readArticle.publicationDate) > 3 &&
+        dispatch(deleteReadArticle(readArticle.id))
+    })
+  }, [dispatch, readArticles])
+
+  const markAsRead = useCallback(
+    (article: ReadArticle) => {
+      deleteOldArticles()
+      if (getDateDiffInDays(article.publicationDate) > 3) {
+        return
+      }
+      if (readArticles.find(readArticle => readArticle.id === article.id)) {
+        return
+      }
+      dispatch(addReadArticle(article))
+    },
+    [deleteOldArticles, dispatch, readArticles],
+  )
+
+  return {markAsRead}
+}
