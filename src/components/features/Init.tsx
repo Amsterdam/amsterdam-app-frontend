694ae19e5d575f535d07e22cd4d7eb98d8a03977
Frank Filius <f.filius@amsterdam.nl>
2022-06-30 16:26:51 +0200

    Call hook on init

--- src/components/features/Init.tsx
+++ src/components/features/Init.tsx
@@ -1,15 +1,23 @@
 import React, {ReactNode, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {useInitSentry} from '../../hooks'
+import {useDeviceRegistration, useInitSentry} from '../../hooks'
 import {selectProjectManager} from '../../modules/construction-work/components/project-manager'
 import {setCredentials} from '../../store/authSlice'
 import {encryptWithAES} from '../../utils'
+import {useAppState} from '@/hooks/useAppState'
 
 type Props = {children: ReactNode}
 
 export const Init = ({children}: Props) => {
   const dispatch = useDispatch()
   const {id: projectManagerId} = useSelector(selectProjectManager)
+  const {deviceRegistration} = useDeviceRegistration()
+
+  useAppState({
+    onForeground: () => {
+      deviceRegistration()
+    },
+  })
 
   useEffect(() => {
     if (projectManagerId) {
