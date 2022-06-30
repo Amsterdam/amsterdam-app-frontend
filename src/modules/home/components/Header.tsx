52e5e3daeb6a7446782832f979782158d7cfdc78
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-06-30 23:49:15 +0200

    Remove unnecessary background color and bottom border from header
    Apparently, the header already gets its background colour from the screen, or it is transparent. Either way, no need to redeclare it.

--- src/modules/home/components/Header.tsx
+++ src/modules/home/components/Header.tsx
@@ -2,36 +2,28 @@ import {StackHeaderProps} from '@react-navigation/stack/lib/typescript/src/types
 import React, {useContext, useMemo} from 'react'
 import {StyleSheet, View} from 'react-native'
 import {EdgeInsets, useSafeAreaInsets} from 'react-native-safe-area-context'
-import {CustomScreenOptions} from '@/app/navigation/screenOptions'
 import {Box} from '@/components/ui'
 import {HeaderContent, HeaderContentForHome} from '@/modules/home/components'
 import {HomeRouteName} from '@/modules/home/routes'
 import {DeviceContext} from '@/providers'
-import {Theme, useThemable} from '@/themes/index'
 
-type Props = Pick<
-  StackHeaderProps,
-  'back' | 'navigation' | 'options' | 'route'
-> &
-  CustomScreenOptions
+type Props = Pick<StackHeaderProps, 'back' | 'navigation' | 'options' | 'route'>
 
 export const Header = (props: Props) => {
-  const {route, screenType} = props
+  const {route} = props
   const isHome = route.name === HomeRouteName.home
   const {isPortrait} = useContext(DeviceContext)
 
   const {left = 0, right = 0, top = 0} = useSafeAreaInsets()
-  const createdStyles = useMemo(
+  const styles = useMemo(
     () =>
       createStyles({
         left,
         top,
         right,
-        screenType,
       }),
-    [left, top, right, screenType],
+    [left, top, right],
   )
-  const styles = useThemable(createdStyles)
 
   return (
     <View style={styles.header}>
@@ -42,22 +34,11 @@ export const Header = (props: Props) => {
   )
 }
 
-const createStyles =
-  ({
-    left,
-    right,
-    top,
-    screenType,
-  }: Omit<EdgeInsets, 'bottom'> & CustomScreenOptions) =>
-  ({color}: Theme) =>
-    StyleSheet.create({
-      header: {
-        backgroundColor: color.screen.background[screenType],
-        paddingTop: top,
-        paddingLeft: left,
-        paddingRight: right,
-        borderBottomWidth: 0,
-        elevation: 0,
-        shadowOpacity: 0,
-      },
-    })
+const createStyles = ({left, right, top}: Omit<EdgeInsets, 'bottom'>) =>
+  StyleSheet.create({
+    header: {
+      paddingTop: top,
+      paddingLeft: left,
+      paddingRight: right,
+    },
+  })
