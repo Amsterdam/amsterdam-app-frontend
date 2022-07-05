e8aad9eab468f23a9a024e398fd9fd7b3841b95e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-05 13:15:37 +0200

    Allow using paragraph variants in `Text`

--- src/components/ui/text/Text.tsx
+++ src/components/ui/text/Text.tsx
@@ -1,18 +1,24 @@
 import React, {ReactNode, useMemo} from 'react'
 import {StyleSheet, Text as TextRN, TextProps} from 'react-native'
 import {Theme, useThemable} from '@/themes'
+import {ParagraphVariants} from '@/themes/tokens'
 
 type Props = {
   children: ReactNode
   fontWeight?: 'bold' | 'demi' | 'regular'
+  variant?: ParagraphVariants
 } & Omit<TextProps, 'style'>
 
 export const Text = ({
   children,
   fontWeight = 'regular',
+  variant = 'body',
   ...otherProps
 }: Props) => {
-  const createdStyles = useMemo(() => createStyles({fontWeight}), [fontWeight])
+  const createdStyles = useMemo(
+    () => createStyles({fontWeight, variant}),
+    [fontWeight, variant],
+  )
   const styles = useThemable(createdStyles)
 
   return (
@@ -23,12 +29,13 @@ export const Text = ({
 }
 
 const createStyles =
-  ({fontWeight}: Required<Pick<Props, 'fontWeight'>>) =>
+  ({fontWeight, variant}: Required<Pick<Props, 'fontWeight' | 'variant'>>) =>
   ({color, text}: Theme) =>
     StyleSheet.create({
       text: {
         color: color.text.default,
         fontFamily: text.fontWeight[fontWeight],
-        fontSize: text.fontSize.body,
+        fontSize: text.fontSize[variant],
+        lineHeight: 1.2 * text.fontSize[variant], // NOTE Doesn’t adhere to design system
       },
     })
