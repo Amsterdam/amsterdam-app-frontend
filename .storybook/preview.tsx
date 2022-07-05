c3f32abab62c7e368e636b80b81bb70b6c63acfd
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-05 19:44:58 +0200

    Configure background colors

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,8 +2,33 @@ import React from 'react'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {RootProvider} from '../src/providers'
 import './preview.css'
+import {baseColor} from '@/tokens'
 
 export const parameters = {
+  backgrounds: {
+    values: [
+      {
+        name: 'primary-blue',
+        value: baseColor.primary.blue,
+      },
+      {
+        name: 'primary-red',
+        value: baseColor.primary.red,
+      },
+      {
+        name: 'primary-black',
+        value: baseColor.primary.black,
+      },
+      {
+        name: 'secondary-yellow',
+        value: baseColor.secondary.yellow,
+      },
+      {
+        name: 'secondary-purple',
+        value: baseColor.secondary.purple,
+      },
+    ],
+  },
   controls: {expanded: true},
 }
 
