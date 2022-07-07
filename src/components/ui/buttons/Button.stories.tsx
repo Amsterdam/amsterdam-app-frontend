e91775e05d570f0bc4f99b7c7fc6856be88a0b16
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-07 12:53:00 +0200

    Unlock ‘show code’ feature by adding args

--- src/components/ui/buttons/Button.stories.tsx
+++ src/components/ui/buttons/Button.stories.tsx
@@ -37,16 +37,30 @@ export const Tertiary: ComponentStoryObj<typeof Button> = {
   },
 }
 
-export const _Inline: ComponentStory<typeof Button> = () => (
-  <Row align="start">
+export const _Inline: ComponentStory<typeof Button & typeof Row> = ({
+  align,
+}) => (
+  <Row align={align}>
     <Button label="Inline button" />
   </Row>
 )
 
-export const __Alignment: ComponentStory<typeof Button> = () => (
-  <Row align="start" gutter="md">
+_Inline.args = {
+  align: 'start',
+}
+
+export const __Alignment: ComponentStory<typeof Button & typeof Row> = ({
+  align,
+  gutter,
+}) => (
+  <Row align={align} gutter={gutter}>
     <Button label="First button" />
     <Button label="Second button" variant="secondary" />
     <Button label="Third button" variant="tertiary" />
   </Row>
 )
+
+__Alignment.args = {
+  align: 'start',
+  gutter: 'md',
+}
