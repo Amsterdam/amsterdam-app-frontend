719e793881b9d8bc1e886651599f02ae56f7316d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-08 09:32:47 +0200

    Revert "Make implementation match design"
    This reverts commit 653a569748f09b1910dfc918eaaebb39b1aefa51.

--- src/components/ui/SwipeToDelete.tsx
+++ src/components/ui/SwipeToDelete.tsx
@@ -3,7 +3,9 @@ import React, {ReactNode, useState} from 'react'
 import {View, Pressable} from 'react-native'
 import {Swipeable} from 'react-native-gesture-handler'
 import {Box} from '@/components/ui'
+import {Row} from '@/components/ui/layout'
 import {Icon} from '@/components/ui/media'
+import {Paragraph} from '@/components/ui/text'
 import {useTheme} from '@/themes'
 
 type Props = {
@@ -21,9 +23,12 @@ export const SwipeToDelete = ({children, onEvent}: Props) => {
         <View>
           <Pressable onPress={onEvent}>
             <Box background="invalid" inset="md">
-              <Icon size={24}>
-                <TrashBin fill={color.text.inverse} />
-              </Icon>
+              <Row align="end" valign="center" gutter="md">
+                <Paragraph variant="small">Verwijderen</Paragraph>
+                <Icon size={24}>
+                  <TrashBin fill={color.text.default} />
+                </Icon>
+              </Row>
             </Box>
           </Pressable>
         </View>
