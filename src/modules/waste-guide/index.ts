13ed4bd6f438ff6d4b0a469fd3ae3c9655145a52
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-08 12:13:06 +0200

    Test employee-only modules with ‘waste guide’ module

--- src/modules/waste-guide/index.ts
+++ src/modules/waste-guide/index.ts
@@ -2,6 +2,7 @@ import {ModuleSlugs} from '../slugs'
 import {ModuleClientConfig} from '../types'
 
 export const module: ModuleClientConfig = {
+  isForEmployees: true,
   linking: {},
   name: 'WasteGuideModule',
   slug: ModuleSlugs['waste-guide'],
