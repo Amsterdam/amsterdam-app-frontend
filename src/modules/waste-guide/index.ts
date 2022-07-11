88ea7f760024d09f8b294b8b13f97f6a8f25c335
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-11 16:18:27 +0200

    Revert "Test employee-only modules with ‘waste guide’ module"
    This reverts commit 13ed4bd6f438ff6d4b0a469fd3ae3c9655145a52.

--- src/modules/waste-guide/index.ts
+++ src/modules/waste-guide/index.ts
@@ -2,7 +2,6 @@ import {ModuleSlugs} from '../slugs'
 import {ModuleClientConfig} from '../types'
 
 export const module: ModuleClientConfig = {
-  isForEmployees: true,
   linking: {},
   name: 'WasteGuideModule',
   slug: ModuleSlugs['waste-guide'],
