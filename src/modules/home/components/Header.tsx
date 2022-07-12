608015b69ca1db6364aab6e1000d8225c9e1344d
Rik Scheffer <rik@oberon.nl>
2022-07-12 16:16:17 +0200

    Fix landscape header inset

--- src/modules/home/components/Header.tsx
+++ src/modules/home/components/Header.tsx
@@ -1,18 +1,16 @@
 import {StackHeaderProps} from '@react-navigation/stack/lib/typescript/src/types'
-import React, {useContext, useMemo} from 'react'
+import React, {useMemo} from 'react'
 import {StyleSheet, View} from 'react-native'
 import {EdgeInsets, useSafeAreaInsets} from 'react-native-safe-area-context'
 import {Box} from '@/components/ui'
 import {HeaderContent, HeaderContentForHome} from '@/modules/home/components'
 import {HomeRouteName} from '@/modules/home/routes'
-import {DeviceContext} from '@/providers'
 
 type Props = Pick<StackHeaderProps, 'back' | 'navigation' | 'options' | 'route'>
 
 export const Header = (props: Props) => {
   const {route} = props
   const isHome = route.name === HomeRouteName.home
-  const {isPortrait} = useContext(DeviceContext)
 
   const {left = 0, right = 0, top = 0} = useSafeAreaInsets()
   const styles = useMemo(
@@ -27,7 +25,7 @@ export const Header = (props: Props) => {
 
   return (
     <View style={styles.header}>
-      <Box insetVertical="md" insetHorizontal={isPortrait ? 'md' : 'no'}>
+      <Box>
         {isHome ? <HeaderContentForHome /> : <HeaderContent {...props} />}
       </Box>
     </View>
