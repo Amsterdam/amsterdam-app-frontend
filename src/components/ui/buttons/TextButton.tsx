bb80101f5f52b4bbc5321a08f1bb7df39b146670
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-12 16:19:11 +0200

    Simplify alignment of button icon with label
    Probably due to better alignment in the new font

--- src/components/ui/buttons/TextButton.tsx
+++ src/components/ui/buttons/TextButton.tsx
@@ -33,16 +33,10 @@ export const TextButton = ({
   const iconColor = useThemable(createIconColor({emphasis, isPressed}))
   const styles = useThemable(createStyles)
 
-  // The `top` style aims to vertically align the icon with the baseline of the text.
-  // As SVG isn’t text, and because React’s flexbox implementation differs from the
-  // CSS spec, I couldn’t find a better approach yet.
   const iconProps: SVGProps<any> = {
     width: 14 * fontScale,
     height: 14 * fontScale,
     fill: iconColor,
-    style: {
-      top: 3,
-    },
   }
 
   return (
@@ -54,7 +48,7 @@ export const TextButton = ({
       onPressOut={() => setIsPressed(false)}
       style={[styles.button, direction && styles.row]}
       {...otherProps}>
-      <Row gutter="xs">
+      <Row gutter="xs" valign="center">
         {direction === 'backward' && <ChevronLeft {...iconProps} />}
         {direction === 'down' && <ChevronDown {...iconProps} />}
         {direction === 'forward' && <ChevronRight {...iconProps} />}
