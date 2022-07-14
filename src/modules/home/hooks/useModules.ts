dd1927e69d88bab98d5c68c90e8b6d30e2bbf1f4
Rogier <rogier.voogt@amsterdam.nl>
2022-07-14 15:45:02 +0200

    Fix persistent issue of modules not displayed

--- src/modules/home/hooks/useModules.ts
+++ src/modules/home/hooks/useModules.ts
@@ -1,26 +1,52 @@
+import {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {useGetModulesForAppQuery} from '@/modules/home/services'
 import {selectDisabledModules} from '@/modules/home/store'
 import {clientModules} from '@/modules/index'
-import {Module} from '@/modules/types'
+import {ModuleServerConfig} from '@/modules/types'
 import {mergeModulesConfig} from '@/utils'
 
+const postProcessModules = (
+  disabledModulesBySlug: string[],
+  modulesFromApi?: ModuleServerConfig[],
+) => {
+  const modules = mergeModulesConfig(clientModules, modulesFromApi)
+  const selectedModules = modules.filter(
+    module => !disabledModulesBySlug?.includes(module.slug),
+  )
+  return {
+    modules,
+    selectedModules,
+    selectedModulesBySlug: selectedModules.map(module => module.slug),
+  }
+}
+
 export const useModules = () => {
-  const {data: modulesFromApi, isLoading: isLoadingModules} =
-    useGetModulesForAppQuery()
+  const {
+    data: modulesFromApi,
+    isLoading,
+    isSuccess,
+  } = useGetModulesForAppQuery()
+
   const {disabledModules: disabledModulesBySlug} = useSelector(
     selectDisabledModules,
   )
-  const modules = mergeModulesConfig(clientModules, modulesFromApi)
-  const selectedModules: Module[] = modules.filter(
-    module => !disabledModulesBySlug?.includes(module.slug),
+
+  const [modulesData, setModulesData] = useState(
+    postProcessModules(disabledModulesBySlug, modulesFromApi),
   )
-  const selectedModulesBySlug = selectedModules.map(module => module.slug)
+
+  useEffect(() => {
+    setModulesData(postProcessModules(disabledModulesBySlug, modulesFromApi))
+  }, [disabledModulesBySlug, modulesFromApi])
+
+  // this prevents the situation where an empty modules array is returned when isLoading is false
+  // we should fix this later by handling the async nature of requests in a better way
+  const isLoadingModules =
+    isLoading || (isSuccess && modulesData.modules.length === 0)
 
   return {
     isLoadingModules,
-    modules,
-    selectedModules,
-    selectedModulesBySlug,
+    ...modulesData,
   }
 }
