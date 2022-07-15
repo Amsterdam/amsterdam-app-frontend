47bdc1a63fcae3f4885e5f15491d33a2c7f0a0db
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-15 13:21:45 +0200

    Wrap address privacy info in a screen component
    Which compensates for a notch with a top inset

--- src/modules/address/screens/AddressPrivacyInfo.screen.tsx
+++ src/modules/address/screens/AddressPrivacyInfo.screen.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import {View} from 'react-native'
 import {Box, Text, Title} from '@/components/ui'
 import {Button, IconButton} from '@/components/ui/buttons'
-import {Column, Gutter, Row, ScrollView} from '@/components/ui/layout'
+import {Column, Gutter, Row, Screen, ScrollView} from '@/components/ui/layout'
 import {Icon} from '@/components/ui/media'
 import {AddressRouteName, AddressStackParams} from '@/modules/address/routes'
 import {useTheme} from '@/themes'
@@ -20,41 +20,43 @@ export const AddressPrivacyInfoScreen = ({
   const {color} = useTheme()
 
   return (
-    <ScrollView grow>
-      <Box grow>
-        <Row align="end">
-          <IconButton
-            accessibilityLabel="Sluiten"
-            icon={
-              <Icon size={20}>
-                <Close fill={color.text.default} />
-              </Icon>
-            }
-            onPress={navigation.goBack}
-          />
-        </Row>
-        <Column align="between" gutter="xl">
-          <View>
-            <Title margin text="Veilig omgaan met uw adres" />
-            <Text margin intro>
-              Wij slaan uw adres niet op. Het staat alleen in de app op uw
-              telefoon. We kunnen uw adres dus aan niemand geven.
-            </Text>
-            <Text margin>
-              Wij gebruiken uw adres alleen om u informatie uit uw buurt te
-              laten zien. De informatie gaat over wegwerkzaamheden,
-              bouwprojecten, het dichtstbijzijnde Stadsloket en informatie over
-              afval.
-            </Text>
-            <Text margin>
-              U kunt uw adres wijzigen of verwijderen. Ga dan naar uw
-              instellingen.
-            </Text>
-          </View>
-          <Button label="Ik begrijp het" onPress={navigation.goBack} />
-        </Column>
-      </Box>
-      <Gutter height="md" />
-    </ScrollView>
+    <Screen handleTopNotch>
+      <ScrollView grow>
+        <Box grow>
+          <Row align="end">
+            <IconButton
+              accessibilityLabel="Sluiten"
+              icon={
+                <Icon size={20}>
+                  <Close fill={color.text.default} />
+                </Icon>
+              }
+              onPress={navigation.goBack}
+            />
+          </Row>
+          <Column align="between" gutter="xl">
+            <View>
+              <Title margin text="Veilig omgaan met uw adres" />
+              <Text margin intro>
+                Wij slaan uw adres niet op. Het staat alleen in de app op uw
+                telefoon. We kunnen uw adres dus aan niemand geven.
+              </Text>
+              <Text margin>
+                Wij gebruiken uw adres alleen om u informatie uit uw buurt te
+                laten zien. De informatie gaat over wegwerkzaamheden,
+                bouwprojecten, het dichtstbijzijnde Stadsloket en informatie
+                over afval.
+              </Text>
+              <Text margin>
+                U kunt uw adres wijzigen of verwijderen. Ga dan naar uw
+                instellingen.
+              </Text>
+            </View>
+            <Button label="Ik begrijp het" onPress={navigation.goBack} />
+          </Column>
+        </Box>
+        <Gutter height="md" />
+      </ScrollView>
+    </Screen>
   )
 }
