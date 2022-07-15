84fb605a4b1c84f403542d8ebc647c1dd7583713
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-15 13:31:45 +0200

    Apply new typography to ‘address privacy info’ screen

--- src/modules/address/screens/AddressPrivacyInfo.screen.tsx
+++ src/modules/address/screens/AddressPrivacyInfo.screen.tsx
@@ -2,10 +2,11 @@ import Close from '@amsterdam/asc-assets/static/icons/Close.svg'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
 import {View} from 'react-native'
-import {Box, Text, Title} from '@/components/ui'
+import {Box} from '@/components/ui'
 import {Button, IconButton} from '@/components/ui/buttons'
-import {Column, Gutter, Row, Screen, ScrollView} from '@/components/ui/layout'
+import {Column, Row, Screen, ScrollView} from '@/components/ui/layout'
 import {Icon} from '@/components/ui/media'
+import {Paragraph, Title} from '@/components/ui/text'
 import {AddressRouteName, AddressStackParams} from '@/modules/address/routes'
 import {useTheme} from '@/themes'
 
@@ -17,46 +18,50 @@ export const AddressPrivacyInfoScreen = ({
     AddressRouteName.addressInfo
   >
 }) => {
-  const {color} = useTheme()
+  const {color, size} = useTheme()
 
   return (
     <Screen handleTopNotch>
-      <ScrollView grow>
-        <Box grow>
+      <ScrollView>
+        <Box inset="lg">
           <Row align="end">
             <IconButton
               accessibilityLabel="Sluiten"
+              hitSlop={size.spacing.md}
               icon={
-                <Icon size={20}>
+                <Icon size={24}>
                   <Close fill={color.text.default} />
                 </Icon>
               }
               onPress={navigation.goBack}
             />
           </Row>
-          <Column align="between" gutter="xl">
-            <View>
-              <Title margin text="Veilig omgaan met uw adres" />
-              <Text margin intro>
+          <View>
+            <Box insetVertical="lg">
+              <Title text="Veilig omgaan met uw adres" />
+            </Box>
+            <Column gutter="md">
+              <Paragraph variant="intro">
                 Wij slaan uw adres niet op. Het staat alleen in de app op uw
                 telefoon. We kunnen uw adres dus aan niemand geven.
-              </Text>
-              <Text margin>
+              </Paragraph>
+              <Paragraph>
                 Wij gebruiken uw adres alleen om u informatie uit uw buurt te
                 laten zien. De informatie gaat over wegwerkzaamheden,
                 bouwprojecten, het dichtstbijzijnde Stadsloket en informatie
                 over afval.
-              </Text>
-              <Text margin>
+              </Paragraph>
+              <Paragraph>
                 U kunt uw adres wijzigen of verwijderen. Ga dan naar uw
                 instellingen.
-              </Text>
-            </View>
-            <Button label="Ik begrijp het" onPress={navigation.goBack} />
-          </Column>
+              </Paragraph>
+            </Column>
+          </View>
         </Box>
-        <Gutter height="md" />
       </ScrollView>
+      <Box inset="lg">
+        <Button label="Ik begrijp het" onPress={navigation.goBack} />
+      </Box>
     </Screen>
   )
 }
