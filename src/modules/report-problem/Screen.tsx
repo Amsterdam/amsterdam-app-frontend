d2597427f3a39a9e31ac404b27f875608f9b8b5a
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-17 00:51:27 +0200

    Wrap more

--- src/modules/report-problem/Screen.tsx
+++ src/modules/report-problem/Screen.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
-import {WebView} from '../../components/ui'
-import {useEnvironment} from '../../store'
-import {selectAddress} from '../address/addressSlice'
+import {WebView} from '@/components/ui'
+import {Screen} from '@/components/ui/layout'
+import {selectAddress} from '@/modules/address/addressSlice'
+import {useEnvironment} from '@/store'
 
 export const ReportProblemScreen = () => {
   const {primary: address} = useSelector(selectAddress)
 
   const environment = useEnvironment()
   return (
-    <WebView
-      url={`${environment.signalsBaseUrl}/incident/beschrijf`}
-      urlParams={{
-        lat: address?.centroid[1] ?? 0,
-        lng: address?.centroid[0] ?? 0,
-      }}
-    />
+    <Screen>
+      <WebView
+        url={`${environment.signalsBaseUrl}/incident/beschrijf`}
+        urlParams={{
+          lat: address?.centroid[1] ?? 0,
+          lng: address?.centroid[0] ?? 0,
+        }}
+      />
+    </Screen>
   )
 }
--- src/modules/user/screens/User.screen.tsx
+++ src/modules/user/screens/User.screen.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import {VersionInfo} from '../../../components/features'
-import {Alert, Box} from '../../../components/ui'
-import {Column, Screen, ScrollView} from '../../../components/ui/layout'
-import {Address} from '../../address/components'
-import {ProjectManagerUserSection} from '../components'
+import {VersionInfo} from '@/components/features'
+import {Alert, Box} from '@/components/ui'
+import {Column, Gutter, Screen, ScrollView} from '@/components/ui/layout'
+import {Address} from '@/modules/address/components'
+import {ProjectManagerUserSection} from '@/modules/user/components'
 
 export const UserScreen = () => (
   <Screen>
-    <ScrollView grow>
+    <ScrollView>
       <Alert />
-      <Column gutter="md">
+      <Column gutter="lg">
         <Box>
           <Address />
         </Box>
         <ProjectManagerUserSection />
+        <Box insetHorizontal="md">
+          <VersionInfo />
+        </Box>
       </Column>
+      <Gutter height="xl" />
     </ScrollView>
-    <Box insetHorizontal="md">
-      <VersionInfo />
-    </Box>
   </Screen>
 )
