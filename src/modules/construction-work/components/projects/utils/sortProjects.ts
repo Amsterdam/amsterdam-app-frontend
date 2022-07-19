20be4da413a571327187db3b39f9fe3d512e44ef
Frank Filius <f.filius@amsterdam.nl>
2022-07-19 14:35:04 +0200

    Upgrade and improve sort function

--- src/modules/construction-work/components/projects/utils/sortProjects.ts
+++ src/modules/construction-work/components/projects/utils/sortProjects.ts
@@ -1,13 +1,33 @@
-import dayjs from 'dayjs'
 import {ProjectsItem} from '@/modules/construction-work/types'
 
-export const sortProjects = (projects: ProjectsItem[]) =>
-  [...projects].sort(
-    (p, q) =>
-      (Number(q.followed) - Number(p.followed)) * 10 +
-      (dayjs(p.recent_articles?.[0]?.publication_date).isAfter(
-        dayjs(q.recent_articles?.[0]?.publication_date),
-      )
-        ? -1
-        : 1),
-  )
+export const getFirstUnreadArticlePublicationDate =
+  (readArticleIds: string[]) => (project: ProjectsItem) =>
+    project.recent_articles?.find(a => !readArticleIds.includes(a.identifier))
+      ?.publication_date ?? '0'
+
+export const compareRecentArticleDates =
+  (readArticleIds: string[]) => (p: ProjectsItem, q: ProjectsItem) => {
+    const getFirstUnreadPublicationDate =
+      getFirstUnreadArticlePublicationDate(readArticleIds)
+    return getFirstUnreadPublicationDate(p) > getFirstUnreadPublicationDate(q)
+      ? -1
+      : 1
+  }
+
+export const sortProjects = (
+  projects: ProjectsItem[],
+  readArticleIds: string[],
+) => {
+  const followedProjects: ProjectsItem[] = []
+  const unfollowedProjects: ProjectsItem[] = []
+
+  projects.forEach(p => {
+    if (p.followed) {
+      followedProjects.push(p)
+      return
+    }
+    unfollowedProjects.push(p)
+  })
+  followedProjects.sort(compareRecentArticleDates(readArticleIds))
+  return [...followedProjects, ...unfollowedProjects]
+}
