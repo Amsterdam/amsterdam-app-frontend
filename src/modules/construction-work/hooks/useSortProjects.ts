9977921456d93e147def4e6adca83f4d8c28e20a
Frank Filius <f.filius@amsterdam.nl>
2022-07-19 16:50:27 +0200

    Refactor hook

--- src/modules/construction-work/hooks/useSortProjects.ts
+++ src/modules/construction-work/hooks/useSortProjects.ts
@@ -1,15 +1,13 @@
 import {useSelector} from 'react-redux'
 import {selectConstructionWorkReadArticles} from '../construction-work.slice'
-import {sortProjects as sortProjectsUtil} from '@/modules/construction-work/components/projects'
+import {sortProjects} from '@/modules/construction-work/components/projects'
 import {ProjectsItem} from '@/modules/construction-work/types'
 
-export const useSortProjects = () => {
+export const useSortProjects = (projects: ProjectsItem[]) => {
   const readArticles = useSelector(selectConstructionWorkReadArticles)
 
-  const sortProjects = (projects: ProjectsItem[]) =>
-    sortProjectsUtil(
-      projects,
-      readArticles.map(r => r.id),
-    )
-  return {sortProjects}
+  return sortProjects(
+    projects,
+    readArticles.map(r => r.id),
+  )
 }
