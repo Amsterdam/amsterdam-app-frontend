df3d0007d8b97cfe709ae9c608fe42536ed1e720
Frank Filius <f.filius@amsterdam.nl>
2022-07-20 14:40:49 +0200

    Keep only relevant props in test projects

--- src/modules/construction-work/components/projects/utils/sortProjects.test.ts
+++ src/modules/construction-work/components/projects/utils/sortProjects.test.ts
@@ -7,33 +7,18 @@ import {ProjectsItem} from '@/modules/construction-work/types'
 
 const project1 = {
   identifier: '15510fbb191ec27dc0554ed2b19fe39d',
-  images: [],
-  subtitle: 'Groot onderhoud',
-  title: 'Waalstraat-Lekstraat',
   followed: false,
-  meter: 450,
-  strides: 608,
   recent_articles: [],
 } as unknown as ProjectsItem
 
 const project2 = {
   identifier: '24b3251d4818b5927e6dd295940762c7',
-  images: [],
-  subtitle: 'Nieuwe inrichting',
-  title: 'Niersstraat',
   followed: true,
-  meter: 489,
-  strides: 660,
 } as unknown as ProjectsItem
 
 const project3 = {
   identifier: 'f266860558f672da156a38eb2bc3f034',
-  images: [],
-  subtitle: 'Verkeersveiliger en meer ruimte',
-  title: 'Ferdinand Bolstraat Zuid',
   followed: true,
-  meter: 597,
-  strides: 806,
   recent_articles: [
     {
       identifier: 'ba190881ed7ae10542687d603e9adbbf',
@@ -44,12 +29,7 @@ const project3 = {
 
 const project4 = {
   identifier: '17a76fcfd4bdcac89fa31e065bb0dffe',
-  images: [],
-  subtitle: 'Groot onderhoud',
-  title: 'Wielingenstraat',
   followed: true,
-  meter: 708,
-  strides: 956,
   recent_articles: [
     {
       identifier: '502a4f80eba13870abfbc531be928679',
@@ -60,12 +40,7 @@ const project4 = {
 
 const project5 = {
   identifier: '17a76fcfd4bdcac89fa31e065bb0dfpo',
-  images: [],
-  subtitle: 'Groot onderhoud',
-  title: 'Wielingenstraat',
   followed: false,
-  meter: 708,
-  strides: 956,
   recent_articles: [
     {
       identifier: '502a4f80eba13870abfbc531be928679',
@@ -76,12 +51,7 @@ const project5 = {
 
 const project6 = {
   identifier: '17a76fcfd4bdcac89fa31e065bb0dfza',
-  images: [],
-  subtitle: 'Groot onderhoud',
-  title: 'Wielingenstraat',
   followed: true,
-  meter: 708,
-  strides: 956,
   recent_articles: [
     {
       identifier: '502a4f80eba13870abfbc531be928688',
