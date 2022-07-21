5fa4493fc132f7c6639ec55f8192c47d8479421e
Frank Filius <f.filius@amsterdam.nl>
2022-07-21 13:23:36 +0200

    Update comp to make it work

--- src/modules/construction-work-editor/components/AuthorizedProjects.tsx
+++ src/modules/construction-work-editor/components/AuthorizedProjects.tsx
@@ -4,12 +4,11 @@ import React, {useContext} from 'react'
 import {StyleSheet} from 'react-native'
 import {FlatGrid} from 'react-native-super-grid'
 import {RootStackParams} from '@/app/navigation'
-import {Attention, Box, PleaseWait} from '@/components/ui'
+import {Box} from '@/components/ui'
 import {EmptyMessage} from '@/components/ui/feedback'
-import {Paragraph} from '@/components/ui/text'
 import {module as constructionWorkModule} from '@/modules/construction-work'
+import {module as constructionWorkEditorModule} from '@/modules/construction-work-editor'
 import {authorizedProjectsMock} from '@/modules/construction-work-editor/authorized-projects.mock'
-import {useProjectManager} from '@/modules/construction-work-editor/hooks'
 import {ProjectCard} from '@/modules/construction-work/components/shared'
 import {ConstructionWorkRouteName} from '@/modules/construction-work/routes'
 import {ProjectsItem} from '@/modules/construction-work/types'
@@ -18,8 +17,13 @@ import {useEnvironment} from '@/store'
 import {useTheme} from '@/themes'
 import {mapImageSources} from '@/utils'
 
+type Navigation = StackNavigationProp<
+  RootStackParams,
+  typeof constructionWorkEditorModule.slug
+>
+
 type ListItemProps = {
-  navigation: StackNavigationProp<RootStackParams, ConstructionWorkRouteName>
+  navigation: Navigation
   project: ProjectsItem
 }
 
@@ -50,51 +54,17 @@ const ListEmptyMessage = () => (
 )
 
 export const AuthorizedProjects = () => {
-  const {authorizedProjects, isLoadingProjects, projectManager} =
-    useProjectManager()
-  const navigation =
-    useNavigation<
-      StackNavigationProp<RootStackParams, ConstructionWorkRouteName>
-    >()
+  const navigation = useNavigation<Navigation>()
 
   const {fontScale} = useContext(DeviceContext)
   const {size} = useTheme()
   const itemDimension = 16 * size.spacing.md * Math.max(fontScale, 1)
 
-  console.log(projectManager)
-  // if (!projectManager) {
-  if (false) {
-    return (
-      <Box>
-        <Attention warning>
-          <Paragraph>
-            U bent niet gemachtigd om pushberichten te sturen.
-          </Paragraph>
-        </Attention>
-      </Box>
-    )
-  }
-
-  if (isLoadingProjects) {
-    return <PleaseWait />
-  }
-
-  // if (!authorizedProjects) {
-  if (false) {
-    return (
-      <Box>
-        <Attention warning>
-          <Paragraph>Geen projecten gevonden.</Paragraph>
-        </Attention>
-      </Box>
-    )
-  }
-
   const mockProjects = authorizedProjectsMock.map(p => p as ProjectsItem)
 
   return (
     <FlatGrid
-      data={authorizedProjects ?? mockProjects}
+      data={mockProjects}
       itemContainerStyle={styles.itemContainer}
       itemDimension={itemDimension}
       keyboardDismissMode="on-drag"
