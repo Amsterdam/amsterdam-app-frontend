c6bd556c872b6ec491b7f4a108049b74502b210a
Frank Filius <f.filius@amsterdam.nl>
2022-07-21 13:22:46 +0200

    Remove commented out code

--- src/modules/construction-work-editor/screens/AuthorizedProjects.screen.tsx
+++ src/modules/construction-work-editor/screens/AuthorizedProjects.screen.tsx
@@ -2,8 +2,5 @@ import React from 'react'
 import {AuthorizedProjects} from '@/modules/construction-work-editor/components'
 
 export const AuthorizedProjectsScreen = () => {
-  // const navigation =
-  //   useNavigation<StackNavigationProp<RootStackParams, TemplateRouteName>>()
-
   return <AuthorizedProjects />
 }
