f5d1028c5ecff10d45cdf8fc12a36b8a29a1f2bd
Frank Filius <f.filius@amsterdam.nl>
2022-07-21 16:41:55 +0200

    Add welcome message to screen

--- src/modules/construction-work-editor/screens/AuthorizedProjects.screen.tsx
+++ src/modules/construction-work-editor/screens/AuthorizedProjects.screen.tsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {
+  selectProjectManagerHasSeenWelcomeMessage,
+  setHasSeenWelcomeMessage,
+} from '../slice'
+import {Alert} from '@/components/ui'
+import {Screen} from '@/components/ui/layout'
 import {AuthorizedProjects} from '@/modules/construction-work-editor/components'
+import {setAlert} from '@/store'
 
 export const AuthorizedProjectsScreen = () => {
-  return <AuthorizedProjects />
+  const dispatch = useDispatch()
+  const hasSeenWelcomeMessage = useSelector(
+    selectProjectManagerHasSeenWelcomeMessage,
+  )
+
+  useEffect(() => {
+    !hasSeenWelcomeMessage &&
+      dispatch(
+        setAlert({
+          content: {
+            text: 'Gelukt, de app herkent je nu als omgevingsmanager voor onderstaande projecten. Klik op het project waarvoor je een bericht wilt plaatsen, om een bericht te maken.',
+          },
+          variant: 'information',
+          isVisible: true,
+        }),
+      )
+    dispatch(setHasSeenWelcomeMessage())
+  }, [dispatch, hasSeenWelcomeMessage])
+
+  return (
+    <Screen scroll={false} stickyHeader={<Alert />}>
+      <AuthorizedProjects />
+    </Screen>
+  )
 }
