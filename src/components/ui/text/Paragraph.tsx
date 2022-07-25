f4e3437d1a54d07208c2b5f4c0ac9869bb6e8677
Frank Filius <f.filius@amsterdam.nl>
2022-07-25 21:44:20 +0200

    Use Paragraph component to show warning

--- src/components/ui/text/Paragraph.tsx
+++ src/components/ui/text/Paragraph.tsx
@@ -6,30 +6,38 @@ import {ParagraphVariants} from '@/themes/tokens'
 type Props = {
   children: ReactNode
   variant?: ParagraphVariants
+  warning?: boolean
 } & Omit<TextProps, 'style'>
 
 export const Paragraph = ({
   children,
   variant = 'body',
+  warning = false,
   ...otherProps
 }: Props) => {
-  const createdStyles = useMemo(() => createStyles({variant}), [variant])
+  const createdStyles = useMemo(
+    () => createStyles({variant, warning}),
+    [variant, warning],
+  )
   const styles = useThemable(createdStyles)
 
   return (
-    <Text style={styles.text} {...otherProps}>
+    <Text
+      accessibilityRole={warning ? 'alert' : 'none'}
+      style={styles.text}
+      {...otherProps}>
       {children}
     </Text>
   )
 }
 
 const createStyles =
-  ({variant}: Required<Pick<Props, 'variant'>>) =>
+  ({variant, warning}: Required<Pick<Props, 'variant' | 'warning'>>) =>
   ({color, text}: Theme) =>
     StyleSheet.create({
       text: {
         flexShrink: 1,
-        color: color.text.default,
+        color: warning ? color.text.warning : color.text.default,
         fontFamily: text.fontWeight.regular,
         fontSize: text.fontSize[variant],
         lineHeight: text.lineHeight[variant] * text.fontSize[variant],
