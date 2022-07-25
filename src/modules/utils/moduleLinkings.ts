0e41c248efb42242f29ab4c0422424eb17b95909
Rik Scheffer <rik@oberon.nl>
2022-07-25 14:30:52 +0200

    fix deeplinks

--- src/modules/utils/moduleLinkings.ts
+++ src/modules/utils/moduleLinkings.ts
@@ -1,6 +1,12 @@
+import {PathConfigMap} from '@react-navigation/core'
+import {RootStackParams} from '@/app/navigation'
 import {clientModules} from '@/modules'
 
-export const moduleLinkings = clientModules.reduce(
-  (linkings, module) => ({...linkings, ...module.linking}),
-  {},
-)
+export const moduleLinkings: PathConfigMap<RootStackParams> =
+  clientModules.reduce(
+    (linkings, module) => ({
+      ...linkings,
+      [module.slug]: {screens: module.linking},
+    }),
+    {},
+  )
