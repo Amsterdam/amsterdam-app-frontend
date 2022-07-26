1a1c365d28c127b156be78bfc2593822365da5c3
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-26 10:41:35 +0200

    Replace incorrect width and height with `grow` prop

--- src/components/ui/layout/Center.tsx
+++ src/components/ui/layout/Center.tsx
@@ -1,19 +1,21 @@
 import React, {ReactNode} from 'react'
 import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native'
+import {layoutStyles} from '@/styles'
 
 type Props = {
   children: ReactNode
+  grow?: boolean
   style?: StyleProp<ViewStyle>
 }
 
-export const Center = ({children, style}: Props) => (
-  <View style={[styles.center, style]}>{children}</View>
+export const Center = ({children, grow, style}: Props) => (
+  <View style={[styles.center, grow && layoutStyles.grow, style]}>
+    {children}
+  </View>
 )
 
 const styles = StyleSheet.create({
   center: {
-    width: '100%',
-    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
