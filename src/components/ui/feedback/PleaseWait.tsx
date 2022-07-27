de441242b29e6eccbdb8203b12e03ff3d15c64aa
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-07-27 10:08:03 +0200

    Remove `style` prop from `Center`

--- src/components/ui/feedback/PleaseWait.tsx
+++ src/components/ui/feedback/PleaseWait.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import {Spinner} from '@/components/ui'
 import {Center} from '@/components/ui/layout'
-import {layoutStyles} from '@/styles'
 
 type Props = {
   grow?: boolean
 }
 
 export const PleaseWait = ({grow}: Props) => (
-  <Center style={!!grow && layoutStyles.grow}>
+  <Center grow={grow}>
     <Spinner />
   </Center>
 )
--- src/components/ui/layout/Center.tsx
+++ src/components/ui/layout/Center.tsx
@@ -1,17 +1,14 @@
 import React, {ReactNode} from 'react'
-import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import {layoutStyles} from '@/styles'
 
 type Props = {
   children: ReactNode
   grow?: boolean
-  style?: StyleProp<ViewStyle>
 }
 
-export const Center = ({children, grow, style}: Props) => (
-  <View style={[styles.center, grow && layoutStyles.grow, style]}>
-    {children}
-  </View>
+export const Center = ({children, grow}: Props) => (
+  <View style={[styles.center, grow && layoutStyles.grow]}>{children}</View>
 )
 
 const styles = StyleSheet.create({
