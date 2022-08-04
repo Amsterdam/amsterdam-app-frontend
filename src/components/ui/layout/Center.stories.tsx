5943aeda17dbe04de2625dab8b9f9c7f161aba46
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-08-04 12:26:22 +0200

    Remove unnecessary `grow` prop from `Center` and simplify story

--- src/components/ui/layout/Center.stories.tsx
+++ src/components/ui/layout/Center.stories.tsx
@@ -1,23 +1,14 @@
 import {ComponentMeta, ComponentStoryObj} from '@storybook/react'
 import React from 'react'
-import {Canvas} from '@/../.storybook/components'
 import {Center} from '@/components/ui/layout'
 import {Phrase} from '@/components/ui/text'
 
 export default {
   component: Center,
-  decorators: [
-    Story => (
-      <Canvas height="256px" highlight maxWidth="512px">
-        {Story()}
-      </Canvas>
-    ),
-  ],
 } as ComponentMeta<typeof Center>
 
 export const Default: ComponentStoryObj<typeof Center> = {
   args: {
-    grow: true,
     children: <Phrase>Ik sta in het midden</Phrase>,
   },
 }
--- src/components/ui/layout/Center.tsx
+++ src/components/ui/layout/Center.tsx
@@ -1,14 +1,12 @@
 import React, {ReactNode} from 'react'
 import {StyleSheet, View} from 'react-native'
-import {layoutStyles} from '@/styles'
 
 type Props = {
   children: ReactNode
-  grow?: boolean
 }
 
-export const Center = ({children, grow}: Props) => (
-  <View style={[styles.center, grow && layoutStyles.grow]}>{children}</View>
+export const Center = ({children}: Props) => (
+  <View style={styles.center}>{children}</View>
 )
 
 const styles = StyleSheet.create({
