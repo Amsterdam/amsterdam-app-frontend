d5c9d5113e987965f427ed656f33a03ac2dbcd3b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-08-04 12:39:11 +0200

    Revert "Remove unnecessary `grow` prop from `Center` and simplify story"
    This reverts commit 5943aeda17dbe04de2625dab8b9f9c7f161aba46.

--- src/components/ui/layout/Center.tsx
+++ src/components/ui/layout/Center.tsx
@@ -1,12 +1,14 @@
 import React, {ReactNode} from 'react'
 import {StyleSheet, View} from 'react-native'
+import {layoutStyles} from '@/styles'
 
 type Props = {
   children: ReactNode
+  grow?: boolean
 }
 
-export const Center = ({children}: Props) => (
-  <View style={styles.center}>{children}</View>
+export const Center = ({children, grow}: Props) => (
+  <View style={[styles.center, grow && layoutStyles.grow]}>{children}</View>
 )
 
 const styles = StyleSheet.create({
