0a23b93f9ff37b64dcdcda73a623275998979038
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-08-05 12:07:31 +0200

    Remove close button

--- src/modules/construction-work-editor/screens/WritingGuide.screen.tsx
+++ src/modules/construction-work-editor/screens/WritingGuide.screen.tsx
@@ -1,16 +1,11 @@
-import Close from '@amsterdam/asc-assets/static/icons/Close.svg'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {Box} from '@/components/ui'
-import {IconButton} from '@/components/ui/buttons'
-import {Row, Screen} from '@/components/ui/layout'
-import {Icon} from '@/components/ui/media'
+import {Screen} from '@/components/ui/layout'
 import {WritingGuide} from '@/modules/construction-work-editor/components'
 import {
   CreateNotificationRouteName,
   CreateNotificationStackParams,
 } from '@/modules/construction-work/screens/create-notification/routes'
-import {useTheme} from '@/themes'
 
 type Props = {
   navigation: StackNavigationProp<
@@ -19,27 +14,8 @@ type Props = {
   >
 }
 
-export const WritingGuideScreen = ({navigation}: Props) => {
-  const {color} = useTheme()
-
-  return (
-    <Screen
-      stickyHeader={
-        <Row align="end">
-          <Box>
-            <IconButton
-              accessibilityLabel="Sluiten"
-              icon={
-                <Icon size={20}>
-                  <Close fill={color.text.default} />
-                </Icon>
-              }
-              onPress={navigation.goBack}
-            />
-          </Box>
-        </Row>
-      }>
-      <WritingGuide navigation={navigation} />
-    </Screen>
-  )
-}
+export const WritingGuideScreen = ({navigation}: Props) => (
+  <Screen>
+    <WritingGuide navigation={navigation} />
+  </Screen>
+)
