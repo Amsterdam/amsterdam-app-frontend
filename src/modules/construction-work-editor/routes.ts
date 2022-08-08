acb763767ccfac6dec13f64f33d2ac4714769c9d
Frank Filius <f.filius@amsterdam.nl>
2022-08-08 14:09:47 +0200

    alphabetize listst

--- src/modules/construction-work-editor/routes.ts
+++ src/modules/construction-work-editor/routes.ts
@@ -1,18 +1,18 @@
 export enum ConstructionWorkEditorRouteName {
+  addMainImageToMessage = 'addMainImageToMessage',
   authorizedProjects = 'authorizedProjects',
   confirmMessage = 'confirmMessage',
   createMessage = 'createMessage',
-  addMainImageToMessage = 'addMainImageToMessage',
 }
 
 export type ConstructionWorkEditorStackParams = {
+  [ConstructionWorkEditorRouteName.addMainImageToMessage]: undefined
   [ConstructionWorkEditorRouteName.authorizedProjects]: {
     id?: string
   }
+  [ConstructionWorkEditorRouteName.confirmMessage]: undefined
   [ConstructionWorkEditorRouteName.createMessage]: {
     projectId: string
     projectTitle: string
   }
-  [ConstructionWorkEditorRouteName.addMainImageToMessage]: undefined
-  [ConstructionWorkEditorRouteName.confirmMessage]: undefined
 }
