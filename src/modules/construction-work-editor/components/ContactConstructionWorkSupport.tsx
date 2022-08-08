1d0f9bb988e14e00dbd5a8ed0fa0640e798aaa9e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-08-08 17:35:34 +0200

    Revert "Add temporary button to writing guide screen"
    This reverts commit a08c5a7ffb1e927a83af7079a7015ad4f9a11516.

--- src/modules/construction-work-editor/components/ContactConstructionWorkSupport.tsx
+++ src/modules/construction-work-editor/components/ContactConstructionWorkSupport.tsx
@@ -1,40 +1,19 @@
-import {useNavigation} from '@react-navigation/native'
-import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {RootStackParams} from '@/app/navigation'
 import {Box} from '@/components/ui'
 import {Button} from '@/components/ui/buttons'
 import {Column} from '@/components/ui/layout'
 import {Paragraph} from '@/components/ui/text'
-import {module as constructionWorkEditorModule} from '@/modules/construction-work-editor'
-import {ConstructionWorkEditorRouteName} from '@/modules/construction-work-editor/routes'
 import {openMailUrl} from '@/utils'
 
-export const ContactConstructionWorkSupport = () => {
-  const navigation =
-    useNavigation<
-      StackNavigationProp<
-        RootStackParams,
-        typeof constructionWorkEditorModule.slug
-      >
-    >()
-
-  return (
-    <Box insetHorizontal="md">
-      <Column gutter="sm">
-        <Paragraph>Ontbreekt er een bouwproject?</Paragraph>
-        <Button
-          label="Neem contact op met de redactie"
-          onPress={() => openMailUrl('redactieprojecten@amsterdam.nl')}
-          variant="secondary"
-        />
-        <Button
-          label="Schrijftips"
-          onPress={() =>
-            navigation.navigate(ConstructionWorkEditorRouteName.writingGuide)
-          }
-        />
-      </Column>
-    </Box>
-  )
-}
+export const ContactConstructionWorkSupport = () => (
+  <Box insetHorizontal="md">
+    <Column gutter="sm">
+      <Paragraph>Ontbreekt er een bouwproject?</Paragraph>
+      <Button
+        label="Neem contact op met de redactie"
+        onPress={() => openMailUrl('redactieprojecten@amsterdam.nl')}
+        variant="secondary"
+      />
+    </Column>
+  </Box>
+)
