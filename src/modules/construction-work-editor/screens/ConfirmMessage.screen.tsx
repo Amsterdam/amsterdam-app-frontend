1abfc09c11ee3af80a53ec7434717f7b7f2e2aad
Frank Filius <f.filius@amsterdam.nl>
2022-08-11 22:10:40 +0200

    Handle submit

--- src/modules/construction-work-editor/screens/ConfirmMessage.screen.tsx
+++ src/modules/construction-work-editor/screens/ConfirmMessage.screen.tsx
@@ -1,15 +1,16 @@
 import {StackNavigationProp} from '@react-navigation/stack'
-import React, {useLayoutEffect} from 'react'
+import React, {useLayoutEffect, useState} from 'react'
 import {View, StyleSheet} from 'react-native'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {RootStackParams} from '@/app/navigation'
 import HeroImage from '@/assets/images/project-warning-hero.svg'
-import {Box} from '@/components/ui'
+import {Alert, Box} from '@/components/ui'
 import {Button, NavigationButton} from '@/components/ui/buttons'
 import {Row, Column, Screen} from '@/components/ui/layout'
 import {Image} from '@/components/ui/media'
 import {Paragraph, Title} from '@/components/ui/text'
 import {
+  clearDraft,
   selectCurrentProjectId,
   selectMainImage,
   selectMainImageDescription,
@@ -17,24 +18,38 @@ import {
   selectProject,
 } from '@/modules/construction-work-editor/messageDraftSlice'
 import {ConstructionWorkEditorRouteName} from '@/modules/construction-work-editor/routes'
+import {
+  useAddProjectWarningImageMutation,
+  useAddProjectWarningMutation,
+} from '@/modules/construction-work-editor/services'
+import {selectConstructionWorkEditorId} from '@/modules/construction-work-editor/slice'
+import {setAlert} from '@/store'
 import {Theme, useThemable} from '@/themes'
+import {Variant} from '@/types'
 
 type Props = {
   navigation: StackNavigationProp<
     RootStackParams,
-    ConstructionWorkEditorRouteName.confirmMessage
+    ConstructionWorkEditorRouteName
   >
 }
 
 export const ConfirmMessageScreen = ({navigation}: Props) => {
+  const dispatch = useDispatch()
+  const [hasAlert, setHasAlert] = useState(true)
+  const constructionWorkEditorId = useSelector(selectConstructionWorkEditorId)
   const currentProjectId = useSelector(selectCurrentProjectId)
   const message = useSelector(selectMessage(currentProjectId))
-  const project = useSelector(selectProject(currentProjectId))
   const mainImage = useSelector(selectMainImage(currentProjectId))
   const mainImageDescription = useSelector(
     selectMainImageDescription(currentProjectId),
   )
+  const project = useSelector(selectProject(currentProjectId))
   const styles = useThemable(createStyles)
+
+  const [addWarning] = useAddProjectWarningMutation()
+
+  const [addProjectWarningImage] = useAddProjectWarningImageMutation()
 
   useLayoutEffect(() => {
     project &&
@@ -42,6 +57,45 @@ export const ConfirmMessageScreen = ({navigation}: Props) => {
         headerTitle: project.title,
       })
   }, [navigation, project])
+
+  const onSubmit = async () => {
+    if (!message) {
+      return
+    }
+    try {
+      const warningResponse = await addWarning(message).unwrap()
+
+      mainImage?.data &&
+        (await addProjectWarningImage({
+          project_warning_id: warningResponse.warning_identifier,
+          image: {
+            main: true,
+            description: mainImageDescription ?? 'Vervangende afbeelding',
+            data: mainImage.data,
+          },
+        }))
+
+      dispatch(clearDraft())
+
+      setHasAlert(false)
+
+      navigation.navigate(ConstructionWorkEditorRouteName.authorizedProjects, {
+        id: constructionWorkEditorId,
+        showSuccesfullySendMessageAlert: true,
+      })
+    } catch (error: unknown) {
+      dispatch(
+        setAlert({
+          content: {
+            title: 'Niet gelukt',
+            text: 'Het bericht opslaan is niet gelukt. Probeer het nog eens.',
+          },
+          variant: Variant.failure,
+          isVisible: true,
+        }),
+      )
+    }
+  }
 
   const image = (
     <Column gutter="sm">
@@ -59,10 +113,11 @@ export const ConfirmMessageScreen = ({navigation}: Props) => {
   return (
     <Screen
       scroll
+      stickyHeader={!!hasAlert && <Alert />}
       stickyFooter={
         <>
           <Box>
-            <Button label="Plaats bericht" />
+            <Button label="Plaats bericht" onPress={onSubmit} />
           </Box>
           <Row align="between" valign="center">
             <NavigationButton
