b50ab1da1121540987091268b1ff62243d70c34c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-08-23 12:04:58 +0200

    Use themable hook for icon props

--- src/modules/contact/components/ContactOptions.tsx
+++ src/modules/contact/components/ContactOptions.tsx
@@ -2,7 +2,7 @@ import Email from '@amsterdam/asc-assets/static/icons/Email.svg'
 import Phone from '@amsterdam/asc-assets/static/icons/Phone.svg'
 import {useNavigation} from '@react-navigation/core'
 import {StackNavigationProp} from '@react-navigation/stack'
-import React, {Key, ReactNode} from 'react'
+import React, {Key, ReactNode, SVGProps} from 'react'
 import {PressableProps} from 'react-native'
 import {PersonalLogin, Whatsapp} from '@/assets/icons'
 import {Box} from '@/components/ui'
@@ -11,7 +11,7 @@ import {Column} from '@/components/ui/layout'
 import {Paragraph, Title} from '@/components/ui/text'
 import {ContactOption} from '@/modules/contact/components'
 import {ContactRouteName, ContactStackParams} from '@/modules/contact/routes'
-import {useTheme} from '@/themes'
+import {Theme, useThemable} from '@/themes'
 import {accessibleText, openPhoneUrl} from '@/utils'
 import {openWebUrl} from '@/utils/openWebUrl'
 
@@ -26,8 +26,7 @@ type ContactOptionType = {
 }
 
 export const ContactOptions = () => {
-  const {color} = useTheme()
-  const iconColor = color.pressable.default.background
+  const iconProps = useThemable(createIconProps)
 
   const navigation =
     useNavigation<
@@ -42,7 +41,7 @@ export const ContactOptions = () => {
         onPress: () => navigation.navigate(ContactRouteName.contactForm),
       },
       contactProps: {
-        icon: <Email fill={iconColor} />,
+        icon: <Email {...iconProps} />,
         text: 'Reactie binnen 1 werkdag',
         title: 'Contactformulier',
       },
@@ -55,7 +54,7 @@ export const ContactOptions = () => {
       },
       contactProps: {
         accessibilityTitle: 'Bel veertien nul twintig',
-        icon: <Phone fill={iconColor} />,
+        icon: <Phone {...iconProps} />,
         text: 'Gemiddeld 5 minuten wachten',
         title: 'Bel 14 020',
       },
@@ -69,7 +68,7 @@ export const ContactOptions = () => {
       contactProps: {
         accessibilityTitle:
           'Whatsapp nul zes vierenveertig vierenveertig nul zes vijfenvijftig',
-        icon: <Whatsapp fill={iconColor} />,
+        icon: <Whatsapp {...iconProps} />,
         text: 'Reactie binnen 2 uur',
         title: 'WhatsApp 06 44 44 06 55',
       },
@@ -78,14 +77,11 @@ export const ContactOptions = () => {
       buttonProps: {
         accessibilityRole: 'link',
         key: 'mijn-amsterdam',
-        onPress: () =>
-          openWebUrl(
-            'https://mijn.amsterdam.nl/?pk_vid=1b2e97fe97357d351658843171e6a58f',
-          ),
+        onPress: () => openWebUrl('https://mijn.amsterdam.nl/'),
       },
       contactProps: {
-        accessibilityTitle: 'Ga naar mijn amsterdam',
-        icon: <PersonalLogin fill={iconColor} />,
+        accessibilityTitle: 'Ga naar Mijn Amsterdam',
+        icon: <PersonalLogin {...iconProps} />,
         text: 'Uw persoonlijke online pagina bij de gemeente Amsterdam.',
         title: 'Mijn Amsterdam',
       },
@@ -117,3 +113,7 @@ export const ContactOptions = () => {
     </Box>
   )
 }
+
+const createIconProps = ({color}: Theme): SVGProps<unknown> => ({
+  fill: color.pressable.default.background,
+})
