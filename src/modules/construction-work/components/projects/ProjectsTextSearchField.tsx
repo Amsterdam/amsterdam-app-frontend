d95579864905dfc18676310fe8bd3bc6bdf12928
Rik Scheffer <rik@oberon.nl>
2022-08-31 16:57:22 +0200

    Fix construction work search

--- src/modules/construction-work/components/projects/ProjectsTextSearchField.tsx
+++ src/modules/construction-work/components/projects/ProjectsTextSearchField.tsx
@@ -1,5 +1,5 @@
 import debounce from 'lodash.debounce'
-import React, {useEffect, useMemo} from 'react'
+import React, {useEffect, useCallback, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {SearchField} from '@/components/ui/forms'
 import {config} from '@/modules/construction-work/components/projects'
@@ -14,20 +14,37 @@ export const ProjectsTextSearchField = () => {
   const searchText = useSelector(selectConstructionWorkSearchText)
 
   /**
-   * Updates ‘search text’ if it has at least three characters, or it is empty.
+   * Keep track of the searchText for the SearchField component
+   */
+  const [searchTextValue, setSearchTextValue] = useState(searchText)
+
+  /**
+   * Updates ‘search text’ in the redux state if it has at least three characters, or it is empty.
+   * With a debounce function to limit the number of requests to the backend.
    * Sets ‘is searching’ to ‘true’ if search text is not empty.
    */
-  const dispatchSearchData = useMemo(
-    () =>
-      debounce((text: string) => {
-        if (text.length > 0 && text.length < 3) {
-          return
-        }
-
-        dispatch(setIsSearching(!!text))
-        dispatch(setSearchText(text))
-      }, config.searchBoxDebounceDuration),
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const storeSearchTextInState = useCallback(
+    debounce((text: string) => {
+      if (text.length > 0 && text.length < 3) {
+        return
+      }
+      dispatch(setIsSearching(!!text))
+      dispatch(setSearchText(text))
+    }, config.searchBoxDebounceDuration),
     [dispatch],
+  )
+
+  /**
+   * Updates ‘search text’ and ‘is searching’ to ‘true’ in the redux state.
+   * Sets the searchTextValue in the component state, which is used in the SearchField
+   */
+  const setSearchData = useCallback(
+    (text: string) => {
+      setSearchTextValue(text)
+      storeSearchTextInState(text)
+    },
+    [setSearchTextValue, storeSearchTextInState],
   )
 
   // Reset search data on start
@@ -39,10 +56,11 @@ export const ProjectsTextSearchField = () => {
   return (
     <SearchField
       blurOnSubmit={true}
-      onChangeText={dispatchSearchData}
+      onChangeText={setSearchData}
       onEndEditing={() => dispatch(setIsSearching(!!searchText))}
       onFocus={() => dispatch(setIsSearching(true))}
       placeholder="Zoek in werkzaamheden"
+      value={searchTextValue}
     />
   )
 }
