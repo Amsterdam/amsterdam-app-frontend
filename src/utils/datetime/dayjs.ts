b352f65690573c486028750173f5e01f0aeb2002
Rik Scheffer <rik@oberon.nl>
2022-09-05 18:29:51 +0200

    solve a problem that was caused by the timezone fix

--- src/utils/datetime/dayjs.ts
+++ src/utils/datetime/dayjs.ts
@@ -5,13 +5,15 @@ import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 import 'dayjs/locale/nl'
 
+const defaultTimezone = 'Europe/Amsterdam'
+
 dayjsFn.locale('nl')
 dayjsFn.extend(utc)
 dayjsFn.extend(timezone)
-dayjsFn.tz.setDefault('Europe/Amsterdam')
+dayjsFn.tz.setDefault(defaultTimezone)
 
 /**
  * This function replaces the default dayjs function to make sure the timezone uses the default that is set above and the locale is set properly
  */
-export const dayjs = (date?: ConfigType, format?: string) =>
-  dayjsFn.tz(date, format)
+export const dayjs = (date?: ConfigType) =>
+  dayjsFn(date).tz(defaultTimezone, true)
