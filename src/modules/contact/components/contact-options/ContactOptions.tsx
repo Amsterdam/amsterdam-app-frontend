a304429dc3af75e2f974866c5e9bb7a37064bf1e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-07 20:27:50 +0200

    Move config out of render function

--- src/modules/contact/components/contact-options/ContactOptions.tsx
+++ src/modules/contact/components/contact-options/ContactOptions.tsx
@@ -24,50 +24,53 @@ type ContactOption = {
   Pick<PressableProps, 'accessibilityLabel' | 'accessibilityRole' | 'onPress'>
 >
 
+const createContactOptions = (
+  iconProps: SVGProps<unknown>,
+): ContactOption[] => [
+  {
+    accessibilityRole: 'link',
+    icon: <Email {...iconProps} />,
+    key: 'email',
+    onPress: () =>
+      openWebUrl(
+        'https://formulieren.amsterdam.nl/tripleforms/DirectRegelen/formulier/nl-NL/evAmsterdam/Klachtenformulier.aspx',
+      ),
+    text: 'Reactie binnen 1 werkdag',
+    title: 'Contactformulier',
+  },
+  {
+    accessibilityLabel: 'Bel veertien nul twintig',
+    accessibilityRole: 'button',
+    icon: <Phone {...iconProps} />,
+    key: 'phone',
+    onPress: () => openPhoneUrl('+3114020'),
+    text: 'Gemiddeld 5 minuten wachten',
+    title: 'Bel 14 020',
+  },
+  {
+    accessibilityLabel:
+      'Whatsapp nul zes vierenveertig vierenveertig nul zes vijfenvijftig',
+    accessibilityRole: 'button',
+    icon: <Whatsapp {...iconProps} />,
+    key: 'whatsapp',
+    onPress: () => openWebUrl('https://wa.me/31644440655'),
+    text: 'Reactie binnen 2 uur',
+    title: `WhatsApp ${formatPhoneNumber('0644440655') ?? ''}`,
+  },
+  {
+    accessibilityLabel: 'Ga naar Mijn Amsterdam',
+    accessibilityRole: 'link',
+    icon: <PersonalLogin {...iconProps} />,
+    key: 'mijn-amsterdam',
+    onPress: () => openWebUrl('https://mijn.amsterdam.nl/'),
+    text: 'Uw persoonlijke online pagina bij de gemeente Amsterdam.',
+    title: 'Mijn Amsterdam',
+  },
+]
+
 export const ContactOptions = () => {
   const iconProps = useThemable(createIconProps)
-
-  const contactOptions: ContactOption[] = [
-    {
-      accessibilityRole: 'link',
-      icon: <Email {...iconProps} />,
-      key: 'email',
-      onPress: () =>
-        openWebUrl(
-          'https://formulieren.amsterdam.nl/tripleforms/DirectRegelen/formulier/nl-NL/evAmsterdam/Klachtenformulier.aspx',
-        ),
-      text: 'Reactie binnen 1 werkdag',
-      title: 'Contactformulier',
-    },
-    {
-      accessibilityRole: 'button',
-      accessibilityLabel: 'Bel veertien nul twintig',
-      icon: <Phone {...iconProps} />,
-      key: 'phone',
-      onPress: () => openPhoneUrl('+3114020'),
-      text: 'Gemiddeld 5 minuten wachten',
-      title: 'Bel 14 020',
-    },
-    {
-      accessibilityRole: 'button',
-      accessibilityLabel:
-        'Whatsapp nul zes vierenveertig vierenveertig nul zes vijfenvijftig',
-      icon: <Whatsapp {...iconProps} />,
-      key: 'whatsapp',
-      onPress: () => openWebUrl('https://wa.me/31644440655'),
-      text: 'Reactie binnen 2 uur',
-      title: `WhatsApp ${formatPhoneNumber('0644440655') ?? ''}`,
-    },
-    {
-      accessibilityRole: 'link',
-      accessibilityLabel: 'Ga naar Mijn Amsterdam',
-      icon: <PersonalLogin {...iconProps} />,
-      key: 'mijn-amsterdam',
-      onPress: () => openWebUrl('https://mijn.amsterdam.nl/'),
-      text: 'Uw persoonlijke online pagina bij de gemeente Amsterdam.',
-      title: 'Mijn Amsterdam',
-    },
-  ]
+  const contactOptions = createContactOptions(iconProps)
 
   return (
     <Box>
