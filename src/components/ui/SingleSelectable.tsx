86058829df09fcf405460d2004df0e449570f583
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-14 20:44:19 +0200

    Just use ViewProps

--- src/components/ui/SingleSelectable.tsx
+++ src/components/ui/SingleSelectable.tsx
@@ -1,14 +1,14 @@
-import React, {ReactNode} from 'react'
+import React from 'react'
 import {StyleSheet, View, ViewProps} from 'react-native'
-
-type Props = {
-  children: ReactNode
-} & ViewProps
 
 /**
  * Groups its children into a single selectable component for screen readers.
  */
-export const SingleSelectable = ({children, style, ...otherProps}: Props) => (
+export const SingleSelectable = ({
+  children,
+  style,
+  ...otherProps
+}: ViewProps) => (
   <View accessible style={[style, styles.singleSelectable]} {...otherProps}>
     {children}
   </View>
