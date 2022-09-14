23ea484aa5c7462d06352fbea8797ba55d633a16
Rik Scheffer <rik@oberon.nl>
2022-09-14 12:25:10 +0200

    add unit tests and move functions to seperate file

--- src/modules/contact/components/city-offices/WaitingTime.tsx
+++ src/modules/contact/components/city-offices/WaitingTime.tsx
@@ -1,5 +1,4 @@
 import React, {SVGProps} from 'react'
-import simplur from 'simplur'
 import {Clock, PersonalLogin} from '@/assets/icons'
 import {Box} from '@/components/ui'
 import {PleaseWait} from '@/components/ui/feedback'
@@ -7,22 +6,15 @@ import {Column, Row} from '@/components/ui/layout'
 import {Icon} from '@/components/ui/media'
 import {Paragraph} from '@/components/ui/text'
 import {useGetWaitingTimesQuery} from '@/modules/contact/service'
+import {
+  getQueuedPhrase,
+  getWaitingTimePhrase,
+} from '@/modules/contact/utils/getPhrase'
 import {Theme, useThemable} from '@/themes'
 
 type Props = {
   cityOfficeId: string
 }
-
-const replaceZero = (q: number) => (q === 0 ? 'geen' : q.toString())
-
-const getQueuedPhrase = (queued: number) =>
-  simplur`Er ${[queued]} [is|zijn] nu ${[queued, replaceZero]} wachtende[|n]`
-
-const getWaitingTimePhrase = (waitingTime: number) =>
-  'De wachttijd is ' +
-  (waitingTime >= 60
-    ? 'meer dan een uur'
-    : simplur`${waitingTime} minu[ut|ten]`)
 
 export const WaitingTime = ({cityOfficeId}: Props) => {
   const {data: waitingTimes, isLoading} = useGetWaitingTimesQuery()
