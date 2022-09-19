2ffb85da9ab8eb5b103823b88ce0bbaea57bdf7c
Frank Filius <f.filius@amsterdam.nl>
2022-09-19 14:33:10 +0200

    Wrap component in canvas and transform into CSF:
    https://storybook.js.org/docs/react/api/csf

--- src/components/ui/layout/Center.stories.tsx
+++ src/components/ui/layout/Center.stories.tsx
@@ -1,16 +1,26 @@
-import {ComponentMeta, ComponentStoryObj} from '@storybook/react'
+import {ComponentMeta, ComponentStory} from '@storybook/react'
 import React from 'react'
-import {Center, Row} from '@/components/ui/layout'
+import {Canvas} from '.storybook/components'
+import {Center} from '@/components/ui/layout'
 import {Phrase} from '@/components/ui/text'
 
 export default {
   component: Center,
-  decorators: [Story => <Row>{Story()}</Row>],
+  decorators: [
+    Story => (
+      <Canvas height="256px" highlight maxWidth="512px">
+        <Story />
+      </Canvas>
+    ),
+  ],
 } as ComponentMeta<typeof Center>
 
-export const Default: ComponentStoryObj<typeof Center> = {
-  args: {
-    grow: true,
-    children: <Phrase>Ik sta in het midden</Phrase>,
-  },
+export const Default: ComponentStory<typeof Center> = args => (
+  <Center {...args}>
+    <Phrase>Ik sta in het midden</Phrase>
+  </Center>
+)
+
+Default.args = {
+  grow: true,
 }
