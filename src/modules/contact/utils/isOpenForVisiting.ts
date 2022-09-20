f5a6e2d4803f0ce72f8756178b03c6cfdbe789ba
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-20 22:34:24 +0200

    Move function declarations out of function declaration

--- src/modules/contact/utils/isOpenForVisiting.ts
+++ src/modules/contact/utils/isOpenForVisiting.ts
@@ -2,27 +2,27 @@ import {holidays} from '@/modules/contact/data'
 import {HoursAndMinutes, VisitingHour} from '@/modules/contact/types'
 import {adjustHoursAndMinutes, dayjs, Dayjs} from '@/utils'
 
+const todayIsWeekDay = (date: Dayjs, dayOfWeek: number) =>
+  date.day() === dayOfWeek
+
+const todayIsNotHoliday = (date: Dayjs) =>
+  !holidays.some(holiday => date.isSame(dayjs(holiday), 'day'))
+
+const nowIsAfterOpeningTime = (date: Dayjs, opening: HoursAndMinutes) =>
+  date.isSame(adjustHoursAndMinutes(date, opening)) ||
+  date.isAfter(adjustHoursAndMinutes(date, opening))
+
+const nowIsBeforeClosingTime = (date: Dayjs, closing: HoursAndMinutes) =>
+  date.isBefore(adjustHoursAndMinutes(date, closing))
+
 export const isOpenForVisiting = (
   visitingHours: VisitingHour[],
   date: Dayjs = dayjs(),
-) => {
-  const todayIsWeekDay = (dayOfWeek: number) => date.day() === dayOfWeek
-
-  const todayIsNotHoliday = () =>
-    !holidays.some(holiday => date.isSame(dayjs(holiday), 'day'))
-
-  const nowIsAfterOpeningTime = (opening: HoursAndMinutes) =>
-    date.isSame(adjustHoursAndMinutes(date, opening)) ||
-    date.isAfter(adjustHoursAndMinutes(date, opening))
-
-  const nowIsBeforeClosingTime = (closing: HoursAndMinutes) =>
-    date.isBefore(adjustHoursAndMinutes(date, closing))
-
-  return visitingHours.some(
+) =>
+  visitingHours.some(
     ({opening, closing, dayOfWeek}) =>
-      todayIsWeekDay(dayOfWeek) &&
-      todayIsNotHoliday() &&
-      nowIsAfterOpeningTime(opening) &&
-      nowIsBeforeClosingTime(closing),
+      todayIsWeekDay(date, dayOfWeek) &&
+      todayIsNotHoliday(date) &&
+      nowIsAfterOpeningTime(date, opening) &&
+      nowIsBeforeClosingTime(date, closing),
   )
-}
