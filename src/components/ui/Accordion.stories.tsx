0113d819eef843e788b68a2db6bf5e7055a156d2
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-20 10:04:53 +0200

    Add example of multiple accordions

--- src/components/ui/Accordion.stories.tsx
+++ src/components/ui/Accordion.stories.tsx
@@ -8,23 +8,39 @@ export default {
 } as ComponentMeta<typeof Accordion>
 
 const paragraphText =
-  'Een accordeon is een klikbare verticaal gestapelde lijst van koppen om bijbehorende inhoud te verbergen of te vertonen. Je toont inhoud op een progressieve manier aan gebruikers. Ze zijn geschikt zodra gebruikers op zoek zijn naar essentiële inhoud op 1 pagina. Door niet essentiële inhoud te verbergen, kunnen gebruikers focussen op de primaire inhoud.'
+  'Een accordeon (Engels: accordion) is een klikbare verticaal gestapelde lijst van koppen om bijbehorende inhoud te verbergen of te vertonen. Je toont inhoud op een progressieve manier aan gebruikers. Ze zijn geschikt zodra gebruikers op zoek zijn naar essentiële inhoud op 1 pagina. Door niet essentiële inhoud te verbergen, kunnen gebruikers focussen op de primaire inhoud.'
 
 export const Default: ComponentStory<typeof Accordion> = args => (
   <Accordion {...args}>
     <Paragraph>{paragraphText}</Paragraph>
   </Accordion>
 )
+
+Default.args = {
+  title: 'Wat is een accordeon?',
+}
+
 export const LongTitle: ComponentStory<typeof Accordion> = args => (
   <Accordion {...args}>
     <Paragraph>{paragraphText}</Paragraph>
   </Accordion>
 )
 
-Default.args = {
-  title: 'Default',
+LongTitle.args = {
+  title:
+    'De titel moet zo kort als mogelijk zijn, maar ook duidelijk en representatief voor de verborgen inhoud',
 }
 
-LongTitle.args = {
-  title: 'De titel moet zo kort als mogelijk zijn, maar ook duidelijk en',
-}
+export const Multiple: ComponentStory<typeof Accordion> = () => (
+  <>
+    {['Grofvuil', 'Restafval', 'Containers in de buurt', 'Afvalpunten'].map(
+      title => (
+        <Accordion title={title}>
+          <Paragraph>
+            Informatie over {title.toLowerCase()} komt hier.
+          </Paragraph>
+        </Accordion>
+      ),
+    )}
+  </>
+)
