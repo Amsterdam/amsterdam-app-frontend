fb9f6970176da8879a01615ab8acfb8bac713b50
Frank Filius <f.filius@amsterdam.nl>
2022-09-20 11:00:14 +0200

    Create story

--- src/components/ui/feedback/Alert.stories.tsx
+++ src/components/ui/feedback/Alert.stories.tsx
@@ -1,9 +1,72 @@
-import {ComponentMeta, ComponentStory} from '@storybook/react'
-import React from 'react'
+import {ComponentMeta, Story} from '@storybook/react'
+import React, {useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import {Alert} from '@/components/ui/feedback'
+import {AlertSliceState, Content, setAlert} from '@/store/alertSlice'
+import {CloseType, Variant} from '@/types'
+
+const title = 'Title'
+const text = 'U kunt een dagontheffing of jaarontheffing aanvragen'
+
+type Contents = {
+  [key in 'withTitle' | 'withoutTitle']: Content
+}
+
+const contents: Contents = {
+  withTitle: {
+    title,
+    text,
+  },
+  withoutTitle: {
+    title: '',
+    text,
+  },
+}
 
 export default {
   component: Alert,
+  argTypes: {
+    closeType: {
+      options: CloseType,
+      control: {type: 'radio'},
+    },
+    content: {
+      control: {
+        type: 'radio',
+      },
+      options: [...Object.keys(contents)],
+      mapping: contents,
+    },
+    isVisible: {
+      table: {
+        disable: true,
+      },
+    },
+    variant: {
+      options: Variant,
+      control: {
+        type: 'radio',
+      },
+    },
+  },
 } as ComponentMeta<typeof Alert>
 
-export const Default: ComponentStory<typeof Alert> = () => <Alert />
+const AlertStory: Story<AlertSliceState> = ({...alertState}) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setAlert({...alertState}))
+  })
+  return <Alert />
+}
+
+export const Default = AlertStory.bind({})
+Default.args = {
+  closeType: CloseType.withoutButton,
+  content: {
+    title,
+    text,
+  },
+  isVisible: true,
+  variant: Variant.information,
+  withIcon: false,
+}
