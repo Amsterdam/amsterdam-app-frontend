c11d774a02f37b1e7fbd0438509cb4890ce65be7
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-26 14:02:39 +0200

    Add initial component for figures

---
+++ src/components/ui/media/Figure.tsx
@@ -0,0 +1,50 @@
+import {ImageAspectRatioTokens} from '_themes/tokens'
+import React, {useMemo} from 'react'
+import {StyleSheet, View, ViewProps} from 'react-native'
+import {Theme, useThemable} from '@/themes'
+
+type Props = {
+  aspectRatio?: keyof ImageAspectRatioTokens
+  height?: number
+} & ViewProps
+
+/**
+ * Horizontally centers a media object.
+ * Applies the aspect ratio and height to the object.
+ */
+export const Figure = ({
+  aspectRatio = 'default',
+  height,
+  children,
+  ...otherProps
+}: Props) => {
+  const createdStyles = useMemo(
+    () => createStyles({aspectRatio, height}),
+    [aspectRatio, height],
+  )
+  const styles = useThemable(createdStyles)
+
+  return (
+    <View {...otherProps} style={styles.figure}>
+      <View style={styles.image}>{children}</View>
+    </View>
+  )
+}
+
+const createStyles =
+  ({
+    aspectRatio,
+    height,
+  }: Required<Pick<Props, 'aspectRatio'>> & Pick<Props, 'height'>) =>
+  ({media}: Theme) =>
+    StyleSheet.create({
+      figure: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+      },
+      image: {
+        maxWidth: '100%',
+        height,
+        aspectRatio: media.aspectRatio[aspectRatio],
+      },
+    })
