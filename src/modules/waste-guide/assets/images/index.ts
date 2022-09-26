adaf59a865349b53c9316cbe8756062982b74770
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-26 21:18:51 +0200

    Add more illustrations

--- src/modules/waste-guide/assets/images/index.ts
+++ src/modules/waste-guide/assets/images/index.ts
@@ -3,11 +3,17 @@ import BulkyWasteNotCollected from './bulky-waste-not-collected.svg'
 import ContainersNearby from './containers-nearby.svg'
 import HouseholdWasteByTheRoad from './household-waste-by-the-road.svg'
 import HouseholdWasteInContainer from './household-waste-in-container.svg'
+import WasteGuideAddressTeaser from './waste-guide-address-teaser.svg'
+import WasteGuideBackground from './waste-guide-background.svg'
+import WasteGuide from './waste-guide.svg'
 
 export {
   BulkyWasteCollected,
-  ContainersNearby,
   BulkyWasteNotCollected,
+  ContainersNearby,
   HouseholdWasteByTheRoad,
   HouseholdWasteInContainer,
+  WasteGuide,
+  WasteGuideAddressTeaser,
+  WasteGuideBackground,
 }
