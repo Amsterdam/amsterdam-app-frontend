910d1640ab9c4f778da5e65a0848ed65cd76e6d5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-27 20:03:49 +0200

    Prevent waste guide from scrolling unnecessarily

--- src/modules/waste-guide/screens/WasteGuide.screen.tsx
+++ src/modules/waste-guide/screens/WasteGuide.screen.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import {selectAddress} from '_modules/address/slice'
+import React, {useContext} from 'react'
+import {useSelector} from 'react-redux'
 import {Screen} from '@/components/ui/layout'
 import {WasteGuideByAddress} from '@/modules/waste-guide/components'
+import {DeviceContext} from '@/providers'
 
-export const WasteGuideScreen = () => (
-  <Screen>
-    <WasteGuideByAddress />
-  </Screen>
-)
+export const WasteGuideScreen = () => {
+  const {isLandscape} = useContext(DeviceContext)
+  const {primary, temp} = useSelector(selectAddress)
+
+  const shouldScroll = [primary, temp].some(a => a !== undefined) || isLandscape
+
+  return (
+    <Screen scroll={shouldScroll}>
+      <WasteGuideByAddress />
+    </Screen>
+  )
+}
