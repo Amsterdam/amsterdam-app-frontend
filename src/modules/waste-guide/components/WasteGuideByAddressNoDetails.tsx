fd9fdcd68873bed86bbda8a789a7353ae3303e00
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-30 13:09:06 +0200

    Indulge Weesp people with collection info for their address

--- src/modules/waste-guide/components/WasteGuideByAddressNoDetails.tsx
+++ src/modules/waste-guide/components/WasteGuideByAddressNoDetails.tsx
@@ -1,7 +1,6 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {Linking} from 'react-native'
 import {RootStackParams} from '@/app/navigation'
 import {Box} from '@/components/ui'
 import {Button} from '@/components/ui/buttons'
@@ -21,50 +20,25 @@ export const WasteGuideByAddressNoDetails = ({address}: Props) => {
       StackNavigationProp<RootStackParams, typeof ModuleSlug['waste-guide']>
     >()
 
-  const content =
-    address.woonplaats === 'Weesp'
-      ? {
-          title: 'GAD',
-          text:
-            'Het afval in Weesp wordt niet opgehaald door Gemeente Amsterdam ' +
-            'maar door het GAD. Bekijk hun website voor meer informatie.',
-          button: {
-            onPress: () => {
-              // eslint-disable-next-line no-void
-              void Linking.openURL(
-                'https://inzamelkalender.gad.nl/adres/' +
-                  [
-                    address.postcode,
-                    address.huisnummer,
-                    address.bag_toevoeging,
-                  ].join(':'),
-              )
-            },
-            label: 'Ga naar GAD.nl',
-          },
-        }
-      : {
-          title: 'Niet gevonden',
-          text: `We konden geen afvalinformatie vinden voor het adres ${address.adres}, ${address.postcode} ${address.woonplaats}.`,
-          button: {
-            onPress: () => {
-              navigation.navigate(WasteGuideRouteName.wasteGuideFeedback)
-            },
-            label: 'Hier klopt iets niet',
-            secondary: true,
-          },
-        }
+  const fullAddress = [
+    `${address.adres},`,
+    address.postcode,
+    address.woonplaats,
+  ].join(' ')
 
   return (
     <Box insetHorizontal="md">
       <Column gutter="sm">
-        <Title level="h2" text={content.title} />
-        <Paragraph>{content.text}</Paragraph>
+        <Title level="h2" text="Niet gevonden" />
+        <Paragraph>
+          We konden geen afvalinformatie vinden voor het adres {fullAddress}.
+        </Paragraph>
         <Row align="start">
           <Button
-            label={content.button.label}
-            onPress={content.button.onPress}
-            variant={content.button.secondary ? 'secondary' : undefined}
+            label="Hier klopt iets niet"
+            onPress={() =>
+              navigation.navigate(WasteGuideRouteName.wasteGuideFeedback)
+            }
           />
         </Row>
       </Column>
--- src/modules/waste-guide/components/WasteGuideForWeesp.tsx
+++ src/modules/waste-guide/components/WasteGuideForWeesp.tsx
@@ -1,26 +1,40 @@
 import ExternalLink from '@amsterdam/asc-assets/static/icons/ExternalLink.svg'
+import {Address} from '_modules/address'
 import React from 'react'
+import {Linking} from 'react-native'
 import {Box} from '@/components/ui'
 import {Button} from '@/components/ui/buttons'
 import {Column, Gutter, Row} from '@/components/ui/layout'
 import {Paragraph} from '@/components/ui/text'
-import {openWebUrl} from '@/utils'
 
-export const WasteGuideForWeesp = () => (
-  <Box>
-    <Column gutter="md">
-      <Paragraph>
-        In Weesp haalt de GAD het afval op. Kijk op hun website hoe dat werkt.
-      </Paragraph>
-    </Column>
-    <Gutter height="lg" />
-    <Row align="start">
-      <Button
-        accessibilityRole="link"
-        icon={ExternalLink}
-        label="Ga naar GAD.nl"
-        onPress={() => openWebUrl('https://www.gad.nl/')}
-      />
-    </Row>
-  </Box>
-)
+type Props = {
+  address: Address
+}
+
+export const WasteGuideForWeesp = ({address}: Props) => {
+  const gadUrl =
+    'https://inzamelkalender.gad.nl/adres/' +
+    [address.postcode, address.huisnummer, address.bag_toevoeging].join(':')
+
+  return (
+    <Box>
+      <Column gutter="md">
+        <Paragraph>
+          In Weesp haalt de GAD het afval op. Kijk op hun website hoe dat werkt.
+        </Paragraph>
+      </Column>
+      <Gutter height="lg" />
+      <Row align="start">
+        <Button
+          accessibilityRole="link"
+          icon={ExternalLink}
+          label="Ga naar GAD.nl"
+          onPress={() => {
+            // eslint-disable-next-line no-void
+            void Linking.openURL(gadUrl)
+          }}
+        />
+      </Row>
+    </Box>
+  )
+}
