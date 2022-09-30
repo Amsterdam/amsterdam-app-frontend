4873fc246ce12f86f16cb433122dc041c96a4e70
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-09-30 23:20:39 +0200

    Spread bulky waste info over two columns on landscape

--- src/modules/waste-guide/screens/WhereToPutBulkyWaste.screen.tsx
+++ src/modules/waste-guide/screens/WhereToPutBulkyWaste.screen.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {StyleSheet, View} from 'react-native'
 import {Box} from '@/components/ui/containers'
-import {Column, Screen} from '@/components/ui/layout'
+import {Column, Row, Screen} from '@/components/ui/layout'
 import {Figure} from '@/components/ui/media'
 import {List, Paragraph, Title} from '@/components/ui/text'
 import {
   BulkyWasteAtRoadsideImage,
   BulkyWasteByCarImage,
 } from '@/modules/waste-guide/assets/images'
+import {DeviceContext} from '@/providers'
 
 const collected = [
   'Banken',
@@ -33,44 +35,61 @@ const notCollected = [
   'Glasplaten en spiegels',
 ]
 
-export const WhereToPutBulkyWasteScreen = () => (
-  <Screen>
-    <Box>
-      <Column gutter="lg">
-        <Column gutter="sm">
-          <Title text="Buiten zetten of naar een Afvalpunt?" />
-          <Paragraph>
-            Grof afval zijn grote spullen die niet in een vuilniszak of
-            ondergrondse afvalcontainer passen. We halen niet alle grote spullen
-            op. Sommige dingen moet u zelf wegbrengen naar een Afvalpunt. Voor
-            bewoners is dit gratis.
-          </Paragraph>
-        </Column>
-        <Column gutter="md">
-          <Figure height={192}>
-            <BulkyWasteAtRoadsideImage />
-          </Figure>
+export const WhereToPutBulkyWasteScreen = () => {
+  const {isLandscape} = useContext(DeviceContext)
+  const Track = isLandscape ? Row : Column
+
+  return (
+    <Screen>
+      <Box>
+        <Column gutter="lg">
           <Column gutter="sm">
-            <Title level="h3" text="Grof afval dat we ophalen" />
+            <Title text="Buiten zetten of naar een Afvalpunt?" />
             <Paragraph>
-              Grote spullen uit uw woning halen we op, zoals:
+              Grof afval zijn grote spullen die niet in een vuilniszak of
+              ondergrondse afvalcontainer passen. We halen niet alle grote
+              spullen op. Sommige dingen moet u zelf wegbrengen naar een
+              Afvalpunt. Voor bewoners is dit gratis.
             </Paragraph>
           </Column>
-          <List items={collected} />
+          <Track gutter="lg">
+            <View style={styles.flex1}>
+              <Column gutter="md">
+                <Figure height={192}>
+                  <BulkyWasteAtRoadsideImage />
+                </Figure>
+                <Column gutter="sm">
+                  <Title level="h3" text="Grof afval dat we ophalen" />
+                  <Paragraph>
+                    Grote spullen uit uw woning halen we op, zoals:
+                  </Paragraph>
+                </Column>
+                <List items={collected} />
+              </Column>
+            </View>
+            <View style={styles.flex1}>
+              <Column gutter="md">
+                <Figure height={192}>
+                  <BulkyWasteByCarImage />
+                </Figure>
+                <Column gutter="sm">
+                  <Title level="h3" text="Dit grof afval halen we niet op" />
+                  <Paragraph>
+                    Deze spullen moet u zelf wegbrengen naar een Afvalpunt.
+                  </Paragraph>
+                </Column>
+                <List items={notCollected} />
+              </Column>
+            </View>
+          </Track>
         </Column>
-        <Column gutter="md">
-          <Figure height={192}>
-            <BulkyWasteByCarImage />
-          </Figure>
-          <Column gutter="sm">
-            <Title level="h3" text="Dit grof afval halen we niet op" />
-            <Paragraph>
-              Deze spullen moet u zelf wegbrengen naar een Afvalpunt.
-            </Paragraph>
-          </Column>
-          <List items={notCollected} />
-        </Column>
-      </Column>
-    </Box>
-  </Screen>
-)
+      </Box>
+    </Screen>
+  )
+}
+
+const styles = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+})
