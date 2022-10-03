d99d23f4d6a775e0188d5441b3e577e41166ad92
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-10-03 15:19:39 +0200

    Remove obsolete icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,4 @@
 import Announcement from './announcement.svg'
-import BellActive from './bell-active.svg'
-import BellInactive from './bell-inactive.svg'
-import BulkyWaste from './bulky-waste.svg'
 import Checkmark from './checkmark.svg'
 import ChevronDown from './chevron-down.svg'
 import ChevronLeft from './chevron-left.svg'
@@ -12,18 +9,12 @@ import CityOffice from './city-office.svg'
 import Clock from './clock.svg'
 import Close from './close.svg'
 import ConstructionWork from './construction-work.svg'
-import Container from './container.svg'
-import DarkMode from './dark-mode.svg'
 import Edit from './edit.svg'
-import Instagram from './instagram.svg'
-import LightMode from './light-mode.svg'
 import Location from './location.svg'
 import Logo from './logo.svg'
 import OrganicWasteContainer from './organic-waste-container.svg'
 import PersonalLogin from './personal-login.svg'
-import Project from './project.svg'
 import QuestionMarkSolid from './question-mark-solid.svg'
-import QuestionMark from './question-mark.svg'
 import Search from './search.svg'
 import Settings from './settings.svg'
 import Strides from './strides.svg'
@@ -32,9 +23,6 @@ import Whatsapp from './whatsapp.svg'
 
 export {
   Announcement,
-  BellActive,
-  BellInactive,
-  BulkyWaste,
   Checkmark,
   ChevronDown,
   ChevronLeft,
@@ -45,17 +33,11 @@ export {
   Clock,
   Close,
   ConstructionWork,
-  Container,
-  DarkMode,
   Edit,
-  Instagram,
-  LightMode,
   Location,
   Logo,
   OrganicWasteContainer,
   PersonalLogin,
-  Project,
-  QuestionMark,
   QuestionMarkSolid,
   Search,
   Settings,
