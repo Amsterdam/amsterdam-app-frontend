7e7e2c22e51dbbe72e76a80247ba42ecdcf0199a
Frank Filius <f.filius@amsterdam.nl>
2022-10-04 11:19:40 +0200

    Remove unused util from project

--- src/utils/object.test.ts
+++ src/utils/object.test.ts
@@ -1,4 +1,4 @@
-import {isEmptyObject, stringifyIfObject} from './object'
+import {isEmptyObject} from './object'
 
 describe('isEmptyObject', () => {
   test('true for empty object', () => {
@@ -6,25 +6,5 @@ describe('isEmptyObject', () => {
   })
   test('false for non-empty object', () => {
     expect(isEmptyObject({a: 1})).toBe(false)
-  })
-})
-
-describe('stringifyIfObject', () => {
-  test('pass through non-object inputs', () => {
-    expect(stringifyIfObject('foo')).toBe('foo')
-    expect(stringifyIfObject(1)).toBe(1)
-    expect(stringifyIfObject(false)).toBe(false)
-    expect(stringifyIfObject(undefined)).toBe(undefined)
-    const func = () => null
-    expect(stringifyIfObject(func)).toStrictEqual(func)
-  })
-  test('pass through null', () => {
-    expect(stringifyIfObject(null)).toBe(null)
-  })
-  test('stringify arrays', () => {
-    expect(stringifyIfObject(['foo'])).toBe('["foo"]')
-  })
-  test('stringify non-array objects', () => {
-    expect(stringifyIfObject({foo: 'bar'})).toBe('{"foo":"bar"}')
   })
 })
