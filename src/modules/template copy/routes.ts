de12a3e7fefd352009be5cda00b0ee54cfbac5ce
Frank Filius <f.filius@amsterdam.nl>
2022-10-10 13:59:41 +0200

    Copy template module

---
+++ src/modules/template copy/routes.ts
@@ -0,0 +1,15 @@
+export enum TemplateRouteName {
+  home = 'Home',
+}
+
+export type TemplateStackParams = {
+  [TemplateRouteName.home]: undefined
+}
+
+export enum TemplateModalName {
+  modal = 'Modal',
+}
+
+export type TemplateModalParams = {
+  [TemplateModalName.modal]: undefined
+}
---
+++ src/modules/template copy/screens/Template.screen.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react'
+import {Box} from '@/components/ui/containers'
+import {Screen} from '@/components/ui/layout'
+import {Paragraph} from '@/components/ui/text'
+
+export const TemplateScreen: FC = () => {
+  // const navigation =
+  //   useNavigation<StackNavigationProp<RootStackParams, TemplateRouteName>>()
+
+  return (
+    <Screen>
+      <Box>
+        <Paragraph>Hallo, ik ben een module! 🎉</Paragraph>
+      </Box>
+    </Screen>
+  )
+}
