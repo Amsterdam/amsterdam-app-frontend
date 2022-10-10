701ce874c5c9c324f814832abb531f691725ef06
Frank Filius <f.filius@amsterdam.nl>
2022-10-10 11:16:57 +0200

    Complete redirects screen

--- src/modules/redirects/screens/Redirects.screen.tsx
+++ src/modules/redirects/screens/Redirects.screen.tsx
@@ -1,16 +1,37 @@
 import React, {FC} from 'react'
+import {Button} from '@/components/ui/buttons'
 import {Box} from '@/components/ui/containers'
-import {Screen} from '@/components/ui/layout'
-import {Paragraph} from '@/components/ui/text'
+import {Column, Screen} from '@/components/ui/layout'
+import {Paragraph, Title} from '@/components/ui/text'
+import {Redirects} from '@/modules/redirects/components'
+import {openWebUrl} from '@/utils'
 
 export const RedirectsScreen: FC = () => {
-  // const navigation =
-  //   useNavigation<StackNavigationProp<RootStackParams, TemplateRouteName>>()
-
   return (
     <Screen>
       <Box>
-        <Paragraph>Hallo, ik ben een module! 🎉</Paragraph>
+        <Column gutter="lg">
+          <Column gutter="xs">
+            <Title text="Direct regelen" />
+            <Paragraph>
+              Niet alle informatie staat in de app. Graag verwijzen we u naar
+              onze website zodat u zelf aan de slag kunt.
+            </Paragraph>
+          </Column>
+          <Redirects />
+          <Column gutter="xs">
+            <Title level="h4" text="Niet gevonden wat u zocht?" />
+            <Column gutter="md">
+              <Paragraph>
+                Kijk dan op onze website waar al onze informatie staat.
+              </Paragraph>
+              <Button
+                label="Naar amsterdam.nl"
+                onPress={() => openWebUrl('https://www.amsterdam.nl')}
+              />
+            </Column>
+          </Column>
+        </Column>
       </Box>
     </Screen>
   )
