f580450671cfa697ea6cb1fa1f7c5c735f0a9ea8
Frank Filius <f.filius@amsterdam.nl>
2022-10-10 11:16:12 +0200

    Add icons to project

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,6 @@
 import Announcement from './announcement.svg'
+import Car from './car.svg'
+import Card from './card.svg'
 import Checkmark from './checkmark.svg'
 import ChevronDown from './chevron-down.svg'
 import ChevronLeft from './chevron-left.svg'
@@ -8,21 +10,29 @@ import Circle from './circle.svg'
 import CityOffice from './city-office.svg'
 import Clock from './clock.svg'
 import Close from './close.svg'
+import Collaborate from './collaborate.svg'
 import ConstructionWork from './construction-work.svg'
+import DocumentText from './document-text.svg'
 import Edit from './edit.svg'
+import Housing from './housing.svg'
 import Location from './location.svg'
+import Login from './login.svg'
 import Logo from './logo.svg'
 import OrganicWasteContainer from './organic-waste-container.svg'
+import PersonDesk from './person-desk.svg'
 import PersonalLogin from './personal-login.svg'
 import QuestionMarkSolid from './question-mark-solid.svg'
 import Search from './search.svg'
 import Settings from './settings.svg'
+import Stadspas from './stadspas.svg'
 import Strides from './strides.svg'
 import TwoPersons from './two-persons.svg'
 import Whatsapp from './whatsapp.svg'
 
 export {
   Announcement,
+  Car,
+  Card,
   Checkmark,
   ChevronDown,
   ChevronLeft,
@@ -32,15 +42,21 @@ export {
   CityOffice,
   Clock,
   Close,
+  Collaborate,
   ConstructionWork,
+  DocumentText,
   Edit,
+  Housing,
   Location,
+  Login,
   Logo,
   OrganicWasteContainer,
   PersonalLogin,
+  PersonDesk,
   QuestionMarkSolid,
   Search,
   Settings,
+  Stadspas,
   Strides,
   TwoPersons,
   Whatsapp,
