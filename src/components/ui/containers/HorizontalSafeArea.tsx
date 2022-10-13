b36e3f3ebe69468866e686cbc1526c628ab02bdf
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-10-13 11:34:20 +0200

    Allow ‘horizontal safe area’ to flex

--- src/components/ui/containers/HorizontalSafeArea.tsx
+++ src/components/ui/containers/HorizontalSafeArea.tsx
@@ -4,15 +4,20 @@ import {EdgeInsets, useSafeAreaInsets} from 'react-native-safe-area-context'
 
 type Props = {
   apply?: boolean
+  flex?: number
 } & Omit<ViewProps, 'style'>
 
 export const HorizontalSafeArea = ({
   apply = true,
+  flex,
   children,
   ...viewProps
 }: Props) => {
   const insets = useSafeAreaInsets()
-  const styles = useMemo(() => createStyles(apply, insets), [apply, insets])
+  const styles = useMemo(
+    () => createStyles({apply, flex}, insets),
+    [apply, flex, insets],
+  )
 
   return (
     <View style={styles.insets} {...viewProps}>
@@ -21,9 +26,10 @@ export const HorizontalSafeArea = ({
   )
 }
 
-const createStyles = (apply: Props['apply'], insets: EdgeInsets) =>
+const createStyles = ({apply, flex}: Props, insets: EdgeInsets) =>
   StyleSheet.create({
     insets: {
+      flex,
       paddingLeft: apply ? insets.left : undefined,
       paddingRight: apply ? insets.right : undefined,
     },
