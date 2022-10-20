d6b171cda9a391354a84f694ed8fe2ae43d262e6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-10-20 13:23:01 +0200

    Display images full-width

--- src/modules/about/screens/AboutTheAppDutch.screen.tsx
+++ src/modules/about/screens/AboutTheAppDutch.screen.tsx
@@ -1,74 +1,80 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
-import {Box} from '@/components/ui/containers'
+import {Box, HorizontalSafeArea} from '@/components/ui/containers'
 import {Column, Screen} from '@/components/ui/layout'
 import {Icon, Image} from '@/components/ui/media'
 import {Paragraph, Title} from '@/components/ui/text'
 
 export const AboutTheAppDutchScreen = () => (
-  <Screen>
+  <Screen withLeftInset={false} withRightInset={false}>
     <Column gutter="lg">
       <Image source={require('@/modules/about/assets/traffic-cycling.jpeg')} />
-      <Box>
-        <Column gutter="xl">
-          <Column gutter="sm">
-            <Title text="1 app voor alle Amsterdammers" />
-            <Paragraph variant="intro">
-              Met de Amsterdam app krijgt u snel informatie die voor u
-              belangrijk is. En u kunt zaken makkelijk regelen. Zoals:
-            </Paragraph>
+      <HorizontalSafeArea>
+        <Box>
+          <Column gutter="xl">
+            <Column gutter="sm">
+              <Title text="1 app voor alle Amsterdammers" />
+              <Paragraph variant="intro">
+                Met de Amsterdam app krijgt u snel informatie die voor u
+                belangrijk is. En u kunt zaken makkelijk regelen. Zoals:
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="trash-bin" size={32} />
+              <Title level="h5" text="Informatie over afval" />
+              <Paragraph>
+                U kunt uw adres invullen. Dan ziet u wanneer u grofvuil (grote
+                stukken afval) aan de weg mag zetten. En u ziet ook waar de
+                dichtstbijzijnde afvalcontainer of flessencontainer is.
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="construction-work" size={32} />
+              <Title level="h5" text="Werk aan de weg" />
+              <Paragraph>
+                De gemeente werkt vaak aan de straten, bruggen, kades of
+                gebouwen. In 'Werkzaaamheden’ ziet u de projecten bij u in de
+                buurt. U kunt een project volgen in de app. Als u een project
+                volgt, dan bent u altijd op de hoogte van de werkzaamheden.
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="alert" size={32} />
+              <Title level="h5" text="Melding maken " />
+              <Paragraph>
+                Is de container voor afval vol? Doet de lantaarnpaal het niet?
+                Of ligt er troep op straat? Dat kunt u allemaal melden met de
+                app. We lossen het probleem zo snel mogelijk voor u op.
+              </Paragraph>
+            </Column>
           </Column>
-          <Column gutter="sm">
-            <Icon name="trash-bin" size={32} />
-            <Title level="h5" text="Informatie over afval" />
-            <Paragraph>
-              U kunt uw adres invullen. Dan ziet u wanneer u grofvuil (grote
-              stukken afval) aan de weg mag zetten. En u ziet ook waar de
-              dichtstbijzijnde afvalcontainer of flessencontainer is.
-            </Paragraph>
+        </Box>
+      </HorizontalSafeArea>
+      <Image source={require('@/modules/about/assets/people-in-park.jpeg')} />
+      <HorizontalSafeArea>
+        <Box>
+          <Column gutter="lg">
+            <Column gutter="sm">
+              <Title
+                level="h2"
+                text="1 app voor alle Amsterdammers en Weespers"
+              />
+              <Paragraph>
+                We maken 1 app voor alle mensen die wonen in Amsterdam en Weesp.
+                De app is zó gemaakt dat iedereen hem kan gebruiken.
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Title level="h2" text="Nog meer onderwerpen in de toekomst" />
+              <Paragraph>
+                Deze versie van de Amsterdam app heeft onderwerpen waar iedereen
+                iets aan heeft. Dit is het begin. In de toekomst komen er meer
+                onderwerpen en talen bij. Zo maken we de app steeds beter.
+              </Paragraph>
+            </Column>
           </Column>
-          <Column gutter="sm">
-            <Icon name="construction-work" size={32} />
-            <Title level="h5" text="Werk aan de weg" />
-            <Paragraph>
-              De gemeente werkt vaak aan de straten, bruggen, kades of gebouwen.
-              In 'Werkzaaamheden’ ziet u de projecten bij u in de buurt. U kunt
-              een project volgen in de app. Als u een project volgt, dan bent u
-              altijd op de hoogte van de werkzaamheden.
-            </Paragraph>
-          </Column>
-          <Column gutter="sm">
-            <Icon name="alert" size={32} />
-            <Title level="h5" text="Melding maken " />
-            <Paragraph>
-              Is de container voor afval vol? Doet de lantaarnpaal het niet? Of
-              ligt er troep op straat? Dat kunt u allemaal melden met de app. We
-              lossen het probleem zo snel mogelijk voor u op.
-            </Paragraph>
-          </Column>
-          <Image
-            source={require('@/modules/about/assets/people-in-park.jpeg')}
-          />
-          <Column gutter="sm">
-            <Title
-              level="h2"
-              text="1 app voor alle Amsterdammers en Weespers"
-            />
-            <Paragraph>
-              We maken 1 app voor alle mensen die wonen in Amsterdam en Weesp.
-              De app is zó gemaakt dat iedereen hem kan gebruiken.
-            </Paragraph>
-          </Column>
-          <Column gutter="sm">
-            <Title level="h2" text="Nog meer onderwerpen in de toekomst" />
-            <Paragraph>
-              Deze versie van de Amsterdam app heeft onderwerpen waar iedereen
-              iets aan heeft. Dit is het begin. In de toekomst komen er meer
-              onderwerpen en talen bij. Zo maken we de app steeds beter.
-            </Paragraph>
-          </Column>
-        </Column>
-      </Box>
+        </Box>
+      </HorizontalSafeArea>
     </Column>
   </Screen>
 )
--- src/modules/about/screens/AboutTheAppEnglish.screen.tsx
+++ src/modules/about/screens/AboutTheAppEnglish.screen.tsx
@@ -1,74 +1,81 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
-import {Box} from '@/components/ui/containers'
+import {Box, HorizontalSafeArea} from '@/components/ui/containers'
 import {Column, Screen} from '@/components/ui/layout'
 import {Icon, Image} from '@/components/ui/media'
 import {Paragraph, Title} from '@/components/ui/text'
 
 export const AboutTheAppEnglishScreen = () => (
-  <Screen>
-    <Column gutter="md">
+  <Screen withLeftInset={false} withRightInset={false}>
+    <Column gutter="lg">
       <Image source={require('@/modules/about/assets/traffic-cycling.jpeg')} />
-      <Box>
-        <Column gutter="lg">
-          <Column gutter="sm">
-            <Title text="1 app for all the citizens of Amsterdam" />
-            <Paragraph variant="intro">
-              With the Amsterdam app you quickly get information tailored for
-              you. You can easily arrange topics like:
-            </Paragraph>
+      <HorizontalSafeArea>
+        <Box>
+          <Column gutter="lg">
+            <Column gutter="sm">
+              <Title text="1 app for all the citizens of Amsterdam" />
+              <Paragraph variant="intro">
+                With the Amsterdam app you quickly get information tailored for
+                you. You can easily arrange topics like:
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="trash-bin" size={32} />
+              <Title level="h5" text="Waste information" />
+              <Paragraph>
+                If you enter your address (optional) you can lookup where to
+                deposit your bulky (oversized) waste. It also helps you where to
+                find the nearest waste or glass containers
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="construction-work" size={32} />
+              <Title level="h5" text="Road work" />
+              <Paragraph>
+                The municipality often has maintenance on streets, quays,
+                bridges and buildings. In 'Werkzaamheden' you can see the
+                projects of your area. You can follow a project in the app. If
+                you follow a project, you are always up to date with any
+                activities.
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Icon name="alert" size={32} />
+              <Title level="h5" text="Report it" />
+              <Paragraph>
+                Is the waste container full, the lamppost not working or rubbish
+                on the street? You can report all of that within the app. We
+                will solve these problems for you as soon as possible.
+              </Paragraph>
+            </Column>
           </Column>
-          <Column gutter="sm">
-            <Icon name="trash-bin" size={32} />
-            <Title level="h5" text="Waste information" />
-            <Paragraph>
-              If you enter your address (optional) you can lookup where to
-              deposit your bulky (oversized) waste. It also helps you where to
-              find the nearest waste or glass containers
-            </Paragraph>
+        </Box>
+      </HorizontalSafeArea>{' '}
+      <Image source={require('@/modules/about/assets/people-in-park.jpeg')} />
+      <HorizontalSafeArea>
+        <Box>
+          <Column gutter="lg">
+            <Column gutter="sm">
+              <Title
+                level="h2"
+                text="1 app for all the citizens of Amsterdam and Weesp"
+              />
+              <Paragraph>
+                We make one app for all the people who live in Amsterdam and
+                Weesp. The app is designed so that anyone can use it.
+              </Paragraph>
+            </Column>
+            <Column gutter="sm">
+              <Title level="h2" text="More topics in the future" />
+              <Paragraph>
+                This version of the Amsterdam app has topics that will benefit
+                everyone. This is the start of many more topics and languages to
+                come. This is how we keep improving the app.
+              </Paragraph>
+            </Column>
           </Column>
-          <Column gutter="sm">
-            <Icon name="construction-work" size={32} />
-            <Title level="h5" text="Road work" />
-            <Paragraph>
-              The municipality often has maintenance on streets, quays, bridges
-              and buildings. In 'Werkzaamheden' you can see the projects of your
-              area. You can follow a project in the app. If you follow a
-              project, you are always up to date with any activities.
-            </Paragraph>
-          </Column>
-          <Column gutter="sm">
-            <Icon name="alert" size={32} />
-            <Title level="h5" text="Report it" />
-            <Paragraph>
-              Is the waste container full, the lamppost not working or rubbish
-              on the street? You can report all of that within the app. We will
-              solve these problems for you as soon as possible.
-            </Paragraph>
-          </Column>
-          <Image
-            source={require('@/modules/about/assets/people-in-park.jpeg')}
-          />
-          <Column gutter="sm">
-            <Title
-              level="h2"
-              text="1 app for all the citizens of Amsterdam and Weesp"
-            />
-            <Paragraph>
-              We make one app for all the people who live in Amsterdam and
-              Weesp. The app is designed so that anyone can use it.
-            </Paragraph>
-          </Column>
-          <Column gutter="sm">
-            <Title level="h2" text="More topics in the future" />
-            <Paragraph>
-              This version of the Amsterdam app has topics that will benefit
-              everyone. This is the start of many more topics and languages to
-              come. This is how we keep improving the app.
-            </Paragraph>
-          </Column>
-        </Column>
-      </Box>
+        </Box>
+      </HorizontalSafeArea>
     </Column>
   </Screen>
 )
