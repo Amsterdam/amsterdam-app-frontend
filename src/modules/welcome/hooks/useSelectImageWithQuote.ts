15b52615b1a48ad5682393574bb0acdd481c6e7b
Frank Filius <f.filius@amsterdam.nl>
2022-11-03 14:46:38 +0100

    Replace images

--- src/modules/welcome/hooks/useSelectImageWithQuote.ts
+++ src/modules/welcome/hooks/useSelectImageWithQuote.ts
@@ -11,16 +11,16 @@ const imagesWithQuotes: ImageWithQuotes[] = [
   {
     quote: 'Om te kunnen scoren heb je een goal nodig',
     imagePortrait:
-      require('@/modules/welcome/assets/voetbal-portrait.jpg') as ImageURISource,
+      require('@/modules/welcome/assets/voetbal-portrait.jpeg') as ImageURISource,
     imageLandscape:
-      require('@/modules/welcome/assets/voetbal-landscape.jpg') as ImageURISource,
+      require('@/modules/welcome/assets/voetbal-landscape.jpeg') as ImageURISource,
   },
   {
     quote: 'We zorgen goed voor onze stad en voor elkaar',
     imagePortrait:
-      require('@/modules/welcome/assets/mercatorplein-portrait.jpg') as ImageURISource,
+      require('@/modules/welcome/assets/mercatorplein-portrait.jpeg') as ImageURISource,
     imageLandscape:
-      require('@/modules/welcome/assets/mercatorplein-landscape.jpg') as ImageURISource,
+      require('@/modules/welcome/assets/mercatorplein-landscape.jpeg') as ImageURISource,
   },
 ]
 
