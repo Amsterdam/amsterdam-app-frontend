fd87e7747a3d73c9efcb85cf0f0b24bf483f599b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-11-07 18:14:52 +0100

    Add background image containing Weesp’s city hall

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
+import CanalHouseAndWeespCityHallFacadesImage from './canal-house-and-weesp-city-hall-facades.svg'
 import CanalHouseFacadesImage from './canal-house-facades.svg'
 
-export {CanalHouseFacadesImage}
+export {CanalHouseAndWeespCityHallFacadesImage, CanalHouseFacadesImage}
