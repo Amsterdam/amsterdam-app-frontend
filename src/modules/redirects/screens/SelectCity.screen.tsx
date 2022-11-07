81db15fba63987951a2b1658ceaacd0e45e856eb
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-11-07 15:59:40 +0100

    Move safe area

--- src/modules/redirects/screens/SelectCity.screen.tsx
+++ src/modules/redirects/screens/SelectCity.screen.tsx
@@ -1,11 +1,12 @@
 import {StackNavigationProp} from '@react-navigation/stack'
-import React from 'react'
+import React, {useContext} from 'react'
 import {RootStackParams} from '@/app/navigation'
 import {Button} from '@/components/ui/buttons'
-import {Box} from '@/components/ui/containers'
+import {Box, HorizontalSafeArea} from '@/components/ui/containers'
 import {Column, Row, Screen} from '@/components/ui/layout'
 import {Title} from '@/components/ui/text'
 import {RedirectsRouteName} from '@/modules/redirects/routes'
+import {DeviceContext} from '@/providers'
 import {openWebUrl} from '@/utils'
 
 type Props = {
@@ -18,31 +19,40 @@ type Props = {
 const appointmentFormWeespUrl =
   'https://formulieren.amsterdam.nl/TriplEforms/DirectRegelen/formulier/nl-NL/evAmsterdam/afspraakmakenweesp.aspx'
 
-export const SelectCityScreen = ({navigation}: Props) => (
-  <Screen>
-    <Box>
-      <Column gutter="md">
-        <Title text="Waar wilt u een afspraak maken?" />
-        <Row gutter="md">
-          <Column flex={1}>
-            <Button
-              accessibilityRole="link"
-              label="Amsterdam"
-              onPress={() =>
-                navigation.navigate(RedirectsRouteName.makeAppointment)
-              }
-            />
+export const SelectCityScreen = ({navigation}: Props) => {
+  const {isPortrait} = useContext(DeviceContext)
+
+  return (
+    <Screen
+      scroll={false}
+      withLeftInset={isPortrait}
+      withRightInset={isPortrait}>
+      <HorizontalSafeArea>
+        <Box>
+          <Column gutter="md">
+            <Title text="Waar wilt u een afspraak maken?" />
+            <Row gutter="md">
+              <Column flex={1}>
+                <Button
+                  accessibilityRole="link"
+                  label="Amsterdam"
+                  onPress={() =>
+                    navigation.navigate(RedirectsRouteName.makeAppointment)
+                  }
+                />
+              </Column>
+              <Column flex={1}>
+                <Button
+                  accessibilityRole="link"
+                  label="Weesp"
+                  onPress={() => openWebUrl(appointmentFormWeespUrl)}
+                  variant="secondary"
+                />
+              </Column>
+            </Row>
           </Column>
-          <Column flex={1}>
-            <Button
-              accessibilityRole="link"
-              label="Weesp"
-              onPress={() => openWebUrl(appointmentFormWeespUrl)}
-              variant="secondary"
-            />
-          </Column>
-        </Row>
-      </Column>
-    </Box>
-  </Screen>
-)
+        </Box>
+      </HorizontalSafeArea>
+    </Screen>
+  )
+}
