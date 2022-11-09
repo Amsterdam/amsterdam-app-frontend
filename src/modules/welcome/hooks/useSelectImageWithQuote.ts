d7ea5c42b6e9a77c66931bd4956f5f24956d8f6d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-11-09 13:52:40 +0100

    Sort object properties

--- src/modules/welcome/hooks/useSelectImageWithQuote.ts
+++ src/modules/welcome/hooks/useSelectImageWithQuote.ts
@@ -9,39 +9,39 @@ type ImageWithQuote = {
 
 const imagesWithQuote: ImageWithQuote[] = [
   {
-    quote: 'Wees jezelf als geen ander',
-    imagePortrait:
-      require('@/modules/welcome/assets/albert-cuijp-markt-portrait.jpg') as ImageURISource,
     imageLandscape:
       require('@/modules/welcome/assets/albert-cuijp-markt-landscape.jpg') as ImageURISource,
+    imagePortrait:
+      require('@/modules/welcome/assets/albert-cuijp-markt-portrait.jpg') as ImageURISource,
+    quote: 'Wees jezelf als geen ander',
   },
   {
-    quote: 'We zorgen goed voor onze stad en voor elkaar',
-    imagePortrait:
-      require('@/modules/welcome/assets/mercatorplein-portrait.jpg') as ImageURISource,
     imageLandscape:
       require('@/modules/welcome/assets/mercatorplein-landscape.jpg') as ImageURISource,
+    imagePortrait:
+      require('@/modules/welcome/assets/mercatorplein-portrait.jpg') as ImageURISource,
+    quote: 'We zorgen goed voor onze stad en voor elkaar',
   },
   {
-    quote: 'Onze stad is altijd beweging',
-    imagePortrait:
-      require('@/modules/welcome/assets/prinsengracht-portrait.jpg') as ImageURISource,
     imageLandscape:
       require('@/modules/welcome/assets/prinsengracht-landscape.jpg') as ImageURISource,
+    imagePortrait:
+      require('@/modules/welcome/assets/prinsengracht-portrait.jpg') as ImageURISource,
+    quote: 'Onze stad is altijd beweging',
   },
   {
-    quote: 'Onderweg komen we elkaar tegen',
-    imagePortrait:
-      require('@/modules/welcome/assets/tramhalte-portrait.jpg') as ImageURISource,
     imageLandscape:
       require('@/modules/welcome/assets/tramhalte-landscape.jpg') as ImageURISource,
+    imagePortrait:
+      require('@/modules/welcome/assets/tramhalte-portrait.jpg') as ImageURISource,
+    quote: 'Onderweg komen we elkaar tegen',
   },
   {
-    quote: 'Om te kunnen scoren heb je een goal nodig',
-    imagePortrait:
-      require('@/modules/welcome/assets/voetbal-portrait.jpg') as ImageURISource,
     imageLandscape:
       require('@/modules/welcome/assets/voetbal-landscape.jpg') as ImageURISource,
+    imagePortrait:
+      require('@/modules/welcome/assets/voetbal-portrait.jpg') as ImageURISource,
+    quote: 'Om te kunnen scoren heb je een goal nodig',
   },
 ]
 
