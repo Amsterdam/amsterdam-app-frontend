b6ad06e2b560ead3a82ebf03fcbb50bda37cdf3e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-11-16 01:11:59 +0100

    Add ‘narrow’ aspect ratio

--- src/themes/tokens/media.ts
+++ src/themes/tokens/media.ts
@@ -4,6 +4,7 @@ export type ImageAspectRatioTokens = typeof mediaTokens.aspectRatio
 export const mediaTokens = {
   aspectRatio: {
     default: 16 / 9,
+    narrow: 5 / 4,
     square: 1,
     vintage: 4 / 3,
     'extra-wide': 940 / 415, // These are the width and height of the project images on amsterdam.nl
