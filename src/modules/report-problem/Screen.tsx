16f08a7d867aac5cf110e2b7fc1116431c581cff
Frank Filius <f.filius@amsterdam.nl>
2022-11-18 09:58:56 +0100

    Update report problem screen to handle onmessage

--- src/modules/report-problem/Screen.tsx
+++ src/modules/report-problem/Screen.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import {StackNavigationProp} from '@react-navigation/stack'
+import React, {useCallback} from 'react'
+import {WebViewMessageEvent} from 'react-native-webview'
+import {RootStackParams} from '@/app/navigation'
 import {WebView} from '@/components/ui/containers'
 import {Screen} from '@/components/ui/layout'
+import {ReportProblemRouteName} from '@/modules/report-problem/routes'
 import {useEnvironment} from '@/store'
 
-export const ReportProblemScreen = () => {
+type Props = {
+  navigation: StackNavigationProp<
+    RootStackParams,
+    ReportProblemRouteName.reportProblem
+  >
+}
+
+const injectedJavascript = `(function() {
+  window.postMessage = function(data) {
+  window.ReactNativeWebView.postMessage(data);
+};
+})()`
+
+const signalsCloseMessage = 'signals/close'
+
+export const ReportProblemScreen = ({navigation}: Props) => {
   const environment = useEnvironment()
+
+  const handleMessage = useCallback(
+    (event: WebViewMessageEvent) => {
+      if (event.nativeEvent.data === signalsCloseMessage) {
+        navigation.goBack()
+      }
+    },
+    [navigation],
+  )
 
   return (
     <Screen scroll={false}>
-      <WebView url={`${environment.signalsBaseUrl}/incident/beschrijf`} />
+      <WebView
+        injectedJavaScript={injectedJavascript}
+        onMessage={handleMessage}
+        url={environment.signalsBaseUrl}
+      />
     </Screen>
   )
 }
