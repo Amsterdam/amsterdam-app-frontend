184905650b0bcd243935a480b8f6a7bf92412fa3
Frank Filius <f.filius@amsterdam.nl>
2022-12-01 14:49:28 +0100

    Show title when no content

--- src/components/ui/containers/TimelineItem.tsx
+++ src/components/ui/containers/TimelineItem.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import {View} from 'react-native'
-import {Accordion} from '@/components/ui/containers'
+import {Accordion, Box} from '@/components/ui/containers'
 import {timelineStyles} from '@/components/ui/containers/timelineStyles'
+import {Row} from '@/components/ui/layout'
 import {Icon} from '@/components/ui/media'
-import {Article} from '@/components/ui/text'
+import {Article, Title} from '@/components/ui/text'
 import {useTheme} from '@/themes'
 import {TimelineItem as TimelineItemType} from '@/types'
 
@@ -20,6 +21,8 @@ export const TimelineItem = ({isFirst, isLast, item}: Props) => {
   const theme = useTheme()
   const styles = timelineStyles(theme, isCurrent, isExpanded, isFirst, isLast)
 
+  const itemHasContent = item.content.some(c => c.body?.html || c.body?.text)
+
   return (
     <View style={styles.item}>
       <View style={styles.header}>
@@ -28,17 +31,25 @@ export const TimelineItem = ({isFirst, isLast, item}: Props) => {
             <Icon color="inverse" name="checkmark" size="sm" />
           )}
         </View>
-        <Accordion
-          key={item.title}
-          title={item.title}
-          initiallyExpanded={isCurrent}
-          onChangeExpanded={state => setIsExpanded(state)}>
-          {item.content.map(c => (
-            <React.Fragment key={c.title}>
-              <Article content={c.body.html} />
-            </React.Fragment>
-          ))}
-        </Accordion>
+        {itemHasContent ? (
+          <Accordion
+            key={item.title}
+            title={item.title}
+            initiallyExpanded={isCurrent}
+            onChangeExpanded={state => setIsExpanded(state)}>
+            {item.content.map(c => (
+              <React.Fragment key={c.title}>
+                <Article content={c.body.html} />
+              </React.Fragment>
+            ))}
+          </Accordion>
+        ) : (
+          <Row flex={1}>
+            <Box>
+              <Title color="link" level="h5" text={item.title} />
+            </Box>
+          </Row>
+        )}
       </View>
       <View style={styles.line} />
     </View>
