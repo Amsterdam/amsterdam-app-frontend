c725a1db18ee1c97e98ac2687b54fa49d9ac2ae3
Rik Scheffer <rik@oberon.nl>
2023-01-18 10:26:35 +0100

    fix linting

--- .config/jest-init.js
+++ .config/jest-init.js
@@ -1,8 +1,6 @@
 jest.mock('@react-native-firebase/messaging', () => ({}))
 
-jest.mock("react-native-bootsplash", () => {
-    return {
-        hide: jest.fn().mockResolvedValueOnce(),
-        getVisibilityStatus: jest.fn().mockResolvedValue("hidden"),
-    };
-});
+jest.mock('react-native-bootsplash', () => ({
+  hide: jest.fn().mockResolvedValueOnce(),
+  getVisibilityStatus: jest.fn().mockResolvedValue('hidden'),
+}))
