7bb6fd26f1781fa608744a87b661dcb690a39b88
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-01-24 16:37:48 +0100

    steps implementation

---
+++ e2e/features/steps/werkzaamheden.steps.js
@@ -0,0 +1,46 @@
+import {expect} from 'detox'
+import {autoBindSteps, loadFeatures} from 'jest-cucumber'
+import homePage from '../pageobjects/home.page'
+import werkzaamhedenPage from '../pageobjects/werkzaamheden.page'
+
+const features = loadFeatures(
+  'e2e/features/werkzaamheden.overzichtspagina.feature',
+  {
+    tagFilter: '@included and not @excluded',
+  },
+)
+
+const werkzaamhedenSteps = ({given, when, then, and}) => {
+  beforeEach(async () => {
+    await device.launchApp({newInstance: true})
+  })
+
+  given(/ik ben op de werkzaamheden pagina/, async () => {
+    await waitFor(element(by.id('WelcomePressableImageAndQuote')))
+      .toBeVisible()
+      .withTimeout(10000)
+    await element(by.id('WelcomePressableImageAndQuote')).tap()
+    await expect(homePage.werkzaamhedenModule).toExist()
+    await homePage.werkzaamhedenModule.tap()
+  })
+
+  then(/de werkzaamheden worden weergegeven met een afbeelding/, async () => {
+    await expect(
+      werkzaamhedenPage.ConstructionWorkCardProjectMiddenweg,
+    ).toBeVisible()
+  })
+
+  and(/een titel van het project/, async () => {
+    await expect(
+      werkzaamhedenPage.ConstructionWorkCardProjectTextTitleMiddenweg,
+    ).toHaveText('Middenweg')
+  })
+
+  and(/een ondertitel met een korte beschrijving van het project/, async () => {
+    await expect(
+      werkzaamhedenPage.ConstructionWorkCardProjectTextSubtitleMiddenweg,
+    ).toHaveText('Groot onderhoud')
+  })
+}
+
+autoBindSteps(features, [werkzaamhedenSteps])
