3ab8bf69c5208876d0318a5f70988091a2ab0925
Frank Filius <f.filius@amsterdam.nl>
2023-02-08 14:04:52 +0100

    Create enum for cache timeouts

--- src/types/datetime.ts
+++ src/types/datetime.ts
@@ -1,3 +1,11 @@
+export enum CacheTimeout {
+  second = 1,
+  minute = 60,
+  fiveMinutes = 60 * 5,
+  hour = 60 * 60,
+  day = 60 * 60 * 24,
+}
+
 export enum Preposition {
   from = 'vanaf',
   until = 'tot',
