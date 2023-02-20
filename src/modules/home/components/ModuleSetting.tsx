1db759f865304c523dd7e696ae17bfdd5ae89668
Frank Filius <f.filius@amsterdam.nl>
2023-02-20 16:19:42 +0100

    Remove switch from inactive modules

--- src/modules/home/components/ModuleSetting.tsx
+++ src/modules/home/components/ModuleSetting.tsx
@@ -2,22 +2,55 @@ import {useDispatch} from 'react-redux'
 import {Box} from '@/components/ui/containers'
 import {Switch} from '@/components/ui/forms'
 import {Column, Row} from '@/components/ui/layout'
-import {Icon} from '@/components/ui/media'
+import {Icon, IconName} from '@/components/ui/media'
 import {Title, Paragraph} from '@/components/ui/text'
 import {useModules} from '@/hooks'
-import {Module} from '@/modules/types'
+import {InactiveModuleMessage} from '@/modules/home/components/InactiveModuleMessage'
+import {Module, ModuleStatus} from '@/modules/types'
 import {toggleModule} from '@/store'
 import {accessibleText} from '@/utils'
 
-type Props = {
+type ModuleSettingContentProps = {
+  description: string
+  disabled: boolean
+  iconName: IconName | 'projects'
+  title: string
+}
+
+const ModuleSettingContent = ({
+  description,
+  iconName,
+  disabled,
+  title,
+}: ModuleSettingContentProps) => {
+  const color = !disabled ? 'secondary' : undefined
+  return (
+    <Column gutter="sm">
+      <Row gutter="sm" valign="center">
+        {/* TODO Remove fallback after updating icon name in database. */}
+        {iconName === 'projects' ? (
+          <Icon color={color} name="construction-work" size="lg" />
+        ) : (
+          !!iconName && <Icon color={color} name={iconName} size="lg" />
+        )}
+        <Title color={color} level="h5" text={title} />
+      </Row>
+      {disabled ? (
+        <Paragraph variant="small">{description}</Paragraph>
+      ) : (
+        <InactiveModuleMessage />
+      )}
+    </Column>
+  )
+}
+
+type ModuleSettingProps = {
   module: Module
 }
 
-const isModuleDisabledText = 'Deze module is momenteel niet beschikbaar.'
-
 export const ModuleSetting = ({
   module: {description, icon: iconName, slug, status, title},
-}: Props) => {
+}: ModuleSettingProps) => {
   const dispatch = useDispatch()
   const {selectedModulesBySlug} = useModules()
 
@@ -25,36 +58,30 @@ export const ModuleSetting = ({
     dispatch(toggleModule(slug))
   }
 
-  const isModuleDisabled = status === 0
-  const isModuleEnabled = status === 1
+  const isModuleActive = status === ModuleStatus.active
 
-  const color = isModuleDisabled ? 'secondary' : undefined
-  const colorParagraph = isModuleDisabled ? 'warning' : undefined
+  const ModuleSettingContentComponent = (
+    <ModuleSettingContent
+      description={description}
+      disabled={isModuleActive}
+      iconName={iconName}
+      title={title}
+    />
+  )
 
   return (
     <Box distinct>
-      <Switch
-        accessibilityLabel={accessibleText(title, description)}
-        disabled={isModuleDisabled}
-        label={
-          <Column gutter="sm">
-            <Row gutter="sm" valign="center">
-              {/* TODO Remove fallback after updating icon name in database. */}
-              {iconName === 'projects' ? (
-                <Icon color={color} name="construction-work" size="lg" />
-              ) : (
-                !!iconName && <Icon color={color} name={iconName} size="lg" />
-              )}
-              <Title color={color} level="h5" text={title} />
-            </Row>
-            <Paragraph color={colorParagraph} variant="small">
-              {isModuleEnabled ? description : isModuleDisabledText}
-            </Paragraph>
-          </Column>
-        }
-        onChange={onChange}
-        value={selectedModulesBySlug.includes(slug) && isModuleEnabled}
-      />
+      {isModuleActive ? (
+        <Switch
+          accessibilityLabel={accessibleText(title, description)}
+          disabled={!isModuleActive}
+          label={ModuleSettingContentComponent}
+          onChange={onChange}
+          value={selectedModulesBySlug.includes(slug) && isModuleActive}
+        />
+      ) : (
+        ModuleSettingContentComponent
+      )}
     </Box>
   )
 }
