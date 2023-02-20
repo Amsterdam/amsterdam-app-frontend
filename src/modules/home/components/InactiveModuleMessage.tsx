8aca237b9d9f365a3871ddceb1702062a10cc320
Frank Filius <f.filius@amsterdam.nl>
2023-02-20 16:16:19 +0100

    Create component to show when a module is inactive

---
+++ src/modules/home/components/InactiveModuleMessage.tsx
@@ -0,0 +1,7 @@
+import {Paragraph} from '@/components/ui/text'
+
+export const InactiveModuleMessage = () => (
+  <Paragraph color="warning" variant="small">
+    Dit onderdeel is nu niet beschikbaar.
+  </Paragraph>
+)
