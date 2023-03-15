396571d4a9cd10376c23e6fb17b8aa5cc2aad232
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-03-15 11:54:46 +0100

    Add test identifiers for ‘open waste container’ module

--- src/modules/open-waste-container/components/ComingSoon.tsx
+++ src/modules/open-waste-container/components/ComingSoon.tsx
@@ -10,15 +10,21 @@ export const ComingSoon = () => (
       source={
         require('@/modules/open-waste-container/assets/images/open-waste-container-coming-soon.jpg') as ImageSourcePropType
       }
+      testID="OpenWasteContainerComingSoonImage"
     />
     <Box insetHorizontal="md" insetVertical="lg">
       <Column gutter="md">
-        <Title text="Open een gft-container met deze app" />
-        <Paragraph variant="intro">
+        <Title
+          testID="OpenWasteContainerComingSoonTitle"
+          text="Open een gft-container met deze app"
+        />
+        <Paragraph
+          testID="OpenWasteContainerComingSoonIntroParagraph"
+          variant="intro">
           Vanaf volgend jaar kunt u een gft-container openen met uw telefoon. U
           kunt dit dan op deze pagina activeren.
         </Paragraph>
-        <Paragraph>
+        <Paragraph testID="OpenWasteContainerComingSoonParagraph">
           We vervangen de plastic passen om kosten te besparen én minder plastic
           is ook beter voor het milieu. Op een gft-container zit een slot zodat
           er niet zomaar ander afval in gegooid kan worden. Hierdoor is het
