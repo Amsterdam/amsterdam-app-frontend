954be729a2d1179a239b1dbe1e38e1cf1c2a8bf8
Frank Filius <f.filius@amsterdam.nl>
2023-04-18 20:34:28 +0200

    Create underline component

--- src/modules/waste-guide/components/ContactCollector.tsx
+++ src/modules/waste-guide/components/ContactCollector.tsx
@@ -1,8 +1,21 @@
 import {Column} from '@/components/ui/layout'
-import {Title} from '@/components/ui/text'
+import {Phrase, Title} from '@/components/ui/text'
+import {openWebUrl} from '@/utils'
+
+const wasteDisposalBusinessesUrl =
+  'https://www.amsterdam.nl/afval-hergebruik/bedrijfsafval/afvalpunt-bedrijven/'
 
 export const ContactCollector = () => (
-  <Column gutter="xl">
+  <Column gutter="md">
     <Title level="h5" text="Neem contact op met uw afvalinzamelaar" />
+    <Phrase>
+      Of kijk voor meer informatie over bedrijfsafval op{' '}
+      <Phrase
+        color="link"
+        onPress={() => openWebUrl(wasteDisposalBusinessesUrl)}
+        underline>
+        onze website
+      </Phrase>
+    </Phrase>
   </Column>
 )
--- src/modules/waste-guide/components/ReportWrongBuildingType.tsx
+++ src/modules/waste-guide/components/ReportWrongBuildingType.tsx
@@ -1,20 +1,21 @@
-import {Column, Row} from '@/components/ui/layout'
-import {Link, Phrase} from '@/components/ui/text'
+import {Column} from '@/components/ui/layout'
+import {Phrase} from '@/components/ui/text'
 import {openWebUrl} from '@/utils'
 
-const url =
+const reactionFormUrl =
   'https://formulier.amsterdam.nl/thema/afval-grondstoffen/klopt-afvalwijzer/Reactie/'
 
 export const ReportWrongBuildingType = () => (
   <Column>
     <Phrase emphasis="strong">Dit is geen woonadres</Phrase>
-    <Row gutter="xs">
-      <Phrase>Klopt dit niet?</Phrase>
-      <Link
-        label="Geef het door"
-        onPress={() => openWebUrl(url)}
-        variant="underline"
-      />
-    </Row>
+    <Phrase>
+      Klopt dit niet?{' '}
+      <Phrase
+        color="link"
+        onPress={() => openWebUrl(reactionFormUrl)}
+        underline>
+        Geef het door
+      </Phrase>
+    </Phrase>
   </Column>
 )
