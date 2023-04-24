a84211207899bc97f61020ecc21c61421f638a7c
Frank Filius <f.filius@amsterdam.nl>
2023-04-24 10:45:34 +0200

    Handle search result correctly

--- src/modules/address/components/StreetInput.tsx
+++ src/modules/address/components/StreetInput.tsx
@@ -3,13 +3,13 @@ import {StyleSheet, TextInput} from 'react-native'
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {SearchField} from '@/components/ui/forms'
 import {BagResponse} from '@/modules/address'
-import {SuggestionButton} from '@/modules/address/components'
-import {config} from '@/modules/address/config'
+import {StreetSearchResult} from '@/modules/address/components'
 
 type Props = {
   bagList: BagResponse | null | undefined
   changeStreet: (text: string) => void
   inputStreetRef: Ref<TextInput>
+  isLoading: boolean
   isStreetSelected: boolean
   selectStreet: (text: string) => void
   street: string
@@ -19,43 +19,31 @@ export const StreetInput = ({
   bagList,
   changeStreet,
   inputStreetRef,
+  isLoading,
   isStreetSelected,
   selectStreet,
   street,
-}: Props) => {
-  const {addressLengthThreshold} = config
-
-  return (
-    <>
-      <SearchField
-        autoFocus={!isStreetSelected}
-        onChangeText={text => {
-          changeStreet(text)
-        }}
-        placeholder="Vul uw straatnaam of postcode in"
-        ref={inputStreetRef}
-        testID="UserAddressStreetInputSearchField"
-        value={street}
+}: Props) => (
+  <>
+    <SearchField
+      autoFocus={!isStreetSelected}
+      onChangeText={text => {
+        changeStreet(text)
+      }}
+      placeholder="Vul uw straatnaam of postcode in"
+      ref={inputStreetRef}
+      testID="UserAddressStreetInputSearchField"
+      value={street}
+    />
+    <KeyboardAwareScrollView
+      keyboardShouldPersistTaps="handled"
+      style={styles.flex}>
+      <StreetSearchResult
+        {...{bagList, isLoading, isStreetSelected, selectStreet, street}}
       />
-      <KeyboardAwareScrollView
-        keyboardShouldPersistTaps="handled"
-        style={styles.flex}>
-        {(!isStreetSelected &&
-          street.length >= addressLengthThreshold &&
-          bagList?.content.map(bagItem => (
-            <SuggestionButton
-              key={bagItem.uri}
-              label={bagItem._display}
-              onPress={() => {
-                selectStreet(bagItem._display)
-              }}
-            />
-          ))) ??
-          null}
-      </KeyboardAwareScrollView>
-    </>
-  )
-}
+    </KeyboardAwareScrollView>
+  </>
+)
 
 const styles = StyleSheet.create({
   flex: {
---
+++ src/modules/address/components/StreetSearchResult.tsx
@@ -0,0 +1,60 @@
+import {Box} from '@/components/ui/containers'
+import {EmptyMessage, Spinner} from '@/components/ui/feedback'
+import {SuggestionButton} from '@/modules/address/components/SuggestionButton'
+import {config} from '@/modules/address/config'
+import {BagResponse} from '@/modules/address/types'
+
+type StreetSearchResultProps = {
+  bagList: BagResponse | null | undefined
+  isLoading: boolean
+  isStreetSelected: boolean
+  selectStreet: (text: string) => void
+  street: string
+}
+
+export const StreetSearchResult = ({
+  bagList,
+  isLoading,
+  isStreetSelected,
+  selectStreet,
+  street,
+}: StreetSearchResultProps) => {
+  const {addressLengthThreshold} = config
+
+  if (isStreetSelected || street.length < addressLengthThreshold) {
+    return null
+  }
+
+  if (isLoading) {
+    return (
+      <Box>
+        <Spinner />
+      </Box>
+    )
+  }
+
+  if (!bagList) {
+    return (
+      <Box insetVertical="md">
+        <EmptyMessage
+          text="Straatnaam niet gevonden. Controleer uw spelling of probeer een adres
+        in de buurt."
+        />
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      {bagList.content.map(bagItem => (
+        <SuggestionButton
+          key={bagItem.uri}
+          label={bagItem._display}
+          onPress={() => {
+            selectStreet(bagItem._display)
+          }}
+        />
+      ))}
+    </>
+  )
+}
