e91e8de166817d064ab36e255cde559675ab7bdc
Frank Filius <f.filius@amsterdam.nl>
2023-04-24 16:20:48 +0200

    Allow possibility for buttons in fractionsection

--- src/modules/construction-work/components/project/ProjectContacts.tsx
+++ src/modules/construction-work/components/project/ProjectContacts.tsx
@@ -1,5 +1,5 @@
 import {View} from 'react-native'
-import {Button} from '@/components/ui/buttons'
+import {Button, PhoneButton} from '@/components/ui/buttons'
 import {Column, Row} from '@/components/ui/layout'
 import {Paragraph, Title} from '@/components/ui/text'
 import {ProjectContact} from '@/modules/construction-work/types'
@@ -8,7 +8,6 @@ import {
   capitalizeString,
   formatPhoneNumber,
   openMailUrl,
-  openPhoneUrl,
 } from '@/utils'
 
 type Props = {
@@ -36,16 +35,13 @@ export const ProjectContacts = ({contacts, emailSubject}: Props) => (
         </View>
         {!!phone && (
           <Row>
-            <Button
+            <PhoneButton
               accessibilityLabel={accessibleText(
                 'Bel',
                 ...(formatPhoneNumber(phone) ?? '').split(' '),
               )}
-              iconName="phone"
               label={formatPhoneNumber(phone)}
-              onPress={() => {
-                openPhoneUrl(phone)
-              }}
+              phoneNumber={phone}
               testID="ConstructionWorkProjectContactPhone"
             />
           </Row>
