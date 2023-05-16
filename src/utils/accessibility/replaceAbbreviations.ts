e8536fc806d6d10495265136e928e06f212377ea
Rik Scheffer <r.scheffer@amsterdam.nl>
2023-05-16 20:23:56 +0200

    add gfe/t pronounciation

--- src/utils/accessibility/replaceAbbreviations.ts
+++ src/utils/accessibility/replaceAbbreviations.ts
@@ -1,9 +1,16 @@
 const dictionary: Record<string, string> = {
+  'gfe/t': 'ge eff ee thee',
   't/m': 'tot en met',
 }
+
+/**
+ * Makes a word lowercase and removes all of the following characters: ()
+ */
+export const wordCleanup = (text: string) =>
+  text.toLowerCase().replaceAll(/[()]/g, '')
 
 export const replaceAbbreviations = (text: string) =>
   text
     .split(' ')
-    .map((word: string) => dictionary[word.toLowerCase()] || word)
+    .map((word: string) => dictionary[wordCleanup(word)] || word)
     .join(' ')
