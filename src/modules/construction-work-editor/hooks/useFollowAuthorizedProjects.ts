f00dca5cbc1f056d60c69e97ac1b49fcd2f2b6f1
Frank Filius <f.filius@amsterdam.nl>
2023-06-08 14:15:05 +0200

    Create hook to follow authorized projects

---
+++ src/modules/construction-work-editor/hooks/useFollowAuthorizedProjects.ts
@@ -0,0 +1,22 @@
+import {useCallback} from 'react'
+import {useRegisterDevice} from '@/hooks'
+import {useFollowProjectMutation} from '@/modules/construction-work/service'
+import {ConstructionWorkEditorResponseProject} from '@/modules/construction-work-editor/types'
+
+export const useFollowAuthorizedProjects = () => {
+  const [followProject, {isSuccess: isFollowProjectSuccess}] =
+    useFollowProjectMutation()
+  const {registerDeviceWithPermission} = useRegisterDevice()
+
+  const follow = useCallback(
+    (authorizedProjects: ConstructionWorkEditorResponseProject[]) => {
+      authorizedProjects.forEach(async ({identifier}) => {
+        await followProject({project_id: identifier})
+      })
+      registerDeviceWithPermission()
+    },
+    [followProject, registerDeviceWithPermission],
+  )
+
+  return {follow, isFollowProjectSuccess}
+}
