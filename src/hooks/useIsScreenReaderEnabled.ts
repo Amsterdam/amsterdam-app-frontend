3ef594ddab6cc566dc8908b5005bf885f2212d50
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-06-14 23:18:08 +0200

    Simplify hook

--- src/hooks/useIsScreenReaderEnabled.ts
+++ src/hooks/useIsScreenReaderEnabled.ts
@@ -1,36 +1,11 @@
-import {DependencyList, useEffect} from 'react'
 import {AccessibilityInfo} from 'react-native'
-import {useAppState} from '@/hooks/useAppState'
-
-type Options = {
-  callback?: (isScreenReaderEnabled: boolean) => void
-  callbackAfterAppStateChange?: boolean
-}
 
 export let isScreenReaderEnabled: boolean | undefined
 
-export const useIsScreenReaderEnabled = (
-  {callback, callbackAfterAppStateChange = true}: Options = {},
-  deps: DependencyList | undefined = [],
-) => {
-  useAppState({
-    onForeground: () => {
-      void AccessibilityInfo.isScreenReaderEnabled().then(value => {
-        isScreenReaderEnabled = value
-        if (callbackAfterAppStateChange) {
-          callback?.(value)
-        }
-      })
-    },
+export const useIsScreenReaderEnabled = () => {
+  void AccessibilityInfo.isScreenReaderEnabled().then(value => {
+    isScreenReaderEnabled = value
   })
-
-  useEffect(() => {
-    void AccessibilityInfo.isScreenReaderEnabled().then(value => {
-      isScreenReaderEnabled = value
-      callback?.(value)
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps)
 
   return isScreenReaderEnabled
 }
