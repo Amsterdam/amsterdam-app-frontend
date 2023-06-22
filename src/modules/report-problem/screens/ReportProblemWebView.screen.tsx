252be3367aa9ff379cab39bbf17320ceebd19274
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-06-22 19:12:35 +0200

    Add back button to error message

--- src/modules/report-problem/screens/ReportProblemWebView.screen.tsx
+++ src/modules/report-problem/screens/ReportProblemWebView.screen.tsx
@@ -1,17 +1,25 @@
-import {useNavigation} from '@react-navigation/native'
 import {RouteProp} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
 import {useCallback} from 'react'
 import {useLayoutEffect} from 'react'
 import {WebViewMessageEvent} from 'react-native-webview'
-import {WebView} from '@/components/ui/containers'
-import {Box} from '@/components/ui/containers'
-import {EmptyMessage} from '@/components/ui/feedback'
-import {Screen} from '@/components/ui/layout'
+import {RootStackParams} from '@/app/navigation'
+import {Box} from '@/components/ui/containers/Box'
+import {WebView} from '@/components/ui/containers/WebView'
+import {EmptyMessage} from '@/components/ui/feedback/EmptyMessage'
+import {Column} from '@/components/ui/layout/Column'
+import {Row} from '@/components/ui/layout/Row'
+import {Screen} from '@/components/ui/layout/Screen'
+import {Link} from '@/components/ui/text/Link'
 import {ReportProblemStackParams} from '@/modules/report-problem/routes'
 import {ReportProblemRouteName} from '@/modules/report-problem/routes'
 import {useEnvironment} from '@/store'
 
 type Props = {
+  navigation: StackNavigationProp<
+    RootStackParams,
+    ReportProblemRouteName.reportProblemWebView
+  >
   route: RouteProp<
     Pick<ReportProblemStackParams, ReportProblemRouteName.reportProblemWebView>
   >
@@ -25,16 +33,17 @@ const injectedJavascript = `(function() {
 
 const signalsCloseMessage = 'signals/close'
 
-export const ReportProblemWebViewScreen = ({route}: Props) => {
-  const navigation = useNavigation()
+export const ReportProblemWebViewScreen = ({navigation, route}: Props) => {
   const environment = useEnvironment()
   const {city} = route.params
   const url = environment[`reportProblem${city}Url`]
 
   useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: `Melding doen voor ${city}`,
-    })
+    if (city) {
+      navigation.setOptions({
+        headerTitle: `Melding doen voor ${city}`,
+      })
+    }
   }, [navigation, city])
 
   const onMessage = useCallback(
@@ -50,7 +59,18 @@ export const ReportProblemWebViewScreen = ({route}: Props) => {
     return (
       <Screen>
         <Box>
-          <EmptyMessage text="Plaats niet gevonden." />
+          <Column gutter="md">
+            <EmptyMessage text="Plaats niet gevonden." />
+            <Row align="start">
+              <Link
+                label="Terug"
+                onPress={() =>
+                  navigation.navigate(ReportProblemRouteName.reportProblem)
+                }
+                variant="backward"
+              />
+            </Row>
+          </Column>
         </Box>
       </Screen>
     )
