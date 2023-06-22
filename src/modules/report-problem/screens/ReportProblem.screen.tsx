ca47be7f4c9467ac07d5f92e4f3e88314e34bf06
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-06-22 19:20:16 +0200

    Add landscape layout, link phone number

--- src/modules/report-problem/screens/ReportProblem.screen.tsx
+++ src/modules/report-problem/screens/ReportProblem.screen.tsx
@@ -1,15 +1,20 @@
 import {StackNavigationProp} from '@react-navigation/stack'
+import {useContext} from 'react'
 import {RootStackParams} from '@/app/navigation'
-import {Button} from '@/components/ui/buttons'
+import {Button} from '@/components/ui/buttons/Button'
 import {Box} from '@/components/ui/containers/Box'
-import {Row} from '@/components/ui/layout'
 import {Column} from '@/components/ui/layout/Column'
+import {Gutter} from '@/components/ui/layout/Gutter'
+import {Row} from '@/components/ui/layout/Row'
+import {Screen} from '@/components/ui/layout/Screen'
 import {InlineLink} from '@/components/ui/text/InlineLink'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
+import {useOpenPhoneUrl} from '@/hooks'
 import {useOpenWebUrl} from '@/hooks/useOpenWebUrl'
 import {ReportProblemRouteName} from '@/modules/report-problem/routes'
 import {City} from '@/modules/report-problem/types'
+import {DeviceContext} from '@/providers'
 
 type Props = {
   navigation: StackNavigationProp<
@@ -19,62 +24,91 @@ type Props = {
 }
 
 export const ReportProblemScreen = ({navigation}: Props) => {
+  const openPhoneUrl = useOpenPhoneUrl()
   const openWebUrl = useOpenWebUrl()
 
+  const {isPortrait} = useContext(DeviceContext)
+  const Track = isPortrait ? Column : Row
+
   return (
-    <Box>
-      <Column gutter="md">
+    <Screen>
+      <Box>
         <Title text="Melding openbare ruimte en overlast" />
-        <Paragraph>
-          Ziet u op straat of in een park iets waarvan u wilt dat het gemaakt of
-          opgeruimd wordt, dan kunt u dat bij de gemeente melden. U kunt ook een
-          gevaarlijke verkeerssituatie of overlast van personen en horeca aan
-          ons doorgeven.
-        </Paragraph>
-        <Paragraph>
-          U kunt op{' '}
-          <InlineLink
-            onPress={() =>
-              openWebUrl('https://meldingen.amsterdam.nl/meldingenkaart')
-            }>
-            de meldingenkaart
-          </InlineLink>{' '}
-          zien welke meldingen al bekend zijn bij de gemeente. Staat uw melding
-          er niet bij? Doe dan hieronder een nieuwe melding.
-        </Paragraph>
-        <Row
-          gutter="md"
-          wrap>
-          <Button
-            label="Doe een melding Amsterdam"
-            onPress={() =>
-              navigation.navigate(ReportProblemRouteName.reportProblemWebView, {
-                city: City.Amsterdam,
-              })
-            }
-          />
-          <Button
-            label="Doe een melding Weesp"
-            onPress={() =>
-              navigation.navigate(ReportProblemRouteName.reportProblemWebView, {
-                city: City.Weesp,
-              })
-            }
-          />
-        </Row>
-        <Paragraph variant="small">
-          Meldingen over afval en containers in Weesp doet u via{' '}
-          <InlineLink
-            onPress={() =>
-              openWebUrl(
-                'https://www.gad.nl/mijn-vraag-melding/iets-melden-of-aanvragen/',
-              )
-            }>
-            de website van de GAD
-          </InlineLink>{' '}
-          of via telefoonnummer 035 699 1888.
-        </Paragraph>
-      </Column>
-    </Box>
+        <Gutter height="md" />
+        <Track gutter="lg">
+          <Column
+            flex={2}
+            gutter="md">
+            <Paragraph>
+              Ziet u op straat of in een park iets waarvan u wilt dat het
+              gemaakt of opgeruimd wordt, dan kunt u dat bij de gemeente melden.
+              U kunt ook een gevaarlijke verkeerssituatie of overlast van
+              personen en horeca aan ons doorgeven.
+            </Paragraph>
+            <Paragraph>
+              U kunt op{' '}
+              <InlineLink
+                onPress={() =>
+                  openWebUrl('https://meldingen.amsterdam.nl/meldingenkaart')
+                }>
+                de meldingenkaart
+              </InlineLink>{' '}
+              zien welke meldingen al bekend zijn bij de gemeente. Staat uw
+              melding er niet bij? Doe dan een nieuwe melding.
+            </Paragraph>
+          </Column>
+          <Column
+            flex={1}
+            gutter="md">
+            <Title
+              level="h5"
+              text="Voor welke plaats wilt u een melding doen?"
+            />
+            <Row
+              gutter="md"
+              wrap>
+              <Button
+                label="Amsterdam"
+                onPress={() =>
+                  navigation.navigate(
+                    ReportProblemRouteName.reportProblemWebView,
+                    {
+                      city: City.Amsterdam,
+                    },
+                  )
+                }
+              />
+              <Button
+                label="Weesp"
+                onPress={() =>
+                  navigation.navigate(
+                    ReportProblemRouteName.reportProblemWebView,
+                    {
+                      city: City.Weesp,
+                    },
+                  )
+                }
+              />
+            </Row>
+            <Paragraph variant="small">
+              Meldingen over afval en containers in Weesp doet u via{' '}
+              <InlineLink
+                onPress={() =>
+                  openWebUrl(
+                    'https://www.gad.nl/mijn-vraag-melding/iets-melden-of-aanvragen/',
+                  )
+                }>
+                de website van de GAD
+              </InlineLink>{' '}
+              of via telefoonnummer{' '}
+              <InlineLink onPress={() => openPhoneUrl('+31356991888')}>
+                035 699 1888
+              </InlineLink>
+              .
+            </Paragraph>
+          </Column>
+        </Track>
+      </Box>
+    </Screen>
   )
 }
