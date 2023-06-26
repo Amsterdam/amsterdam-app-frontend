20d53290dd25cb3f9e8785b1627bb3849cb22aef
rogier83 <rogier.voogt@amsterdam.nl>
2023-06-26 22:15:10 +0200

    Update template slice

--- src/modules/template/slice.ts
+++ src/modules/template/slice.ts
@@ -1,21 +1,21 @@
-import {createSlice} from '@reduxjs/toolkit'
+// import {createSlice} from '@reduxjs/toolkit'
 // import {ReduxKey, RootState} from '@/store/types'
 
-export type TemplateState = {
-  something?: unknown
-}
+// export type TemplateState = {
+//   something?: unknown
+// }
 
-const initialState: TemplateState = {}
+// const initialState: TemplateState = {}
 
-export const templateSlice = createSlice({
-  name: 'template', // add a ReduxKey for this
-  initialState,
-  reducers: {
-    action: state => state,
-  },
-})
+// export const templateSlice = createSlice({
+//   name: ReduxKey.template,
+//   initialState,
+//   reducers: {
+//     action: state => state,
+//   },
+// })
 
-export const {action} = templateSlice.actions
+// export const {action} = templateSlice.actions
 
 // export const selectSomething = (state: RootState) =>
 //   state[ReduxKey.template].something
