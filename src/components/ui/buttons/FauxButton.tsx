6041a536afe4957de4ea104832e4daae7e413b7a
Rogier <rogier.voogt@amsterdam.nl>
2023-07-12 12:20:55 +0200

    Add description faux button

--- src/components/ui/buttons/FauxButton.tsx
+++ src/components/ui/buttons/FauxButton.tsx
@@ -7,6 +7,9 @@ type Props = {
   children: ReactNode
 }
 
+/**
+ * A faux button is not an actual button, but a helper component for a specific design pattern: when label and a button are horizontally aligned, this component can be used to align the baseline of the label text with the baseline of the button text.
+ */
 export const FauxButton = ({children}: Props) => (
   <View style={styles.button}>
     <Phrase>{children}</Phrase>
