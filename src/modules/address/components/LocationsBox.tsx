4441be3c154d782ca03956fc74319cf8e3b6f930
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-07-21 20:23:33 +0200

    Use plural for locations

--- src/modules/address/components/LocationsBox.tsx
+++ src/modules/address/components/LocationsBox.tsx
@@ -3,13 +3,13 @@ import {Column} from '@/components/ui/layout/Column'
 import {Title} from '@/components/ui/text/Title'
 import {DisplayAddress} from '@/modules/address/components/DisplayAddress'
 
-export const LocationBox = () => (
+export const LocationsBox = () => (
   <Box distinct>
     <Column gutter="md">
       <Title
         level="h2"
         testID="AddressTitle"
-        text="Locatie"
+        text="Locaties"
       />
       <DisplayAddress />
     </Column>
--- src/modules/user/screens/User.screen.tsx
+++ src/modules/user/screens/User.screen.tsx
@@ -1,14 +1,14 @@
 import {Box} from '@/components/ui/containers/Box'
 import {Alert} from '@/components/ui/feedback/Alert'
 import {Screen} from '@/components/ui/layout/Screen'
-import {LocationBox} from '@/modules/address/components/LocationBox'
+import {LocationsBox} from '@/modules/address/components/LocationsBox'
 
 export const UserScreen = () => (
   <Screen
     stickyHeader={<Alert />}
     testID="UserScreen">
     <Box>
-      <LocationBox />
+      <LocationsBox />
     </Box>
   </Screen>
 )
