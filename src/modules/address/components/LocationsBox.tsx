f9fb238654dcb0120c3143da31c4efe82354b0f9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-07-22 13:06:26 +0200

    Link to privacy info modal from my profile screen

--- src/modules/address/components/LocationsBox.tsx
+++ src/modules/address/components/LocationsBox.tsx
@@ -1,17 +1,39 @@
+import {useNavigation} from '@react-navigation/native'
+import {StackNavigationProp} from '@react-navigation/stack'
+import {RootStackParams} from '@/app/navigation'
 import {Box} from '@/components/ui/containers/Box'
 import {Column} from '@/components/ui/layout/Column'
+import {InlineLink} from '@/components/ui/text/InlineLink'
 import {Title} from '@/components/ui/text/Title'
 import {DisplayAddress} from '@/modules/address/components/DisplayAddress'
+import {AddressModalName} from '@/modules/address/routes'
+import {userModule} from '@/modules/user'
 
-export const LocationsBox = () => (
-  <Box distinct>
+export const LocationsBox = () => {
+  const navigation =
+    useNavigation<
+      StackNavigationProp<RootStackParams, typeof userModule.slug>
+    >()
+
+  return (
     <Column gutter="md">
-      <Title
-        level="h2"
-        testID="AddressTitle"
-        text="Locaties"
-      />
-      <DisplayAddress />
+      <Box distinct>
+        <Column gutter="md">
+          <Title
+            level="h2"
+            testID="AddressTitle"
+            text="Locaties"
+          />
+          <DisplayAddress />
+        </Column>
+      </Box>
+      <InlineLink
+        onPress={() => {
+          navigation.navigate(AddressModalName.privacyInfo)
+        }}
+        phraseVariant="small">
+        Hoe gebruiken we uw adres en locatie?
+      </InlineLink>
     </Column>
-  </Box>
-)
+  )
+}
