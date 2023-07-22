5ee7dff3dcae89c2982419ebac43e5b1951c633b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-07-22 13:01:58 +0200

    Require test identifier on close modal button

--- src/components/ui/buttons/CloseModalButton.tsx
+++ src/components/ui/buttons/CloseModalButton.tsx
@@ -3,20 +3,25 @@ import {StackNavigationProp} from '@react-navigation/stack'
 import {RootStackParams} from '@/app/navigation/types'
 import {Button} from '@/components/ui/buttons'
 import {Box} from '@/components/ui/containers'
+import {Track} from '@/components/ui/layout/Track'
+import {TestProps} from '@/components/ui/types'
 
 type Props = {
   label: string
-}
+} & Required<TestProps>
 
-export const CloseModalButton = ({label}: Props) => {
+export const CloseModalButton = ({label, testID}: Props) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
 
   return (
     <Box>
-      <Button
-        label={label}
-        onPress={navigation.goBack}
-      />
+      <Track>
+        <Button
+          label={label}
+          onPress={navigation.goBack}
+          testID={testID}
+        />
+      </Track>
     </Box>
   )
 }
--- src/modules/construction-work-editor/screens/WritingGuide.screen.tsx
+++ src/modules/construction-work-editor/screens/WritingGuide.screen.tsx
@@ -1,9 +1,9 @@
 import {RouteProp} from '@react-navigation/core'
 import {RootStackParams} from '@/app/navigation/types'
-import {CloseModalButton} from '@/components/ui/buttons'
-import {ModalHeader} from '@/components/ui/containers'
-import {Screen} from '@/components/ui/layout'
-import {WritingGuide} from '@/modules/construction-work-editor/components'
+import {CloseModalButton} from '@/components/ui/buttons/CloseModalButton'
+import {ModalHeader} from '@/components/ui/containers/ModalHeader'
+import {Screen} from '@/components/ui/layout/Screen'
+import {WritingGuide} from '@/modules/construction-work-editor/components/WritingGuide'
 import {ConstructionWorkEditorModalName} from '@/modules/construction-work-editor/routes'
 
 type Props = {
@@ -15,7 +15,12 @@ type Props = {
 
 export const WritingGuideScreen = ({route}: Props) => (
   <Screen
-    stickyFooter={<CloseModalButton label="Aan de slag!" />}
+    stickyFooter={
+      <CloseModalButton
+        label="Aan de slag!"
+        testID="ConstructionWorkEditorWritingGuideModalCloseButton"
+      />
+    }
     stickyHeader={
       <ModalHeader
         testID="ConstructionWorkEditorWritingGuideModalHeader"
