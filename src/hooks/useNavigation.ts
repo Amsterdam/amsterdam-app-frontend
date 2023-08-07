880d5aba31f538e18628d3cad7c77cd33a590e08
Rogier <rogier.voogt@amsterdam.nl>
2023-08-07 17:25:33 +0200

    Improve nav hook typing

--- src/hooks/useNavigation.ts
+++ src/hooks/useNavigation.ts
@@ -2,5 +2,5 @@ import {useNavigation as useNavigationOriginal} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import {RootStackParams} from '@/app/navigation/types'
 
-export const useNavigation = () =>
-  useNavigationOriginal<StackNavigationProp<RootStackParams>>()
+export const useNavigation = <RouteName extends keyof RootStackParams>() =>
+  useNavigationOriginal<StackNavigationProp<RootStackParams, RouteName>>()
