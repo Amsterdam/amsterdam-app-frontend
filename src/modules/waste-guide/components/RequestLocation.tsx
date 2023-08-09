a2161b627c792ffbf301139bd35c2e2832a41ead
Rogier <rogier.voogt@amsterdam.nl>
2023-08-09 10:43:10 +0200

    Unrevert changes

--- src/modules/waste-guide/components/RequestLocation.tsx
+++ src/modules/waste-guide/components/RequestLocation.tsx
@@ -1,21 +1,19 @@
-import {Button} from '@/components/ui/buttons/Button'
+import {TopTaskButton} from '@/components/ui/buttons/TopTaskButton'
 import {Box} from '@/components/ui/containers/Box'
 import {HorizontalSafeArea} from '@/components/ui/containers/HorizontalSafeArea'
 import {Column} from '@/components/ui/layout/Column'
 import {Row} from '@/components/ui/layout/Row'
 import {FigureWithFacadesBackground} from '@/components/ui/media/FigureWithFacadesBackground'
 import {Title} from '@/components/ui/text/Title'
-import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useDeviceContext} from '@/hooks/useDeviceContext'
-import {AddressModalName} from '@/modules/address/routes'
 import {HouseholdWasteToContainerImage} from '@/modules/waste-guide/assets/images'
-import {WasteGuideRouteName} from '@/modules/waste-guide/routes'
+import {useBottomSheet} from '@/store/slices/bottomSheet'
 import {useTheme} from '@/themes/useTheme'
 
 export const RequestLocation = () => {
-  const navigation = useNavigation<WasteGuideRouteName>()
   const {isLandscape} = useDeviceContext()
   const {media} = useTheme()
+  const {open} = useBottomSheet()
 
   return (
     <Column
@@ -24,15 +22,14 @@ export const RequestLocation = () => {
       <HorizontalSafeArea>
         <Box>
           <Column gutter="md">
-            <Title text="Voor welk adres wilt u informatie over afval?" />
+            <Title text="Voor welke locatie wilt u informatie over afval?" />
             <Row>
-              <Button
+              <TopTaskButton
                 iconName="location"
-                label="Vul uw adres in"
-                onPress={() =>
-                  navigation.navigate(AddressModalName.addressForm)
-                }
-                testID="WasteGuideEnterAddressButton"
+                onPress={open}
+                testID="WasteGuideRequestLocationButton"
+                title="Geef uw locatie door"
+                titleIconName="chevron-down"
               />
             </Row>
           </Column>
