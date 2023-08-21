11cc475de29b3d25e4cdef6823329a43447518f2
Rogier <rogier.voogt@amsterdam.nl>
2023-08-21 17:27:27 +0200

    SelectLocationTypeBottomSheet replace callbacks by slug

--- src/modules/address/components/location/SelectLocationTypeBottomSheet.tsx
+++ src/modules/address/components/location/SelectLocationTypeBottomSheet.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useCallback, useEffect} from 'react'
 import {BottomSheet} from '@/components/ui/containers/BottomSheet'
 import {Box} from '@/components/ui/containers/Box'
 import {Column} from '@/components/ui/layout/Column'
@@ -11,24 +11,49 @@ import {useAddress} from '@/modules/address/hooks/useAddress'
 import {useCurrentCoordinates} from '@/modules/address/hooks/useCurrentCoordinates'
 import {useGetCurrentCoordinates} from '@/modules/address/hooks/useGetCurrentCoordinates'
 import {AddressModalName} from '@/modules/address/routes'
-import {addLastKnownCoordinates} from '@/modules/address/slice'
+import {addLastKnownCoordinates, setLocationType} from '@/modules/address/slice'
+import {ModuleSlug} from '@/modules/slugs'
 import {useBottomSheet} from '@/store/slices/bottomSheet'
 
 type Props = {
-  onSelectAddress: () => void
-  onSelectLocation: () => void
+  slug: ModuleSlug
 }
 
-export const SelectLocationTypeBottomSheet = ({
-  onSelectAddress,
-  onSelectLocation,
-}: Props) => {
+export const SelectLocationTypeBottomSheet = ({slug}: Props) => {
   const navigation = useNavigation<AddressModalName>()
   const dispatch = useDispatch()
   const {close, isOpen} = useBottomSheet()
   const address = useAddress()
   const {getCurrentCoordinates, pending} = useGetCurrentCoordinates()
   const currentCoordinates = useCurrentCoordinates()
+
+  const onPressAddressButton = useCallback(() => {
+    if (!address) {
+      navigation.navigate(AddressModalName.addressForm)
+    }
+
+    dispatch(
+      setLocationType({
+        locationType: 'address',
+        slug,
+      }),
+    )
+    close()
+  }, [address, close, dispatch, navigation, slug])
+
+  const onPressLocationButton = useCallback(() => {
+    if (currentCoordinates) {
+      dispatch(addLastKnownCoordinates(currentCoordinates))
+    }
+
+    dispatch(
+      setLocationType({
+        locationType: 'location',
+        slug,
+      }),
+    )
+    close()
+  }, [close, currentCoordinates, dispatch, slug])
 
   useEffect(() => {
     if (isOpen) {
@@ -45,28 +70,14 @@ export const SelectLocationTypeBottomSheet = ({
             text="Locaties"
           />
           <AddressTopTaskButton
-            onPress={() => {
-              if (!address) {
-                navigation.navigate(AddressModalName.addressForm)
-              }
-
-              onSelectAddress()
-              close()
-            }}
+            onPress={onPressAddressButton}
             testID="BottomSheetSelectAddressButton"
           />
           {/* TODO: handle unhappy flow, e.g. user does not give permission */}
           <LocationTopTaskButton
             lastKnown={false}
             loading={pending}
-            onPress={() => {
-              if (currentCoordinates) {
-                dispatch(addLastKnownCoordinates(currentCoordinates))
-              }
-
-              onSelectLocation()
-              close()
-            }}
+            onPress={onPressLocationButton}
             testID="BottomSheetSelectLocationButton"
           />
         </Column>
--- src/modules/waste-guide/screens/WasteGuide.screen.tsx
+++ src/modules/waste-guide/screens/WasteGuide.screen.tsx
@@ -1,11 +1,8 @@
-import {useCallback} from 'react'
-import {useDispatch} from 'react-redux'
 import {Alert} from '@/components/ui/feedback/Alert'
 import {Screen} from '@/components/ui/layout/Screen'
 import {useDeviceContext} from '@/hooks/useDeviceContext'
 import {SelectLocationTypeBottomSheet} from '@/modules/address/components/location/SelectLocationTypeBottomSheet'
 import {useLastKnownCoordinates} from '@/modules/address/hooks/useLastKnownCoordinates'
-import {setLocationType} from '@/modules/address/slice'
 import {LocationType} from '@/modules/address/types'
 import {ModuleSlug} from '@/modules/slugs'
 import {RequestLocation} from '@/modules/waste-guide/components/RequestLocation'
@@ -21,25 +18,10 @@ const getShouldRequestLocation = (
   (locationType === 'address' && !hasAddress) ||
   (locationType === 'location' && !hasCoordinates)
 
-const setLocationTypeForWasteGuide = (locationType: LocationType) =>
-  setLocationType({
-    locationType,
-    slug: ModuleSlug['waste-guide'],
-  })
-
 export const WasteGuideScreen = () => {
   const {isPortrait} = useDeviceContext()
   const {address, locationType} = useSelectedAddressForWasteGuide()
   const coordinates = useLastKnownCoordinates()
-  const dispatch = useDispatch()
-  const onSelectAddress = useCallback(
-    () => dispatch(setLocationTypeForWasteGuide('address')),
-    [dispatch],
-  )
-  const onSelectLocation = useCallback(
-    () => dispatch(setLocationTypeForWasteGuide('location')),
-    [dispatch],
-  )
 
   const shouldRequestLocation = getShouldRequestLocation(
     locationType,
@@ -51,10 +33,7 @@ export const WasteGuideScreen = () => {
     <Screen
       scroll={!shouldRequestLocation}
       stickyFooter={
-        <SelectLocationTypeBottomSheet
-          onSelectAddress={onSelectAddress}
-          onSelectLocation={onSelectLocation}
-        />
+        <SelectLocationTypeBottomSheet slug={ModuleSlug['waste-guide']} />
       }
       stickyHeader={<Alert />}
       withLeftInset={isPortrait}
