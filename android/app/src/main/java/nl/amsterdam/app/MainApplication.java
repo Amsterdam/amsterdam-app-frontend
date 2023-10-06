39bf5d8a239af34c818cdcb15ddc3f9a09bc139e
Frank Filius <f.filius@amsterdam.nl>
2023-10-06 17:01:44 +0200

    Create workaround for textinput a11y

--- android/app/src/main/java/nl/amsterdam/app/MainApplication.java
+++ android/app/src/main/java/nl/amsterdam/app/MainApplication.java
@@ -1,5 +1,6 @@
 package nl.amsterdam.app;
 
+import nl.amsterdam.app.textinput.RCTEditTextPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -24,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RCTEditTextPackage());
           return packages;
         }
 
