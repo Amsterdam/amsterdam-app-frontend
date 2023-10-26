7fdaa1d54ffd5597ecc040f99dbde5f5ab9074ac
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2023-10-26 17:08:23 +0200

    replaceAll instead of join and a add describe in test

--- src/utils/accessibility/replaceDots.test.ts
+++ src/utils/accessibility/replaceDots.test.ts
@@ -1,11 +1,12 @@
 import {replaceDots} from './replaceDots'
 
-test(`empty string`, () => expect(replaceDots('')).toBe(''))
+describe('replaceDots', () => {
+  it(`empty string`, () => expect(replaceDots('')).toBe(''))
 
-//@ts-ignore
-test(`string with dots`, () =>
-  expect(replaceDots('guus.dijkhuis@amsterdam.nl')).toBe(
-    'guus punt dijkhuis@amsterdam punt nl',
-  ))
+  it(`string with dots`, () =>
+    expect(replaceDots('guus.dijkhuis@amsterdam.nl')).toBe(
+      'guus punt dijkhuis@amsterdam punt nl',
+    ))
 
-test(`string without dots`, () => expect(replaceDots('text')).toBe('text'))
+  it(`string without dots`, () => expect(replaceDots('text')).toBe('text'))
+})
--- src/utils/accessibility/replaceDots.ts
+++ src/utils/accessibility/replaceDots.ts
@@ -1,4 +1,4 @@
 /**
  * Add spaces before and after all dots in an string
  */
-export const replaceDots = (value: string) => value.split('.').join(' punt ')
+export const replaceDots = (value: string) => value.replaceAll('.', ' punt ')
