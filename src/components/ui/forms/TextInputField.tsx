8fd6b81124373a469b9ec6d4a3777503549936cf
Frank Filius <f.filius@amsterdam.nl>
2023-10-27 15:08:52 +0200

    Announce errors when present

--- src/components/ui/forms/TextInputField.tsx
+++ src/components/ui/forms/TextInputField.tsx
@@ -5,6 +5,7 @@ import {TextInput} from '@/components/ui/forms/TextInput'
 import {Column} from '@/components/ui/layout/Column'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {TestProps} from '@/components/ui/types'
+import {useAccessibilityAnnounce} from '@/hooks/accessibility/useAccessibilityAnnounce'
 
 type Props = {
   label: string
@@ -25,41 +26,49 @@ export const TextInputField = ({
   placeholder,
   rules,
   testID,
-}: Props) => (
-  <Controller
-    defaultValue={defaultValue as string}
-    name={name}
-    render={({field: {onChange, value}, fieldState: {error}}) => (
-      <>
-        <Column gutter="xs">
-          <TextInput
-            accessibilityLabel={label}
-            autoFocus={autoFocus}
-            label={label}
-            multiline={!!numberOfLines}
-            numberOfLines={numberOfLines ?? 1}
-            onChangeText={onChange}
-            placeholder={placeholder}
-            testID={`${testID}Input`}
-            value={value as string}
-            warning={!!error}
-          />
-        </Column>
-        {!!maxCharacters && (
-          <CharactersLeftDisplay
-            maxCharacters={maxCharacters}
-            numOfCharacters={(value as string).length}
-          />
-        )}
-        {!!error && (
-          <Paragraph
-            color="warning"
-            testID={`${testID}ErrorText`}>
-            {error.message}
-          </Paragraph>
-        )}
-      </>
-    )}
-    rules={rules}
-  />
-)
+}: Props) => {
+  const accessibilityAnnounce = useAccessibilityAnnounce({queue: true})
+
+  return (
+    <Controller
+      defaultValue={defaultValue as string}
+      name={name}
+      render={({field: {onChange, value}, fieldState: {error}}) => {
+        error?.message && accessibilityAnnounce(error.message)
+
+        return (
+          <>
+            <Column gutter="xs">
+              <TextInput
+                accessibilityLabel={label}
+                autoFocus={autoFocus}
+                label={label}
+                multiline={!!numberOfLines}
+                numberOfLines={numberOfLines ?? 1}
+                onChangeText={onChange}
+                placeholder={placeholder}
+                testID={`${testID}Input`}
+                value={value as string}
+                warning={!!error}
+              />
+            </Column>
+            {!!maxCharacters && (
+              <CharactersLeftDisplay
+                maxCharacters={maxCharacters}
+                numOfCharacters={(value as string).length}
+              />
+            )}
+            {!!error && (
+              <Paragraph
+                color="warning"
+                testID={`${testID}ErrorText`}>
+                {error.message}
+              </Paragraph>
+            )}
+          </>
+        )
+      }}
+      rules={rules}
+    />
+  )
+}
