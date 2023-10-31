641d2518e642b350581039d2248216682f06e279
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2023-10-31 09:42:45 +0100

    reduce addresspronounce

--- src/utils/accessibility/addressPronounce.ts
+++ src/utils/accessibility/addressPronounce.ts
@@ -1,23 +1,4 @@
-export const addressPronounce = (street: string) => {
-  // Output: Herengracht 412A, 3 hoog
-  if (/ (\d{1,})([a-zA-Z])-(\d)/.test(street)) {
-    return street.replace(/ (\d{1,})([a-zA-Z])-(\d)/, ' $1 $2, $3 hoog')
-  }
-
-  // Output: Herengracht 3, 2 hoog
-  if (/ (\d)-(\d)/.test(street)) {
-    return street.replace(/ (\d)-(\d)/, ' $1, $2 hoog')
-  }
-
-  // Output: Herengracht 3 H
-  if (/ (\d)-([a-zA-Z])/.test(street)) {
-    return street.replace(/ (\d)-([a-zA-Z])/, ' $1 $2')
-  }
-
-  // Output: Herengracht 3 H
-  if (/ (\d{1,})([a-zA-Z])/.test(street)) {
-    return street.replace(/ (\d{1,})([a-zA-Z])/, ' $1 $2')
-  }
-
-  return street
-}
+export const addressPronounce = (street: string) =>
+  street
+    .replace(/ (\d+[a-zA-Z]?)-(\d+)/, ' $1, $2 hoog')
+    .replace(/ (\d+)-?([a-zA-Z])/, ' $1 $2')
