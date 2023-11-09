b6f7d1934bc23af978cab5a75d1a36ed3dfa5144
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2023-11-09 15:44:42 +0100

    swipe to delete address

--- src/modules/address/components/DisplayAddress.tsx
+++ src/modules/address/components/DisplayAddress.tsx
@@ -1,4 +1,6 @@
+import {ReactNode} from 'react'
 import {Button} from '@/components/ui/buttons/Button'
+import {SwipeToDelete} from '@/components/ui/buttons/SwipeToDelete'
 import {TopTaskButton} from '@/components/ui/buttons/TopTaskButton'
 import {
   AlertCloseType,
@@ -11,8 +13,40 @@ import {useDispatch} from '@/hooks/redux/useDispatch'
 import {useSelector} from '@/hooks/redux/useSelector'
 import {AddressModalName} from '@/modules/address/routes'
 import {removeAddress, selectAddress} from '@/modules/address/slice'
+import {Address} from '@/modules/address/types'
 import {setAlert} from '@/store/slices/alert'
 import {useTheme} from '@/themes/useTheme'
+
+type AddressDeleteButtonProps = {
+  address?: Address
+  children: ReactNode
+  onDelete: () => void
+}
+
+const AddressDeleteButton = ({
+  address,
+  onDelete,
+  children,
+}: AddressDeleteButtonProps) => {
+  if (address) {
+    return (
+      <SwipeToDelete onEvent={onDelete}>
+        <Row>
+          {children}
+          <Button
+            accessibilityLabel="Verwijder mijn adres"
+            iconName="trash-bin"
+            onPress={onDelete}
+            testID="AddressDeleteButton"
+            variant="tertiary"
+          />
+        </Row>
+      </SwipeToDelete>
+    )
+  }
+
+  return <>{children}</>
+}
 
 export const DisplayAddress = () => {
   const address = useSelector(selectAddress)
@@ -39,27 +73,20 @@ export const DisplayAddress = () => {
   return (
     <Row>
       <Column flex={1}>
-        <TopTaskButton
-          accessibilityHint="Tik om het adres te wijzigen"
-          hitSlop={{bottom: size.spacing.md, top: size.spacing.md}}
-          iconName="location"
-          onPress={() => navigation.navigate(AddressModalName.addressForm)}
-          testID="AddressAddButton"
-          text={address?.addressLine1 ?? 'Vul een adres in'}
-          title="Mijn adres"
-        />
-      </Column>
-      {!!address && (
-        <Row>
-          <Button
-            accessibilityLabel="Verwijder mijn adres"
-            iconName="trash-bin"
-            onPress={removeAddressAndShowAlert}
-            testID="AddressDeleteButton"
-            variant="tertiary"
+        <AddressDeleteButton
+          address={address}
+          onDelete={removeAddressAndShowAlert}>
+          <TopTaskButton
+            accessibilityHint="Tik om het adres te wijzigen"
+            hitSlop={{bottom: size.spacing.md, top: size.spacing.md}}
+            iconName="location"
+            onPress={() => navigation.navigate(AddressModalName.addressForm)}
+            testID="AddressAddButton"
+            text={address?.addressLine1 ?? 'Vul een adres in'}
+            title="Mijn adres"
           />
-        </Row>
-      )}
+        </AddressDeleteButton>
+      </Column>
     </Row>
   )
 }
