67ea376bab7bd1e0d919d4a3896e5a39fe7f53bd
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2023-11-13 12:16:23 +0100

    save

--- src/modules/onboarding/components/Carousel.tsx
+++ src/modules/onboarding/components/Carousel.tsx
@@ -1,14 +1,13 @@
 import {forwardRef} from 'react'
-import {StyleSheet} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import {SwiperFlatList} from 'react-native-swiper-flatlist'
+import AmsterdamAndWeespFacadesImage from '@/assets/images/amsterdam-and-weesp-facades.svg'
+import {useIsScreenReaderEnabled} from '@/hooks/useIsScreenReaderEnabled'
 import {CarouselSlide} from '@/modules/onboarding/components/CarouselSlide'
 import {setHasSeenOnboarding} from '@/modules/onboarding/slice'
-import {
-  CarouselSlideItemType,
-  CarouselSlideType,
-} from '@/modules/onboarding/types'
+import {CarouselSlideItemType} from '@/modules/onboarding/types'
+import {Theme} from '@/themes/themes'
 import {baseColor} from '@/themes/tokens/base-color'
-import {sizeTokens} from '@/themes/tokens/size'
 import {useThemable} from '@/themes/useThemable'
 
 type Props = {
@@ -20,45 +19,59 @@ export const Carousel = forwardRef<SwiperFlatList, Props>(
     const styles = useThemable(createStyles)
 
     setHasSeenOnboarding(false)
+    const isScreenReaderEnabled = useIsScreenReaderEnabled()
 
     return (
       <SwiperFlatList
         data={items}
+        disableGesture={isScreenReaderEnabled}
         paginationActiveColor={baseColor.primary.blue}
         paginationDefaultColor={baseColor.primary.blue}
         paginationStyle={styles.paginationContainer}
         paginationStyleItem={styles.paginationItem}
         paginationStyleItemActive={styles.paginationItemActive}
         ref={ref}
-        renderItem={({item}: CarouselSlideType) => (
-          <CarouselSlide
-            image={item.image}
-            subText={item.subText}
-            title={item.title}
-          />
-        )}
-        showPagination
-      />
+        showPagination>
+        <>
+          <View style={{}}>
+            <AmsterdamAndWeespFacadesImage />
+          </View>
+          {items.map((item: CarouselSlideItemType) => (
+            <CarouselSlide
+              image={item.image}
+              subText={item.subText}
+              title={item.title}
+            />
+          ))}
+        </>
+      </SwiperFlatList>
     )
   },
 )
 
-const createStyles = () =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      // gap: sizeTokens.spacing.md,
-      backgroundColor: 'white',
+const createStyles = ({media, size}: Theme) => {
+  const imageWidth = media.figureHeight.lg
+  const imageHeight = imageWidth / media.aspectRatio.extraWide
+
+  return StyleSheet.create({
+    backgroundImage: {
+      aspectRatio: media.illustrationAspectRatio.facades,
+      position: 'absolute',
+      height: imageHeight,
+      alignSelf: 'center',
     },
     paginationContainer: {
-      gap: sizeTokens.spacing.md,
+      bottom: 0,
+      height: 'auto',
+      maxWidth: 'auto',
     },
     paginationItem: {
-      width: sizeTokens.spacing.sm,
-      height: sizeTokens.spacing.sm,
+      width: size.spacing.sm,
+      height: size.spacing.sm,
     },
     paginationItemActive: {
-      height: sizeTokens.spacing.sm,
-      width: sizeTokens.spacing.md,
+      height: size.spacing.sm,
+      width: size.spacing.md,
     },
   })
+}
