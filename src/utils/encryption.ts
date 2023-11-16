203e5f075f4d0c0ea0fc9c25122a06ecaee65ce7
Frank Filius <f.filius@amsterdam.nl>
2023-11-16 13:20:16 +0100

    Workaround to prevent crypto-js errors

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -1,3 +1,7 @@
+/* Importing react-native-get-random-values fixes the error "Native crypto module could not be used to get secure random number."
+ * It seems to be required when using crypto-js with RN; see https://github.com/brix/crypto-js/pull/259#issuecomment-799973769
+ */
+import 'react-native-get-random-values'
 import {AES, SHA256, enc} from 'crypto-js'
 // eslint-disable-next-line no-restricted-imports
 import {getUniqueId} from 'react-native-device-info'
