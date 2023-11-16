4a9050640a587419b2189d2de779191519dc6db8
Frank Filius <f.filius@amsterdam.nl>
2023-11-16 10:29:21 +0100

    Simplify hook

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -3,26 +3,20 @@ import {useRef, useCallback, useEffect} from 'react'
 export const useTimeout = () => {
   const timeoutRef = useRef<NodeJS.Timeout | undefined>()
 
-  const startTimeout = useCallback((callback: () => void, delay: number) => {
+  useEffect(
+    () => () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      } // Clear the timeout when the hook unmounts
+    },
+    [],
+  )
+
+  return useCallback((callback: () => void, delay: number) => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current)
     }
 
     timeoutRef.current = setTimeout(callback, delay)
   }, [])
-
-  const cancelTimeout = useCallback(() => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-  }, [])
-
-  useEffect(
-    () => () => {
-      cancelTimeout() // Clear the timeout when the hook unmounts
-    },
-    [cancelTimeout],
-  )
-
-  return startTimeout
 }
