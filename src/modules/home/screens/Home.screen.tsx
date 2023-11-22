f8fbbb1a8f4d5ef8df4dc18446fb1a5fee137c07
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2023-11-22 15:53:07 +0100

    add onboarding button to about the app module

--- src/modules/home/screens/Home.screen.tsx
+++ src/modules/home/screens/Home.screen.tsx
@@ -3,15 +3,12 @@ import {LayoutRectangle} from 'react-native'
 import {OnboardingTipWrapper} from '@/components/features/onboarding/OnboardingTipWrapper'
 import {Tip} from '@/components/features/onboarding/types'
 import {AddButton} from '@/components/ui/buttons/AddButton'
-import {Button} from '@/components/ui/buttons/Button'
 import {Box} from '@/components/ui/containers/Box'
 import {Screen} from '@/components/ui/layout/Screen'
 import {Placement} from '@/components/ui/types'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
-import {useDispatch} from '@/hooks/redux/useDispatch'
 import {Modules} from '@/modules/home/components/Modules'
 import {HomeRouteName} from '@/modules/home/routes'
-import {setHasSeenOnboarding} from '@/modules/onboarding/slice'
 
 const ONBOARDING_TIP = 'Voeg onderwerpen toe of haal weg wat u niet wilt zien'
 
@@ -19,8 +16,6 @@ export const HomeScreen = () => {
   const navigation = useNavigation<HomeRouteName>()
   const [onboardingTipTargetLayout, setTipComponentLayout] =
     useState<LayoutRectangle>()
-
-  const dispatch = useDispatch()
 
   return (
     <Screen
@@ -32,10 +27,6 @@ export const HomeScreen = () => {
           text={ONBOARDING_TIP}
           tipSlug={Tip.homeModuleSettingsButton}>
           <Box onLayout={e => setTipComponentLayout(e.nativeEvent.layout)}>
-            <Button
-              label="Onboarding"
-              onPress={() => dispatch(setHasSeenOnboarding(false))}
-            />
             <AddButton
               accessibilityHint={ONBOARDING_TIP}
               accessibilityLabel="Instellingen"
