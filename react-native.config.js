3c5384c39fee65f702dc51c11bf0bf24183e16dc
Rogier Voogt <rogiervoogt@amsterdam.nl>
2023-12-18 13:52:26 +0100

    Update RN 0.73.1

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    },
-    android: {},
-  },
   assets: ['./src/assets/fonts/'],
   dependencies: {
     ...(process.env.NO_FLIPPER
