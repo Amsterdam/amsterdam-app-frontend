31a418ba6f6b53a7bf2daae93ef624edbcc7a4a4
Frank Filius <f.filius@amsterdam.nl>
2024-01-04 16:37:32 +0100

    Implement pop-ups

--- src/modules/construction-work/screens/Project.screen.tsx
+++ src/modules/construction-work/screens/Project.screen.tsx
@@ -1,14 +1,24 @@
 import {NavigationProps} from '@/app/navigation/types'
+import {Tip} from '@/components/features/onboarding/types'
 import {Screen} from '@/components/ui/layout/Screen'
+import {useSelector} from '@/hooks/redux/useSelector'
 import {Project} from '@/modules/construction-work/components/project/Project'
 import {ConstructionWorkRouteName} from '@/modules/construction-work/routes'
+import {selectSeenTips} from '@/store/slices/product-tour.slice'
 
 type Props = NavigationProps<ConstructionWorkRouteName.project>
 
-export const ProjectScreen = ({route}: Props) => (
-  <Screen
-    withLeftInset={false}
-    withRightInset={false}>
-    <Project id={route.params.id} />
-  </Screen>
-)
+export const ProjectScreen = ({route}: Props) => {
+  const hasUnseenTips = useSelector(selectSeenTips).includes(
+    Tip.constructionWorkProjectFollowButton,
+  )
+
+  return (
+    <Screen
+      trackScroll={hasUnseenTips}
+      withLeftInset={false}
+      withRightInset={false}>
+      <Project id={route.params.id} />
+    </Screen>
+  )
+}
