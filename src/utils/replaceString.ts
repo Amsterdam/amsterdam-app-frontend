3781a51f9b50d43608ae219abbb79bb0da2797f9
Frank Filius <f.filius@amsterdam.nl>
2024-01-15 14:54:33 +0100

    Allow forward slash

--- src/utils/replaceString.ts
+++ src/utils/replaceString.ts
@@ -3,7 +3,8 @@ type ReplaceStringType = 'address' | 'number'
 export const replaceString = (s: string, type: ReplaceStringType) => {
   switch (type) {
     case 'address':
-      return s.replace(/[^\p{L}\p{N} \-\\.,‘’']+/gu, '')
+      // eslint-disable-next-line no-useless-escape
+      return s.replace(/[^\p{L}\p{N} \-\/\\.,‘’']+/gu, '')
     case 'number':
       return s.replace(/[^0-9a-zA-Z\- ]/g, '')
     default:
