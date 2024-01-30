b52e3b0d243126f4234c4e8c758aacaf71963ab4
Rogier Voogt <rogiervoogt@amsterdam.nl>
2024-01-30 16:57:44 +0100

    Clean up

--- src/processes/sanitize-url.ts
+++ src/processes/sanitize-url.ts
@@ -1,7 +1,7 @@
 import {parse, stringify} from 'qs'
 
 /**
- * Takes a query string and replaces all values with `___`. E.g., `foo=bar&quux=1` becomes `?foo=___&quux=___`
+ * Takes a query string and replaces all values with `___`. E.g. `foo=bar&quux=1` becomes `?foo=___&quux=___`
  */
 export const getSanitizedQueryString = (query?: string): string => {
   if (!query) {
@@ -18,7 +18,7 @@ export const getSanitizedQueryString = (query?: string): string => {
 }
 
 /**
- * Takes a hash string and replaces all values with `___`. E.g., `foo/bar/quux` becomes `#___/___/___`
+ * Takes a hash string and replaces all values with `___`. E.g. `foo/bar/quux` becomes `#___/___/___`
  */
 export const getSanitizedHashString = (hash?: string): string => {
   if (!hash) {
@@ -32,7 +32,7 @@ export const getSanitizedHashString = (hash?: string): string => {
 }
 
 /**
- * Takes a URL and sanitizes it if it contains a query string and/or hash, i.e., replace the values with `___`
+ * Takes a URL and sanitizes it if it contains a query string and/or hash, i.e., replaces the values with `___`
  */
 export const sanitizeUrl = (url = ''): string => {
   const [urlWithoutHash, hash, ...rest1] = url.split('#')
