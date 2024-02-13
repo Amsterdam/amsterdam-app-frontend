5c00a9b1591a2c4dcc244d4dee2f49e191ba239e
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-02-13 14:39:14 +0100

    fix devLog condiitons

--- src/processes/development.ts
+++ src/processes/development.ts
@@ -15,14 +15,14 @@ export const isProductionApp = appFlavour === AppFlavour.production
 export const isLocalApp = appFlavour === AppFlavour.local
 
 export const devLog = (...args: unknown[]) => {
-  if (isDevApp) {
+  if (appFlavour === 'local') {
     // eslint-disable-next-line no-console
     console.log(...args)
   }
 }
 
 export const devError = (...args: unknown[]) => {
-  if (isDevApp) {
+  if (appFlavour === 'local') {
     // eslint-disable-next-line no-console
     console.error(...args)
   }
