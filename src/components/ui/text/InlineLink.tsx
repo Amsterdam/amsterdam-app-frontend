09b17f9c5940045e3e2a54d9831fea3d0965364c
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-02-15 17:16:32 +0100

    Add logging to InlineLink

--- src/components/ui/text/InlineLink.tsx
+++ src/components/ui/text/InlineLink.tsx
@@ -1,28 +1,40 @@
-import {ReactNode} from 'react'
-import {TextProps} from 'react-native'
-import {Phrase, PhraseProps} from '@/components/ui/text/Phrase'
-import {TestProps} from '@/components/ui/types'
+import {type ReactNode} from 'react'
+import {type TextProps} from 'react-native'
+import {Phrase, type PhraseProps} from '@/components/ui/text/Phrase'
+import {type TestProps} from '@/components/ui/types'
+import {usePiwikTrackCustomEventFromProps} from '@/processes/piwik/hooks/usePiwikTrackCustomEventFromProps'
+import {type LogProps, PiwikAction} from '@/processes/piwik/types'
 
 type Props = {
   children: ReactNode
   onPress: () => void
   phraseVariant?: PhraseProps['variant']
 } & TestProps &
-  Omit<TextProps, 'style'>
+  Omit<TextProps, 'style'> &
+  LogProps
 
 export const InlineLink = ({
   children,
   onPress,
   phraseVariant,
+  logAction = PiwikAction.buttonPress,
   ...otherProps
-}: Props) => (
-  <Phrase
-    accessibilityRole="link"
-    color="link"
-    onPress={onPress}
-    underline
-    variant={phraseVariant}
-    {...otherProps}>
-    {children}
-  </Phrase>
-)
+}: Props) => {
+  const onEvent = usePiwikTrackCustomEventFromProps({
+    ...otherProps,
+    logAction,
+    onEvent: onPress,
+  })
+
+  return (
+    <Phrase
+      accessibilityRole="link"
+      color="link"
+      onPress={onEvent}
+      underline
+      variant={phraseVariant}
+      {...otherProps}>
+      {children}
+    </Phrase>
+  )
+}
