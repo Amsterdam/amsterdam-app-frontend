6d1fa80cd4dcea3e7b4850086d57eba0a50d5fdc
Guus Dijkhuis <guus.dijkhuis@oberon.nl>
2024-02-22 14:57:43 +0100

    feat: external url

--- src/modules/waste-guide/external-links.ts
+++ src/modules/waste-guide/external-links.ts
@@ -4,3 +4,5 @@ export const SCHEDULE_WASTE_PICKUP = 'https://afvalopafspraak.app.seenons.com'
 export const WASTE_COLLECTION_POINTS =
   'https://kaart.amsterdam.nl/recyclepunten'
 export const WASTE_CONTAINERS = 'https://kaart.amsterdam.nl/afvalcontainers'
+export const WASTE_DISPOSAL_BUSINESS_URL =
+  'https://www.amsterdam.nl/afval-hergebruik/bedrijfsafval/recyclepunten-bedrijven/'
