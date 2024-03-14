6d35c2501d3eaa8cadbe025c22e0e7dbcd8faa96
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-03-14 14:29:23 +0100

    remove permission logging

--- src/processes/piwik/hooks/useLogGeneralAnalytics.ts
+++ src/processes/piwik/hooks/useLogGeneralAnalytics.ts
@@ -1,11 +1,9 @@
 import {useLogAccessibilityAnalytics} from '@/processes/piwik/hooks/useLogAccessibilityAnalytics'
 import {useLogDeviceInfoAnalytics} from '@/processes/piwik/hooks/useLogDeviceInfoAnalytics'
 import {useLogModuleAnalytics} from '@/processes/piwik/hooks/useLogModuleAnalytics'
-import {useLogPermissionAnalytics} from '@/processes/piwik/hooks/useLogPermissionAnalytics'
 
 export const useLogGeneralAnalytics = () => {
   useLogAccessibilityAnalytics()
   useLogDeviceInfoAnalytics()
-  useLogPermissionAnalytics()
   useLogModuleAnalytics()
 }
