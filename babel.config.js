329d536027a031576345473f65fa4fdb1cb48ce8
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-03-18 15:34:53 +0100

    Add api key header

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,17 @@ module.exports = {
       {
         moduleName: '@env',
         path: '.env',
+        allowlist: [
+          'API_KEY',
+          'AUTH_PASSWORD',
+          'AUTH_SHARED_SECRET',
+          'BUILD_NUMBER',
+          'PIWIK_PRO_ID',
+          'PIWIK_PRO_ID_ACCEPT',
+          'PIWIK_PRO_URL',
+          'PIWIK_PRO_URL_ACCEPT',
+          'VERSION',
+        ],
       },
     ],
     [
