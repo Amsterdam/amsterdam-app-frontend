5b9500276b8da09623cf24bb8dda4f0d3b6ce828
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-05-14 11:35:49 +0200

    remove device register calls on module settings change

--- src/modules/home/components/ModuleSettings.tsx
+++ src/modules/home/components/ModuleSettings.tsx
@@ -1,28 +1,15 @@
 import {pascalCase} from 'pascal-case'
-import {useEffect} from 'react'
 import {getVersion} from 'react-native-device-info'
 import {Box} from '@/components/ui/containers/Box'
 import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
 import {Column} from '@/components/ui/layout/Column'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {useModules} from '@/hooks/useModules'
-import {useRegisterDevice} from '@/hooks/useRegisterDevice'
 import {ModuleSetting} from '@/modules/home/components/ModuleSetting'
 import {ModulesWarning} from '@/modules/home/components/ModulesWarning'
 
 export const ModuleSettings = () => {
-  const {toggleableModules, modulesLoading, enabledModules} = useModules()
-
-  const {registerDeviceWithPermission, unregisterDevice} =
-    useRegisterDevice(false)
-
-  useEffect(() => {
-    if (enabledModules?.some(module => module.requiresFirebaseToken)) {
-      registerDeviceWithPermission()
-    } else {
-      void unregisterDevice(undefined)
-    }
-  }, [registerDeviceWithPermission, enabledModules, unregisterDevice])
+  const {toggleableModules, modulesLoading} = useModules()
 
   if (modulesLoading) {
     return (
