0d812b9a7021816b1e6cb882719c0f2e69eacd21
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-05-14 11:46:24 +0200

    More fixes

--- src/hooks/useDeviceRegistration.ts
+++ src/hooks/useDeviceRegistration.ts
@@ -4,27 +4,29 @@ import {useRegisterDevice} from '@/hooks/useRegisterDevice'
 import {Module} from '@/modules/types'
 
 /**
- * Register or unregister the device for push notifications, on start up and on foreground
+ * Register or unregister the device for push notifications, on start up and on foreground and on module enable/disable
  */
 export const useDeviceRegistration = (enabledModules?: Module[]) => {
   const {registerDeviceWithPermission, unregisterDevice} =
     useRegisterDevice(false)
 
-  const handleDeviceRegistration = useCallback(() => {
-    if (!enabledModules) {
-      return
-    }
+  const isAnyFirebaseModuleEnabled = enabledModules?.some(
+    ({requiresFirebaseToken}) => requiresFirebaseToken,
+  )
 
-    if (
-      enabledModules.some(({requiresFirebaseToken}) => requiresFirebaseToken)
-    ) {
+  const handleDeviceRegistration = useCallback(() => {
+    if (isAnyFirebaseModuleEnabled) {
       // Because tokens refresh regularly, we need to re-register regularly
       registerDeviceWithPermission()
     } else {
       // When the user has disabled all modules that require a Firebase token, we unregister the device so the user stops receiving push notifications
       void unregisterDevice(undefined)
     }
-  }, [enabledModules, registerDeviceWithPermission, unregisterDevice])
+  }, [
+    isAnyFirebaseModuleEnabled,
+    registerDeviceWithPermission,
+    unregisterDevice,
+  ])
 
   useEffect(handleDeviceRegistration, [handleDeviceRegistration])
 
