a92d2f0c79b86a034e5362c22e875e3ba0c977b0
Frank Filius <f.filius@amsterdam.nl>
2024-06-03 11:19:33 +0200

    Undo refetching on screen focus

--- src/modules/construction-work/screens/ConstructionWork.screen.tsx
+++ src/modules/construction-work/screens/ConstructionWork.screen.tsx
@@ -1,27 +1,22 @@
 import {Screen} from '@/components/features/screen/Screen'
-import {useIsFocusedEffect} from '@/hooks/navigation/useIsFocusedEffect'
 import {SelectLocationTypeBottomSheet} from '@/modules/address/components/location/SelectLocationTypeBottomSheet'
 import {HighAccuracyPurposeKey} from '@/modules/address/types'
 import {Projects} from '@/modules/construction-work/components/projects/Projects'
 
-export const ConstructionWorkScreen = () => {
-  const isFocused = useIsFocusedEffect()
-
-  return isFocused ? (
-    <Screen
-      bottomSheet={
-        <SelectLocationTypeBottomSheet
-          highAccuracyPurposeKey={
-            HighAccuracyPurposeKey.PreciseLocationAddressConstructionWork
-          }
-        />
-      }
-      scroll={false}
-      testID="ConstructionWorkScreen"
-      withBottomInset={false}
-      withLeftInset={false}
-      withRightInset={false}>
-      <Projects />
-    </Screen>
-  ) : null
-}
+export const ConstructionWorkScreen = () => (
+  <Screen
+    bottomSheet={
+      <SelectLocationTypeBottomSheet
+        highAccuracyPurposeKey={
+          HighAccuracyPurposeKey.PreciseLocationAddressConstructionWork
+        }
+      />
+    }
+    scroll={false}
+    testID="ConstructionWorkScreen"
+    withBottomInset={false}
+    withLeftInset={false}
+    withRightInset={false}>
+    <Projects />
+  </Screen>
+)
