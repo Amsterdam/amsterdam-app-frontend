d0429e9fc37cabb506d6478208dbaa30817a6577
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-06-25 09:10:43 +0200

    Prevent iOS startup times logging > 3 minutes

--- src/processes/logging/hooks/useLogStartup.ts
+++ src/processes/logging/hooks/useLogStartup.ts
@@ -1,13 +1,22 @@
 import {useCallback} from 'react'
+import {Platform} from 'react-native'
 import {getStartupTimeSync} from 'react-native-device-info'
 import {EventLogKey} from '@/processes/logging/types'
-import {useAppInsights} from '@/providers/appinsights.provider'
+import {
+  APP_START_MAX_DURATION_IOS,
+  useAppInsights,
+} from '@/providers/appinsights.provider'
 
 export const useLogStartup = () => {
   const {appInsights} = useAppInsights()
 
   return useCallback(() => {
-    if (!__DEV__) {
+    if (
+      !__DEV__ &&
+      (Platform.OS !== 'ios' ||
+        new Date().getTime() - Number(getStartupTimeSync()) <
+          APP_START_MAX_DURATION_IOS)
+    ) {
       appInsights.trackMetric({
         name: EventLogKey.fullStartup,
         average: new Date().getTime() - Number(getStartupTimeSync()),
