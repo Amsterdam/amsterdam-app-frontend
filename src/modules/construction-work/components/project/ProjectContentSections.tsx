8af76d6436dce7bde82063e7a96c1476a5272558
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-06-26 09:08:22 +0200

    Add external links to construction-work project sections

--- src/modules/construction-work/components/project/ProjectContentSections.tsx
+++ src/modules/construction-work/components/project/ProjectContentSections.tsx
@@ -1,15 +1,19 @@
 import {Column} from '@/components/ui/layout/Column'
 import {HtmlContent} from '@/components/ui/text/HtmlContent'
+import {Link} from '@/components/ui/text/Link'
 import {Title} from '@/components/ui/text/Title'
 import {type TestProps} from '@/components/ui/types'
+import {useOpenWebUrl} from '@/hooks/linking/useOpenWebUrl'
 import {ProjectSection} from '@/modules/construction-work/types/api'
 
 type Props = {
   sections: ProjectSection[]
 } & TestProps
 
-export const ProjectContentSections = ({sections, testID}: Props) =>
-  sections.map(({body, title}, index) =>
+export const ProjectContentSections = ({sections, testID}: Props) => {
+  const openWebUrl = useOpenWebUrl()
+
+  return sections.map(({body, links, title}, index) =>
     body ? (
       <Column
         gutter="sm"
@@ -26,6 +30,16 @@ export const ProjectContentSections = ({sections, testID}: Props) =>
             testID={`${testID}${index}Html`}
           />
         )}
+        {links?.map(({url, label}, linkIndex) => (
+          <Link
+            key={label}
+            label={label}
+            onPress={() => openWebUrl(url)}
+            testID={`${testID}${index}Link${linkIndex}`}
+            variant="external"
+          />
+        ))}
       </Column>
     ) : null,
   )
+}
