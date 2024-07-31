2a377e3febe00bdce5ec1c78d7fde051ad36806d
Frank Filius <frank.filius@gmail.com>
2024-07-31 13:31:50 +0200

    Downgrade bottom-sheet package again to prevent a11y bug

--- src/components/ui/containers/BottomSheet.tsx
+++ src/components/ui/containers/BottomSheet.tsx
@@ -7,6 +7,7 @@ import BottomSheetOriginal, {
 import {type ReactNode, useCallback, useEffect, useRef} from 'react'
 import {SafeArea} from '@/components/ui/containers/SafeArea'
 import {type TestProps} from '@/components/ui/types'
+import {useIsReduceMotionEnabled} from '@/hooks/accessibility/useIsReduceMotionEnabled'
 import {useBlurEffect} from '@/hooks/navigation/useBlurEffect'
 import {useBottomSheet} from '@/store/slices/bottomSheet'
 
@@ -65,10 +66,18 @@ export const BottomSheet = ({
   ...rest
 }: Props) => {
   const {onChange: onChangeHandler, ref} = useBottomSheetHandler()
-  // const isReduceMotionEnabled = useIsReduceMotionEnabled()
+  const isReduceMotionEnabled = useIsReduceMotionEnabled()
 
   return (
     <BottomSheetOriginal
+      animateOnMount={!isReduceMotionEnabled}
+      animationConfigs={
+        isReduceMotionEnabled
+          ? {
+              duration: 1,
+            }
+          : undefined
+      }
       backdropComponent={Backdrop}
       enableDynamicSizing
       enablePanDownToClose
