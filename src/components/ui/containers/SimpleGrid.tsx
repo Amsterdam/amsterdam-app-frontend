d57e802492c03d3fc4eb0f48b5977ef74fa28aa4
Frank Filius <frank.filius@gmail.com>
2024-08-06 14:01:57 +0200

    Prevent super-grid from blocking previous component for Talkback

--- src/components/ui/containers/SimpleGrid.tsx
+++ src/components/ui/containers/SimpleGrid.tsx
@@ -1,14 +1,20 @@
+import {View} from 'react-native'
 // eslint-disable-next-line no-restricted-imports
 import {SimpleGrid as SimpleGridComponent} from 'react-native-super-grid'
 import {useIsScreenReaderEnabled} from '@/hooks/accessibility/useIsScreenReaderEnabled'
+
+const style = {backgroundColor: 'transparent'}
 
 export const SimpleGrid: typeof SimpleGridComponent = ({...props}) => {
   const isScreenReaderEnabled = useIsScreenReaderEnabled()
 
   return (
-    <SimpleGridComponent
-      {...props}
-      maxItemsPerRow={isScreenReaderEnabled ? 1 : props.maxItemsPerRow}
-    />
+    // This View component is needed to prevent SimpleGrid from blocking the previous element when using Talkback
+    <View style={style}>
+      <SimpleGridComponent
+        {...props}
+        maxItemsPerRow={isScreenReaderEnabled ? 1 : props.maxItemsPerRow}
+      />
+    </View>
   )
 }
