5151f955eab03b4b655f24d58de37c488f1b8b3b
Frank Filius <frank.filius@gmail.com>
2024-08-08 14:18:11 +0200

    Remove redundant promise wrapper

--- src/modules/city-pass/hooks/useLogout.ts
+++ src/modules/city-pass/hooks/useLogout.ts
@@ -8,18 +8,13 @@ export const useLogout = () => {
   const removeSecureItems = useRemoveSecureItems()
   // TODO: Call API to delete the user from the server once the API is available
 
-  return useCallback(
-    () =>
-      new Promise((resolve, reject) => {
-        setCityPassRegistered(false)
-        removeSecureItems([
-          SecureItemKey.cityPassAccessToken,
-          SecureItemKey.cityPassRefreshToken,
-          SecureItemKey.cityPasses,
-        ])
-          .then(resolve)
-          .catch(reject)
-      }),
-    [removeSecureItems, setCityPassRegistered],
-  )
+  return useCallback(() => {
+    setCityPassRegistered(false)
+
+    return removeSecureItems([
+      SecureItemKey.cityPassAccessToken,
+      SecureItemKey.cityPassRefreshToken,
+      SecureItemKey.cityPasses,
+    ])
+  }, [removeSecureItems, setCityPassRegistered])
 }
