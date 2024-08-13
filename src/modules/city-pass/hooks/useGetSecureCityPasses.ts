53d70345cc767ac1adc5d03728b09a9c3736e3ee
Frank Filius <frank.filius@gmail.com>
2024-08-13 11:53:02 +0200

    Improve syntax

--- src/modules/city-pass/hooks/useGetSecureCityPasses.ts
+++ src/modules/city-pass/hooks/useGetSecureCityPasses.ts
@@ -1,32 +1,25 @@
-import {useEffect, useState} from 'react'
+import {useMemo} from 'react'
 import {useGetSecureItem} from '@/hooks/secureStorage/useGetSecureItem'
-import {CityPassPass, SecureCityPass} from '@/modules/city-pass/types'
+import {SecureCityPass} from '@/modules/city-pass/types'
 import {SecureItemKey} from '@/utils/secureStorage'
 
 const transformResponse = (cityPasses: SecureCityPass[]) =>
-  cityPasses.reduce((acc: CityPassPass[], item) => {
-    const newItem = {
-      dateEndFormatted: item.d,
-      firstname: item.f,
-      ...(item.i && {infix: item.i}),
-      lastname: item.l,
-      passNumberComplete: item.p,
-    }
-
-    return [...acc, newItem]
-  }, [])
+  cityPasses.map(cityPass => ({
+    dateEndFormatted: cityPass.d,
+    firstname: cityPass.f,
+    ...(cityPass.i && {infix: cityPass.i}),
+    lastname: cityPass.l,
+    passNumberComplete: cityPass.p,
+  }))
 
 export const useGetSecureCityPasses = () => {
-  const [cityPasses, setCityPasses] = useState<SecureCityPass[]>([])
   const {item: secureCityPasses} = useGetSecureItem(SecureItemKey.cityPasses)
 
-  useEffect(() => {
-    if (secureCityPasses) {
-      const parsedCityPasses = JSON.parse(secureCityPasses) as SecureCityPass[]
-
-      setCityPasses(parsedCityPasses)
-    }
-  }, [secureCityPasses])
-
-  return transformResponse(cityPasses)
+  return useMemo(
+    () =>
+      secureCityPasses
+        ? transformResponse(JSON.parse(secureCityPasses) as SecureCityPass[])
+        : [],
+    [secureCityPasses],
+  )
 }
