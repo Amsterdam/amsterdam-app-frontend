0e38f9185557ac4786b568ffc426e77da46d740d
Frank Filius <frank.filius@gmail.com>
2024-08-14 16:40:45 +0200

    Rename var

--- ios/scripts/version-number.sh
+++ ios/scripts/version-number.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -n "$BUILD_NUMBER" ]; then
-    BUNDLE_VERSION=$BUILD_NUMBER
+    build_number=$BUILD_NUMBER
 else
-    BUNDLE_VERSION=1
+    build_number=1
 fi
 
 app_version=$(cat $PROJECT_DIR/../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
@@ -12,7 +12,7 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 for plist in "$target_plist" "$dsym_plist"; do
 if [ -f "$plist" ]; then
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUNDLE_VERSION" "$plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" "$plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $app_version" "$plist"
 fi
 done
