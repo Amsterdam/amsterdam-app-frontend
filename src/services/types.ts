599bcf075a91469e66669152186d6f4611c60ad0
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-08-27 16:08:39 +0200

    improve type

--- src/services/types.ts
+++ src/services/types.ts
@@ -5,27 +5,23 @@ import {
   FetchBaseQueryMeta,
 } from '@reduxjs/toolkit/query'
 
+type RTKQueryAPI = Pick<
+  BaseQueryApi,
+  'endpoint' | 'getState' | 'type' | 'extra' | 'forced' | 'dispatch'
+>
+
 export type PrepareHeaders = (
   headers: Headers,
-  api: Pick<
-    BaseQueryApi,
-    'endpoint' | 'getState' | 'type' | 'extra' | 'forced'
-  >,
+  api: Omit<RTKQueryAPI, 'dispatch'>,
 ) => Headers
 
 export type AfterBaseQuerySuccessFn<T = unknown> = (
   result: QueryReturnValue<T, FetchBaseQueryError, FetchBaseQueryMeta>,
-  api: Pick<
-    BaseQueryApi,
-    'endpoint' | 'getState' | 'type' | 'extra' | 'forced' | 'dispatch'
-  >,
+  api: RTKQueryAPI,
 ) => void | Promise<void>
 
 export type AfterBaseQueryErrorFn<T = unknown> = (
   result: QueryReturnValue<T, FetchBaseQueryError, FetchBaseQueryMeta>,
-  api: Pick<
-    BaseQueryApi,
-    'endpoint' | 'getState' | 'type' | 'extra' | 'forced' | 'dispatch'
-  >,
+  api: RTKQueryAPI,
   failRetry: (e?: unknown) => void,
 ) => void | Promise<void>
