8e9131320d522d1d808962035229cf9541c7f999
Frank Filius <frank.filius@gmail.com>
2024-09-24 13:34:17 +0200

    Add time to user chat balloon

--- src/modules/chat/components/ChatAgentName.tsx
+++ src/modules/chat/components/ChatAgentName.tsx
@@ -14,31 +14,50 @@ type Props = {
   message: ChatMessage
 }
 
+const getChatAgentName = (agent: ChatMessageAgent, timestamp: number) => {
+  const formattedTimestamp = dayjs(timestamp).format('HH:mm')
+
+  if (agent === ChatMessageAgent.user) {
+    return formattedTimestamp
+  }
+
+  if (agent === ChatMessageAgent.bot) {
+    return `${BOT_NAME} - ${formattedTimestamp}`
+  }
+
+  return `${EMPLOYEE_NAME} - ${formattedTimestamp}`
+}
+
 export const ChatAgentName = ({
   message: {agent, timestamp},
   isLastOfType,
 }: Props) => {
-  const styles = useThemable(createStyles)
+  const styles = useThemable(theme =>
+    createStyles(theme, agent === ChatMessageAgent.user),
+  )
 
-  return !!isLastOfType && agent !== ChatMessageAgent.user ? (
-    <>
-      <Gutter height="xs" />
-      <View style={styles.container}>
-        <Phrase
-          color="secondary"
-          testID={`ChatHistoryGroupName${agent}`}
-          variant="extraSmall">
-          {`${agent === ChatMessageAgent.bot ? BOT_NAME : EMPLOYEE_NAME} - ${dayjs(timestamp).format('HH:mm')}`}
-        </Phrase>
-      </View>
-    </>
-  ) : null
+  return (
+    !!isLastOfType && (
+      <>
+        <Gutter height="xs" />
+        <View style={styles.container}>
+          <Phrase
+            color="secondary"
+            testID={`ChatHistoryGroupName${agent}`}
+            variant="extraSmall">
+            {getChatAgentName(agent, timestamp)}
+          </Phrase>
+        </View>
+      </>
+    )
+  )
 }
 
 const AVATAR_DIMENSION = 40
-const createStyles = ({size}: Theme) =>
+const createStyles = ({size}: Theme, isUser: boolean) =>
   StyleSheet.create({
     container: {
-      paddingLeft: AVATAR_DIMENSION + size.spacing.sm,
+      paddingLeft: isUser ? undefined : AVATAR_DIMENSION + size.spacing.sm,
+      alignItems: isUser ? 'flex-end' : 'flex-start',
     },
   })
