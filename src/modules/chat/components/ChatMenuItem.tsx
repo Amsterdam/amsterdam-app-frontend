40efccf12da6950a1413d1c5c6a9af1e11fcfe07
Frank Filius <frank.filius@gmail.com>
2024-11-05 18:12:52 +0100

    Make chat menu items full width

--- src/modules/chat/components/ChatMenuItem.tsx
+++ src/modules/chat/components/ChatMenuItem.tsx
@@ -1,5 +1,6 @@
 import {Pressable} from '@/components/ui/buttons/Pressable'
 import {Box} from '@/components/ui/containers/Box'
+import {Size} from '@/components/ui/layout/Size'
 import {Phrase} from '@/components/ui/text/Phrase'
 import {type TestProps} from '@/components/ui/types'
 import {type Theme} from '@/themes/themes'
@@ -11,17 +12,19 @@ type Props = {
 } & TestProps
 
 export const ChatMenuItem = ({onPress, testID, label, color}: Props) => (
-  <Pressable
-    onPress={onPress}
-    testID={testID}>
-    <Box
-      insetHorizontal="md"
-      insetVertical="sm">
-      <Phrase
-        color={color}
-        testID={`${testID}Phrase`}>
-        {label}
-      </Phrase>
-    </Box>
-  </Pressable>
+  <Size width={'100%'}>
+    <Pressable
+      onPress={onPress}
+      testID={testID}>
+      <Box
+        insetHorizontal="md"
+        insetVertical="sm">
+        <Phrase
+          color={color}
+          testID={`${testID}Phrase`}>
+          {label}
+        </Phrase>
+      </Box>
+    </Pressable>
+  </Size>
 )
