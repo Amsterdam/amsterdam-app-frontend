7316c104ee3a1f0d88bd1dd2703267dd82df09ec
Frank Filius <f.filius@amsterdam.nl>
2025-01-06 10:46:17 +0100

    Refetch notification history each time the screen gets rendered

--- src/modules/notification-history/components/NotificationHistory.tsx
+++ src/modules/notification-history/components/NotificationHistory.tsx
@@ -1,24 +1,21 @@
 import {useCallback} from 'react'
 import {FlatList, type ListRenderItem} from 'react-native'
-import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
-import {FullScreenError} from '@/components/ui/feedback/error/FullScreenError'
-import {ConstructionWorkFigure} from '@/components/ui/media/errors/ConstructionWorkFigure'
 import {useBlurEffect} from '@/hooks/navigation/useBlurEffect'
-import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useModules} from '@/hooks/useModules'
-import {NotificationHistoryEmpty} from '@/modules/notification-history/components/NotificationHistoryEmpty'
 import {NotificationHistoryItem} from '@/modules/notification-history/components/NotificationHistoryItem'
 import {NotificationHistoryListFooter} from '@/modules/notification-history/components/NotificationHistoryListFooter'
+import {useMarkAllNotificationsReadMutation} from '@/modules/notification-history/service'
 import {
-  useGetNotificationsQuery,
-  useMarkAllNotificationsReadMutation,
-} from '@/modules/notification-history/service'
-import {type Notification} from '@/modules/notification-history/types'
+  GetNotificationsResult,
+  type Notification,
+} from '@/modules/notification-history/types'
 
-export const NotificationHistory = () => {
-  const {data, isLoading, isError, error} = useGetNotificationsQuery()
+type Props = {
+  data?: GetNotificationsResult
+}
+
+export const NotificationHistory = ({data}: Props) => {
   const {enabledModules} = useModules()
-  const navigation = useNavigation()
 
   const renderItem = useCallback<ListRenderItem<Notification>>(
     ({item}) => (
@@ -34,27 +31,6 @@ export const NotificationHistory = () => {
   useBlurEffect(() => {
     void markAllNotificationsRead()
   })
-
-  if (isLoading) {
-    return <PleaseWait testID="NotificationHistoryPleaseWait" />
-  }
-
-  if (isError) {
-    return (
-      <FullScreenError
-        buttonLabel="Ga terug"
-        error={error}
-        Image={ConstructionWorkFigure}
-        onPress={() => navigation.goBack()}
-        testID="NotificationHistoryError"
-        title="Er kunnen geen meldingen worden getoond"
-      />
-    )
-  }
-
-  if (data?.length === 0) {
-    return <NotificationHistoryEmpty />
-  }
 
   return (
     <FlatList
--- src/modules/notification-history/screens/NotificationHistory.screen.tsx
+++ src/modules/notification-history/screens/NotificationHistory.screen.tsx
@@ -1,10 +1,52 @@
+import {useFocusEffect} from '@react-navigation/core'
+import {useCallback} from 'react'
+import {NavigationProps} from '@/app/navigation/types'
 import {Screen} from '@/components/features/screen/Screen'
+import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
+import {FullScreenError} from '@/components/ui/feedback/error/FullScreenError'
+import {ConstructionWorkFigure} from '@/components/ui/media/errors/ConstructionWorkFigure'
 import {NotificationHistory} from '@/modules/notification-history/components/NotificationHistory'
+import {NotificationHistoryEmpty} from '@/modules/notification-history/components/NotificationHistoryEmpty'
+import {NotificationHistoryRouteName} from '@/modules/notification-history/routes'
+import {useGetNotificationsQuery} from '@/modules/notification-history/service'
 
-export const NotificationHistoryScreen = () => (
-  <Screen
-    scroll={false}
-    testID="NotificationHistoryScreen">
-    <NotificationHistory />
-  </Screen>
-)
+type Props = NavigationProps<NotificationHistoryRouteName.NotificationHistory>
+
+export const NotificationHistoryScreen = ({navigation}: Props) => {
+  const {data, isLoading, isError, error, refetch} = useGetNotificationsQuery()
+
+  useFocusEffect(
+    useCallback(() => {
+      void refetch()
+    }, [refetch]),
+  )
+
+  if (isLoading) {
+    return <PleaseWait testID="NotificationHistoryPleaseWait" />
+  }
+
+  if (isError) {
+    return (
+      <FullScreenError
+        buttonLabel="Ga terug"
+        error={error}
+        Image={ConstructionWorkFigure}
+        onPress={() => navigation.goBack()}
+        testID="NotificationHistoryError"
+        title="Er kunnen geen meldingen worden getoond"
+      />
+    )
+  }
+
+  if (data?.length === 0) {
+    return <NotificationHistoryEmpty />
+  }
+
+  return (
+    <Screen
+      scroll={false}
+      testID="NotificationHistoryScreen">
+      <NotificationHistory data={data} />
+    </Screen>
+  )
+}
