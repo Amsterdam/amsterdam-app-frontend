e16ed5851e2fbd4c39fd96d2f882360f8cd28a12
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-01-09 16:55:11 +0100

    fix android block screenshot

--- react-native-block-screenshot/android/src/main/java/com/blockscreenshot/BlockScreenshotModule.kt
+++ react-native-block-screenshot/android/src/main/java/com/blockscreenshot/BlockScreenshotModule.kt
@@ -1,20 +1,53 @@
 package com.blockscreenshot
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.Promise
 import com.facebook.react.module.annotations.ReactModule
+import android.view.WindowManager;
+import android.os.Bundle;
+import android.app.Activity;
+import com.facebook.react.bridge.Callback
 
 @ReactModule(name = BlockScreenshotModule.NAME)
 class BlockScreenshotModule(reactContext: ReactApplicationContext) :
   NativeBlockScreenshotSpec(reactContext) {
 
+  private val currentReactContext: ReactApplicationContext = reactContext
+
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun enableBlockScreenshot(params: ReadableMap, promise: Promise) {
+    val activity: Activity? = currentReactContext.currentActivity
+    if (activity != null) {
+      activity.runOnUiThread {
+        activity.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        promise.resolve(null)
+      }
+    } else {
+      promise.reject("E_NO_ACTIVITY", "No activity found")
+    }
+  }
+
+  override fun addListener(eventName: String) {
+    // Not implemented
+  }
+  override fun removeListeners(count: Double) {
+    // Not implemented
+  }
+
+  override fun disableBlockScreenshot(promise: Promise) {
+    val activity: Activity? = currentReactContext.currentActivity
+    if (activity != null) {
+      activity.runOnUiThread {
+        activity.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+        promise.resolve(null)
+      }
+    } else {
+      promise.reject("E_NO_ACTIVITY", "No activity found")
+    }
   }
 
   companion object {
