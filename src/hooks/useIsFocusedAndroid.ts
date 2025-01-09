a1a20a36dabfc86b9d3fc91249599a0ffaa4a680
Frank Filius <f.filius@amsterdam.nl>
2025-01-09 16:50:44 +0100

    Improve naming of hook

--- src/hooks/useIsFocusedAndroid.ts
+++ src/hooks/useIsFocusedAndroid.ts
@@ -1,4 +1,4 @@
 /**
  * Convenience hook to fix Android-specific issues: it passes through the result of the useIsFocused hook, but only for Android. Returns true for any other OS.
  */
-export const useIsFocusedOrNotAndroid = () => true
+export const useIsFocusedAndroid = () => true
