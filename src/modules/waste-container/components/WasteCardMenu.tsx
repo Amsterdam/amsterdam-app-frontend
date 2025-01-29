451488be42045f5dda9e2a10843a1b77dc85b0b8
Frank Filius <f.filius@amsterdam.nl>
2025-01-29 12:49:04 +0100

    Delete waste card with menu button

--- src/modules/waste-container/components/WasteCardMenu.tsx
+++ src/modules/waste-container/components/WasteCardMenu.tsx
@@ -1,16 +1,48 @@
+import {useCallback} from 'react'
+import {Alert} from 'react-native'
 import {PopUpMenu} from '@/components/ui/menus/PopUpMenu'
 import {PopupMenuItem, PopupMenuOrientation} from '@/components/ui/menus/types'
+import {useNavigation} from '@/hooks/navigation/useNavigation'
+import {useDispatch} from '@/hooks/redux/useDispatch'
 import {useSelector} from '@/hooks/redux/useSelector'
-import {selectIsMenuVisible} from '@/modules/waste-container/slice'
+import {useRemoveSecureItems} from '@/hooks/secureStorage/useRemoveSecureItems'
+import {
+  selectIsMenuVisible,
+  setIsMenuVisible,
+} from '@/modules/waste-container/slice'
+import {SecureItemKey} from '@/utils/secureStorage'
 
 export const WasteCardMenu = () => {
+  const navigation = useNavigation()
+  const dispatch = useDispatch()
   const isMenuVisible = useSelector(selectIsMenuVisible)
+  const removeSecureItems = useRemoveSecureItems()
+
+  const onPress = useCallback(() => {
+    dispatch(setIsMenuVisible(false))
+    Alert.alert(
+      'Weet u zeker dat u de pas wilt verwijderen?',
+      'Deze actie kan niet ongedaan worden gemaakt.',
+      [
+        {text: 'Annuleren', style: 'cancel', onPress: () => null},
+        {
+          text: 'Verwijderen',
+          onPress: () => {
+            void removeSecureItems([SecureItemKey.wasteCardNumber]).then(() => {
+              navigation.pop()
+            })
+          },
+        },
+      ],
+      {cancelable: true},
+    )
+  }, [dispatch, navigation, removeSecureItems])
 
   const menuItems: PopupMenuItem[] = [
     {
       color: 'warning',
       label: 'Verwijder afvalpas',
-      onPress: () => null,
+      onPress,
       testID: 'WasteCardMenuDeleteButton',
     },
   ]
