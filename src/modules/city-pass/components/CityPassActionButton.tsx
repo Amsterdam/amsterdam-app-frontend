ab613c1403809928524ae5a72de595b6436c7e18
Frank Filius <f.filius@amsterdam.nl>
2025-03-04 16:17:56 +0100

    Don't show action button when not registered as pass owner

--- src/modules/city-pass/components/CityPassActionButton.tsx
+++ src/modules/city-pass/components/CityPassActionButton.tsx
@@ -3,13 +3,16 @@ import {ActionButton} from '@/components/ui/buttons/ActionButton'
 import {Column} from '@/components/ui/layout/Column'
 import {Gutter} from '@/components/ui/layout/Gutter'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
+import {useSelector} from '@/hooks/redux/useSelector'
 import {useGetSecureCityPasses} from '@/modules/city-pass/hooks/useGetSecureCityPasses'
 import {CityPassRouteName} from '@/modules/city-pass/routes'
+import {selectIsCityPassOwnerRegistered} from '@/modules/city-pass/slice'
 import {ModuleSlug} from '@/modules/slugs'
 
 export const CityPassActionButton = () => {
   const {navigate} = useNavigation()
   const secureCityPasses = useGetSecureCityPasses()
+  const isCityPassOwnerRegistered = useSelector(selectIsCityPassOwnerRegistered)
 
   const onPress = useCallback(() => {
     navigate(ModuleSlug['city-pass'], {
@@ -17,7 +20,7 @@ export const CityPassActionButton = () => {
     })
   }, [navigate])
 
-  if (!secureCityPasses?.length) {
+  if (!secureCityPasses?.length || !isCityPassOwnerRegistered) {
     return null
   }
 
