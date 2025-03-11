2297a3d1468df2f00a7d806d6dcd27e56898a2c7
Frank Filius <f.filius@amsterdam.nl>
2025-03-11 13:15:14 +0100

    Increase header button size

---
+++ src/modules/parking/components/license-plates/AddLicensePlateHeaderButton.tsx
@@ -0,0 +1,51 @@
+import {skipToken} from '@reduxjs/toolkit/query'
+import {IconButton} from '@/components/ui/buttons/IconButton'
+import {Icon} from '@/components/ui/media/Icon'
+import {useNavigation} from '@/hooks/navigation/useNavigation'
+import {MAX_LICENSE_PLATES} from '@/modules/parking/constants'
+import {useGetCurrentPermit} from '@/modules/parking/hooks/useGetCurrentPermit'
+import {useGetSecureParkingAccount} from '@/modules/parking/hooks/useGetSecureParkingAccount'
+import {ParkingRouteName} from '@/modules/parking/routes'
+import {useLicensePlatesQuery} from '@/modules/parking/service'
+
+export const AddLicensePlateHeaderButton = () => {
+  const {navigate} = useNavigation()
+  const {secureParkingAccount, isLoading: isLoadingSecureParkingAccount} =
+    useGetSecureParkingAccount()
+  const {data: licensePlates, isLoading} = useLicensePlatesQuery(
+    secureParkingAccount
+      ? {
+          accessToken: secureParkingAccount.accessToken,
+          reportCode: secureParkingAccount.reportCode,
+        }
+      : skipToken,
+  )
+  const {currentPermit, isLoading: isLoadingCurrentPermit} =
+    useGetCurrentPermit()
+
+  if (
+    isLoadingSecureParkingAccount ||
+    isLoading ||
+    isLoadingCurrentPermit ||
+    currentPermit?.forced_license_plate_list ||
+    !licensePlates ||
+    licensePlates?.length >= MAX_LICENSE_PLATES
+  ) {
+    return null
+  }
+
+  return (
+    <IconButton
+      accessibilityLabel="Voeg een kenteken toe"
+      icon={
+        <Icon
+          color="link"
+          name="add"
+          size="lgx"
+        />
+      }
+      onPress={() => navigate(ParkingRouteName.addLicensePlate)}
+      testID="ParkingAddLicensePlateHeaderButton"
+    />
+  )
+}
