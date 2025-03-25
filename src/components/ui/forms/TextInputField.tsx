ebf9cffd94b6ad2c43478c43c5bd6a471d156fd9
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-03-25 09:21:01 +0100

    fixes

--- src/components/ui/forms/TextInputField.tsx
+++ src/components/ui/forms/TextInputField.tsx
@@ -1,5 +1,6 @@
+import {forwardRef} from 'react'
 import {Controller, UseControllerProps} from 'react-hook-form'
-import {TextInputProps} from 'react-native'
+import {TextInputProps, TextInput as TextInputRN} from 'react-native'
 import {CharactersLeftDisplay} from '@/components/ui/forms/CharactersLeftDisplay'
 import {TextInput, TextInputSharedProps} from '@/components/ui/forms/TextInput'
 import {Column} from '@/components/ui/layout/Column'
@@ -14,69 +15,83 @@ type Props = {
   UseControllerProps &
   Pick<
     TextInputProps,
-    'autoFocus' | 'autoCapitalize' | 'autoComplete' | 'autoCorrect'
+    | 'autoFocus'
+    | 'autoCapitalize'
+    | 'autoComplete'
+    | 'autoCorrect'
+    | 'keyboardType'
+    | 'enterKeyHint'
+    | 'onSubmitEditing'
+    | 'submitBehavior'
+    | 'returnKeyType'
   >
 
-export const TextInputField = ({
-  autoFocus,
-  defaultValue = '',
-  inputInstructions,
-  label,
-  maxCharacters,
-  numberOfLines,
-  name,
-  placeholder,
-  rules,
-  testID,
-  textTransform,
-  ...textInputProps
-}: Props) => {
-  const accessibilityAnnounce = useAccessibilityAnnounce()
+export const TextInputField = forwardRef<TextInputRN, Props>(
+  (
+    {
+      autoFocus,
+      defaultValue = '',
+      inputInstructions,
+      label,
+      maxCharacters,
+      numberOfLines,
+      name,
+      placeholder,
+      rules,
+      testID,
+      textTransform,
+      ...textInputProps
+    }: Props,
+    ref,
+  ) => {
+    const accessibilityAnnounce = useAccessibilityAnnounce()
 
-  return (
-    <Controller
-      defaultValue={defaultValue as string}
-      name={name}
-      render={({field: {onChange, value}, fieldState: {error}}) => {
-        error?.message && accessibilityAnnounce(error.message)
+    return (
+      <Controller
+        defaultValue={defaultValue as string}
+        name={name}
+        render={({field: {onChange, value}, fieldState: {error}}) => {
+          error?.message && accessibilityAnnounce(error.message)
 
-        return (
-          <Column gutter="sm">
-            <Column gutter="xs">
-              <TextInput
-                accessibilityLabel={label}
-                accessibilityLanguage="nl-NL"
-                autoFocus={autoFocus}
-                inputInstructions={inputInstructions}
-                label={label}
-                multiline={!!numberOfLines}
-                numberOfLines={numberOfLines}
-                onChangeText={onChange}
-                placeholder={placeholder}
-                testID={`${testID}Input`}
-                textTransform={textTransform}
-                value={value as string}
-                warning={!!error}
-                {...textInputProps}
-              />
+          return (
+            <Column gutter="sm">
+              <Column gutter="xs">
+                <TextInput
+                  accessibilityLabel={label}
+                  accessibilityLanguage="nl-NL"
+                  autoFocus={autoFocus}
+                  inputInstructions={inputInstructions}
+                  label={label}
+                  multiline={!!numberOfLines}
+                  numberOfLines={numberOfLines}
+                  onChangeText={onChange}
+                  placeholder={placeholder}
+                  ref={ref}
+                  testID={`${testID}Input`}
+                  textTransform={textTransform}
+                  value={value as string}
+                  warning={!!error}
+                  {...textInputProps}
+                />
+              </Column>
+              {!!maxCharacters && (
+                <CharactersLeftDisplay
+                  maxCharacters={maxCharacters}
+                  numOfCharacters={(value as string).length}
+                />
+              )}
+              {!!error && (
+                <Paragraph
+                  color="warning"
+                  testID={`${testID}ErrorText`}>
+                  {error.message}
+                </Paragraph>
+              )}
             </Column>
-            {!!maxCharacters && (
-              <CharactersLeftDisplay
-                maxCharacters={maxCharacters}
-                numOfCharacters={(value as string).length}
-              />
-            )}
-            {!!error && (
-              <Paragraph
-                color="warning"
-                testID={`${testID}ErrorText`}>
-                {error.message}
-              </Paragraph>
-            )}
-          </Column>
-        )
-      }}
-      rules={rules}
-    />
-  )
-}
+          )
+        }}
+        rules={rules}
+      />
+    )
+  },
+)
--- src/modules/parking/components/login/ParkingLoginForm.tsx
+++ src/modules/parking/components/login/ParkingLoginForm.tsx
@@ -1,24 +1,95 @@
+import {useRef} from 'react'
+import {useFormContext} from 'react-hook-form'
+import {Platform, TextInput} from 'react-native'
+import {Button} from '@/components/ui/buttons/Button'
+import {SomethingWentWrong} from '@/components/ui/feedback/SomethingWentWrong'
 import {TextInputField} from '@/components/ui/forms/TextInputField'
 import {Column} from '@/components/ui/layout/Column'
+import {Gutter} from '@/components/ui/layout/Gutter'
+import {useSetSecureParkingAccount} from '@/modules/parking/hooks/useSetSecureParkingAccount'
+import {useLoginParkingMutation} from '@/modules/parking/service'
+import {useCurrentParkingAccount} from '@/modules/parking/slice'
+import {
+  ParkingAccountLogin,
+  ParkingLoginEndpointRequest,
+} from '@/modules/parking/types'
 
-export const ParkingLoginForm = () => (
-  <Column gutter="md">
-    <TextInputField
-      autoFocus
-      label="Meldcode"
-      name="reportCode"
-      rules={{
-        required: 'Vul een meldcode in',
-      }}
-      testID="ParkingLoginFormReportCodeInputField"
-    />
-    <TextInputField
-      label="Pincode"
-      name="pin"
-      rules={{
-        required: 'Vul een pincode in',
-      }}
-      testID="ParkingLoginFormPinCodeInputField"
-    />
-  </Column>
-)
+export const ParkingLoginForm = () => {
+  const pincodeRef = useRef<TextInput | null>(null)
+
+  const {setCurrentAccountType} = useCurrentParkingAccount()
+  const {handleSubmit} = useFormContext<ParkingAccountLogin>()
+  const [loginParking, {error, isError, isLoading}] = useLoginParkingMutation()
+  const isForbiddenError = error && 'status' in error && error.status === 403
+  const setSecureParkingAccount = useSetSecureParkingAccount()
+
+  const errorSentence = isForbiddenError
+    ? 'Controleer uw meldcode en pincode en probeer het opnieuw.'
+    : 'Er is iets misgegaan. Probeer het opnieuw.'
+
+  const onSubmit = handleSubmit(({pin, reportCode}) => {
+    void loginParking({
+      pin,
+      report_code: reportCode,
+    } as ParkingLoginEndpointRequest)
+      .unwrap()
+      .then(({access_token, scope}) => {
+        setCurrentAccountType(scope)
+        void setSecureParkingAccount({
+          accessToken: access_token,
+          pin,
+          reportCode,
+          scope,
+        })
+      })
+  })
+
+  return (
+    <Column gutter="md">
+      <TextInputField
+        autoFocus
+        // enterKeyHint="next"
+        keyboardType="number-pad"
+        label="Meldcode"
+        name="reportCode"
+        onSubmitEditing={() => {
+          pincodeRef.current?.focus()
+        }}
+        returnKeyType={Platform.OS !== 'ios' ? 'done' : undefined}
+        rules={{
+          required: 'Vul een meldcode in',
+        }}
+        submitBehavior="submit"
+        testID="ParkingLoginFormReportCodeInputField"
+      />
+      <TextInputField
+        enterKeyHint="send"
+        keyboardType="number-pad"
+        label="Pincode"
+        name="pin"
+        onSubmitEditing={onSubmit}
+        ref={pincodeRef}
+        rules={{
+          required: 'Vul een pincode in',
+        }}
+        testID="ParkingLoginFormPinCodeInputField"
+      />
+
+      {!!isError && !isLoading && (
+        <>
+          <SomethingWentWrong
+            testID="ParkingLoginFormSomethingWentWrong"
+            text={errorSentence}
+            title="Inloggen mislukt"
+          />
+          <Gutter height="lg" />
+        </>
+      )}
+      <Button
+        label="Inloggen"
+        onPress={onSubmit}
+        testID="ParkingLoginFormSubmitButton"
+      />
+    </Column>
+  )
+}
