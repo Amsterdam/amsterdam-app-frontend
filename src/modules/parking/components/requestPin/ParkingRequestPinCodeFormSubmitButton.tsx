847d19ab1b69a0a8223e11f8df26c1aeef4ca915
Frank Filius <f.filius@amsterdam.nl>
2025-05-28 09:06:45 +0200

    WIP

--- src/modules/parking/components/requestPin/ParkingRequestPinCodeFormSubmitButton.tsx
+++ src/modules/parking/components/requestPin/ParkingRequestPinCodeFormSubmitButton.tsx
@@ -1,14 +1,15 @@
 import {useFormContext} from 'react-hook-form'
 import {Button} from '@/components/ui/buttons/Button'
 import {Box} from '@/components/ui/containers/Box'
+import {useParkingPinCodeMutation} from '@/modules/parking/service'
 import {RequestPinCode} from '@/modules/parking/types'
-import {devLog} from '@/processes/development'
 
 export const ParkingRequestPincodeFormSubmitButton = () => {
   const {handleSubmit} = useFormContext<RequestPinCode>()
+  const [pincode] = useParkingPinCodeMutation()
 
-  const onSubmit = handleSubmit(({phoneLastFourDigits, reportCode}) => {
-    devLog({phoneLastFourDigits, reportCode})
+  const onSubmit = handleSubmit(async ({phoneLastFourDigits, reportCode}) => {
+    await pincode({phoneLastFourDigits, reportCode})
   })
 
   return (
