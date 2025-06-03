5896235d33aeab45e7c6cd67bc9624220447d397
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-06-03 11:47:18 +0200

    Improve parking permit names

---
+++ src/modules/parking/utils/fixPermitNames.test.ts
@@ -0,0 +1,53 @@
+import {ParkingPermit} from '@/modules/parking/types'
+import {fixPermitNames} from '@/modules/parking/utils/fixPermitNames'
+
+describe('fixPermitNames', () => {
+  it('voegt report_code toe aan permit_name als er meerdere permits met hetzelfde type zijn', () => {
+    const permits: ParkingPermit[] = [
+      {
+        permit_type: 'A',
+        permit_name: 'Parkeerbewijs',
+        report_code: '123',
+        id: 1,
+      } as unknown as ParkingPermit,
+      {
+        permit_type: 'A',
+        permit_name: 'Parkeerbewijs',
+        report_code: '456',
+        id: 2,
+      } as unknown as ParkingPermit,
+      {
+        permit_type: 'B',
+        permit_name: 'Ander bewijs',
+        report_code: '789',
+        id: 3,
+      } as unknown as ParkingPermit,
+    ]
+    const result = fixPermitNames(permits)
+
+    expect(result[0].permit_name).toBe('Parkeerbewijs (123)')
+    expect(result[1].permit_name).toBe('Parkeerbewijs (456)')
+    expect(result[2].permit_name).toBe('Ander bewijs')
+  })
+
+  it('laat permit_name ongemoeid als het type uniek is', () => {
+    const permits: ParkingPermit[] = [
+      {
+        permit_type: 'A',
+        permit_name: 'Parkeerbewijs',
+        report_code: '123',
+        id: 1,
+      } as unknown as ParkingPermit,
+      {
+        permit_type: 'B',
+        permit_name: 'Ander parkeerbewijs',
+        report_code: '789',
+        id: 2,
+      } as unknown as ParkingPermit,
+    ]
+    const result = fixPermitNames(permits)
+
+    expect(result[0].permit_name).toBe('Parkeerbewijs')
+    expect(result[1].permit_name).toBe('Ander parkeerbewijs')
+  })
+})
---
+++ src/modules/parking/utils/fixPermitNames.ts
@@ -0,0 +1,26 @@
+import {ParkingPermit} from '@/modules/parking/types'
+
+/*
+ * This function checks whether there are 2 permits with the same type and if so it appends the report code of the permit to the name.
+ */
+export const fixPermitNames = (permits: ParkingPermit[]) => {
+  const permitNameCounts = permits.reduce(
+    (acc, permit) => {
+      acc[permit.permit_type] = (acc[permit.permit_type] || 0) + 1
+
+      return acc
+    },
+    {} as Record<string, number>,
+  )
+
+  return permits.map(permit => {
+    if (permitNameCounts[permit.permit_type] > 1) {
+      return {
+        ...permit,
+        permit_name: `${permit.permit_name} (${permit.report_code})`,
+      }
+    }
+
+    return permit
+  })
+}
