371c9f44179cc401d4688b0d86ade320b11599a1
Frank Filius <f.filius@amsterdam.nl>
2025-06-17 13:41:47 +0200

    Enable user to get out of the login screen when login in with another account

--- src/modules/parking/screens/ParkingLogin.screen.tsx
+++ src/modules/parking/screens/ParkingLogin.screen.tsx
@@ -1,14 +1,31 @@
+import {useEffect} from 'react'
 import {Screen} from '@/components/features/screen/Screen'
 import {Box} from '@/components/ui/containers/Box'
 import {Column} from '@/components/ui/layout/Column'
 import {Title} from '@/components/ui/text/Title'
 import {useBlurEffect} from '@/hooks/navigation/useBlurEffect'
+import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {ParkingLoginForm} from '@/modules/parking/components/login/ParkingLoginForm'
 import {useShouldShowLoginScreen} from '@/modules/parking/hooks/useShouldShowLoginScreen'
+import {useIsLoggingInAdditionalAccount} from '@/modules/parking/slice'
 
 export const ParkingLoginScreen = () => {
   const {shouldShowLoginScreen, setShouldShowLoginScreen} =
     useShouldShowLoginScreen()
+  const {isLoggingInAdditionalAccount, setIsLoggingInAdditionalAccount} =
+    useIsLoggingInAdditionalAccount()
+  const navigation = useNavigation()
+
+  useEffect(
+    () =>
+      navigation.addListener('beforeRemove', e => {
+        if (isLoggingInAdditionalAccount) {
+          e.preventDefault()
+          setIsLoggingInAdditionalAccount(false)
+        }
+      }),
+    [isLoggingInAdditionalAccount, setIsLoggingInAdditionalAccount, navigation],
+  )
 
   useBlurEffect(() => {
     shouldShowLoginScreen && setShouldShowLoginScreen(false)
