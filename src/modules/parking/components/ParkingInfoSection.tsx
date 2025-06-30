fbb809677de1af107c832919acd6f08fa997fbab
Frank Filius <f.filius@amsterdam.nl>
2025-06-30 13:31:53 +0200

    Replace manual implementation of redirect button by standard one

--- src/modules/parking/components/ParkingInfoSection.tsx
+++ src/modules/parking/components/ParkingInfoSection.tsx
@@ -1,36 +1,26 @@
-import {Button} from '@/components/ui/buttons/Button'
+import {RedirectButton} from '@/components/ui/buttons/RedirectButton'
 import {Column} from '@/components/ui/layout/Column'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
-import {useOpenWebUrl} from '@/hooks/linking/useOpenWebUrl'
-import {useGetRedirectUrlsQuery} from '@/modules/redirects/service'
+import {RedirectKey} from '@/modules/redirects/types'
 
-export const ParkingInfoSection = () => {
-  const openWebUrl = useOpenWebUrl()
-  const {data: redirectUrls} = useGetRedirectUrlsQuery()
-
-  return (
-    <Column gutter="md">
-      <Title
-        level="h2"
-        text="Alles over parkeren"
+export const ParkingInfoSection = () => (
+  <Column gutter="md">
+    <Title
+      level="h2"
+      text="Alles over parkeren"
+    />
+    <Column gutter="lg">
+      <Paragraph variant="intro">
+        In de app regelt u parkeersessies. Op de website staat meer informatie,
+        zoals over vergunningen, zones, boetes en tarieven.
+      </Paragraph>
+      <RedirectButton
+        label="Lees meer over parkeren"
+        redirectKey={RedirectKey.parking}
+        testID="ParkingHomeParkingInformationButton"
+        variant="secondary"
       />
-      <Column gutter="lg">
-        <Paragraph variant="intro">
-          In de app regelt u parkeersessies. Op de website staat meer
-          informatie, zoals over vergunningen, zones, boetes en tarieven.
-        </Paragraph>
-        <Button
-          iconName="external-link"
-          iconSize="md"
-          label="Lees meer over parkeren"
-          onPress={() => {
-            redirectUrls && openWebUrl(redirectUrls?.parking)
-          }}
-          testID="ParkingHomeParkingInformationButton"
-          variant="secondary"
-        />
-      </Column>
     </Column>
-  )
-}
+  </Column>
+)
