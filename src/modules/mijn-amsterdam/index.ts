e1f091aff14edd2a7da8d7cc3913a895759cef5f
Frank Filius <f.filius@amsterdam.nl>
2025-07-31 14:25:29 +0200

    Add Mijn Amsterdam module

---
+++ src/modules/mijn-amsterdam/index.ts
@@ -0,0 +1,8 @@
+import {ModuleSlug} from '@/modules/slugs'
+import {ModuleClientConfig} from '@/modules/types'
+
+export const mijnAmsterdamModule: ModuleClientConfig = {
+  hiddenInMenu: true,
+  name: 'MijnAmsterdamModule',
+  slug: ModuleSlug['mijn-amsterdam'],
+}
