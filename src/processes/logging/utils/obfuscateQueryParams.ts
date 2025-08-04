23e5f246548cdec0042299073e95754902033fcf
Frank Filius <f.filius@amsterdam.nl>
2025-08-04 15:13:34 +0200

    Obfuscate all query params

--- src/processes/logging/utils/obfuscateQueryParams.ts
+++ src/processes/logging/utils/obfuscateQueryParams.ts
@@ -1,4 +1,11 @@
-export const obfuscateQueryParams = (str?: string) =>
-  str
-    ?.replace(/(report_code=)[^&]+/gi, '$1***')
-    .replace(/(vehicle_id=)[^&]+/gi, '$1***')
+export const obfuscateQueryParams = (str?: string) => {
+  if (!str) {
+    return str
+  }
+
+  // Replace all query param values (plain and encoded)
+  // Matches: ?foo=bar&baz=qux or &foo=bar
+  return str
+    .replace(/([?&][^=&#]+)=([^&#]*)/g, '$1=***')
+    .replace(/(%3F|%26)([^=&#]+)%3D([^&#]*)/gi, '$1$2%3D***')
+}
