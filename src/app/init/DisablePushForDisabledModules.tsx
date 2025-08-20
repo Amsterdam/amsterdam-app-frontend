2bd5c5a4e5325cd4342a279d962555715bd48ab4
Frank Filius <f.filius@amsterdam.nl>
2025-08-20 17:47:53 +0200

    Only disable push for push modules

--- src/app/init/DisablePushForDisabledModules.tsx
+++ src/app/init/DisablePushForDisabledModules.tsx
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 import {ApiSlug} from '@/environment'
 import {usePermission} from '@/hooks/permissions/usePermission'
 import {useSelector} from '@/hooks/redux/useSelector'
+import {clientModules} from '@/modules/modules'
 import {useAddDisabledPushModuleMutation} from '@/modules/user/service'
 import {selectDisabledModules} from '@/store/slices/modules'
 import {Permissions} from '@/types/permissions'
@@ -14,7 +15,13 @@ export const DisablePushForDisabledModules = () => {
   useEffect(() => {
     if (hasPermission) {
       userDisabledModulesBySlug.forEach(module => {
-        void addDisabledPushModule(module as ApiSlug) // TODO this will be 1 call when this ticket is done: #152184
+        const clientModule = clientModules.find(
+          c => c.slug === (module as ApiSlug),
+        )
+
+        if (clientModule?.requiresFirebaseToken) {
+          void addDisabledPushModule(module as ApiSlug) // TODO this will be 1 call when this ticket is done: #152184
+        }
       })
     }
   }, [addDisabledPushModule, hasPermission, userDisabledModulesBySlug])
