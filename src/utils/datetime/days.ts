1a96ba65705726fcd376dc9e699c7392617572be
Frank Filius <f.filius@amsterdam.nl>
2025-09-26 14:39:07 +0200

    Remove unused date utils from project

--- src/utils/datetime/days.ts
+++
@@ -1,9 +0,0 @@
-export const days = [
-  'zondag',
-  'maandag',
-  'dinsdag',
-  'woensdag',
-  'donderdag',
-  'vrijdag',
-  'zaterdag',
-]
--- src/utils/datetime/parseTime.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Extracts a time (hh:mm) from a string.
- * @param input
- */
-export const parseTime = (input: string): string | undefined => {
-  const time = input.match(/\b[0-9]{2}\.[0-9]{2}\b/)
-
-  return time?.[0]
-}
