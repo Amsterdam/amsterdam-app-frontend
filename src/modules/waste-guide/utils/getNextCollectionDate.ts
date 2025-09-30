fb8026a048a5d6e961dea6615015be2225e07dd5
Frank Filius <f.filius@amsterdam.nl>
2025-09-30 12:27:00 +0200

    Show vandaag en morgen for ophaaldag

--- src/modules/waste-guide/utils/getNextCollectionDate.ts
+++ src/modules/waste-guide/utils/getNextCollectionDate.ts
@@ -2,7 +2,22 @@ import {WasteType} from '@/modules/waste-guide/types'
 import {capitalizeString} from '@/utils/capitalizeString'
 import {dayjs} from '@/utils/datetime/dayjs'
 
-export const getNextCollectionDate = (fraction: WasteType) =>
-  fraction.next_date
-    ? capitalizeString(dayjs(fraction.next_date).format('dddd D MMMM'))
-    : ''
+export const getNextCollectionDate = (fraction: WasteType) => {
+  const nextDate = dayjs(fraction.next_date)
+
+  const getDateToDisplay = () => {
+    if ((dayjs().isSame(nextDate), 'day')) {
+      return 'vandaag'
+    }
+
+    if (dayjs().add(1, 'day').isSame(nextDate, 'day')) {
+      return 'morgen'
+    }
+
+    return nextDate.format('dddd D MMMM')
+  }
+
+  const dateToDisplay = getDateToDisplay()
+
+  return fraction.next_date ? capitalizeString(dateToDisplay) : ''
+}
