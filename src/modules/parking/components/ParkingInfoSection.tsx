200b660fb2b93d88bfbc54d4783fdf1416fd077e
Frank Filius <f.filius@amsterdam.nl>
2025-10-08 11:48:05 +0200

    UX request

--- src/modules/parking/components/ParkingInfoSection.tsx
+++ src/modules/parking/components/ParkingInfoSection.tsx
@@ -4,18 +4,14 @@ import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
 import {RedirectKey} from '@/modules/redirects/types'
 
-type Props = {
-  isIntro?: boolean
-}
-
-export const ParkingInfoSection = ({isIntro}: Props) => (
+export const ParkingInfoSection = () => (
   <Column gutter="md">
     <Title
       level="h2"
       text="Alles over parkeren"
     />
     <Column gutter="lg">
-      <Paragraph variant={isIntro ? 'intro' : 'body'}>
+      <Paragraph>
         In de app regelt u parkeersessies. Op de website staat meer informatie,
         zoals over vergunningen, zones, boetes en tarieven.
       </Paragraph>
