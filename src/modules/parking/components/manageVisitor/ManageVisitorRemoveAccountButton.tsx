a43432f264d83da70751b82f3c98fb4f7878b278
Frank Filius <f.filius@amsterdam.nl>
2025-10-08 13:29:15 +0200

    Show isLoading until account is removed from state

--- src/modules/parking/components/manageVisitor/ManageVisitorRemoveAccountButton.tsx
+++ src/modules/parking/components/manageVisitor/ManageVisitorRemoveAccountButton.tsx
@@ -1,4 +1,4 @@
-import {useCallback} from 'react'
+import {useCallback, useState} from 'react'
 import {Alert} from 'react-native'
 import {Button} from '@/components/ui/buttons/Button'
 import {useCurrentParkingPermit} from '@/modules/parking/hooks/useCurrentParkingPermit'
@@ -6,8 +6,8 @@ import {useManageVisitorRemoveAccountMutation} from '@/modules/parking/service'
 
 export const ManageVisitorRemoveAccountButton = () => {
   const currentPermit = useCurrentParkingPermit()
-  const [removeAccount, {isLoading, isError}] =
-    useManageVisitorRemoveAccountMutation()
+  const [isButtonPressed, setIsButtonPressed] = useState(false)
+  const [removeAccount, {isError}] = useManageVisitorRemoveAccountMutation()
   const onPress = useCallback(() => {
     Alert.alert(
       'Weet u zeker dat u het bezoekersaccount wilt verwijderen?',
@@ -21,6 +21,7 @@ export const ManageVisitorRemoveAccountButton = () => {
           text: 'Verwijderen',
           style: 'destructive',
           onPress: () => {
+            setIsButtonPressed(true)
             void removeAccount(currentPermit.report_code)
           },
         },
@@ -32,7 +33,7 @@ export const ManageVisitorRemoveAccountButton = () => {
   return (
     <Button
       isError={isError}
-      isLoading={isLoading}
+      isLoading={!!isButtonPressed && !!currentPermit.visitor_account}
       label="Verwijder account"
       onPress={onPress}
       testID="ParkingManageVisitorRemoveAccountButton"
