c5e43f7e1326a06c266f16263c91776de14ee358
Wouter van der Laan <woutervanderlaan@macbook-pro-van-wouter.home>
2025-10-09 11:06:24 +0200

    Add sections to user sreen, adjust typing

--- src/modules/user/components/UserMenu.tsx
+++ src/modules/user/components/UserMenu.tsx
@@ -1,16 +1,22 @@
 import {View} from 'react-native'
+import {CopyButton} from '@/components/ui/buttons/CopyButton'
 import {NavigationButton} from '@/components/ui/buttons/NavigationButton'
+import {Box} from '@/components/ui/containers/Box'
 import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
 import {Column} from '@/components/ui/layout/Column'
+import {Gutter} from '@/components/ui/layout/Gutter'
 import {Title} from '@/components/ui/text/Title'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useModules} from '@/hooks/useModules'
 import {useAccessCodeBiometrics} from '@/modules/access-code/hooks/useAccessCodeBiometrics'
 import {useGetSecureAccessCode} from '@/modules/access-code/hooks/useGetSecureAccessCode'
 import {AddressRouteName} from '@/modules/address/routes'
+import {OnboardingRouteName} from '@/modules/onboarding/routes'
 import {ModuleSlug} from '@/modules/slugs'
 import {UserRouteName} from '@/modules/user/routes'
 import {UserMenuSection, UserMenuSectionItem} from '@/modules/user/types'
+import {SHA256EncryptedDeviceId} from '@/utils/encryption'
+import {VERSION_NUMBER_WITH_BUILD} from '@/utils/version'
 
 const accessCodeSection: UserMenuSection = {
   title: 'Beveiliging',
@@ -54,6 +60,39 @@ const getSections = (
   },
 ]
 
+const getAboutSections = (): UserMenuSection[] => [
+  {
+    title: 'Hulp en informatie',
+    navigationItems: [
+      {
+        label: 'Over deze app',
+        route: UserRouteName.appSummary,
+      },
+      {
+        label: 'About this app',
+        route: UserRouteName.aboutEnglish,
+      },
+      {
+        label: 'Zo werkt de app',
+        route: OnboardingRouteName.onboarding,
+        moduleSlug: ModuleSlug.onboarding,
+      },
+      {
+        label: 'Geef uw mening over de app',
+        route: UserRouteName.feedback,
+      },
+      {
+        label: 'Privacy verklaring',
+        route: UserRouteName.privacyStatement,
+      },
+      {
+        label: 'Toegankelijkheidsverklaring',
+        route: UserRouteName.accessibilityStatement,
+      },
+    ],
+  },
+]
+
 const MenuSection = ({title, navigationItems}: UserMenuSection) => {
   const {biometricsLabel, isEnrolled} = useAccessCodeBiometrics()
   const {navigate} = useNavigation()
@@ -61,10 +100,12 @@ const MenuSection = ({title, navigationItems}: UserMenuSection) => {
   return (
     <Column gutter="sm">
       {!!title && (
-        <Title
-          level="h5"
-          text={title}
-        />
+        <Box insetLeft="md">
+          <Title
+            level="h5"
+            text={title}
+          />
+        </Box>
       )}
       <Column gutter="xxs">
         {navigationItems.map(item =>
@@ -97,10 +138,13 @@ const MenuSection = ({title, navigationItems}: UserMenuSection) => {
 export const UserMenu = () => {
   const {accessCode} = useGetSecureAccessCode()
   const {enabledModules, modulesLoading} = useModules()
+
   const moduleMenuSections = enabledModules
     ? enabledModules.flatMap(m => m.userMenuSection?.navigationItems || [])
     : []
+
   const sections = getSections(moduleMenuSections)
+  const aboutSections = getAboutSections()
 
   if (modulesLoading) {
     return <PleaseWait testID="UserMenuPleaseWait" />
@@ -116,7 +160,37 @@ export const UserMenu = () => {
               {...section}
             />
           ))}
-        {!!accessCode && <MenuSection {...accessCodeSection} />}
+
+        {!accessCode && <MenuSection {...accessCodeSection} />}
+
+        {!!aboutSections.length &&
+          aboutSections.map(section => (
+            <MenuSection
+              key={section.title}
+              {...section}
+            />
+          ))}
+      </Column>
+
+      <Gutter height="md" />
+
+      <Column>
+        <CopyButton
+          insetHorizontal="no"
+          label={`Versie ${VERSION_NUMBER_WITH_BUILD}`}
+          testID="AboutVersionNumberText"
+          textToCopy={VERSION_NUMBER_WITH_BUILD}
+          variant="tertiary"
+        />
+        <CopyButton
+          ellipsizeMode="tail"
+          insetHorizontal="no"
+          label={`Installatie-id ${SHA256EncryptedDeviceId}`}
+          numberOfLines={1}
+          testID="AboutInstallationIdText"
+          textToCopy={SHA256EncryptedDeviceId}
+          variant="tertiary"
+        />
       </Column>
     </View>
   )
