93370e2f37af959b93a0748f4e6aeddbe93d7e9e
Frank Filius <f.filius@amsterdam.nl>
2025-10-13 09:18:53 +0200

    Allow elections module to be turned off in settings

--- src/modules/elections/index.ts
+++ src/modules/elections/index.ts
@@ -2,7 +2,6 @@ import {ModuleSlug} from '@/modules/slugs'
 import {ModuleClientConfig} from '@/modules/types'
 
 export const electionsModule: ModuleClientConfig = {
-  alwaysEnabled: true,
   name: 'ElectionsModule',
   slug: ModuleSlug.elections,
 }
