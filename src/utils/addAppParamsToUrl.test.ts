7dcc2ec8089e6b67a4cf8ba2579c61f88fa7dc15
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2025-10-14 14:41:43 +0200

    Restore module slug test

--- src/utils/addAppParamsToUrl.test.ts
+++ src/utils/addAppParamsToUrl.test.ts
@@ -19,6 +19,14 @@ describe('isAmsterdamNlUrl', () => {
 })
 
 describe('addAppParamsToUrl', () => {
+  it('should add app parameters to Amsterdam URLs without existing query string', () => {
+    const url = 'https://amsterdam.nl'
+    const slug: ModuleSlug = ModuleSlug.user
+    const result = addAppParamsToUrl(url, slug)
+
+    expect(result).toBe('https://amsterdam.nl?app_from=1&app_module=user')
+  })
+
   it('should add app parameters to Amsterdam URLs with existing query string', () => {
     const url = 'https://amsterdam.nl/path?existing_param=value'
     const slug: ModuleSlug = ModuleSlug['waste-guide']
