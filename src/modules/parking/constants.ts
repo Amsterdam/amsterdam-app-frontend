afda6c0bc65ecc51e02a68c2963bf8c96c3243b9
Frank Filius <f.filius@amsterdam.nl>
2025-10-22 13:51:00 +0200

    Add components to choose parking machine when starting a session

--- src/modules/parking/constants.ts
+++ src/modules/parking/constants.ts
@@ -4,6 +4,7 @@ export enum ParkingSessionBottomSheetVariant {
   amount = 'amount',
   endTime = 'endTime',
   licensePlate = 'licensePlate',
+  parkingMachine = 'parkingMachine',
   paymentZone = 'paymentZone',
   startTime = 'startTime',
 }
