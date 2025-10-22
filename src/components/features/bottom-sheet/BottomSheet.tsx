b7da1637c73362dbfa2f478c1b7e803a81a59f2b
Frank Filius <f.filius@amsterdam.nl>
2025-10-22 13:45:20 +0200

    Make children of the scroll catch taps so submit is triggered

--- src/components/features/bottom-sheet/BottomSheet.tsx
+++ src/components/features/bottom-sheet/BottomSheet.tsx
@@ -40,7 +40,9 @@ type Props = Partial<
   )
 
 const ScrollWrapper = ({children}: {children: ReactNode}) => (
-  <BottomSheetScrollView>{children}</BottomSheetScrollView>
+  <BottomSheetScrollView keyboardShouldPersistTaps="handled">
+    {children}
+  </BottomSheetScrollView>
 )
 
 /**
