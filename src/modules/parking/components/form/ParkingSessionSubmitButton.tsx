58f0c77e5ee4b14ddda61d62a8e0ea8afe5b0e4f
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2025-10-27 17:35:53 +0100

    Api version check to render button

--- src/modules/parking/components/form/ParkingSessionSubmitButton.tsx
+++ src/modules/parking/components/form/ParkingSessionSubmitButton.tsx
@@ -1,11 +1,14 @@
 import {ParkingActivateLicensePlateButton} from '@/modules/parking/components/form/ParkingActivateLicensePlateButton'
 import {ParkingStartSessionButton} from '@/modules/parking/components/form/ParkingStartSessionButton'
+import {useCurrentParkingApiVersion} from '@/modules/parking/hooks/useCurrentParkingApiVersion'
 import {useCurrentParkingPermit} from '@/modules/parking/hooks/useCurrentParkingPermit'
+import {ParkingApiVersion} from '@/modules/parking/types'
 
 export const ParkingSessionSubmitButton = () => {
   const currentPermit = useCurrentParkingPermit()
+  const apiVersion = useCurrentParkingApiVersion()
 
-  if (currentPermit.no_endtime) {
+  if (currentPermit.no_endtime && apiVersion === ParkingApiVersion.v2) {
     return <ParkingActivateLicensePlateButton />
   }
 
