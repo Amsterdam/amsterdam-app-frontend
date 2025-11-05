c91554000947c5421890124113c565c03d3847c7
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-11-05 14:02:27 +0100

    filter out driver permit

--- src/modules/parking/utils/filterPermits.ts
+++ src/modules/parking/utils/filterPermits.ts
@@ -1,20 +1,16 @@
 import type {ParkingPermit} from '@/modules/parking/types'
 
-const KNOWN_PERMIT_NAME_KEYWORDS = [
-  'GA-parkeervergunning',
-  'mantelzorg',
-  'Bezoekersparkeervergunning',
-  'Kraskaartparkeervergunning',
-  'Bedrijfsvergunning',
-  'met wisselend kenteken',
-  'Sportverenigingvergunning',
+const KNOWN_PERMIT_NAME_KEYWORDS: RegExp[] = [
+  /ga-parkeervergunning.*\(passagiers\)/i,
+  /mantelzorg/i,
+  /bezoekersparkeervergunning/i,
+  /kraskaartparkeervergunning/i,
+  /bedrijfsvergunning/i,
+  /met wisselend kenteken/i,
+  /sportverenigingvergunning/i,
 ]
 
 export const filterPermits = (permits: ParkingPermit[]) =>
-  permits.filter(({permit_name}) => {
-    const lowerCasePermitName = permit_name.toLowerCase()
-
-    return KNOWN_PERMIT_NAME_KEYWORDS.some(keyword =>
-      lowerCasePermitName.includes(keyword.toLowerCase()),
-    )
-  })
+  permits.filter(({permit_name}) =>
+    KNOWN_PERMIT_NAME_KEYWORDS.some(keyword => keyword.test(permit_name)),
+  )
