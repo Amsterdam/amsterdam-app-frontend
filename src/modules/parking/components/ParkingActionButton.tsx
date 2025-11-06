fdb78c191b9b3e4a6497a3b333f26c934c0d7d91
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2025-11-06 17:16:26 +0100

    Move parking action button content into parking action button; Remove usePermits hook in favor of cached data

--- src/modules/parking/components/ParkingActionButton.tsx
+++ src/modules/parking/components/ParkingActionButton.tsx
@@ -4,15 +4,10 @@ import {Gutter} from '@/components/ui/layout/Gutter'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useGetSecureAccessCode} from '@/modules/access-code/hooks/useGetSecureAccessCode'
 import {parkingModule} from '@/modules/parking'
-import {useGetPermits} from '@/modules/parking/hooks/useGetPermits'
 import {useLoginSteps} from '@/modules/parking/hooks/useLoginSteps'
 import {ParkingRouteName} from '@/modules/parking/routes'
 import {useParkingAccount} from '@/modules/parking/slice'
-import {
-  ParkingPermitScope,
-  PermitType,
-  type ParkingPermit,
-} from '@/modules/parking/types'
+import {ParkingPermitScope, PermitType} from '@/modules/parking/types'
 import {ModuleSlug} from '@/modules/slugs'
 import {useGetCachedServerModule} from '@/store/slices/modules'
 
@@ -22,51 +17,37 @@ const ALLOWED_PERMIT_TYPES = [
   PermitType['GA-bezoekerskaart'],
 ]
 
-const ParkingActionButtonContent = ({
-  cachedPermits = [],
-}: {
-  cachedPermits?: ParkingPermit[]
-}) => {
-  const {navigate} = useNavigation()
-  const {isInactive} = useGetCachedServerModule(parkingModule.slug)
-  const {permits} = useGetPermits(isInactive)
-
-  const optimisticPermits = permits ?? cachedPermits
-
-  return (
-    optimisticPermits?.length === 1 &&
-    ALLOWED_PERMIT_TYPES.includes(optimisticPermits[0].permit_type) && (
-      <Column>
-        <ActionButton
-          disabled={!permits}
-          iconName="parkingSession"
-          isModuleInactive={isInactive}
-          label={'Parkeersessie\nstarten'}
-          onPress={() => {
-            navigate(ModuleSlug.parking, {
-              screen: ParkingRouteName.startSession,
-            })
-          }}
-          testID="ParkingActionButton"
-        />
-        <Gutter height="lg" />
-      </Column>
-    )
-  )
-}
-
 export const ParkingActionButton = () => {
+  const {navigate} = useNavigation()
+
   const {accessCode} = useGetSecureAccessCode()
   const {isLoginStepsActive} = useLoginSteps()
   const parkingAccount = useParkingAccount()
+  const {isInactive} = useGetCachedServerModule(parkingModule.slug)
 
-  if (
-    !accessCode ||
-    parkingAccount?.scope !== ParkingPermitScope.permitHolder ||
-    isLoginStepsActive
-  ) {
+  const isAllowedPermit =
+    parkingAccount?.permits?.length === 1 &&
+    ALLOWED_PERMIT_TYPES.includes(parkingAccount.permits[0].permit_type) &&
+    parkingAccount.scope === ParkingPermitScope.permitHolder
+
+  if (!accessCode || isLoginStepsActive || !isAllowedPermit) {
     return null
   }
 
-  return <ParkingActionButtonContent cachedPermits={parkingAccount?.permits} />
+  return (
+    <Column>
+      <ActionButton
+        iconName="parkingSession"
+        isModuleInactive={isInactive}
+        label={'Parkeersessie\nstarten'}
+        onPress={() => {
+          navigate(ModuleSlug.parking, {
+            screen: ParkingRouteName.startSession,
+          })
+        }}
+        testID="ParkingActionButton"
+      />
+      <Gutter height="lg" />
+    </Column>
+  )
 }
