107e10a1f8e2430465842ba8fa25655ac7594ed8
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2025-11-26 15:16:00 +0100

    Code formatting

--- src/modules/parking/components/permit-zone/ParkingPermitZoneMapMarkers.tsx
+++ src/modules/parking/components/permit-zone/ParkingPermitZoneMapMarkers.tsx
@@ -36,7 +36,7 @@ export const ParkingPermitZoneMapMarkers = ({
 
   const markerVariant = getMarkerVariant(
     selectedParkingMachineId,
-    parking_machine_favorite ? [parking_machine_favorite] : [],
+    parking_machine_favorite ?? parking_machine_favorite,
   )
 
   return (
