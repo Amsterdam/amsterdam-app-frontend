3760e536027e5c5d508777e72c4b8ee33be39ea5
Frank Filius <f.filius@amsterdam.nl>
2025-11-26 10:55:07 +0100

    Improve module config

--- src/modules/survey/index.ts
+++ src/modules/survey/index.ts
@@ -2,6 +2,8 @@ import {ModuleSlug} from '@/modules/slugs'
 import {ModuleClientConfig} from '@/modules/types'
 
 export const surveyModule: ModuleClientConfig = {
+  alwaysEnabled: true,
+  hiddenInMenu: true,
   name: 'SurveyModule',
   slug: ModuleSlug.survey,
 }
