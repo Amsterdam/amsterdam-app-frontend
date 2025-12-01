0dedbfc01e5549564392387e751f7351fb08f4c8
Frank Filius <f.filius@amsterdam.nl>
2025-12-01 15:24:09 +0100

    Improve component structure

--- src/modules/burning-guide/screens/BurningGuideTips.screen.tsx
+++ src/modules/burning-guide/screens/BurningGuideTips.screen.tsx
@@ -7,25 +7,23 @@ import {BurningGuideRisksButton} from '@/modules/burning-guide/components/Burnin
 
 export const BurningGuideTipsScreen = () => (
   <Screen testID="BurningGuideTipsScreen">
-    <Box
-      insetBottom="xl"
-      insetLeft="md"
-      insetRight="md"
-      insetTop="md">
-      <Column gutter="lg">
-        <Title text="Tips om bewuster te stoken" />
-        <List
-          items={[
-            'Stook alleen schoon, droog en gekloofd kachelhout. Afval (bewerkt hout, karton en papier) is verboden.',
-            'Zorg altijd voor goede luchttoevoer tijdens het stoken en het uitbranden.',
-            'Laat het vuur uit bij windstil of mistig weer. ',
-            "Stop 's avonds op tijd met stoken. Dovend vuur rookt nog lang na. Uw buren kunnen dan geen raam openzetten voor ventilatie.",
-            'Laat de schoorsteen minstens 1 keer per jaar goed vegen.',
-          ]}
-          testID="BurningGuideTipsList"
-        />
-      </Column>
-    </Box>
-    <BurningGuideRisksButton />
+    <Column gutter="lg">
+      <Box>
+        <Column gutter="lg">
+          <Title text="Tips om bewuster te stoken" />
+          <List
+            items={[
+              'Stook alleen schoon, droog en gekloofd kachelhout. Afval (bewerkt hout, karton en papier) is verboden.',
+              'Zorg altijd voor goede luchttoevoer tijdens het stoken en het uitbranden.',
+              'Laat het vuur uit bij windstil of mistig weer. ',
+              "Stop 's avonds op tijd met stoken. Dovend vuur rookt nog lang na. Uw buren kunnen dan geen raam openzetten voor ventilatie.",
+              'Laat de schoorsteen minstens 1 keer per jaar goed vegen.',
+            ]}
+            testID="BurningGuideTipsList"
+          />
+        </Column>
+      </Box>
+      <BurningGuideRisksButton />
+    </Column>
   </Screen>
 )
