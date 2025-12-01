4e9d532d893c6b1aacad2cfccf76046bf46d3fd8
Frank Filius <f.filius@amsterdam.nl>
2025-12-01 14:50:07 +0100

    Reuse components

--- src/components/ui/containers/OrientationBasedLayout.tsx
+++ src/components/ui/containers/OrientationBasedLayout.tsx
@@ -2,12 +2,15 @@ import {Column, type ColumnProps} from '@/components/ui/layout/Column'
 import {Row, type RowProps} from '@/components/ui/layout/Row'
 import {LayoutOrientation} from '@/components/ui/types'
 
-type Props = {
+export type OrientationBasedLayoutProps = {
   orientation: LayoutOrientation
 } & ColumnProps &
   RowProps
 
-export const OrientationBasedLayout = ({orientation, ...props}: Props) =>
+export const OrientationBasedLayout = ({
+  orientation,
+  ...props
+}: OrientationBasedLayoutProps) =>
   orientation === LayoutOrientation.horizontal ? (
     <Row {...props} />
   ) : (
