543551d09328625b1c03c216d54b09b886cb7363
Filius, Frank <f.filius@amsterdam.nl>
2025-12-01 13:40:38 +0000

    Apply suggestions from code review

--- src/modules/burning-guide/screens/BurningGuideRisks.screen.tsx
+++ src/modules/burning-guide/screens/BurningGuideRisks.screen.tsx
@@ -7,28 +7,26 @@ import {BurningGuideTipsButton} from '@/modules/burning-guide/components/Burning
 
 export const BurningGuideRisksScreen = () => (
   <Screen testID="BurningGuideRisksScreen">
-    <Box
-      insetBottom="xl"
-      insetLeft="md"
-      insetRight="md"
-      insetTop="md">
-      <Column gutter="lg">
-        <Title text="Tips om bewuster te stoken" />
-        <Paragraph variant="intro">
-          De houtkachel of open haard aansteken is gezellig. Maar wist u dat de
-          rook uit de schoorsteen slecht is voor de gezondheid van uw buren? Ook
-          in uw eigen huis blijven ongezonde stoffen hangen. Gelukkig kunt u
-          schoner en bewuster stoken.
-        </Paragraph>
-        <Paragraph>
-          De rook bevat fijnstof. Dit kan zorgen voor hoesten, benauwdheid en
-          prikkende ogen, keel of luchtwegen. Als u vaak rook inademt, kan dat
-          op lange termijn leiden tot hart- en longziekten. Bovendien is
-          houtrook slecht voor het milieu: één avond stoken veroorzaakt evenveel
-          fijnstof als een autorit van 1000 kilometer.
-        </Paragraph>
-      </Column>
-    </Box>
-    <BurningGuideTipsButton />
+<Column gutter="lg">
+      <Box>
+        <Column gutter="lg">
+          <Title text="Tips om bewuster te stoken" />
+          <Paragraph variant="intro">
+            De houtkachel of open haard aansteken is gezellig. Maar wist u dat
+            de rook uit de schoorsteen slecht is voor de gezondheid van uw
+            buren? Ook in uw eigen huis blijven ongezonde stoffen hangen.
+            Gelukkig kunt u schoner en bewuster stoken.
+          </Paragraph>
+          <Paragraph>
+            De rook bevat fijnstof. Dit kan zorgen voor hoesten, benauwdheid en
+            prikkende ogen, keel of luchtwegen. Als u vaak rook inademt, kan dat
+            op lange termijn leiden tot hart- en longziekten. Bovendien is
+            houtrook slecht voor het milieu: één avond stoken veroorzaakt
+            evenveel fijnstof als een autorit van 1000 kilometer.
+          </Paragraph>
+        </Column>
+      </Box>
+      <BurningGuideTipsButton />
+    </Column>
   </Screen>
 )
