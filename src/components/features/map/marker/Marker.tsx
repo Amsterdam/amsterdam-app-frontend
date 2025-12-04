c2537bb19b170a49b3668d704418318ff493968d
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2025-12-04 14:50:04 +0100

    Add marker offset so pin point is on coords instead of center

--- src/components/features/map/marker/Marker.tsx
+++ src/components/features/map/marker/Marker.tsx
@@ -1,11 +1,18 @@
 import {memo} from 'react'
-// eslint-disable-next-line no-restricted-imports
-import {Marker as MarkerRN, type MapMarkerProps} from 'react-native-maps'
+import {
+  // eslint-disable-next-line no-restricted-imports
+  Marker as MarkerRN,
+  type MapMarkerProps,
+  type Point,
+} from 'react-native-maps'
 import {
   MARKER_IMAGES,
   MarkerVariant,
 } from '@/components/features/map/marker/markers'
 import {checkMarkerPropsHaveChanged} from '@/components/features/map/utils/checkMarkerPropsHaveChanged'
+
+const GOOGLE_MAPS_MARKER_OFFSET: Point = {x: 0.25, y: 1}
+const APPLE_MAPS_MARKER_OFFSET: Point = {x: 0, y: -20}
 
 export type MarkerProps = {
   variant?: MarkerVariant
@@ -16,6 +23,8 @@ const DEFAULT_HIT_SLOP = 20
 export const Marker = memo(
   ({variant, hitSlop = DEFAULT_HIT_SLOP, ...markerProps}: MarkerProps) => (
     <MarkerRN
+      anchor={GOOGLE_MAPS_MARKER_OFFSET}
+      centerOffset={APPLE_MAPS_MARKER_OFFSET}
       hitSlop={hitSlop}
       image={variant ? MARKER_IMAGES[variant] : undefined}
       {...markerProps}
