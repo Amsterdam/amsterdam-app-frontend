2e9007843a6eb7793a7bb1bb7be2d6eed8ede991
Frank Filius <f.filius@amsterdam.nl>
2025-12-04 15:38:27 +0100

    Revert feedback screen

--- src/modules/user/screens/Feedback.screen.tsx
+++ src/modules/user/screens/Feedback.screen.tsx
@@ -1,32 +1,45 @@
 import {Screen} from '@/components/features/screen/Screen'
 import {ExternalLinkButton} from '@/components/ui/buttons/ExternalLinkButton'
 import {Box} from '@/components/ui/containers/Box'
-import {SomethingWentWrong} from '@/components/ui/feedback/SomethingWentWrong'
 import {Column} from '@/components/ui/layout/Column'
+import {Figure} from '@/components/ui/media/Figure'
+import {Paragraph} from '@/components/ui/text/Paragraph'
+import {Title} from '@/components/ui/text/Title'
 import {RedirectKey} from '@/modules/redirects/types'
-import {Survey} from '@/modules/survey/exports/Survey'
+import TwoPersonsHighFiveImage from '@/modules/user/assets/images/two-persons-high-five.svg'
 
 export const FeedbackScreen = () => (
   <Screen testID="AboutFeedbackScreen">
-    <Box>
-      <Survey
-        entryPoint="general"
-        FallbackComponent={<FallbackComponent />}
-      />
+    <Box grow>
+      <Column
+        align="between"
+        grow={1}
+        gutter="lg">
+        <Column gutter="md">
+          <Title
+            testID="AboutFeedbackTitle"
+            text="Uw mening"
+          />
+          <Paragraph
+            testID="AboutFeedbackIntroParagraph"
+            variant="intro">
+            Mist u onderwerpen in de app? Of ziet u iets dat niet klopt of niet
+            handig werkt?
+          </Paragraph>
+          <ExternalLinkButton
+            label="Laat het ons weten"
+            redirectKey={RedirectKey.feedbackForm}
+            testID="AboutFeedbackExternalLinkButton"
+          />
+        </Column>
+        <Column
+          align="center"
+          flex={1}>
+          <Figure height={256}>
+            <TwoPersonsHighFiveImage />
+          </Figure>
+        </Column>
+      </Column>
     </Box>
   </Screen>
-)
-
-const FallbackComponent = () => (
-  <Column gutter="lg">
-    <SomethingWentWrong
-      testID="FeedbackSurveySomethingWentWrong"
-      text="Dit onderdeel werkt nu niet. Gebruik het contactformulier als tijdelijke oplossing."
-    />
-    <ExternalLinkButton
-      label="Contactformulier"
-      redirectKey={RedirectKey.contactForm}
-      testID="InactiveModuleGuardFallbackUrlExternalLinkButton"
-    />
-  </Column>
 )
