7b6b2737f65a55fcdc93021092dd40340e161999
Frank Filius <f.filius@amsterdam.nl>
2025-12-08 09:31:53 +0100

    Show burning-guide with location as well as address

--- src/modules/burning-guide/screens/BurningGuide.screen.tsx
+++ src/modules/burning-guide/screens/BurningGuide.screen.tsx
@@ -2,16 +2,15 @@ import {BottomSheet} from '@/components/features/bottom-sheet/BottomSheet'
 import {Screen} from '@/components/features/screen/Screen'
 import {Box} from '@/components/ui/containers/Box'
 import {Column} from '@/components/ui/layout/Column'
-import {useSelector} from '@/hooks/redux/useSelector'
 import {SelectLocationTypeBottomSheetContent} from '@/modules/address/components/location/SelectLocationTypeBottomSheetContent'
-import {selectAddress} from '@/modules/address/slice'
+import {useSelectedAddress} from '@/modules/address/hooks/useSelectedAddress'
 import {HighAccuracyPurposeKey} from '@/modules/address/types'
 import {BurningGuide} from '@/modules/burning-guide/components/BurningGuide'
 import {BurningGuideAddress} from '@/modules/burning-guide/components/BurningGuideAddress'
 import {BurningGuideInfoButtons} from '@/modules/burning-guide/components/BurningGuideInfoButtons'
 
 export const BurningGuideScreen = () => {
-  const address = useSelector(selectAddress)
+  const {address} = useSelectedAddress()
 
   return (
     <Screen
