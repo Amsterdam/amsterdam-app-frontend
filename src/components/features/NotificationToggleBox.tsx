3b91849b265e219bd16aba939e9f139d892443b6
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-12-17 16:11:04 +0100

    trigger request permissions and permission instruction screen

--- src/components/features/NotificationToggleBox.tsx
+++ src/components/features/NotificationToggleBox.tsx
@@ -1,3 +1,4 @@
+import {useCallback} from 'react'
 import type {TestProps} from '@/components/ui/types'
 import {Box} from '@/components/ui/containers/Box'
 import {Switch} from '@/components/ui/forms/Switch'
@@ -6,6 +7,9 @@ import {Row} from '@/components/ui/layout/Row'
 import {Icon} from '@/components/ui/media/Icon'
 import {Phrase} from '@/components/ui/text/Phrase'
 import {Title} from '@/components/ui/text/Title'
+import {useRegisterDevice} from '@/hooks/useRegisterDevice'
+import {useNavigateToInstructionsScreen} from '@/modules/address/hooks/useNavigateToInstructionsScreen'
+import {Permissions} from '@/types/permissions'
 import {accessibleText} from '@/utils/accessibility/accessibleText'
 
 type Props = {
@@ -32,31 +36,51 @@ export const NotificationToggleBox = ({
   testID,
   onChange,
   disabled,
-}: Props) => (
-  <Switch
-    accessibilityLabel={`"${accessibleText(description)}" staat ${value ? 'aan' : 'uit'}`}
-    disabled={disabled}
-    label={
-      <Column
-        grow={1}
-        gutter="sm">
-        <Row gutter="sm">
-          <Icon
-            name="bell"
-            size="lg"
-            testID={`${testID}Icon`}
-          />
-          <Title
-            level="h5"
-            text="Meldingen"
-          />
-        </Row>
-        <Phrase>{description}</Phrase>
-      </Column>
+}: Props) => {
+  const navigateToInstructionsScreen = useNavigateToInstructionsScreen(
+    Permissions.notifications,
+  )
+  const {registerDeviceIfPermitted} = useRegisterDevice()
+  const onChangeFn = useCallback(() => {
+    if (value) {
+      onChange(false)
+    } else {
+      void registerDeviceIfPermitted(true).then(hasPermission => {
+        if (hasPermission) {
+          onChange(true)
+        } else {
+          navigateToInstructionsScreen()
+        }
+      })
     }
-    onChange={() => onChange(!value)}
-    testID={`${testID}Switch`}
-    value={value}
-    wrapper={Wrapper}
-  />
-)
+  }, [onChange, value, registerDeviceIfPermitted, navigateToInstructionsScreen])
+
+  return (
+    <Switch
+      accessibilityLabel={`"${accessibleText(description)}" staat ${value ? 'aan' : 'uit'}`}
+      disabled={disabled}
+      label={
+        <Column
+          grow={1}
+          gutter="sm">
+          <Row gutter="sm">
+            <Icon
+              name="bell"
+              size="lg"
+              testID={`${testID}Icon`}
+            />
+            <Title
+              level="h5"
+              text="Meldingen"
+            />
+          </Row>
+          <Phrase>{description}</Phrase>
+        </Column>
+      }
+      onChange={onChangeFn}
+      testID={`${testID}Switch`}
+      value={value}
+      wrapper={Wrapper}
+    />
+  )
+}
