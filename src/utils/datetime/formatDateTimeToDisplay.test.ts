36248555390e485d5e9ab9d397e373c32e0197d3
Rik Scheffer <r.scheffer@amsterdam.nl>
2026-01-05 11:45:14 +0100

    fix unit test

--- src/utils/datetime/formatDateTimeToDisplay.test.ts
+++ src/utils/datetime/formatDateTimeToDisplay.test.ts
@@ -1,8 +1,16 @@
 import {formatDateToDisplay} from '@/utils/datetime/formatDateToDisplay'
 
 describe('formatDateToDisplay', () => {
+  afterAll(() => {
+    jest.useRealTimers()
+  })
   it('should format and cut the date string correctly', () => {
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date('2025-10-01'))
+    expect(formatDateToDisplay('2026-01-01')).toBe('1 januari 2026')
     expect(formatDateToDisplay('2025-01-01')).toBe('1 januari')
+    jest.useRealTimers()
+    expect(formatDateToDisplay('2025-01-01')).toBe('1 januari 2025')
   })
 
   it('should return an empty string for an empty input', () => {
