ea89c899a3522bf04223de8664b3c76a7544c453
Rik Scheffer <r.scheffer@amsterdam.nl>
2026-01-06 16:02:58 +0100

    cleanup

--- src/modules/waste-guide/index.ts
+++ src/modules/waste-guide/index.ts
@@ -3,14 +3,9 @@ import {createClientModule} from '@/modules/utils/createModule'
 import {fractionIconConfig} from '@/modules/waste-guide/constants'
 import {onMyAddressChanged} from '@/modules/waste-guide/onMyAddressChanged'
 import {WasteGuideRouteName} from '@/modules/waste-guide/routes'
-import {
-  wasteGuideSlice,
-  type WasteGuideState,
-} from '@/modules/waste-guide/slice'
+import {wasteGuideSlice} from '@/modules/waste-guide/slice'
 import {PiwikSessionDimension} from '@/processes/piwik/types'
 import {ReduxKey} from '@/store/types/reduxKey'
-
-const persistWhitelist: (keyof WasteGuideState)[] = []
 
 export const wasteGuideModule = createClientModule({
   logDimension: PiwikSessionDimension.wasteGuideModule,
@@ -20,7 +15,6 @@ export const wasteGuideModule = createClientModule({
       key: ReduxKey.wasteGuide,
       persistVersion: 0,
       slice: wasteGuideSlice,
-      persistWhitelist,
     },
   ],
   requiresFirebaseToken: true,
