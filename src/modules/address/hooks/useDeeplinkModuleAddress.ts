2b0d7420308234169ad274e9c5a7f2345691178e
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-01-06 17:29:37 +0100

    Implement deeplink support for waste guide address module

---
+++ src/modules/address/hooks/useDeeplinkModuleAddress.ts
@@ -0,0 +1,47 @@
+import {skipToken} from '@reduxjs/toolkit/query'
+import {useEffect} from 'react'
+import {useDispatch} from '@/hooks/redux/useDispatch'
+import {useSelectedAddress} from '@/modules/address/hooks/useSelectedAddress'
+import {useSetLocationType} from '@/modules/address/hooks/useSetLocationType'
+import {useGetAddressSuggestionsQuery} from '@/modules/address/service'
+import {addRecentAddress, setModuleCustomAddress} from '@/modules/address/slice'
+import {addDerivedAddressFields} from '@/modules/address/utils/addDerivedAddressFields'
+import {ModuleSlug} from '@/modules/slugs'
+
+export const useDeeplinkModuleAddress = (
+  params?: {
+    adres?: string
+  },
+  moduleSlug: ModuleSlug = ModuleSlug.address,
+) => {
+  const dispatch = useDispatch()
+
+  const {address} = useSelectedAddress(moduleSlug)
+  const setLocationType = useSetLocationType(moduleSlug)
+
+  const {data: addressFromDeeplink} = useGetAddressSuggestionsQuery(
+    params?.adres ? {address: params.adres} : skipToken,
+    {
+      selectFromResult: result => ({
+        data: result?.data?.[0]?.type === 'adres' ? result.data[0] : null,
+      }),
+    },
+  )
+
+  useEffect(() => {
+    if (addressFromDeeplink && addressFromDeeplink?.bagId !== address?.bagId) {
+      const transformedAddress = addDerivedAddressFields(addressFromDeeplink)
+
+      dispatch(addRecentAddress(transformedAddress))
+
+      dispatch(
+        setModuleCustomAddress({
+          moduleSlug,
+          address: transformedAddress,
+        }),
+      )
+      setLocationType('custom')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [addressFromDeeplink])
+}
--- src/modules/waste-guide/components/WasteGuideAddressSwitch.tsx
+++ src/modules/waste-guide/components/WasteGuideAddressSwitch.tsx
@@ -1,14 +1,24 @@
+import type {WasteGuideRouteName} from '@/modules/waste-guide/routes'
+import {useRoute} from '@/hooks/navigation/useRoute'
 import {AddressSwitch} from '@/modules/address/components/AddressSwitch'
+
+import {useDeeplinkModuleAddress} from '@/modules/address/hooks/useDeeplinkModuleAddress'
 import {HighAccuracyPurposeKey} from '@/modules/address/types'
 import {ModuleSlug} from '@/modules/slugs'
 
-export const WasteGuideAddressSwitch = () => (
-  <AddressSwitch
-    highAccuracyPurposeKey={
-      HighAccuracyPurposeKey.PreciseLocationAddressWasteGuide
-    }
-    moduleSlug={ModuleSlug['waste-guide']}
-    noAddressText="Voer een adres in om uw afvalinformatie te bekijken."
-    testID="WasteGuideNoAddressAddressSwitch"
-  />
-)
+export const WasteGuideAddressSwitch = () => {
+  const {params} = useRoute<WasteGuideRouteName.wasteGuide>()
+
+  useDeeplinkModuleAddress(params, ModuleSlug['waste-guide'])
+
+  return (
+    <AddressSwitch
+      highAccuracyPurposeKey={
+        HighAccuracyPurposeKey.PreciseLocationAddressWasteGuide
+      }
+      moduleSlug={ModuleSlug['waste-guide']}
+      noAddressText="Voer een adres in om uw afvalinformatie te bekijken."
+      testID="WasteGuideNoAddressAddressSwitch"
+    />
+  )
+}
