f20ac1954f93ecc750200dbe2e1faed3fa0a0100
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-01-14 11:27:35 +0100

    Add election crowd markers and update related components

--- src/components/features/map/marker/markers.ts
+++ src/components/features/map/marker/markers.ts
@@ -1,10 +1,19 @@
 import {Platform, type ImageURISource} from 'react-native'
 import distinctPinIcon from '@/assets/images/map/distinct_pin.png'
+import electionsCrowdBusyPinIcon from '@/assets/images/map/elections_crowd_busy_pin.png'
+import electionsCrowdCalmPinIcon from '@/assets/images/map/elections_crowd_calm_pin.png'
+import electionsCrowdMediumPinIcon from '@/assets/images/map/elections_crowd_medium_pin.png'
+import electionsCrowdUnknownPinIcon from '@/assets/images/map/elections_crowd_unknown_pin.png'
+
 import pinIcon from '@/assets/images/map/pin.png'
 import selectedPinIcon from '@/assets/images/map/selected_pin.png'
 
 export enum MarkerVariant {
   distinctPin = 'distinctPin',
+  electionsCrowdBusyPin = 'elections_crowd_busy_pin',
+  electionsCrowdCalmPin = 'elections_crowd_calm_pin',
+  electionsCrowdMediumPin = 'elections_crowd_medium_pin',
+  electionsCrowdUnknownPin = 'elections_crowd_unknown_pin',
   pin = 'pin',
   selectedPin = 'selectedPin',
 }
@@ -23,5 +32,21 @@ export const MARKER_IMAGES: Record<
   [MarkerVariant.distinctPin]: Platform.select({
     ios: distinctPinIcon,
     android: {uri: 'distinct_pin'},
+  }),
+  [MarkerVariant.electionsCrowdCalmPin]: Platform.select({
+    ios: electionsCrowdCalmPinIcon,
+    android: {uri: 'elections_crowd_calm_pin'},
+  }),
+  [MarkerVariant.electionsCrowdMediumPin]: Platform.select({
+    ios: electionsCrowdMediumPinIcon,
+    android: {uri: 'elections_crowd_medium_pin'},
+  }),
+  [MarkerVariant.electionsCrowdBusyPin]: Platform.select({
+    ios: electionsCrowdBusyPinIcon,
+    android: {uri: 'elections_crowd_busy_pin'},
+  }),
+  [MarkerVariant.electionsCrowdUnknownPin]: Platform.select({
+    ios: electionsCrowdUnknownPinIcon,
+    android: {uri: 'elections_crowd_unknown_pin'},
   }),
 }
--- src/modules/elections/components/PollingStationsMap.tsx
+++ src/modules/elections/components/PollingStationsMap.tsx
@@ -1,13 +1,14 @@
 import {useState} from 'react'
 import {MapBase} from '@/components/features/map/MapBase'
 import {Clusterer} from '@/components/features/map/clusters/Clusterer'
+import {MarkerVariant} from '@/components/features/map/marker/markers'
 import {ControlVariant} from '@/components/features/map/types'
 import {getMarkerVariant} from '@/components/features/map/utils/getMarkerVariant'
 import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
 import {SomethingWentWrong} from '@/components/ui/feedback/SomethingWentWrong'
 import {Address} from '@/modules/address/types'
 import {useSelectedPollingStationId} from '@/modules/elections/slice'
-import {PollingStation} from '@/modules/elections/types'
+import {ElectionsState, PollingStation} from '@/modules/elections/types'
 import {ModuleSlug} from '@/modules/slugs'
 
 type Props = {
@@ -18,6 +19,13 @@ type Props = {
   pollingStations?: PollingStation[]
 }
 
+const ElectionsMarkerVariantMap: Record<ElectionsState, MarkerVariant> = {
+  [ElectionsState.calm]: MarkerVariant.electionsCrowdCalmPin,
+  [ElectionsState.medium]: MarkerVariant.electionsCrowdMediumPin,
+  [ElectionsState.busy]: MarkerVariant.electionsCrowdBusyPin,
+  [ElectionsState.unknown]: MarkerVariant.electionsCrowdUnknownPin,
+}
+
 export const PollingStationsMap = ({
   address,
   isLoading,
@@ -25,8 +33,9 @@ export const PollingStationsMap = ({
   onPress,
   pollingStations,
 }: Props) => {
-  const selectedPollingStationId = useSelectedPollingStationId()
-  const markerVariant = getMarkerVariant(selectedPollingStationId)
+  const pollingStationId = useSelectedPollingStationId()
+  const markerVariant = getMarkerVariant(pollingStationId)
+
   const [region, setRegion] = useState(
     address?.coordinates
       ? {
@@ -58,7 +67,14 @@ export const PollingStationsMap = ({
           properties: {
             ...props,
             id: String(id),
-            variant: markerVariant(id),
+            variant: markerVariant(
+              id,
+              ElectionsMarkerVariantMap[
+                props.lastUpdate.time
+                  ? props.lastUpdate.state
+                  : ElectionsState.unknown
+              ],
+            ),
             onMarkerPress: () => onPress(id),
           },
 
